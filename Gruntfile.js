9af7dc299daa6d4e28d197a299afae2fb74a853b
Iván Diaz Marinas <idmarinas@icloud.com>
2015-06-30 10:24:13 +0200

    Corregida la exportación al local de los archivos creados

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,8 @@ module.exports = function (grunt) {
         files : [
           {
             expand: true,
-            src : ['dist/**'],
+            cwd : 'dist',
+            src : ['**'],
             dest : '/Users/Ivan/Sites/lotgd/'
           }
         ]
