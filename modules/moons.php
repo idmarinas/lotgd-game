33e1360114f1c42574f708ae13d2062a3366f4d6
Iván Diaz Marinas <idmarinas@icloud.com>
2015-07-17 13:25:55 +0200

    Modificación para hacerlo compatible con landsky

--- modules/moons.php
+++ modules/moons.php
@@ -69,6 +69,7 @@ function moons_install(){
 	module_addhook("newday");
 	module_addhook("village-desc");
 	module_addhook("forest-desc");
+	module_addhook("landsky-moons");
 	module_addhook("journey-desc"); // for the worldmap
 
 	if (!get_module_setting("moon1place", "moons")) {
@@ -96,109 +97,52 @@ function moons_dohook($hookname,$args)
 	if (!$moon1 && !$moon2 && !$moon3) return $args;
 
 	switch($hookname){
-	case "newday-runonce":
-		$changed=false;
-		if ($moon1) {
-			$place = get_module_setting("moon1place");
-			$max = get_module_setting("moon1cycle");
-			$place++;
-			if ($place > $max) $place = 1;
-			set_module_setting("moon1place", $place);
-			$changed=true;
-		}
-		if ($moon2) {
-			$place = get_module_setting("moon2place");
-			$max = get_module_setting("moon2cycle");
-			$place++;
-			if ($place > $max) $place = 1;
-			set_module_setting("moon2place", $place);
-			$changed=true;
-		}
-		if ($moon3) {
-			$place = get_module_setting("moon3place");
-			$max = get_module_setting("moon3cycle");
-			$place++;
-			if ($place > $max) $place = 1;
-			set_module_setting("moon3place", $place);
-			$changed=true;
-		}
-		//tracking changed just in case a bojo has moons in place but all disabled.
-		if ($changed) modulehook("moon-cyclechange");
-		break;
-	case "newday":
-		output_notl("`n");
-		$msg = "`^The moon `&%s`^ is %s`^.`0`n";
-		if ($moon1) {
-			$place = get_module_setting("moon1place");
-			$max = get_module_setting("moon1cycle");
-			output($msg, get_module_setting("moon1name"),
-					moons_phase($place, $max));
-		}
-		if ($moon2) {
-			$place = get_module_setting("moon2place");
-			$max = get_module_setting("moon2cycle");
-			output($msg, get_module_setting("moon2name"),
-					moons_phase($place, $max));
-		}
-		if ($moon3) {
-			$place = get_module_setting("moon3place");
-			$max = get_module_setting("moon3cycle");
-			output($msg, get_module_setting("moon3name"),
-					moons_phase($place, $max));
-		}
-		break;
-	case "village-desc":
-	case "forest-desc":
-	case "journey-desc":
-		// The dwarven town is underground, so let's handle that specially.
-		// If we are inside the town and the dwarf modules is active AND
-	    // then, if this user is in the dwarf town, show no moons.
-		if ($hookname == "village-desc" && is_module_active('racedwarf')) {
-			if (get_module_setting('villagename', 'racedwarf') ==
-					$session['user']['location']) {
-				break;
+		case "newday-runonce":
+			$changed=false;
+			show_moons($moon1, $moon2, $moon3, $place);
+			//tracking changed just in case a bojo has moons in place but all disabled.
+			if ($changed) modulehook("moon-cyclechange");
+			break;
+		case "newday":
+			if (is_module_active("landsky")) return $args;
+			output_notl("`n");
+			show_moons($moon1, $moon2, $moon3, $place);
+			break;
+		case "village-desc":
+		case "forest-desc":
+		case "journey-desc":
+			if (is_module_active("landsky")) return $args;
+			// The dwarven town is underground, so let's handle that specially.
+			// If we are inside the town and the dwarf modules is active AND
+		    // then, if this user is in the dwarf town, show no moons.
+			if ($hookname == "village-desc" && is_module_active('racedwarf')) {
+				if (get_module_setting('villagename', 'racedwarf') ==
+						$session['user']['location']) {
+					break;
+				}
 			}
-		}
-		output_notl("`n");
-		$count = 0;
-		if ($moon1) $count++;
-		if ($moon2) $count++;
-		if ($moon3) $count++;
-		// Okay, we're above ground.
-		$m = "moon is";
-		if ($count > 1) $m = "%s moons are";
-		if ($hookname=="village-desc") {
-			$prefix = "`&Over the buildings of the town, the ".$m." visible.";
-		} else {
-			$prefix = "`&High above the trees around you, the ".$m." visible.";
-		}
-		if ($count == 1) {
-			output($prefix);
-		} else {
-			output($prefix, $count);
-		}
-
-		$msg = "`&The moon `#%s`& is %s`&.`0";
-		if ($moon1) {
-			$place = get_module_setting("moon1place");
-			$max = get_module_setting("moon1cycle");
-			output($msg, get_module_setting("moon1name"),
-					moons_phase($place, $max));
-		}
-		if ($moon2) {
-			$place = get_module_setting("moon2place");
-			$max = get_module_setting("moon2cycle");
-			output($msg, get_module_setting("moon2name"),
-					moons_phase($place, $max));
-		}
-		if ($moon3) {
-			$place = get_module_setting("moon3place");
-			$max = get_module_setting("moon3cycle");
-			output($msg, get_module_setting("moon3name"),
-					moons_phase($place, $max));
-		}
-		output_notl("`n");
-
+			
+			$count = 0;
+			if ($moon1) $count++;
+			if ($moon2) $count++;
+			if ($moon3) $count++;
+			// Okay, we're above ground.
+			$m = "moon is";
+			if ($count > 1) $m = "%s moons are";
+			if ($hookname=="village-desc") {
+				$prefix = "`&Over the buildings of the town, the ".$m." visible.";
+			} else {
+				$prefix = "`&High above the trees around you, the ".$m." visible.";
+			}
+			if ($count == 1) {
+				output($prefix);
+			} else {
+				output($prefix, $count);
+			}
+			show_moons($moon1, $moon2, $moon3, $place);
+		break;
+		case "landsky-moons":
+			show_moons($moon1, $moon2, $moon3, $place);
 		break;
 	}
 
@@ -209,6 +153,33 @@ function moons_runevent($type){
 }
 
 function moons_run(){
+}
+
+function show_moons($moon1, $moon2, $moon3, $place)
+{
+	if (!is_module_active("landsky")) output_notl("`n");
+	$msg = "`&The moon `#%s`& is %s`&.`0";
+	if ($moon1) {
+		$place = get_module_setting("moon1place");
+		$max = get_module_setting("moon1cycle");
+		output($msg, get_module_setting("moon1name"),
+				moons_phase($place, $max));
+	}
+	if (is_module_active("landsky")) output_notl("`n");
+	if ($moon2) {
+		$place = get_module_setting("moon2place");
+		$max = get_module_setting("moon2cycle");
+		output($msg, get_module_setting("moon2name"),
+				moons_phase($place, $max));
+	}
+	if (is_module_active("landsky")) output_notl("`n");
+	if ($moon3) {
+		$place = get_module_setting("moon3place");
+		$max = get_module_setting("moon3cycle");
+		output($msg, get_module_setting("moon3name"),
+				moons_phase($place, $max));
+	}
+	output_notl("`n");
 }
 
 ?>
