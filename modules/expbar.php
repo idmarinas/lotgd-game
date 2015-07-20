0593d17e4f8af1c2f464bc1f3c1f0824fa4e0dfa
Iván Diaz Marinas <idmarinas@icloud.com>
2015-07-20 14:30:34 +0200

    Ya no muestra la barra cuando se está muerto

--- modules/expbar.php
+++ modules/expbar.php
@@ -32,66 +32,68 @@ function expbar_uninstall(){
 function expbar_dohook($hookname,$args){
 	global $session;
 	switch($hookname){
-	case "charstats":
-		require_once("lib/experience.php");
-		$level = $session['user']['level'];
-		$dks = $session['user']['dragonkills'];
-		$min = exp_for_next_level($level-1, $dks);
-		$req = exp_for_next_level($level, $dks);
-		$exp = round($session['user']['experience'], 0) . check_temp_stat("experience",1);
-
-		// If the user has dropped below the previous level, make that the
-		// min and they need 100%  They will continue to need 100% until
-		// they reach 'min' again.
-		if ($exp < $min) $min = $exp;
-		if ($req-$min > 0) $nonpct = floor(($req-$exp)/($req-$min) * 100);
-		else $nonpct = 0;
-		$pct = 100-$nonpct;
-		if ($pct > 100) {
-			$pct = 100;
-			$nonpct = 0;
-		}
-		if ($pct < 0) {
-			$pct = 0;
-			$nonpct = 100;
-		}
-		if ($exp >= $req) {
-			$animated = "animated flash";
-			$color = "progress-expbar-ready";
-			$text = "<i class='fa fa-arrow-up'></i> " . translate_inline("Ready");
-			$script = "<script>
-						var animatedName = 'animated flash';
-						$('.expbar').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-							var self = this;
-							$(self).removeClass(animatedName);
-							setTimeout(function(){
-								$(self).addClass(animatedName);
-							},5000);
-						});
-					</script>";
-		} else {
-			$animated = "active";
-			$color = "progress-expbar-in-progress progress-expbar-striped";
-			$texr = "";
-			$script = "";
-		}
-		$old = getcharstat("Character Info", "Experience");
-		$new = "";
-		$shownum = get_module_setting("showexpnumber");
-		$shownext = get_module_setting("shownextgoal");
-		$showbar = get_module_setting("showbar");
-		if (!$shownum && !$showbar) $new="`b`\$hidden`b";
-		if ($shownum) $new .= $old;
-		if ($shownum && $shownext) $new .= "`0/`@$req`0";
-		if ($showbar) {
-			if ($shownum) $new .= "<br />";			
-			$new .= "<div class='expbar $animated'>
-					  <div class='progress-expbar $color' style='width: $pct%;'>$text</div>
-					</div>
-					$script
-					";
-		}
-		setcharstat("Character Info", "Experience", $new);
+		case "charstats":
+			if (!$session['user']['alive']) return $args;
+			
+			require_once("lib/experience.php");
+			$level = $session['user']['level'];
+			$dks = $session['user']['dragonkills'];
+			$min = exp_for_next_level($level-1, $dks);
+			$req = exp_for_next_level($level, $dks);
+			$exp = round($session['user']['experience'], 0) . check_temp_stat("experience",1);
+	
+			// If the user has dropped below the previous level, make that the
+			// min and they need 100%  They will continue to need 100% until
+			// they reach 'min' again.
+			if ($exp < $min) $min = $exp;
+			if ($req-$min > 0) $nonpct = floor(($req-$exp)/($req-$min) * 100);
+			else $nonpct = 0;
+			$pct = 100-$nonpct;
+			if ($pct > 100) {
+				$pct = 100;
+				$nonpct = 0;
+			}
+			if ($pct < 0) {
+				$pct = 0;
+				$nonpct = 100;
+			}
+			if ($exp >= $req) {
+				$animated = "animated flash";
+				$color = "progress-expbar-ready";
+				$text = "<i class='fa fa-arrow-up'></i> " . translate_inline("Ready");
+				$script = "<script>
+							var animatedName = 'animated flash';
+							$('.expbar').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+								var self = this;
+								$(self).removeClass(animatedName);
+								setTimeout(function(){
+									$(self).addClass(animatedName);
+								},5000);
+							});
+						</script>";
+			} else {
+				$animated = "active";
+				$color = "progress-expbar-in-progress progress-expbar-striped";
+				$texr = "";
+				$script = "";
+			}
+			$old = getcharstat("Character Info", "Experience");
+			$new = "";
+			$shownum = get_module_setting("showexpnumber");
+			$shownext = get_module_setting("shownextgoal");
+			$showbar = get_module_setting("showbar");
+			if (!$shownum && !$showbar) $new="`b`\$hidden`b";
+			if ($shownum) $new .= $old;
+			if ($shownum && $shownext) $new .= "`0/`@$req`0";
+			if ($showbar) {
+				if ($shownum) $new .= "<br />";			
+				$new .= "<div class='expbar $animated'>
+						  <div class='progress-expbar $color' style='width: $pct%;'>$text</div>
+						</div>
+						$script
+						";
+			}
+			setcharstat("Character Info", "Experience", $new);
 		break;
 	}
 	return $args;
