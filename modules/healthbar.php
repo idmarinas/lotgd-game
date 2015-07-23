23d60c09f31eb969327bb23f795cf68f33c4b596
Iván Diaz Marinas <idmarinas@icloud.com>
2015-07-23 21:49:30 +0200

    Ahora se permite mostrar la salud en número, desde las preferencias de cuenta

--- modules/healthbar.php
+++ modules/healthbar.php
@@ -6,15 +6,14 @@
 function healthbar_getmoduleinfo(){
 	$info = array(
 		"name"=>"Health Bar",
-		"version"=>"2.0.0",
+		"version"=>"2.1.0",
 		"author"=>"Iván D.M based on idea JT Traub",
 		"category"=>"Stat Display",
 		"download"=>"",
-		"settings"=>array(
-			"Health Bar Module Settings,title",
-			"showcurrent"=>"Show current health number,bool|1",
-			"showmax"=>"Show max health (only if current ),bool|1",
-			"showbar"=>"Show health level as a bar,bool|1",
+		"prefs"=>array(
+			"Barra de salud,title",
+			"user_showcurrent"=>"Show current health number,bool|0",
+			"user_showmax"=>"Show max health (only if current ),bool|0"
 		)
 	);
 	return $info;
@@ -60,17 +59,14 @@ function healthbar_dohook($hookname,$args){
 		if ($pct > 60) {
 			if ($session['user']['alive']) $color = "progress-healthbar-alive-60 ";
 			else $color = "progress-healthbar-dead-60";
-			$ccode = "`@";
 		} elseif ($pct > 25) {
 			if ($session['user']['alive']) $color = "progress-healthbar-alive-25 animated pulse";
 			else $color = "progress-healthbar-dead-25";
-			$ccode = "`^";
 			$text = '<i class="fa fa-exclamation fa-fw"></i>';
 			
 		} else {
 			if ($session['user']['alive']) $color = "progress-healthbar-alive-0 animated flash";
 			else $color = "progress-healthbar-dead-0";
-			$ccode = "`$";
 			$text = '<i class="fa fa-exclamation-triangle fa-fw"></i>';
 			$script = '<script>
 				$(".healthbar").on("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){
@@ -83,26 +79,21 @@ function healthbar_dohook($hookname,$args){
 				});
 			</script>';
 		}
-		$hicode = "`&";
-		if (!$session['user']['alive']) {
-			$ccode = "`7";
-		}
+			
+		$showcur = get_module_pref("user_showcurrent");
+		$showmax = get_module_pref("user_showmax");
+		$showbar = get_module_pref("user_showbar");
 		
-		$showcur = get_module_setting("showcurrent");
-		$showmax = get_module_setting("showmax");
-		$showbar = get_module_setting("showbar");
-		$new = "";
-		if (!$showcur && !$showbar) $new="`b`\$hidden`b";
-		if ($showcur) $new .= $ccode . $cur . $cur_adjustment;
-		if ($showcur && $showmax) $new .= "`0/`&$realmax`0" . $max_adjustment;
-		if ($showbar) {
-			if ($showcur) $new .= "<br />";
-			$new .= "<div class='healthbar $animated'>
-					 	<div class='progress-healthbar $color' style='width: $pct%;'>$text</div>
-					 </div>
-					 $script
-			";
-		}
+		if (!$showcur && !$showbar) $text.="";
+		if ($showcur) $text .= $cur . $cur_adjustment;
+		if ($showcur && $showmax) $text .= "`0/`&$realmax`0" . $max_adjustment;
+		
+		$new = "<div class='healthbar $animated'>
+				 	<div class='progress-healthbar $color' style='width: $pct%;'>$text</div>
+				</div>
+				$script
+		";
+			
 		setcharstat("Character Info", $stat, $new);
 		break;
 	}
