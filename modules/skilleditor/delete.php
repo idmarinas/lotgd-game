4762c363cc770abe0ff9037c3687b76a6c426012
Iván Diaz Marinas <idmarinas@icloud.com>
2015-08-11 12:01:51 +0200

    Se va a usar un sistema de habilidades propio

--- modules/skilleditor/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$sql = "DELETE FROM " . db_prefix("skills") . " WHERE id='$id'";
-	db_query($sql);
-	output("Item deleted!`n`n");
-	redirect($from."op=editor&what=view");
-	$op = "";
-	httpset("op", $op);
-?>
--- modules/skills/skills_uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$skills = db_prefix("skills");
-	$skillsbuffs = db_prefix("skillsbuffs");
-	$sql = "DROP TABLE $skills, $skillsbuffs";
-	db_query($sql);
-?>
