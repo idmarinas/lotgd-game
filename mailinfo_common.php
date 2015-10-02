6edd3d75773475d5c140487afecc95666a4083a8
Iván Diaz Marinas <idmarinas@icloud.com>
2015-10-02 13:07:19 +0200

    Corregido conflicto con instalaciones en una carpeta superior al dominio

--- mailinfo_common.php
+++ mailinfo_common.php
@@ -3,7 +3,7 @@
 require("lib/xajax/xajax_core/xajax.inc.php");
 $xajax = new xajax("mailinfo_server.php");
 //$xajax->setFlag("debug",true);
-$xajax->configure('javascript URI','/lib/xajax/');
+$xajax->configure('javascript URI','lib/xajax/');
 $xajax->register(XAJAX_FUNCTION,"mail_status");
 $xajax->register(XAJAX_FUNCTION,"timeout_status");
 
