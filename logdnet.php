8eb17e8096446de2f2d4cf774fef616722e63ac8
Iván Diaz Marinas <idmarinas@icloud.com>
2016-02-09 17:29:32 +0100

    Se cambia el nombre para diferenciarlos

--- logdnet.php
+++ logdnet.php
@@ -189,7 +189,7 @@ if ($op==""){
 }else{
 	page_header("LoGD Net");
 	addnav('Login page');
-	addnav("Login page","index.php");
+	addnav("Login","index.php");
 	output("`@Below are a list of other LoGD servers that have registered with the LoGD Net.`n");
 	output("`2It should be noted that this list is subject to editing and culling by the administrators of logdnet.logd.com. ");
 	output("Normally this list is a comprehensive list of all servers that have elected to register with LoGDnet, but I'm making changes to that. ");
