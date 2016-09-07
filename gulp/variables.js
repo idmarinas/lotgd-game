bc725cccab06d853599df70b9e39dfa79381bd9b
Iván Diaz Marinas <idmarinas@icloud.com>
2016-09-07 17:43:55 +0200

    Primera anotación

---
+++ gulp/variables.js
@@ -0,0 +1,8 @@
+/**
+ * Modify with your data
+ */
+module.exports = {
+    development_dir: '/Users/Ivan/Sites/ignis', //-- Directory for your test server (localhost server)
+	production_dir: '../production', //-- Folder when save files when you want upload to your production server
+	build_dir: 'dist' //-- Name of build folder
+};
