308c330989dfa1dbfb768e7a12401ac765506dad
Iván Diaz Marinas <idmarinas@icloud.com>
2016-10-29 17:48:32 +0200

    Se cambia el nombre de destino

--- gulp/variables.js
+++ gulp/variables.js
@@ -2,8 +2,8 @@
  * Modify with your data
  */
 module.exports = {
-    development_dir: '/Users/Ivan/Sites/ignis', //-- Directory for your test server (localhost server)
-    development_test_dir: '/Users/Ivan/Sites/ignis-dev', //-- Directory for your test-dev server (localhost server)
+    development_dir: '/Users/Ivan/Sites/lotgd-ignis', //-- Directory for your test server (localhost server)
+    development_test_dir: '/Users/Ivan/Sites/lotgd-alpha', //-- Directory for your test-dev server (localhost server)
 	production_dir: '../production', //-- Folder when save files when you want upload to your production server
 	build_dir: 'dist' //-- Name of build folder
 };
