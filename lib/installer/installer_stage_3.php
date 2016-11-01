64b2a69592f727ed230a6d7f38597a39193b9b4a
Iván Diaz Marinas <idmarinas@icloud.com>
2016-11-01 10:39:24 +0100

    Se agrega el campo para seleccionar el tipo de base de datos

--- lib/installer/installer_stage_3.php
+++ lib/installer/installer_stage_3.php
@@ -19,23 +19,23 @@ else
 	output("`iIf you are unsure of the answer to any of these questions, please check with your server's ISP, or read the documentation on MySQL`i`n");
 
 	output('`nWhat is the driver of your DataBase?`n');
-	rawoutput('<select name="DB_DBDRIVER"><option value="Mysqli">Mysqli</option><option value="Pdo_Mysql" selected>Pdo_Mysql</option></select>');
+	rawoutput('<select name="DB_DRIVER"><option value="Mysqli">Mysqli</option><option value="Pdo_Mysql" selected>Pdo_Mysql</option></select>');
 	tip('Select de driver to connect to your Database. Default is PDO Mysql.');
 
 	output("`nWhat is the address of your database server?`n");
-	rawoutput("<input name='DB_HOST' value=\"".htmlentities($session['dbinfo']['DB_HOST'], ENT_COMPAT, getsetting('charset', 'UTF-8'))."\">");
+	rawoutput("<input name='DB_HOST' value=\"".htmlentities($session['dbinfo']['DB_HOST'], ENT_COMPAT, getsetting('charset', 'utf-8'))."\">");
 	tip("If you are running LoGD from the same server as your database, use 'localhost' here.  Otherwise, you will have to find out what the address is of your database server.  Your server's ISP might be able to provide this information.");
 
 	output("`nWhat is the username you use to connect to the database server?`n");
-	rawoutput("<input name='DB_USER' value=\"".htmlentities($session['dbinfo']['DB_USER'], ENT_COMPAT, getsetting('charset', 'UTF-8'))."\">");
+	rawoutput("<input name='DB_USER' value=\"".htmlentities($session['dbinfo']['DB_USER'], ENT_COMPAT, getsetting('charset', 'utf-8'))."\">");
 	tip("This username does not have to be the same one you use to connect to the database server for administrative reasons.  However, in order to use this installer, and to install some of the modules, the account you provide here must have the ability to create, modify, and drop tables.  If you want the installer to create a new database for LoGD, the account will also have to have the ability to create databases.  Finally, to run the game, this account must at a minimum be able to select, insert, update, and delete records, and be able to lock tables.  If you're uncertain, grant the account the following privileges: SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, and ALTER.");
 
 	output("`nWhat is the password for this username?`n");
-	rawoutput("<input name='DB_PASS' value=\"".htmlentities($session['dbinfo']['DB_PASS'], ENT_COMPAT, getsetting('charset', 'UTF-8'))."\">");
+	rawoutput("<input name='DB_PASS' value=\"".htmlentities($session['dbinfo']['DB_PASS'], ENT_COMPAT, getsetting('charset', 'utf-8'))."\">");
 	tip("The password is necessary here in order for the game to successfully connect to the database server.  This information is not shared with anyone, it is simply used to configure the game.");
 
 	output("`nWhat is the name of the database you wish to install LoGD in?`n");
-	rawoutput("<input name='DB_NAME' value=\"".htmlentities($session['dbinfo']['DB_NAME'], ENT_COMPAT, getsetting('charset', 'UTF-8'))."\">");
+	rawoutput("<input name='DB_NAME' value=\"".htmlentities($session['dbinfo']['DB_NAME'], ENT_COMPAT, getsetting('charset', 'utf-8'))."\">");
 	tip("Database servers such as MySQL can control many different databases.  This is very useful if you have many different programs each needing their own database.  Each database has a unique name.  Provide the name you wish to use for LoGD in this field.");
 
 	output("`nDo you want to use datacaching (high load optimization)?`n");
@@ -46,7 +46,7 @@ else
 	tip("Do you want to use a datacache for the sql queries? Many internal queries produce the same results and can be cached. This feature is *highly* recommended to use as the MySQL server is usually high frequented. When using in an environment where Safe Mode is enabled; this needs to be a path that has the same UID as the web server runs.");
 
 	output("`nIf yes, what is the path to the datacache directory?`n");
-	rawoutput("<input name='DB_DATACACHEPATH' value=\"".htmlentities($session['dbinfo']['DB_DATACACHEPATH'], ENT_COMPAT, getsetting('charset', 'UTF-8'))."\">");
+	rawoutput("<input name='DB_DATACACHEPATH' value=\"".htmlentities($session['dbinfo']['DB_DATACACHEPATH'], ENT_COMPAT, getsetting('charset', 'utf-8'))."\">");
 	tip("If you have chosen to use the datacache function, you have to enter a path here to where temporary files may be stored. Verify that you have the proper permission (777) set to this folder, else you will have lots of errors. Do NOT end with a slash / ... just enter the dir");
 
 	/*
