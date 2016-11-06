d4350f0ede51fc3b6f1ad394c692073a526c09c1
Iván Diaz Marinas <idmarinas@icloud.com>
2016-11-06 20:32:44 +0100

    La intalación no crea la base de datos

--- lib/installer/installer_functions.php
+++ lib/installer/installer_functions.php
@@ -1,23 +1,4 @@
 <?php
-function create_db($dbname){
-	output("`n`2Attempting to create your database...`n");
-	$sql = "CREATE DATABASE $dbname";
-	mysql_query($sql);
-	$error = mysql_error();
-	if ($error == ""){
-		if (mysql_select_db($dbname)){
-			output("`@Success!`2  I was able to create the database and connect to it!`n");
-		}else{
-			output("`\$It seems I was not successful.`2  I didn't get any errors trying to create the database, but I was not able to connect to it.");
-			output("I'm not sure what would have caused this error, you might try asking around in <a href='http://lotgd.net/forum/' target='_blank'>the LotGD.net forums</a>.");
-		}
-	}else{
-		output("`\$It seems I was not successful.`2 ");
-		output("The error returned by the database server was:");
-		rawoutput("<blockquote>$error</blockquote>");
-	}
-
-}
 
 $tipid=0;
 function tip(){
