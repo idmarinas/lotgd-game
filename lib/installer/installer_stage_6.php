94fce450c61bbe0d038a7a61a30d416f560390f1
Iván Diaz Marinas <idmarinas@icloud.com>
2016-11-06 19:24:13 +0100

    Se genera el nuevo archivo de conexión

--- lib/installer/installer_stage_6.php
+++ lib/installer/installer_stage_6.php
@@ -1,48 +1,79 @@
 <?php
-if (file_exists("dbconnect.php")){
-	$success=true;
-	$initial=false;
-}else{
+
+use Zend\Code\Generator\DocBlockGenerator;
+use Zend\Code\Generator\FileGenerator;
+
+$body = '$adapter = ['  . PHP_EOL;
+$body .= '	\'driver\' => \'' . $session['dbinfo']['DB_DRIVER'] . '\','  . PHP_EOL;
+$body .= '	\'hostname\' => \'' . $session['dbinfo']['DB_HOST'] . '\','  . PHP_EOL;
+$body .= '	\'database\' => \'' . $session['dbinfo']['DB_NAME'] . '\','  . PHP_EOL;
+$body .= '	\'charset\' => \'utf8\','  . PHP_EOL;
+$body .= '	\'username\' => \'' . $session['dbinfo']['DB_USER'] . '\','  . PHP_EOL;
+$body .= '	\'password\' => \'' . $session['dbinfo']['DB_PASS'] . '\'' . PHP_EOL;
+$body .= '];' . PHP_EOL . PHP_EOL;
+$body .= '$DB_PREFIX = \'' . $session['dbinfo']['DB_PREFIX'] . '\';' . PHP_EOL;
+$body .= '$DB_USEDATACACHE = \'' . $session['dbinfo']['DB_USEDATACACHE'] . '\';' . PHP_EOL;
+$body .= '$DB_DATACACHEPATH = \'' . $session['dbinfo']['DB_DATACACHEPATH'] . '\';' . PHP_EOL;
+
+$file = FileGenerator::fromArray([
+	'docblock' => DocBlockGenerator::fromArray([
+		'shortDescription' => 'This file is automatically created by installer.php',
+		'longDescription' => null,
+		'tags' => [
+			[
+				'name' => 'create',
+				'description' => date('M d, Y h:i a'),
+			],
+		],
+	]),
+	'body' => $body,
+]);
+unset($body);
+
+$code = $file->generate();
+
+if (file_exists('dbconnect.php'))
+{
+	$success = true;
+	$initial = false;
+}
+else
+{
 	$initial = true;
 	output("`@`c`bWriting your dbconnect.php file`b`c");
 	output("`2I'm attempting to write a file named 'dbconnect.php' to your site root.");
 	output("This file tells LoGD how to connect to the database, and is necessary to continue installation.`n");
-	$dbconnect =
-	"<?php\n"
-	."//This file automatically created by installer.php on ".date("M d, Y h:i a")."\n"
-	."\$DB_HOST = \"{$session['dbinfo']['DB_HOST']}\";\n"
-	."\$DB_USER = \"{$session['dbinfo']['DB_USER']}\";\n"
-	."\$DB_PASS = \"{$session['dbinfo']['DB_PASS']}\";\n"
-	."\$DB_NAME = \"{$session['dbinfo']['DB_NAME']}\";\n"
-	."\$DB_PREFIX = \"{$session['dbinfo']['DB_PREFIX']}\";\n"
-	."\$DB_USEDATACACHE = ". ((int)$session['dbinfo']['DB_USEDATACACHE']) .";\n"
-	."\$DB_DATACACHEPATH = \"{$session['dbinfo']['DB_DATACACHEPATH']}\";\n"
-	."?>\n";
-	$fp = @fopen("dbconnect.php","w+");
-	if ($fp){
-		if (fwrite($fp, $dbconnect)!==false){
-			output("`n`@Success!`2  I was able to write your dbconnect.php file, you can continue on to the next step.");
-		}else{
-			$failure=true;
-		}
-		fclose($fp);
-	}else{
-		$failure=true;
-	}
-	if ($failure){
+
+	$result = file_put_contents('dbconnect.php', $code);
+
+	if (false !== $result) output("`n`@Success!`2  I was able to write your dbconnect.php file, you can continue on to the next step.");
+	else $failure = true;
+
+	if ($failure)
+	{
 		output("`n`\$Unfortunately, I was not able to write your dbconnect.php file.");
 		output("`2You will have to create this file yourself, and upload it to your web server.");
 		output("The contents of this file should be as follows:`3");
-		rawoutput("<blockquote><pre>".htmlentities($dbconnect, ENT_COMPAT, getsetting("charset", "ISO-8859-1"))."</pre></blockquote>");
-		output("`2Create a new file, past the entire contents from above into it (everything from and including `3<?php`2 up to and including `3?>`2 ).");
+		rawoutput('<blockquote><pre>'.htmlentities($code, ENT_COMPAT, getsetting('charset', 'UTF-8')).'</pre></blockquote>');
+		output("`2Create a new file, past the entire contents from above into it`2 ).");
 		output("When you have that done, save the file as 'dbconnect.php' and upload this to the location you have LoGD at.");
 		output("You can refresh this page to see if you were successful.");
-	}else{
-		$success=true;
 	}
+	else $success = true;
 }
-if ($success && !$initial){
-	$version = getsetting("installer_version","-1");
+
+if(! $success) $session['stagecompleted'] = 5;
+
+if (! $initial)
+{
+	output('`$The file "dbconnect.php" was created before. If you want update with new data, please delete it.`n');
+	output('For now, game cant do update installation.`0`n');
+}
+
+/*
+if ($success && ! $initial)
+{
+	$version = getsetting('installer_version', '-1');
 	$sub = substr($version, 0, 5);
 	$sub = (int)str_replace(".", "", $sub);
 	if ($sub < 110) {
@@ -106,4 +137,4 @@ if ($success && !$initial){
 }else if(!$success) {
 	$session['stagecompleted']=5;
 }
-?>
+*/
