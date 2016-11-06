391b6daa50906333d50e65b654c0ea0c7caad6c1
Iván Diaz Marinas <idmarinas@icloud.com>
2016-11-06 19:47:26 +0100

    Primera anotación

---
+++ lib/cache.php
@@ -0,0 +1,34 @@
+<?php
+
+use Zend\Cache\Storage\Adapter\Filesystem;
+
+use Zend\Cache\Storage\Plugin\PluginOptions;
+use Zend\Cache\Storage\Plugin\Serializer;
+use Zend\Cache\Storage\Plugin\ExceptionHandler;
+
+class LotgdCache extends Filesystem
+{
+	public function __construct(array $options = [])
+	{
+		if (empty($options))
+		{
+			$options = [
+				'namespace' => 'lotgd',
+				'cache_dir' => 'cache/',
+				'ttl' => 900
+			];
+		}
+
+		parent::__construct($options);
+
+		//-- Add plugins to cache system
+
+		$this->addPlugin(new Serializer);
+
+		$plugin = new ExceptionHandler;
+		$plugin->setOptions(new PluginOptions([ 'throw_exceptions' => false ]));
+		$this->addPlugin($plugin);
+	}
+}
+
+$lotgd_cache = new LotgdCache;
