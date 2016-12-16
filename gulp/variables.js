d081473bcb6bc5a8e996d05cf5add3603b5f2f53
Iván Diaz Marinas <idmarinas@icloud.com>
2016-12-16 13:21:41 +0100

    Ya no se hace un seguimiento de cambios

--- gulp/variables.js
+++
@@ -1,9 +0,0 @@
-/**
- * Modify with your data
- */
-module.exports = {
-    development_dir: '/Users/Ivan/Sites/lotgd-ignis', //-- Directory for your test server (localhost server)
-    development_test_dir: '/Users/Ivan/Sites/lotgd-alpha', //-- Directory for your test-dev server (localhost server)
-	production_dir: '../production', //-- Folder when save files when you want upload to your production server
-	build_dir: 'dist' //-- Name of build folder
-};
