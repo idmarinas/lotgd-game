258f3850b074169ca3f6aa1d931dafa2706f6f00
Iván Diaz Marinas <idmarinas@icloud.com>
2016-12-22 16:47:12 +0100

    Se reducen las tareas

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -5,37 +5,13 @@ var strip = require('gulp-strip-comments');
 var variables = require('../variables');
 
 //-- Construir el archivo de LOTGD
-gulp.task('js-lotgd', function () {
+gulp.task('lotgd', function () {
 	return gulp.src([
-		'assets/lotgd.js',
-		'assets/components/*.js'
-	])
+			'assets/lotgd.js',
+			'assets/components/*.js'
+		])
 		.pipe(concat('lotgd.js'))
 		.pipe(strip({ safe: true }))
-		.pipe(gulp.dest(variables.build_dir + '/resources/'));
-});
-
-//-- Construir el archivo UIKIT
-gulp.task('js-uikit', function () {
-	return gulp.src([
-		'bower_components/uikit/js/uikit.js',
-		// 'bower_components/uikit/js/core/tab.js',
-		// 'bower_components/uikit/js/core/switcher.js',
-		// 'bower_components/uikit/js/core/dropdown.js',
-		// 'bower_components/uikit/js/core/nav.js',
-		'bower_components/uikit/js/components/tooltip.js',
-		'bower_components/uikit/js/components/notify.js',
-		'bower_components/uikit/js/components/grid.js',
-		// 'bower_components/uikit/js/components/modal.js',
-		// 'bower_components/uikit/js/components/alert.js',
-	])
-		.pipe(concat('uikit.js'))
-		.pipe(gulp.dest(variables.build_dir + '/resources/'));
-});
-
-//-- Copiar el archivo jquery
-gulp.task('js-jquery', function () {
-	return gulp.src('bower_components/jquery/dist/jquery.js')
-		.pipe(gulp.dest(variables.build_dir + '/resources/'))
-		;
+		.pipe(gulp.dest(variables.build.dir + '/resources/'))
+	;
 });
