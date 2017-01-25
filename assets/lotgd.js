db050359787895c2ea30acfa179102007c7fe6a5
Iván Diaz Marinas <idmarinas@icloud.com>
2017-01-25 19:05:21 +0100

    Se agrega configuración para complementos de Semantic UI

--- assets/lotgd.js
+++ assets/lotgd.js
@@ -7,4 +7,17 @@
 
 $(function(){
 	$('.ui.dropdown').dropdown();
+	$('.ui.popup.lotgd.form .item').tab({
+        onVisible : function (tabPath)
+        {
+            var text = $('.ui.popup.lotgd.form .item[data-tab="' + tabPath + '"]').text();
+            $('.ui.menu.lotgd.form .header.item').text(text);
+        },
+    });
+    $('.ui.checkbox').checkbox();
+    $('.ui.menu.form.lotgd .browse').popup({
+        inline     : true,
+        hoverable  : true,
+        position   : 'bottom left',
+    });
 });
