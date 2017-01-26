3a17093d6d62ceadfb29dc62c4c8f728970ee1b9
Iván Diaz Marinas <idmarinas@icloud.com>
2017-01-26 15:34:15 +0100

    Se comprueba que existe la variable

--- motd.php
+++ motd.php
@@ -106,7 +106,7 @@ if ($op=="") {
 	rawoutput(" <input type='submit' value='".translate_inline("Submit")."' class='button'>");
 	rawoutput("</form>");
 
-	if ($session['user']['online']) commentdisplay("`n`@Commentary:`0`n", "motd");
+	if (isset($session['user']['online']) && $session['user']['online']) commentdisplay("`n`@Commentary:`0`n", "motd");
 }
 
 $session['needtoviewmotd']=false;
