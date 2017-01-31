06c9509344b92490d186f5a0ec2cf0d7d4cd4169
Iván Diaz Marinas <idmarinas@icloud.com>
2017-01-31 12:07:02 +0100

    Se agregan las clases de estilo

--- lib/user/user_edit.php
+++ lib/user/user_edit.php
@@ -19,26 +19,31 @@ if ($session['user']['superuser'] & SU_EDIT_DONATIONS) {
 addnav("Bans");
 addnav("Set up ban","bans.php?op=setupban&userid={$row['acctid']}");
 if (httpget("subop")==""){
-	rawoutput("<form action='user.php?op=special&userid=$userid$returnpetition' method='POST'>");
+	rawoutput("<br><form action='user.php?op=special&userid=$userid$returnpetition' method='POST'>");
 	addnav("","user.php?op=special&userid=$userid$returnpetition");
 	$grant = translate_inline("Grant New Day");
-	rawoutput("<input type='submit' class='button' name='newday' value='$grant'>");
+	rawoutput("<input type='submit' class='ui primary button' name='newday' value='$grant'>");
 	$fix = translate_inline("Fix Broken Navs");
-	rawoutput("<input type='submit' class='button' name='fixnavs' value='$fix'>");
+	rawoutput("<input type='submit' class='ui primary button' name='fixnavs' value='$fix'>");
 	$mark = translate_inline("Mark Email As Valid");
-	rawoutput("<input type='submit' class='button' name='clearvalidation' value='$mark'>");
+	rawoutput("<input type='submit' class='ui primary button' name='clearvalidation' value='$mark'>");
 	rawoutput("</form>");
 		//Show a user's usertable
 	rawoutput("<form action='user.php?op=save&userid=$userid$returnpetition' method='POST'>");
 	addnav("","user.php?op=save&userid=$userid$returnpetition");
 	$save = translate_inline("Save");
-	rawoutput("<input type='submit' class='button' value='$save'>");
-	if ($row['loggedin']==1 && $row['laston']>date("Y-m-d H:i:s",strtotime("-".getsetting("LOGINTIMEOUT",900)." seconds"))){
+	if ($row['loggedin']==1 && $row['laston']>date("Y-m-d H:i:s",strtotime("-".getsetting("LOGINTIMEOUT",900)." seconds")))
+	{
+		rawoutput("<br><input type='submit' class='ui red button' value='$save'>");
 		output_notl("`\$");
 		rawoutput("<span style='font-size: 20px'>");
 		output("`\$Warning:`0");
 		rawoutput("</span>");
 		output("`\$This user is probably logged in at the moment!`0");
+	}
+	else
+	{
+		rawoutput("<br><input type='submit' class='ui primary button' value='$save'>");
 	}
 	//Add new composita attack
 	$row['totalattack']=get_player_attack($row['acctid']);
