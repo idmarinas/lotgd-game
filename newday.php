e90af5586cf7d188ba8ea74556212c6fe5fc0f62
Iván Diaz Marinas <idmarinas@icloud.com>
2017-02-07 16:53:09 +0100

    Se puede elegir entre usar el sistema de turnos o de stamina

--- newday.php
+++ newday.php
@@ -56,10 +56,9 @@ if ((count($session['user']['dragonpoints']) <
 	}
 }
 
-$labels = array(
+$labels = [
 		"General Stuff,title",
-		"hp"=>"Max Hitpoints + 5",
-		// "ff"=>"Forest Fights + 1",
+		"ff"=>"Forest Fights + 1",
 		"Attributes,title",
 		"str"=>"Strength +1",
 		"dex"=>"Dexterity +1",
@@ -70,11 +69,12 @@ $labels = array(
 		/*well, to be precise you can if you write a module that sets $canbuy['at']=1 ;) */
 		"at"=>"Attack + 1",
 		"de"=>"Defense + 1",
+		"hp"=>"Max Hitpoints + 5",
 		"unknown"=>"Unknown Spends (contact an admin to investigate!)",
-);
-$canbuy = array(
+];
+$canbuy = [
 		"hp"=>0,
-		"ff"=>0,
+		"ff"=>1,
 		"str"=>1,
 		"dex"=>1,
 		"con"=>1,
@@ -83,7 +83,9 @@ $canbuy = array(
 		"at"=>0,
 		"de"=>0,
 		"unknown"=>0,
-);
+];
+if (is_module_active('staminasystem')) $canbuy['ff'] = 0;
+
 $retargs = modulehook("dkpointlabels", array('desc'=>$labels, 'buy'=>$canbuy));
 $labels = $retargs['desc'];
 $canbuy = $retargs['buy'];
@@ -93,16 +95,17 @@ $pdk=httpget("pdk");
 $dp = count($session['user']['dragonpoints']);
 $dkills = $session['user']['dragonkills'];
 
-if ($pdk==1){
-	require("lib/newday/dp_recalc.php");
+if ($pdk == 1)
+{
+	require 'lib/newday/dp_recalc.php';
 }
 
 if ($dp < $dkills) {
-	require("lib/newday/dragonpointspend.php");
+	require 'lib/newday/dragonpointspend.php';
 } elseif (!$session['user']['race'] || $session['user']['race']==RACE_UNKNOWN){
-	require("lib/newday/setrace.php");
+	require 'lib/newday/setrace.php';
 }elseif ($session['user']['specialty']==""){
-	require("lib/newday/setspecialty.php");
+	require 'lib/newday/setspecialty.php';
 }else{
 	page_header("It is a new day!");
 	rawoutput("<font size='+1'>");
@@ -110,37 +113,47 @@ if ($dp < $dkills) {
 	rawoutput("</font>");
 	$resurrection = httpget('resurrection');
 
-	if ($session['user']['alive']!=true){
+	if ($session['user']['alive']!=true)
+	{
 		$session['user']['resurrections']++;
 		output("`@You are resurrected!  This is resurrection number %s.`0`n",$session['user']['resurrections']);
 		$session['user']['alive']=true;
 		invalidatedatacache("list.php-warsonline");
 	}
 	$session['user']['age']++;
-	$session['user']['seenmaster']=0;
+	$session['user']['seenmaster'] = 0;
 	output("You open your eyes to discover that a new day has been bestowed upon you. It is day number `^%s.`0",$session['user']['age']);
 	output("You feel refreshed enough to take on the world!`n");
-	// output("`2Turns for today set to `^%s`2.`n",$turnsperday);
+	if (! is_module_active('staminasystem')) output("`2Turns for today set to `^%s`2.`n",$turnsperday);
 
 	$turnstoday = "Base: $turnsperday";
-	$args = modulehook("pre-newday",
-			array("resurrection"=>$resurrection, "turnstoday"=>$turnstoday));
+	$args = modulehook('pre-newday', ['resurrection' => $resurrection, 'turnstoday' => $turnstoday]);
 	$turnstoday = $args['turnstoday'];
 
 	$interestrate = e_rand($mininterest*100,$maxinterest*100)/(float)100;
-	if ($session['user']['turns']>getsetting("fightsforinterest",4) && $session['user']['goldinbank']>=0) {
-		$interestrate=1;
+	if (is_module_active('staminasystem'))
+	{
+		require_once 'modules/staminasystem/lib/lib.php';
+		$stamina = get_stamina(3);
+		$canGetInterest = ($stamina <= 40);
+	}
+	else $canGetInterest = ($session['user']['turns'] > getsetting('fightsforinterest', 4) && $session['user']['goldinbank'] >= 0)
+
+	if ($canGetInterest)
+	{
+		$interestrate = 1;
 		output("`2Today's interest rate: `^0% (Bankers in this village only give interest to those who work for it)`2.`n");
-	} elseif (getsetting("maxgoldforinterest", 100000) && $session['user']['goldinbank']>=getsetting("maxgoldforinterest", 100000)) {
-		$interestrate=1;
-		output("`2Today's interest rate: `^0%% (The bank will not pay interest on accounts equal or greater than %s to retain solvency)`2.`n", getsetting("maxgoldforinterest", 100000));
-	}else{
+	}
+	elseif (getsetting('maxgoldforinterest', 100000) && $session['user']['goldinbank'] >= getsetting('maxgoldforinterest', 100000))
+	{
+		$interestrate = 1;
+		output("`2Today's interest rate: `^0%% (The bank will not pay interest on accounts equal or greater than %s to retain solvency)`2.`n", getsetting('maxgoldforinterest', 100000));
+	}
+	else
+	{
 		output("`2Today's interest rate: `^%s%% `n",($interestrate-1)*100);
-		if ($session['user']['goldinbank']>=0){
-			output("`2Gold earned from interest: `^%s`2.`n",(int)($session['user']['goldinbank']*($interestrate-1)));
-		}else{
-			output("`2Interest Accrued on Debt: `^%s`2 gold.`n",-(int)($session['user']['goldinbank']*($interestrate-1)));
-		}
+		if ($session['user']['goldinbank']>=0) output("`2Gold earned from interest: `^%s`2.`n",(int)($session['user']['goldinbank']*($interestrate-1)));
+		else output("`2Interest Accrued on Debt: `^%s`2 gold.`n",-(int)($session['user']['goldinbank']*($interestrate-1)));
 	}
 
 	//clear all standard buffs
@@ -168,30 +181,35 @@ if ($dp < $dkills) {
 
 	output("`2Hitpoints have been restored to `^%s`2.`n",$session['user']['maxhitpoints']);
 
-
 	//-- Can use dragonkill to upgrade turns
-	// reset($session['user']['dragonpoints']);
-	$dkff=0;
-	// while(list($key,$val)=each($session['user']['dragonpoints'])){
-	// 	if ($val=="ff"){
-	// 		$dkff++;
-	// 	}
-	// }
-	if ($session['user']['hashorse']){
+	reset($session['user']['dragonpoints']);
+	$dkff = 0;
+	if (! is_module_active('staminasystem'))
+	{
+		while(list($key, $val) = each($session['user']['dragonpoints']))
+		{
+			if ($val == 'ff') $dkff++;
+		}
+		if ($dkff>0)
+		{
+			output("`n`2You gain `^%s`2 forest %s from spent dragon points!", $dkff, translate_inline($dkff == 1?"fight":"fights"));
+		}
+	}
+
+	if ($session['user']['hashorse'])
+	{
 		$buff = unserialize($playermount['mountbuff']);
 		if (!isset($buff['schema']) || $buff['schema'] == "")
 			$buff['schema']="mounts";
 		apply_buff('mount',$buff);
 	}
-	// if ($dkff>0) {
-		// output("`n`2You gain `^%s`2 forest %s from spent dragon points!",
-				// $dkff, translate_inline($dkff == 1?"fight":"fights"));
-	// }
+
 	$r1 = e_rand(-1,1);
 	$r2 = e_rand(-1,1);
 	$spirits = $r1+$r2;
-	$resurrectionturns=$spirits;
-	if ($resurrection=="true"){
+	$resurrectionturns = $spirits;
+	if ($resurrection == "true")
+	{
 		addnews("`&%s`& has been resurrected by %s`&.",$session['user']['name'],getsetting('deathoverlord','`$Ramius'));
 		$spirits=-6;
 		$resurrectionturns=getsetting('resurrectionturns',-6);
@@ -207,18 +225,22 @@ if ($dp < $dkills) {
 		$session['user']['restorepage']="village.php?c=1";
 	}
 
-	$sp = array((-6)=>"Resurrected", (-2)=>"Very Low", (-1)=>"Low",
-			(0)=>"Normal", 1=>"High", 2=>"Very High");
+	$sp = [(-6) => 'Resurrected', (-2) => 'Very Low', (-1) => 'Low', (0) => 'Normal', 1 => 'High', 2 => 'Very High'];
 	$sp = translate_inline($sp);
 	output("`n`2You are in `^%s`2 spirits today!`n",$sp[$spirits]);
-	if (abs($spirits)>0){
-		if($resurrectionturns>0){
-			$gain=translate_inline("gain");
-		}else{
-			$gain=translate_inline("lose");
+	if (abs($spirits)>0)
+	{
+		if($resurrectionturns > 0) $gain = translate_inline("gain");
+		else $gain= translate_inline("lose");
+
+		if (is_module_active('staminasystem')) output('`2As a result, you `^%s some Stamina`2 for today!`n', $gain);
+		else
+		{
+			$sff = abs($resurrectionturns);
+			output("`2As a result, you `^%s %s forest %s`2 for today!`n", $gain, $sff, translate_inline($sff==1?"fight":"fights"));
 		}
-		output("`2As a result, you `^%s some Stamina`2 for today!`n", $gain);
 	}
+
 	$rp = $session['user']['restorepage'];
 	$x = max(strrpos("&",$rp),strrpos("?",$rp));
 	if ($x>0) $rp = substr($rp,0,$x);
@@ -240,11 +262,10 @@ if ($dp < $dkills) {
 		debuglog(($nbgold >= 0 ? "earned " : "paid ") . abs($nbgold) . " gold in interest");
 	}
 	$turnstoday .= ", Spirits: $resurrectionturns, DK: $dkff";
-	$session['user']['turns']=$turnsperday+$resurrectionturns+$dkff;
+	$session['user']['turns'] = $turnsperday+$resurrectionturns+$dkff;
 	$session['user']['hitpoints'] = $session['user']['maxhitpoints'];
 	$session['user']['spirits'] = $spirits;
-	if ($resurrection != "true")
-		$session['user']['playerfights'] = $dailypvpfights;
+	if ($resurrection != "true") $session['user']['playerfights'] = $dailypvpfights;
 	$session['user']['transferredtoday'] = 0;
 	$session['user']['amountouttoday'] = 0;
 	$session['user']['seendragon'] = 0;
@@ -257,12 +278,13 @@ if ($dp < $dkills) {
 	$session['user']['boughtroomtoday'] = 0;
 	$session['user']['recentcomments']=$session['user']['lasthit'];
 	$session['user']['lasthit'] = gmdate("Y-m-d H:i:s");
-	if ($session['user']['hashorse']){
+	if ($session['user']['hashorse'])
+	{
 		$msg = $playermount['newday'];
-		require_once("lib/substitute.php");
+		require_once 'lib/substitute.php';
 		$msg = substitute_array("`n`&".$msg."`0`n");
 		output($msg);
-		require_once("lib/mountname.php");
+		require_once 'lib/mountname.php';
 		list($name, $lcname) = getmountname();
 
 		$mff = (int)$playermount['mountforestfights'];
@@ -283,10 +305,11 @@ if ($dp < $dkills) {
 		output("`n`&You strap your `%%s`& to your back and head out for some adventure.`0",$session['user']['weapon']);
 	}
 
-	require_once("lib/extended-battle.php");
+	require_once 'lib/extended-battle.php';
 	unsuspend_companions("allowinshades");
 
-	if (!getsetting("newdaycron",0)) {
+	if (! getsetting("newdaycron",0))
+	{
 		//check last time we did this vs now to see if it was a different game day.
 		$lastnewdaysemaphore = convertgametime(strtotime(getsetting("newdaySemaphore","0000-00-00 00:00:00") . " +0000"));
 		$gametoday = gametime();
@@ -303,7 +326,7 @@ if ($dp < $dkills) {
                 savesetting("newdaySemaphore",gmdate("Y-m-d H:i:s"));
                 $sql = "UNLOCK TABLES";
                 DB::query($sql);
-				require("lib/newday/newday_runonce.php");
+				require 'lib/newday/newday_runonce.php';
 			}else{
 	            //someone else beat us to it, unlock.
                 $sql = "UNLOCK TABLES";
@@ -312,24 +335,24 @@ if ($dp < $dkills) {
 		}
 
 	}
-	$args = modulehook("newday",
-			array("resurrection"=>$resurrection, "turnstoday"=>$turnstoday));
+	$args = modulehook('newday', ['resurrection' => $resurrection, 'turnstoday' => $turnstoday]);
 	$turnstoday = $args['turnstoday'];
-	//## Para que procese la stamina por el estado de ánimo
-	modulehook('stamina-newday', array('spirits'=>$spirits));
+	//## Process stamina for spirit
+	modulehook('stamina-newday', ['spirits' => $spirits]);
 
 	debuglog("New Day Turns: $turnstoday");
 
 	//legacy support if you have no playername set
-	if ($session['user']['playername']=='') {
+	if ($session['user']['playername'] == '')
+	{
 		//set it
-		require_once("lib/names.php");
-		$session['user']['playername']=get_player_basename(false);
+		require_once 'lib/names.php';
+		$session['user']['playername'] = get_player_basename(false);
 	}
 
 }
 
-$session['user']['sentnotice']=0;
+$session['user']['sentnotice'] = 0;
 
 page_footer();
 ?>
