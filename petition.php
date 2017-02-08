83e6906ed3e6634d9958a9eb83b7eea20ba42e0e
Iván Diaz Marinas <idmarinas@icloud.com>
2017-02-08 16:19:47 +0100

    Formateo del texto

--- petition.php
+++ petition.php
@@ -4,19 +4,21 @@
 // mail ready
 define("ALLOW_ANONYMOUS",true);
 define("OVERRIDE_FORCED_NAV",true);
-require_once("common.php");
-require_once("lib/systemmail.php");
-require_once("lib/output_array.php");
-require_once("lib/http.php");
-require_once("lib/stripslashes_deep.php");
+
+require_once 'common.php';
+require_once 'lib/systemmail.php';
+require_once 'lib/output_array.php';
+require_once 'lib/http.php';
+require_once 'lib/stripslashes_deep.php';
+
 $op = httpget('op');
 
 switch ($op) {
 	case "primer": case "faq": case "faq1": case "faq2": case "faq3":
-		require("lib/petition/petition_$op.php");
+		require 'lib/petition/petition_$op.php';
 		break;
 	default:
-		require("lib/petition/petition_default.php");
+		require 'lib/petition/petition_default.php';
 		break;
 }
 popup_footer();
