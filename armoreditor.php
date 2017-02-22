77c6cd62e3ae7639e381c54d2c2f605fa16e7001
Iván Diaz Marinas <idmarinas@icloud.com>
2017-02-22 17:00:41 +0100

    Se corrige error y se adapta a Semantic UI

--- armoreditor.php
+++ armoreditor.php
@@ -8,9 +8,9 @@
 * @see armor.php
 *
 */
-require_once("common.php");
-require_once("lib/showform.php");
-require_once("lib/http.php");
+require_once 'common.php';
+require_once 'lib/showform.php';
+require_once 'lib/http.php';
 
 check_su_access(SU_EDIT_EQUIPMENT);
 
@@ -18,7 +18,7 @@ tlschema("armor");
 
 page_header("Armor Editor");
 $armorlevel = (int)httpget('level');
-require_once("lib/superusernav.php");
+require_once 'lib/superusernav.php';
 superusernav();
 addnav("Armor Editor");
 addnav("Armor Editor Home","armoreditor.php?level=$armorlevel");
@@ -46,7 +46,7 @@ if($op=="edit" || $op=="add"){
 	}
 	rawoutput("<form action='armoreditor.php?op=save&level=$armorlevel' method='POST'>");
 	addnav("","armoreditor.php?op=save&level=$armorlevel");
-	lotgd_showform($armorarray,$row);
+	showform($armorarray,$row);
 	rawoutput("</form>");
 }else if($op=="del"){
 	$sql = "DELETE FROM " . DB::prefix("armor") . " WHERE armorid='$id'";
@@ -61,12 +61,16 @@ if($op=="edit" || $op=="add"){
 	if ($armorid>0){
 		$sql = "UPDATE " . DB::prefix("armor") . " SET armorname=\"$armorname\",defense=\"$defense\",value=".$values[$defense]." WHERE armorid='$armorid'";
 	}else{
-		$sql = "INSERT INTO " . DB::query($sql);
+		$sql = "INSERT INTO " . DB::prefix("armor") . " (level,defense,armorname,value) VALUES ($armorlevel,\"$defense\",\"$armorname\",".$values[$defense].")";
+	}
+	DB::query($sql);
 	$op = "";
 	httpset("op", $op);
 }
 if ($op==""){
-	$sql = "SELECT max(level+1) AS level FROM " . DB::fetch_assoc($res);
+	$sql = "SELECT max(level+1) AS level FROM " . DB::prefix("armor");
+	$res = DB::query($sql);
+	$row = DB::fetch_assoc($res);
 	$max = $row['level'];
 	for ($i=0;$i<=$max;$i++){
 		if ($i == 1)
@@ -74,7 +78,8 @@ if ($op==""){
 		else
 			addnav(array("Armor for %s DKs",$i),"armoreditor.php?level=$i");
 	}
-	$sql = "SELECT * FROM " . DB::query($sql);
+	$sql = "SELECT * FROM " . DB::prefix("armor") . " WHERE level=$armorlevel ORDER BY defense";
+	$result= DB::query($sql);
 	$ops = translate_inline("Ops");
 	$name = translate_inline("Name");
 	$cost = translate_inline("Cost");
@@ -84,10 +89,12 @@ if ($op==""){
 	$del = translate_inline("Del");
 	$delconfirm = translate_inline("Are you sure you wish to delete this armor?");
 
-	rawoutput("<table border=0 cellpadding=2 cellspacing=1 bgcolor='#999999'>");
-	rawoutput("<tr class='trhead'><td>$ops</td><td>$name</td><td>$cost</td><td>$defense</td><td>$level</td></tr>");
-	$number=DB::fetch_assoc($result);
-		rawoutput("<tr class='".($i%2?"trdark":"trlight")."'>");
+	rawoutput("<table class='ui very compact striped selectable table'>");
+	rawoutput("<thead><tr><th>$ops</th><th>$name</th><th>$cost</th><th>$defense</th><th>$level</th></tr></thead>");
+	$number=DB::num_rows($result);
+	for ($i=0;$i<$number;$i++){
+		$row = DB::fetch_assoc($result);
+		rawoutput("<tr>");
 		rawoutput("<td>[<a href='armoreditor.php?op=edit&id={$row['armorid']}&level=$armorlevel'>$edit</a>|<a href='armoreditor.php?op=del&id={$row['armorid']}&level=$armorlevel' onClick='return confirm(\"$delconfirm\");'>$del</a>]</td>");
 		addnav("","armoreditor.php?op=edit&id={$row['armorid']}&level=$armorlevel");
 		addnav("","armoreditor.php?op=del&id={$row['armorid']}&level=$armorlevel");
@@ -105,4 +112,3 @@ if ($op==""){
 	rawoutput("</table>");
 }
 page_footer();
-?>
