6f9d9c9661bfd5469489509881683631bf7a612f
Iván Diaz Marinas <idmarinas@icloud.com>
2017-02-24 16:33:52 +0100

    Se corrige error con variable no definida

--- lib/partner.php
+++ lib/partner.php
@@ -2,28 +2,39 @@
 function get_partner($player=false)
 {
 	global $session;
-	if ($session['user']['prefs']['sexuality']=='')
-		$session['user']['prefs']['sexuality']=!$session['user']['sex'];
-	if ($player === false) {
+	if (! isset($session['user']['prefs']['sexuality']) || $session['user']['prefs']['sexuality']=='')
+		$session['user']['prefs']['sexuality'] = ! $session['user']['sex'];
+	if ($player === false)
+	{
 		$partner = getsetting("barmaid", "`%Violet");
 		if ($session['user']['prefs']['sexuality'] == SEX_MALE) {
 			$partner = getsetting("bard", "`^Seth");
 		}
-	} else {
-		if ($session['user']['marriedto'] == INT_MAX) {
+	}
+	else
+	{
+		if ($session['user']['marriedto'] == INT_MAX)
+		{
 			$partner = getsetting("barmaid", "`%Violet");
-			if ($session['user']['prefs']['sexuality'] == SEX_MALE) {
+			if ($session['user']['prefs']['sexuality'] == SEX_MALE)
+			{
 				$partner = getsetting("bard", "`^Seth");
 			}
-		} else {
+		}
+		else
+		{
 			$sql = "SELECT name FROM ".DB::prefix("accounts")." WHERE acctid = {$session['user']['marriedto']}";
 			$result = DB::query($sql);
-			if ($row = DB::fetch_assoc($result)) {
+			if ($row = DB::fetch_assoc($result))
+			{
 				$partner = $row['name'];
-			} else {
+			}
+			else
+			{
 				$session['user']['marriedto'] = 0;
 				$partner = getsetting("barmaid", "`%Violet");
-				if ($session['user']['prefs']['sexuality']  == SEX_MALE) {
+				if ($session['user']['prefs']['sexuality']  == SEX_MALE)
+				{
 					$partner = getsetting("bard", "`^Seth");
 				}
 			}
