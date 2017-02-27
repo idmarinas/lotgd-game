a1c3df550184d9f0c5f1eae64eb44e772f5e4db7
Iván Diaz Marinas <idmarinas@icloud.com>
2017-02-27 15:58:41 +0100

    Se cambia el estilo del botón al de por defecto

--- lib/mail/case_address.php
+++ lib/mail/case_address.php
@@ -17,6 +17,6 @@ rawoutput("<form action='mail.php?op=write' method='post' class='ui form'>");
 output("`b`2Address:`b`n");
 rawoutput(sprintf("<div class='inline field'><label>%s</label> <div class='ui action input'>", appoencode("`2$to`0")));
 output_notl("<input autofocus name='to' id='to' value=\"".htmlentities(stripslashes(httpget('prepop')), ENT_COMPAT, getsetting("charset", "UTF-8"))."\">",true);
-output_notl("<button type='submit' class='ui primary button'>$search</button>", true);
+output_notl("<button type='submit' class='ui button'>$search</button>", true);
 rawoutput($forwardlink.'</div>');
 rawoutput('</form>');
