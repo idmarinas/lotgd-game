334992a209e29a30c3bbe2715931276758a9fb74
Iván Diaz Marinas <idmarinas@icloud.com>
2017-03-02 13:41:45 +0100

    Se añade nueva función para generar las estadísticas base de las criaturas

--- lib/creaturefunctions.php
+++ lib/creaturefunctions.php
@@ -1,9 +1,45 @@
-<?php 
+<?php
+
+/**
+ * Generate a base creature stats
+ * Can use for generated your own creatures in your modules
+ */
+function lotgd_generate_creature_levels()
+{
+	$stats = [];
+	$maxlvl = getsetting('maxlevel', 15) + 4;
+	$creatureexp = 14;
+	$creaturegold = 36;
+	$creaturedefense = 0;
+	for ($i = 1; $i <= $maxlvl; $i++)
+	{
+		//apply algorithmic creature generation.
+		$creaturehealth = ($i*10) + ($i-1) - round(sqrt($i-1));
+		$creatureattack = 1+($i-1) * 2;
+		$creaturedefense += ($i%2?1:2);
+		if ($i > 1)
+		{
+			$creatureexp += round(10 + 1.5 * log($i));
+			$creaturegold += (31 * ($i<4?2:1));
+			//give lower levels more gold
+		}
+		$stats[$i] = [
+			'creaturelevel' => $i,
+			'creaturehealth' => $creaturehealth,
+			'creatureattack' => $creatureattack,
+			'creaturedefense' => $creaturedefense,
+			'creatureexp' => $creatureexp,
+			'creaturegold' => $creaturegold,
+		];
+	}
+
+	return $stats;
+}
 
 function get_creature_stats($dk = 0)
 {
     global $session;
-    
+
     if (0 == $dk) $dk = $session['user']['dragonkills'];
 
     //-- Estan colocados por orden de importancia
@@ -15,47 +51,47 @@ function get_creature_stats($dk = 0)
     $dk -= $dex;
     $int = e_rand(0,$dk);
     $wis = ($dk - $int);
-    
+
     return ['str' => $str, 'dex' => $dex, 'con' => $con, 'int' => $int, 'wis' => $wis ];
 }
 
 function get_creature_hitpoints($attrs)
-{   
+{
     $conbonus = $attrs['con'] * .5;
 	$wisbonus = $attrs['wis'] * .2;
 	$strbonus = $attrs['str'] * .3;
-        
+
     $hitpoints = round($conbonus + $wisbonus + $strbonus, 0);
-    
+
     return max($hitpoints, 0);
 }
 
-function get_creature_attack($attrs) 
+function get_creature_attack($attrs)
 {
 	$strbonus = (1/3) * $attrs['str'];
 	$speedbonus = (1/3) * get_creature_speed($attrs);
 	$wisdombonus = (1/6) * $attrs['wis'];
 	$intbonus = (1/6) * $attrs['int'];
-	
+
 	$attack = $strbonus + $wisdombonus + $intbonus;
-    
+
 	return max($attack,0);
 }
 
-function get_creature_defense($attrs) 
+function get_creature_defense($attrs)
 {
 	$wisdombonus = (1/4) * $attrs['wis'];
 	$constbonus = (3/8) * $attrs['con'];
 	$speedbonus = (3/8) * get_player_speed($attrs);
-    
+
 	$defense = $wisdombonus + $constbonus;
-    
+
 	return max($defense,0);
 }
 
-function get_creature_speed($attrs) 
+function get_creature_speed($attrs)
 {
 	$speed = (1/2) * $attrs['dex'] + (1/4) * $attrs['int'] + (5/2);
-    
+
 	return max($speed,0);
 }
