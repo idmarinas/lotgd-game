428d7e5b3167dec1beea11c5949cdaa4ca1b3445
Iván Diaz Marinas <idmarinas@icloud.com>
2017-03-08 13:18:08 +0100

    Se agrega el tema por defecto

--- lib/installer/versions/2-0-0-IDMarinas-Edition.php
+++ lib/installer/versions/2-0-0-IDMarinas-Edition.php
@@ -4,15 +4,16 @@ return [
 	"INSERT INTO `settings`
 		(`setting`, `value`)
 	VALUES
-		('deathoverlord','`\$Ramius`0'),
-		('villagename','Degolburg'),
-		('innname','The Boar\'s Head Inn'),
-		('barkeep','`tCedrik`0'),
-		('barmaid','`%Violet`0'),
-		('bard','`^Seth`0'),
-		('clanregistrar','`%Karissa`0'),
-		('bankername','`@Elessa`0'),
-		('motditems','5'),
-		('petition_types','Bug,General')
+		('deathoverlord', '`\$Ramius`0'),
+		('villagename', 'Degolburg'),
+		('innname', 'The Boar\'s Head Inn'),
+		('barkeep', '`tCedrik`0'),
+		('barmaid', '`%Violet`0'),
+		('bard', '`^Seth`0'),
+		('clanregistrar', '`%Karissa`0'),
+		('bankername', '`@Elessa`0'),
+		('motditems', '5'),
+		('petition_types', 'Bug,General'),
+		('defaultskin', 'jade.html')
 	ON DUPLICATE KEY UPDATE value=VALUES(value);"
 ];
