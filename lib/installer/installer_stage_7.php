7d08ce6fd19a7279e6c7fcfe750ed5bf09c0a960
Iván Diaz Marinas <idmarinas@icloud.com>
2017-03-15 10:52:25 +0100

    Se permite la actuliazación

--- lib/installer/installer_stage_7.php
+++ lib/installer/installer_stage_7.php
@@ -19,27 +19,25 @@ if (! isset($session['fromversion']) || $session['fromversion'] == '')
 {
 	output("`@`c`bConfirmation`b`c");
 	output("`2Please confirm the following:`0`n");
-	rawoutput("<form action='installer.php?stage=7' method='POST'>");
+	rawoutput("<form class='ui form' action='installer.php?stage=7' method='POST'>");
 	rawoutput("<table class='ui very basic table'><tr><td>");
 	output("`2I should:`0");
 	rawoutput("</td><td>");
 	$version = getsetting('installer_version', '-1');
 	if ($version != '-1') $session['dbinfo']['upgrade'] = true;
 	else $session['dbinfo']['upgrade'] = false;
-	// rawoutput("<input type='radio' value='upgrade' name='type' ".($session['dbinfo']['upgrade']? 'checked' : '').">");
+	rawoutput("<input type='radio' value='upgrade' name='type' ".($session['dbinfo']['upgrade']? 'checked' : '').">");
 	output(" `2Perform an upgrade from ");
-	output('`$For now cant do a upgrade installation.`2`n');
-	// if ($version == '-1') $version = '0.9.7';
-	// reset($sql_upgrade_statements);
-	// rawoutput("<select name='version'>");
-	// while(list($key, $val) = each($sql_upgrade_statements))
-	// {
-	// 	if ($key != '-1')
-	// 	{
-	// 		rawoutput("<option value='$key'".($version==$key?" selected":"").">$key</option>");
-	// 	}
-	// }
-	// rawoutput("</select>");
+	// output('`$For now cant do a upgrade installation.`2`n');
+	if ($version == '-1') $version = '0.9.7';
+	reset($sql_upgrade_statements);
+    unset($sql_upgrade_statements['-1']);
+	rawoutput("<select name='version' class='ui search dropdown'>");
+	foreach($sql_upgrade_statements as $key => $val)
+	{
+		rawoutput("<option value='$key'".($version==$key?" selected":"").">$key</option>");
+	}
+	rawoutput("</select>");
 	rawoutput("<br><input type='radio' value='install' name='type' ".($session['dbinfo']['upgrade']? '' : 'checked').">");
 	output(" `2Perform a clean install.");
 	rawoutput("</td></tr></table>");
