0579045528221af534e8a29c953ee4649e12def1
Iván Diaz Marinas <idmarinas@icloud.com>
2017-04-17 11:04:15 +0200

    Pequeña optimización y formateo de texto

--- lib/settings.class.php
+++ lib/settings.class.php
@@ -4,21 +4,24 @@
 // mail ready
 
 
-class settings {
+class settings
+{
 
-	private $tablename,$settings;
+	protected $tablename;
+    protected $settings = [];
 
 	function __construct($tablename=false)
 	{
 		if ($tablename === false) $tablename = DB::prefix('settings');
-		else $tablename=DB::prefix($tablename);
+		else $tablename = DB::prefix($tablename);
 
 		$this->tablename = $tablename;
 		$this->settings = '';
 		$this->loadSettings();
 	}
 
-	function saveSetting($settingname,$value){
+	function saveSetting($settingname,$value)
+    {
 		$this->loadSettings();
 		if (!isset($this->settings[$settingname]) && $value)
 		{ //value needs to be elimintated - once we have our defaults in lib/data/settings.php ... this can GO
@@ -35,11 +38,9 @@ class settings {
 		DB::query($sql);
 		$this->settings[$settingname] = $value;
 		invalidatedatacache("game".$this->tablename);
-		if (DB::affected_rows()>0) {
-			return true;
-		}else{
-			return false;
-		}
+
+		if (DB::affected_rows()>0) return true;
+		else return false;
 	}
 
 	function loadSettings()
@@ -54,11 +55,10 @@ class settings {
 					$this->settings = [];
 					$sql = "SELECT * FROM " . $this->tablename;
 					$result = DB::query($sql);
-					while ($row = DB::fetch_assoc($result))
+					foreach($result as $row)
 					{
 						$this->settings[$row['setting']] = $row['value'];
 					}
-					DB::free_result($result);
 					updatedatacache('game'.$this->tablename, $this->settings);
 				}
 				catch( \Exception $ex)
@@ -75,25 +75,37 @@ class settings {
 		$this->settings="";
 	}
 
-	function getSetting($settingname,$default=false){
+	function getSetting($settingname,$default=false)
+    {
 		global $DB_USEDATACACHE,$DB_DATACACHEPATH;
-		if ($settingname=="usedatacache") return $DB_USEDATACACHE;
-			elseif ($settingname=="datacachepath") return $DB_DATACACHEPATH;
-		if ( 'object' == gettype($this) && !isset($this->settings[$settingname])) {
+
+		if ($settingname == 'usedatacache') return $DB_USEDATACACHE;
+		elseif ($settingname == 'datacachepath') return $DB_DATACACHEPATH;
+
+        if ( 'object' == gettype($this) && !isset($this->settings[$settingname]))
+        {
 			$this->loadSettings();
-		}else {
+		}
+        else
+        {
 			return $this->settings[$settingname];
 		}
-		if (!isset($this->settings[$settingname])){
+
+		if (!isset($this->settings[$settingname]))
+        {
 			//nothing set, we have to use the default value
 			if (file_exists("lib/data/".$this->tablename.".php")) require("lib/data/".$this->tablename.".php");
-			if ($default===false) {
+			if ($default===false)
+            {
 				if (isset($defaults[$settingname])) $setDefault=$defaults[$settingname];
 					else $setDefault='';
-			} else $setDefault=$default;
+			}
+            else $setDefault=$default;
 			$this->saveSetting($settingname,$setDefault);
 			return $setDefault;
-		}else{
+		}
+        else
+        {
 			return $this->settings[$settingname];
 		}
 	}
