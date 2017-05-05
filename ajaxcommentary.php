bf23e82897a4160d383c1c17d9cff9db3aed2eab
Iván Diaz Marinas <idmarinas@icloud.com>
2017-05-05 16:16:28 +0200

    Corregido error con variable no definida

--- ajaxcommentary.php
+++ ajaxcommentary.php
@@ -1,11 +1,11 @@
 <?php
 
-global $session,$fiveminuteload;
+global $session, $fiveminuteload;
 
-define("ALLOW_ANONYMOUS",true);
-define("OVERRIDE_FORCED_NAV",true);
+define('ALLOW_ANONYMOUS', true);
+define('OVERRIDE_FORCED_NAV', true);
 
-require_once "common.php";
+require_once 'common.php';
 
 $now = time();
 $minute = round($now/60)*60;
@@ -13,7 +13,8 @@ $minute = round($now/60)*60;
 if (! isset($session['chatrequests'][$minute])) $session['chatrequests'][$minute] = 0;
 $session['chatrequests'][$minute] += 1;
 //echo("Chat requests: ".$session['chatrequests'][$minute]." this minute (minute number ".$minute.").<br />");
-if ($session['chatrequests'][$minute] >= 50){
+if ($session['chatrequests'][$minute] >= 50)
+{
 	echo("Please don't run multiple Global Banter windows, it puts a tremendous strain on the server.  I've logged you out.  You'll be able to log back in again in a few minutes - please clear your cookies.");
 	$session['user']['loggedin'] = false;
 	saveuser();
@@ -24,13 +25,16 @@ if ($session['chatrequests'][$minute] >= 50){
 	// echo ("Server load is too high for auto-update at the moment.  This will hopefully balance out in a few minutes.");
 	// exit();
 // }
-
-$expiresin = strtotime($session['user']['laston']) + 600;
+if (isset($session['user']['laston'])) $expiresin = strtotime($session['user']['laston']) + 600;
+else $expiresin = strtotime(date('now')) + 600;
 $section = $_REQUEST['section'];
-if ($now > $expiresin || (isset($session['user']['chatloc']) && $session['user']['chatloc'] != "global_banter" && $section != "global_banter" && $session['user']['chatloc'] != $section  && $session['user']['chatloc']."_aux" != $section)){
+if ($now > $expiresin || (isset($session['user']['chatloc']) && $session['user']['chatloc'] != "global_banter" && $section != "global_banter" && $session['user']['chatloc'] != $section  && $session['user']['chatloc']."_aux" != $section))
+{
 	echo "Chat disabled due to inactivity";
-} else {
-	require_once "lib/commentary.php";
+}
+else
+{
+	require_once 'lib/commentary.php';
 	$message = $_REQUEST['message'];
 	$limit = $_REQUEST['limit'];
 	$talkline = $_REQUEST['talkline'];
@@ -38,11 +42,8 @@ if ($now > $expiresin || (isset($session['user']['chatloc']) && $session['user']
 	$showmodlink = (isset($_REQUEST['showmodlink']) ? $_REQUEST['showmodlink'] : '');
 
 	$commentary = preparecommentaryblock($section,$message,$limit,$talkline,$schema=false,$skipfooter=false,$customsql=false,$skiprecentupdate=false,$showmodlink,$returnlink);
-	$commentary = appoencode("`n".$commentary."`n",true);
+	$commentary = '<br>'.$commentary.'<br>';
 	echo($commentary);
 }
 saveuser();
 exit();
-
-
-?>
