304bf0b6519839453c099f680d22ba31ee64f9f9
Iván Diaz Marinas <idmarinas@icloud.com>
2017-05-11 13:12:44 +0200

    Se agrega el archivo faltante

--- lib/newday/newday_runonce.php
+++ lib/newday/newday_runonce.php
@@ -1,21 +1,21 @@
 <?php
 //newday runonce
-	//Let's do a new day operation that will only fire off for
-	//one user on the whole server.
-	//run the hook.
-	modulehook('newday-runonce', []);
 
-	//Do some high-load-cleanup
-    if (datacache_clearExpired()) gamelog('Expired cache data has been deleted');
+require_once 'lib/gamelog.php';
 
-    if (datacache_optimize()) gamelog('Cache data has been optimized', 'maintenance');
+//Let's do a new day operation that will only fire off for
+//one user on the whole server.
+//run the hook.
+modulehook('newday-runonce', []);
 
-	//only if not done by cron
-	if (! getsetting('newdaycron', 0))
-	{
-		require_once 'lib/newday/dbcleanup.php';
-		require 'lib/newday/commentcleanup.php';
-		require 'lib/newday/charcleanup.php';
-	}
+//Do some high-load-cleanup
+if (datacache_clearExpired()) gamelog('Expired cache data has been deleted');
+if (datacache_optimize()) gamelog('Cache data has been optimized', 'maintenance');
 
-?>
+//only if not done by cron
+if (! getsetting('newdaycron', 0))
+{
+	require_once 'lib/newday/dbcleanup.php';
+	require 'lib/newday/commentcleanup.php';
+	require 'lib/newday/charcleanup.php';
+}
