3a9cdb1d780461ff678ff865e337b8aff201d1f0
Iván Diaz Marinas <idmarinas@icloud.com>
2017-05-22 17:09:07 +0200

    Se limpia de variables obsoletas

--- lib/errorhandling.php
+++ lib/errorhandling.php
@@ -20,9 +20,6 @@ if (!get_magic_quotes_gpc()){
 	set_magic_quotes($_POST);
 	set_magic_quotes($_SESSION);
 	set_magic_quotes($_COOKIE);
-	set_magic_quotes($HTTP_GET_VARS);
-	set_magic_quotes($HTTP_POST_VARS);
-	set_magic_quotes($HTTP_COOKIE_VARS);
 	ini_set("magic_quotes_gpc",1);
 }
 
