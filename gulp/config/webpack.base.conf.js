c79a782e4ef131dd95c476b7d64f46bc41537f4a
Iván Diaz Marinas <idmarinas@icloud.com>
2017-05-23 12:57:24 +0200

    Se agregan los plugins por defecto a la configuración

--- gulp/config/webpack.base.conf.js
+++ gulp/config/webpack.base.conf.js
@@ -1,20 +1,16 @@
 var
-	//-- Dependencias
-	path = require('path'),
-	utils = require('./utils'),
+    //-- Dependencias
+    path = require('path'),
+    utils = require('./utils'),
 
-	//-- Configuración
-	config = require('./default')
+    //-- Configuración
+    config = require('./default')
 ;
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
-}
-
 module.exports = {
-	entry: {
-		lotgd: './assets/index.js'
-	},
+    entry: {
+        lotgd: './assets/index.js'
+    },
     output: {
         path: config.paths.build,
         filename: '[name].js',
@@ -59,5 +55,18 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+         new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+        })
+    ]
 }
