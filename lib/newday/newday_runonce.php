6506fddc01a6c2ee470a57b7d995d2864a005282
Iván Diaz Marinas <idmarinas@icloud.com>
2017-05-25 13:00:42 +0200

    La optimización de la cache también es por CronJob por lo que sólo se ejecuta si no está activo el cron

--- lib/newday/newday_runonce.php
+++ lib/newday/newday_runonce.php
@@ -1,20 +1,18 @@
 <?php
 //newday runonce
 
-require_once 'lib/gamelog.php';
-
 //Let's do a new day operation that will only fire off for
 //one user on the whole server.
 //run the hook.
 modulehook('newday-runonce', []);
 
-//Do some high-load-cleanup
-if (datacache_clearExpired()) gamelog('Expired cache data has been deleted');
-if (datacache_optimize()) gamelog('Cache data has been optimized', 'maintenance');
-
 //only if not done by cron
 if (! getsetting('newdaycron', 0))
 {
+    //Do some high-load-cleanup
+    if (datacache_clearExpired()) gamelog('Expired cache data has been deleted', 'maintenance');
+    if (datacache_optimize()) gamelog('Cache data has been optimized', 'maintenance');
+
 	require_once 'lib/newday/dbcleanup.php';
 	require 'lib/newday/commentcleanup.php';
 	require 'lib/newday/charcleanup.php';
