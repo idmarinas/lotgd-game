3f40f4482542816f05e5fddb45c2434f51f50512
Iván Diaz Marinas <idmarinas@icloud.com>
2017-06-13 11:54:22 +0200

    Se realiza una pequeña optimización

--- lib/newday/dbcleanup.php
+++ lib/newday/dbcleanup.php
@@ -1,15 +1,22 @@
 <?php
+
+require_once 'lib/gamelog.php';
+
 //db cleanup
-savesetting("lastdboptimize",date("Y-m-d H:i:s"));
+savesetting('lastdboptimize', date('Y-m-d H:i:s'));
+
 $result = DB::query("SHOW TABLES");
-$tables = array();
+
+$tables = [];
 $start = getmicrotime();
-for ($i=0;$i<DB::num_rows($result);$i++){
-	list($key,$val)=each(DB::fetch_assoc($result));
-	DB::query("OPTIMIZE TABLE $val");
-	array_push($tables,$val);
+
+foreach($result as $key => $value)
+{
+    list($clave, $valor) = each($value);
+    DB::query("OPTIMIZE TABLE $valor");
+    array_push($tables, $valor);
 }
+
 $time = round(getmicrotime() - $start,2);
-require_once("lib/gamelog.php");
+
 gamelog("Optimized tables: ".join(", ",$tables)." in $time seconds.","maintenance");
-?>
