c2a60eda28fdeebf0bf4dbdd8eed76101e00ae9c
Iván Diaz Marinas <idmarinas@icloud.com>
2017-09-30 16:47:43 +0200

    Se fuerza a usar la caché para guardar la configuración

--- lib/settings.class.php
+++ lib/settings.class.php
@@ -1,84 +1,106 @@
 <?php
-// translator ready
-// addnews ready
-// mail ready
-
 
 class settings
 {
-
 	protected $tablename;
     protected $settings = [];
+    protected $settingsKey = 'game-settings-';
 
-	function __construct($tablename=false)
+	function __construct($tablename = false)
 	{
 		if ($tablename === false) $tablename = DB::prefix('settings');
 		else $tablename = DB::prefix($tablename);
 
 		$this->tablename = $tablename;
-		$this->settings = '';
+		$this->settings = [];
 		$this->loadSettings();
 	}
 
-	function saveSetting($settingname,$value)
+    /**
+     * Save setting in to Data Base
+     *
+     * @param string $settingname
+     * @param mixed $value
+     *
+     * @return bool
+     */
+	function saveSetting($settingname, $value)
     {
-		$this->loadSettings();
-		if (!isset($this->settings[$settingname]) && $value)
+        $this->loadSettings();
+        $key = $this->settingsKey . $this->tablename;
+        $settingname = (string) $settingname;
+
+		if (! isset($this->settings[$settingname]) && $value)
 		{ //value needs to be elimintated - once we have our defaults in lib/data/settings.php ... this can GO
-			$sql = "INSERT INTO " . $this->tablename . " (setting,value) VALUES (\"".addslashes($settingname)."\",\"".addslashes($value)."\")";
+            $sql = DB::insert($this->tablename);
+            $sql->values([
+                'setting' => $settingname,
+                'value' => $value
+            ]);
 		}
 		elseif (isset($this->settings[$settingname]))
 		{
-			$sql = "UPDATE " . $this->tablename . " SET value=\"".addslashes($value)."\" WHERE setting=\"".addslashes($settingname)."\"";
+            $sql = DB::update($this->tablename);
+            $sql->set(['value' => $value])
+                ->where->equalTo('setting', $settingname)
+            ;
 		}
 		else
 		{
 			return false;
-		}
-		DB::query($sql);
-		$this->settings[$settingname] = $value;
-		invalidatedatacache("game".$this->tablename);
+        }
 
-		if (DB::affected_rows()>0) return true;
+        DB::query($sql);
+
+        $this->settings[$settingname] = $value;
+
+		updatedatacache($key, $this->settings, true);
+
+		if (DB::affected_rows() > 0) return true;
 		else return false;
 	}
 
+    /**
+     * Load all settings in table
+     *
+     * @return void
+     */
 	function loadSettings()
 	{
-		if (!is_array($this->settings))
+        $key = $this->settingsKey . $this->tablename;
+        $this->settings = datacache($key, 86400, true);
+
+		if (! is_array($this->settings) || empty($this->settings))
 		{
-			$this->settings = datacache('game' . $this->tablename);
-			if (! is_array($this->settings))
-			{
-				try
-				{
-					$this->settings = [];
-					$sql = "SELECT * FROM " . $this->tablename;
-					$result = DB::query($sql);
+			try
+            {
+                $this->settings = [];
+                $select = DB::select($this->tablename);
+                $result = DB::execute($select);
 
-					if (! $result) return;
+                if (! $result->count()) return;
 
-					foreach($result as $row)
-					{
-						$this->settings[$row['setting']] = $row['value'];
-					}
-					updatedatacache('game'.$this->tablename, $this->settings);
-				}
-				catch( \Exception $ex)
-				{
-					debug('Cant get Settings.');
-				}
-			}
+                foreach($result as $row)
+                {
+                    $this->settings[$row['setting']] = $row['value'];
+                }
+                updatedatacache($key, $this->settings, true);
+            }
+            catch( \Exception $ex)
+            {
+                debug('Cant get Settings.');
+            }
 		}
 	}
 
-	function clearSettings(){
+    function clearSettings()
+    {
 		//scraps the $this->loadSettings() data to force it to reload.
-		invalidatedatacache("game".$this->tablename);
-		$this->settings="";
+		invalidatedatacache($this->settingsKey . $this->tablename, true);
+		$this->settings = [];
 	}
 
-	function getSetting($settingname,$default=false)
+	function getSetting($settingname, $default = false)
     {
 		global $DB_USEDATACACHE,$DB_DATACACHEPATH;
 
@@ -97,14 +119,16 @@ class settings
 		if (!isset($this->settings[$settingname]))
         {
 			//nothing set, we have to use the default value
-			if (file_exists("lib/data/".$this->tablename.".php")) require("lib/data/".$this->tablename.".php");
-			if ($default===false)
+			if (file_exists("lib/data/{$this->tablename}.php")) { require "lib/data/{$this->tablename}.php"; }
+			if ($default === false)
             {
 				if (isset($defaults[$settingname])) $setDefault=$defaults[$settingname];
-					else $setDefault='';
+				else $setDefault = '';
 			}
-            else $setDefault=$default;
-			$this->saveSetting($settingname,$setDefault);
+            else $setDefault = $default;
+
+            $this->saveSetting($settingname, $setDefault);
+
 			return $setDefault;
 		}
         else
@@ -113,8 +137,5 @@ class settings
 		}
 	}
 
-	function getArray() {
-		return $this->settings;
-	}
+	function getArray() { return $this->settings; }
 }
-?>
