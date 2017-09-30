373590821fbe6c8821f6765716098283e3543ef6
Iván Diaz Marinas <idmarinas@icloud.com>
2017-09-30 15:55:36 +0200

    Formateo y optimización

--- runmodule.php
+++ runmodule.php
@@ -3,51 +3,51 @@
 // addnews ready
 // mail ready
 
-define("ALLOW_ANONYMOUS",true);
-define("OVERRIDE_FORCED_NAV",true);
+define('ALLOW_ANONYMOUS', true);
+define('OVERRIDE_FORCED_NAV', true);
 
-require_once("lib/http.php");
+require_once 'common.php';
+require_once 'lib/http.php';
+require_once 'lib/dump_item.php';
+require_once 'lib/modules.php';
+require_once 'lib/villagenav.php';
 
-require_once("common.php");
-require_once("lib/dump_item.php");
-require_once("lib/modules.php");
-require_once("lib/villagenav.php");
-
-if (injectmodule(httpget('module'), (httpget('admin')?true:false))){
+if (injectmodule(httpget('module'), (httpget('admin') ? true : false)))
+{
 	$info = get_module_info(httpget('module'));
-	if (!isset($info['allowanonymous'])){
-		$allowanonymous=false;
-	}else{
-		$allowanonymous = $info['allowanonymous'];
-	}
-	if (!isset($info['override_forced_nav'])){
-		$override_forced_nav=false;
-	}else{
-		$override_forced_nav=$info['override_forced_nav'];
-	}
-	do_forced_nav($allowanonymous,$override_forced_nav);
+    if (!isset($info['allowanonymous'])) { $allowanonymous = false; }
+    else { $allowanonymous = $info['allowanonymous']; }
+
+    if (!isset($info['override_forced_nav'])) { $override_forced_nav = false; }
+    else { $override_forced_nav=$info['override_forced_nav']; }
+
+	do_forced_nav($allowanonymous, $override_forced_nav);
 
 	$starttime = microtime(true);
-	$fname = $mostrecentmodule."_run";
+    $fname = $mostrecentmodule."_run";
+
 	tlschema("module-$mostrecentmodule");
 	$fname();
-	$endtime = microtime(true);
-	if (($endtime - $starttime >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT))){
-		debug("Slow Module (".round($endtime-$starttime,2)."s): $mostrecentmodule`n");
+    $endtime = microtime(true);
+    $time = $endtime - $starttime;
+    if (($time >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT)))
+    {
+		debug("Slow Module (".round($time, 2)."s): $mostrecentmodule`n");
 	}
 	tlschema();
-}else{
-	do_forced_nav(false,false);
+}
+else
+{
+	do_forced_nav(false, false);
 
-	tlschema("badnav");
+	tlschema('badnav');
 
 	page_header("Error");
-	if ($session['user']['loggedin']){
-		villagenav();
-	}else{
-		addnav("L?Return to the Login","index.php");
-	}
-	output("You are attempting to use a module which is no longer active, or has been uninstalled.");
-	page_footer();
+    if ($session['user']['loggedin']) { villagenav(); }
+    else { addnav('L?Return to the Login","index.php'); }
+
+	output('You are attempting to use a module which is no longer active, or has been uninstalled.');
+
+    page_footer();
 }
 ?>
