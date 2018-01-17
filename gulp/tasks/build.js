f79938eb5e397db0b0e6094b0b8b97810f792aee
Iván Diaz Marinas <idmarinas@icloud.com>
2018-01-17 13:36:11 +0100

    La copia de los archivos y la generación del tema, se hacen en paralelo

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -10,8 +10,7 @@ module.exports = function (callback)
 
     runSequence(
         'delete',
-        'main',
-        'theme',
+        ['main', 'theme'],
         'assets',
         'lotgd-js',
         'clean',
