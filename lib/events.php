d544d697d6ab700f9a880954ee311696f390298d
Iván Diaz Marinas <idmarinas@icloud.com>
2018-01-25 16:36:07 +0100

    Se formatea el texto

--- lib/events.php
+++ lib/events.php
@@ -2,67 +2,75 @@
 // addnews ready
 // translator ready
 // mail ready
-require_once("lib/constants.php");
-require_once("lib/http.php");
+require_once 'lib/constants.php';
+require_once 'lib/http.php';
 
 // This file encapsulates all the special event handling for most locations
 
 // Returns whether or not the description should be skipped
 function handle_event($location, $baseLink=false, $needHeader=false)
 {
-	if ($baseLink === false){
+    global $session, $playermount, $badguy;
+
+    if ($baseLink === false)
+    {
 		global $PHP_SELF;
 		$baseLink = substr($PHP_SELF,strrpos($PHP_SELF,"/")+1)."?";
-	}else{
+    }
+    else
+    {
 		//debug("Base link was specified as $baseLink");
 		//debug(debug_backtrace());
 	}
-	global $session, $playermount, $badguy;
 	$skipdesc = false;
 
-	tlschema("events");
+	tlschema('events');
 	$allowinactive = false;
 	$eventhandler = httpget('eventhandler');
-	if (($session['user']['superuser'] & SU_DEVELOPER) && $eventhandler!=""){
+    if (($session['user']['superuser'] & SU_DEVELOPER) && $eventhandler != '')
+    {
 		$allowinactive = true;
 		$array = preg_split("/[:-]/", $eventhandler);
-		if ($array[0] == "module") {
-			$session['user']['specialinc'] = "module:" . $array[1];
-		} else {
-			$session['user']['specialinc'] = "";
-		}
+        if ($array[0] == "module") { $session['user']['specialinc'] = "module:" . $array[1]; }
+        else { $session['user']['specialinc'] = ''; }
 	}
 
 	$_POST['i_am_a_hack'] = 'true';
 
-	if ($session['user']['specialinc']!=""){
+    if (isset($session['user']['specialinc']) && $session['user']['specialinc'] != '')
+    {
 		$specialinc = $session['user']['specialinc'];
-		$session['user']['specialinc'] = "";
-		if ($needHeader !== false) {
-			page_header($needHeader);
-		}
+        $session['user']['specialinc'] = '';
 
-		output("`^`c`bSomething Special!`c`b`0");
-		if (strchr($specialinc, ":")) {
+		if ($needHeader !== false) { page_header($needHeader); }
+
+		output('`^`c`bSomething Special!`c`b`0');
+        if (strchr($specialinc, ":"))
+        {
 			$array = explode(":", $specialinc);
 			$starttime = microtime(true);
-			module_do_event($location, $array[1], $allowinactive,$baseLink);
+			module_do_event($location, $array[1], $allowinactive, $baseLink);
 			$endtime = microtime(true);
-			if (($endtime - $starttime >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT))){
+            if (($endtime - $starttime >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT)))
+            {
 				debug("Slow Event (".round($endtime-$starttime,2)."s): $hookname - {$row['modulename']}`n");
 			}
-		}
-		if (checknavs()) {
+        }
+
+        if (checknavs())
+        {
 			// The page rendered some linkage, so we just want to exit.
 			page_footer();
-		} else {
+        }
+        else
+        {
 			$skipdesc=true;
-			$session['user']['specialinc'] = "";
-			$session['user']['specialmisc'] = "";
-			httpset("op", "");
+			$session['user']['specialinc'] = '';
+			$session['user']['specialmisc'] = '';
+			httpset("op", '');
 		}
 	}
-	tlschema();
+    tlschema();
+
 	return $skipdesc;
 }
-?>
