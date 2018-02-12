cbc15cbb5575cb36687d2516dbfb17fb85ef9777
Iván Diaz Marinas <idmarinas@icloud.com>
2018-02-12 14:04:57 +0100

    La tarea clean se ejecuta la última

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -5,6 +5,8 @@ var del = require('del')
 var config = require('../config/default')
 var configTasks = require('../config/tasks')
 
+var isProduction = configTasks.isProduction()
+
 var themeName = configTasks.theme()
 
 module.exports = function (callback)
@@ -14,6 +16,9 @@ module.exports = function (callback)
     //     .pipe(gulp.dest(config.paths.build + '/themes'))
 
     del(config.paths.build + '/themes/' + themeName + '/' + themeName + '.html')
+
+    //-- JSON files not is necesary in production
+    if (isProduction) { del(config.paths.build + '/**/*.json') }
 
     return callback()
 }
