422aab21ca9747b65b266bc16f510a98daad08db
Iván Diaz Marinas <idmarinas@icloud.com>
2018-05-31 12:06:03 +0200

    Se extrae el texto del archivo php en archivos twig

--- lib/clan/list.php
+++ lib/clan/list.php
@@ -1,59 +1,51 @@
 <?php
-	page_header("Clan Listing");
-	$registrar=getsetting('clanregistrar','`%Karissa');
-	addnav("Clan Options");
-	$order=(int)httpget('order');
-	switch ($order) {
-		case 1:
-			$order='clanname ASC';
-			break;
-		case 2:
-			$order='clanshort ASC';
-			break;
-		default:
-			$order='c DESC';
-			break;
-	}
-	$sql = "SELECT MAX(" . DB::prefix("clans") . ".clanid) AS clanid, MAX(clanshort) AS clanshort, MAX(clanname) AS clanname,count(" . DB::prefix("accounts") . ".acctid) AS c FROM " . DB::prefix("clans") . " LEFT JOIN " . DB::prefix("accounts") . " ON " . DB::prefix("clans") . ".clanid=" . DB::prefix("accounts") . ".clanid AND clanrank>".CLAN_APPLICANT." GROUP BY " . DB::prefix("clans") . ".clanid ORDER BY $order";
-	$result = DB::query($sql);
-	if (DB::num_rows($result)>0){
-		output("`7You ask %s`7 for the clan listings.  She points you toward a marquee board near the entrance of the lobby that lists the clans.`0`n`n",$registrar);
-		$v = 0;
-		$memb_n = translate_inline("(%s members)");
-		$memb_1 = translate_inline("(%s member)");
-		rawoutput('<table class="ui very compact striped selectable table">');
-		while ($row = DB::fetch_assoc($result)){
-			if ($row['c']==0){
-				$sql = "DELETE FROM " . DB::prefix("clans") . " WHERE clanid={$row['clanid']}";
-				DB::query($sql);
-			}else{
-				rawoutput('<tr><td>', true);
-				if ($row['c'] == 1) {
-					$memb = sprintf($memb_1, $row['c']);
-				} else {
-					$memb = sprintf($memb_n, $row['c']);
-				}
-				output_notl("&#149; &#60;%s&#62; <a href='clan.php?detail=%s'>%s</a> %s`n",
-						$row['clanshort'],
-						$row['clanid'],
-						htmlentities(full_sanitize($row['clanname']), ENT_COMPAT, getsetting("charset", "UTF-8")),
-						$memb, true);
-				rawoutput('</td></tr>');
-				addnav("","clan.php?detail={$row['clanid']}");
-				$v++;
-			}
-		}
-		rawoutput("</table>", true);
-		addnav("Return to the Lobby","clan.php");
-		addnav("Sorting");
-		addnav("Order by Membercount","clan.php?op=list&order=0");
-		addnav("Order by Clanname","clan.php?op=list&order=1");
-		addnav("Order by Shortname","clan.php?op=list&order=2");
-	}else{
-		output("`7You ask %s`7 for the clan listings.  She stares at you blankly for a few moments, then says, \"`5Sorry pal, no one has had enough gumption to start up a clan yet.  Maybe that should be you, eh?`7\"",$registrar);
-		addnav("Apply for a New Clan","clan.php?op=new");
-		addnav("Return to the Lobby","clan.php");
-	}
+page_header('Clan Listing');
 
-	page_footer();
-?>
+$registrar = getsetting('clanregistrar', '`%Karissa');
+$order = (int) httpget('order');
+
+//-- Frist delete clans with 0 members
+DB::query("DELETE FROM `clans` WHERE (SELECT COUNT(`acctid`) FROM `accounts` WHERE `clans`.`clanid` = `accounts`.`clanid` AND `accounts`.`clanrank` > " . CLAN_APPLICANT . ") = 0");
+
+//-- Select clans and total members
+$select = DB::select('clans');
+$select->columns(['clanid', 'clanshort', 'clanname'])
+    ->join('accounts', DB::expression('`accounts`.`clanid` = `clans`.`clanid` AND `accounts`.`clanrank` > ' . CLAN_APPLICANT), ['count' => DB::expression('COUNT(`accounts`.`acctid`)')])
+    ->group('clans.clanid')
+;
+
+//-- Order of results
+if ($order == 1) { $select->order('clans.clanname DESC'); }
+elseif ($order == 2) { $select->order('clans.clanshort DESC'); }
+else { $select->order('count DESC'); }
+
+$result = DB::execute($select);
+
+addnav('Clan Options');
+if ($result->count())
+{
+    addnav('Return to the Lobby', 'clan.php');
+    addnav('Sorting');
+    addnav('Order by Membercount', 'clan.php?op=list&order=0');
+    addnav('Order by Clanname', 'clan.php?op=list&order=1');
+    addnav('Order by Shortname', 'clan.php?op=list&order=2');
+
+    $twig = [
+        'clans' => $result,
+        'registrar' => $registrar
+    ];
+
+    rawoutput($lotgdTpl->renderThemeTemplate('pages/clan/list/clans.twig', $twig));
+}
+else
+{
+    addnav('Apply for a New Clan', 'clan.php?op=new');
+    addnav('Return to the Lobby', 'clan.php');
+
+    $twig = [
+        'registrar' => $registrar
+    ];
+
+    rawoutput($lotgdTpl->renderThemeTemplate('pages/clan/list/none.twig', $twig));
+}
+page_footer();
