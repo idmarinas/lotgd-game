f633de377f98ebcf46e58aa1b5ade3a9f0f498b8
Iván Diaz Marinas <idmarinas@icloud.com>
2018-05-31 12:05:12 +0200

    Se formate el texto

--- lib/clan/func.php
+++ lib/clan/func.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Clan functions
- * 
+ *
  * This file contains functions
  * that are specific to the clan
  * system.
- * 
+ *
  * @copyright Copyright © 2002-2005, Eric Stevens & JT Traub, © 2006-2009, Dragonprime Development Team
  * @version Lotgd 1.1.2 DragonPrime Edition
  * @package Core
@@ -17,17 +17,20 @@
  *
  * @param array $ranks The ranks
  * @param int $current The numeric value of the current rank
+ *
  * @return int The numeric value of the next rank
  */
-function clan_nextrank($ranks,$current) {
-	$temp=array_pop($ranks);
-	$ranks=array_keys($ranks);
-	while (count($ranks)>0) {
-		$key=array_shift($ranks);
-		if ($key>$current) return $key;
-	}
-	return 30;
+function clan_nextrank($ranks, $current)
+{
+	$temp = array_pop($ranks);
+	$ranks = array_keys($ranks);
+    while (count($ranks) > 0)
+    {
+		$key = array_shift($ranks);
+		if ($key > $current) return $key;
+    }
 
+	return 30;
 }
 
 /**
@@ -35,15 +38,19 @@ function clan_nextrank($ranks,$current) {
  *
  * @param array $ranks The ranks
  * @param int $current The numeric value of the current rank
+ *
  * @return int The numeric value of the previous rank
  */
-function clan_previousrank($ranks,$current) {
-	$temp=array_pop($ranks);
-	$ranks=array_keys($ranks);
-	while (count($ranks)>0) {
-		$key=array_pop($ranks);
-		if ($key<$current) return $key;
-	}
+function clan_previousrank($ranks, $current)
+{
+	$temp = array_pop($ranks);
+    $ranks = array_keys($ranks);
+
+    while (count($ranks) > 0)
+    {
+		$key = array_pop($ranks);
+		if ($key < $current) return $key;
+    }
+
 	return 0;
 }
-?>
