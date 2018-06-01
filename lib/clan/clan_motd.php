31045d03a0c4bbb82c3fabb62beb58ab55f2bff5
Iván Diaz Marinas <idmarinas@icloud.com>
2018-06-01 17:15:47 +0200

    Se extrae el texto a plantillas twig

--- lib/clan/clan_motd.php
+++ lib/clan/clan_motd.php
@@ -1,71 +1,65 @@
 <?php
-		page_header("Update Clan Description / MoTD");
-		addnav("Clan Options");
-		if ($session['user']['clanrank']>=CLAN_OFFICER){
-			$clanmotd = sanitize_mb(mb_substr(httppost('clanmotd'),0,4096));
-			if (httppostisset('clanmotd') &&
-					stripslashes($clanmotd)!=$claninfo['clanmotd']){
-				$sql = "UPDATE " . DB::prefix("clans") . " SET clanmotd='$clanmotd',motdauthor={$session['user']['acctid']} WHERE clanid={$claninfo['clanid']}";
-				DB::query($sql);
-				invalidatedatacache("clandata-{$claninfo['clanid']}");
-				$claninfo['clanmotd']=stripslashes($clanmotd);
-				output("Updating MoTD`n");
-				$claninfo['motdauthor']=$session['user']['acctid'];
-			}
-			$clandesc = httppost('clandesc');
-			if (httppostisset('clandesc') &&
-					stripslashes($clandesc)!=$claninfo['clandesc'] &&
-					$claninfo['descauthor']!=4294967295){
-				$sql = "UPDATE " . DB::prefix("clans") . " SET clandesc='".addslashes(substr(stripslashes($clandesc),0,4096))."',descauthor={$session['user']['acctid']} WHERE clanid={$claninfo['clanid']}";
-				DB::query($sql);
-				invalidatedatacache("clandata-{$claninfo['clanid']}");
-				output("Updating description`n");
-				$claninfo['clandesc']=stripslashes($clandesc);
-				$claninfo['descauthor']=$session['user']['acctid'];
-			}
-			$customsay = httppost('customsay');
-			if (httppostisset('customsay') && $customsay!=$claninfo['customsay'] && $session['user']['clanrank']>=CLAN_LEADER){
-				$sql = "UPDATE " . DB::prefix("clans") . " SET customsay='$customsay' WHERE clanid={$claninfo['clanid']}";
-				DB::query($sql);
-				invalidatedatacache("clandata-{$claninfo['clanid']}");
-				output("Updating custom say line`n");
-				$claninfo['customsay']=stripslashes($customsay);
-			}
-			$sql = "SELECT name FROM " . DB::prefix("accounts") . " WHERE acctid={$claninfo['motdauthor']}";
-			$result = DB::query($sql);
-			$row = DB::fetch_assoc($result);
-			$motdauthname = $row['name'];
 
-			$sql = "SELECT name FROM " . DB::prefix("accounts") . " WHERE acctid={$claninfo['descauthor']}";
-			$result = DB::query($sql);
-			$row = DB::fetch_assoc($result);
-			$descauthname = $row['name'];
+page_header('Update Clan Description / MoTD');
 
-			output("`&`bCurrent MoTD:`b `#by %s`2`n",$motdauthname);
-			output_notl(nltoappon($claninfo['clanmotd'])."`n");
-			output("`&`bCurrent Description:`b `#by %s`2`n",$descauthname);
-			output_notl(nltoappon($claninfo['clandesc'])."`n");
+addnav('Clan Options');
+if ($session['user']['clanrank'] >= CLAN_OFFICER)
+{
+	$clanmotd = sanitize_mb(mb_substr(httppost('clanmotd'), 0, 4096));
+    if (httppostisset('clanmotd') && stripslashes($clanmotd) != $claninfo['clanmotd'])
+    {
+		$sql = "UPDATE " . DB::prefix("clans") . " SET clanmotd='$clanmotd',motdauthor={$session['user']['acctid']} WHERE clanid={$claninfo['clanid']}";
+		DB::query($sql);
+		invalidatedatacache("clandata-{$claninfo['clanid']}");
+		$claninfo['clanmotd'] = stripslashes($clanmotd);
+		output("Updating MoTD`n");
+		$claninfo['motdauthor'] = $session['user']['acctid'];
+    }
 
-			rawoutput("<form action='clan.php?op=motd' method='POST'>");
-			addnav("","clan.php?op=motd");
-			output("`&`bMoTD:`b `7(4096 chars)`n");
-			rawoutput("<textarea name='clanmotd' cols='50' rows='10' class='input' style='width: 66%'>".htmlentities($claninfo['clanmotd'], ENT_COMPAT, getsetting("charset", "UTF-8"))."</textarea><br>");
-			output("`n`&`bDescription:`b `7(4096 chars)`n");
-			$blocked = translate_inline("Your clan has been blocked from posting a description.`n");
-			if ($claninfo['descauthor']==INT_MAX){
-				output_notl($blocked);
-			}else{
-				rawoutput("<textarea name='clandesc' cols='50' rows='10' class='input' style='width: 66%'>".htmlentities($claninfo['clandesc'], ENT_COMPAT, getsetting("charset", "UTF-8"))."</textarea><br>");
-			}
-			if ($session['user']['clanrank']>=CLAN_LEADER){
-				output("`n`&`bCustom Talk Line`b `7(blank means \"says\" -- 15 chars max)`n");
-				rawoutput("<input name='customsay' value=\"".htmlentities($claninfo['customsay'], ENT_COMPAT, getsetting("charset", "UTF-8"))."\" class='input' maxlength=\"15\"><br/>");
-			}
-			$save = translate_inline("Save");
-			rawoutput("<input type='submit' class='button' value='$save'>");
-			rawoutput("</form>");
-		}else{
-			output("You do not have authority to change your clan's motd or description.");
-		}
-		addnav("Return to your clan hall","clan.php");
-?>
+	$clandesc = httppost('clandesc');
+    if (httppostisset('clandesc') && stripslashes($clandesc) != $claninfo['clandesc'] && $claninfo['descauthor'] != 4294967295)
+    {
+		$sql = "UPDATE " . DB::prefix("clans") . " SET clandesc='".addslashes(substr(stripslashes($clandesc),0,4096))."',descauthor={$session['user']['acctid']} WHERE clanid={$claninfo['clanid']}";
+		DB::query($sql);
+		invalidatedatacache("clandata-{$claninfo['clanid']}");
+		output("Updating description`n");
+		$claninfo['clandesc'] = stripslashes($clandesc);
+		$claninfo['descauthor'] = $session['user']['acctid'];
+    }
+
+	$customsay = httppost('customsay');
+    if (httppostisset('customsay') && $customsay!=$claninfo['customsay'] && $session['user']['clanrank'] >= CLAN_LEADER)
+    {
+		$sql = "UPDATE " . DB::prefix("clans") . " SET customsay='$customsay' WHERE clanid={$claninfo['clanid']}";
+		DB::query($sql);
+		invalidatedatacache("clandata-{$claninfo['clanid']}");
+		output("Updating custom say line`n");
+		$claninfo['customsay'] = stripslashes($customsay);
+    }
+
+	$select = DB::select('accounts');
+    $select->columns([
+            'motdauthname' => 'name',
+            'descauthname' => DB::expression("(SELECT `name` FROM `accounts` WHERE `acctid` = '{$claninfo['descauthor']}')")
+        ])
+        ->where->equalTo('acctid', $claninfo['motdauthor'])
+    ;
+    $result = DB::execute($select);
+
+    $twig = [
+        'registrar' => $registrar,
+        'claninfo' => $claninfo,
+        'motdauthname' => $result['motdauthname'],
+        'descauthname' => $result['descauthname'],
+        'descriptionblocked' => ($claninfo['descauthor'] == INT_MAX),
+        'rankleader' => ($session['user']['clanrank'] >= CLAN_LEADER)
+    ];
+
+    rawoutput($lotgdTpl->renderThemeTemplate('pages/clan/start/motd/edit.twig', $twig));
+}
+else
+{
+    rawoutput($lotgdTpl->renderThemeTemplate('pages/clan/start/motd/prohibited.twig', ['registrar' => $registrar]));
+}
+
+addnav('Return to your clan hall', 'clan.php');
