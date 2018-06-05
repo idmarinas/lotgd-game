b2a69bb5fd0616ef14fae46b67c42fbfff72e4d6
Iván Diaz Marinas <idmarinas@icloud.com>
2018-06-05 17:34:43 +0200

    Se transfiere el texto a plantillas Twig

--- clan.php
+++ clan.php
@@ -10,41 +10,54 @@ require_once 'lib/sanitize.php';
 require_once 'lib/http.php';
 require_once 'lib/villagenav.php';
 
-tlschema("clans");
+tlschema('clans');
 
-
-addnav("Village");
+addnav('Village');
 villagenav();
-addnav("Clan Options");
-addnav("C?List Clans","clan.php?op=list");
-addcommentary();
-$gold = getsetting("goldtostartclan",10000);
-$gems = getsetting("gemstostartclan",15);
-$ranks = array(CLAN_APPLICANT=>"`!Applicant`0",CLAN_MEMBER=>"`#Member`0",CLAN_OFFICER=>"`^Officer`0",CLAN_ADMINISTRATIVE=>"`\$Administrative`0",CLAN_LEADER=>"`&Leader`0", CLAN_FOUNDER=>"`\$Founder");
-$args = modulehook("clanranks", array("ranks"=>$ranks, "clanid"=>$session['user']['clanid']));
-$ranks = translate_inline($args['ranks']);
+addnav('Clan Options');
+addnav('C?List Clans', 'clan.php?op=list');
 
-$apply_short = "`@Clan App: `&%s`0";
-$apply_subj = array($apply_short, $session['user']['name']);
+addcommentary();
 
 $op = httpget('op');
-
 $detail = httpget('detail');
+
+$registrar = getsetting('clanregistrar', '`%Karissa');
+$gold = getsetting('goldtostartclan', 10000);
+$gems = getsetting('gemstostartclan', 15);
+
+$defaultRanks = [
+    CLAN_APPLICANT => '`!Applicant`0',
+    CLAN_MEMBER => '`#Member`0',
+    CLAN_OFFICER => '`^Officer`0',
+    CLAN_ADMINISTRATIVE => '`$Administrative`0',
+    CLAN_LEADER => '`&Leader`0',
+    CLAN_FOUNDER => '`4Founder`0'
+];
+$args = modulehook('clanranks', ['ranks' => $defaultRanks, 'clanid' => $session['user']['clanid']]);
+$ranks = $args['ranks'];
+
+$apply_short = '`@Clan App: `&%s`0';
+$apply_subj = [$apply_short, $session['user']['name']];
+
 if ($detail > 0) { require_once 'lib/clan/detail.php'; }
 elseif ($op == 'list') { require_once 'lib/clan/list.php'; }
 elseif ($op == 'waiting') { require_once 'lib/clan/waiting.php'; }
 elseif ($session['user']['clanrank'] == CLAN_APPLICANT) { require_once 'lib/clan/applicant.php'; }
 else { require_once 'lib/clan/clan_start.php'; }
 
+tlschema();
 
 page_footer();
 
 function clanform()
 {
+    global $lotgdTpl;
+
     $data = [
         'clanname' => htmlentities(stripslashes(httppost('clanname')), ENT_COMPAT, getsetting('charset', 'UTF-8')),
         'clanshort' => htmlentities(stripslashes(httppost('clanshort')), ENT_COMPAT, getsetting('charset', 'UTF-8'))
     ];
 
-    output_notl($lotgd_tpl->renderThemeTemplate('pages/clan/new.twig', $data), true);
+    return $lotgdTpl->renderThemeTemplate('pages/clan/form/new.twig', $data);
 }
