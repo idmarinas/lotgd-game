1d7c447cf2d5befc3ce1b35df45563f461ff477b
Iván Diaz Marinas <idmarinas@icloud.com>
2018-07-03 12:54:43 +0200

    Se crea una plantilla Twig

--- gardens.php
+++ gardens.php
@@ -46,11 +46,7 @@ if (! $skipgardendesc)
 {
     checkday();
 
-    output('`b`c`2The Gardens`0`c`b');
-
-    output('`n`nYou walk through a gate and on to one of the many winding paths that makes its way through the well-tended gardens.');
-    output('From the flowerbeds that bloom even in darkest winter, to the hedges whose shadows promise forbidden secrets, these gardens provide a refuge for those seeking out the Green Dragon; a place where they can forget their troubles for a while and just relax.`n`n');
-    output('One of the fairies buzzing about the garden flies up to remind you that the garden is a place for roleplaying and peaceful conversation, and to confine out-of-character comments to the other areas of the game.`n`n');
+    rawoutput($lotgd_tpl->renderThemeTemplate('pages/gardens.twig', []));
 }
 
 villagenav();
