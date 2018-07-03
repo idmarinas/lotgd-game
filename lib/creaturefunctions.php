872f6dfdb71be30a82e52d609f87414719303190
Iván Diaz Marinas <idmarinas@icloud.com>
2018-07-03 10:42:00 +0200

    Optimizado con CS Fixer

--- lib/creaturefunctions.php
+++ lib/creaturefunctions.php
@@ -2,7 +2,7 @@
 
 /**
  * Generate a base creature stats
- * Can use for generated your own creatures in your modules
+ * Can use for generated your own creatures in your modules.
  *
  * @param false|int $level Level of creature
  *
@@ -10,24 +10,26 @@
  */
 function lotgd_generate_creature_levels($level = false)
 {
-	$maxlvl = getsetting('maxlevel', 15) + 5;
-	$stats = datacache("lotgd-generate-creature-levels-$maxlvl", 83000, true);
+    $maxlvl = getsetting('maxlevel', 15) + 5;
+    $stats = datacache("lotgd-generate-creature-levels-$maxlvl", 83000, true);
 
     if (empty($stats))
     {
         $creatureexp = 14;
         $creaturegold = 36;
         $creaturedefense = 0;
+
         for ($i = 1; $i <= $maxlvl; $i++)
         {
             //apply algorithmic creature generation.
-            $creaturehealth = ($i*10) + ($i-1) - round(sqrt($i-1));
-            $creatureattack = 1+($i-1) * 2;
-            $creaturedefense += ($i%2?1:2);
+            $creaturehealth = ($i * 10) + ($i - 1) - round(sqrt($i - 1));
+            $creatureattack = 1 + ($i - 1) * 2;
+            $creaturedefense += ($i % 2 ? 1 : 2);
+
             if ($i > 1)
             {
                 $creatureexp += round(10 + 1.5 * log($i));
-                $creaturegold += (31 * ($i<4?2:1));
+                $creaturegold += (31 * ($i < 4 ? 2 : 1));
                 //give lower levels more gold
             }
             $stats[$i] = [
@@ -43,30 +45,57 @@ function lotgd_generate_creature_levels($level = false)
         updatedatacache("lotgd-generate-creature-levels-$maxlvl", $stats, true);
     }
 
-    if (false === $level) return $stats;
-    else if (isset($stats[$level])) return $stats[$level];
-    else $stats;
+    if (false === $level)
+    {
+        return $stats;
+    }
+    elseif (isset($stats[$level]))
+    {
+        return $stats[$level];
+    }
+    else
+    {
+        $stats;
+    }
 }
 
 /**
- * Transform creature to adapt to player
+ * Transform creature to adapt to player.
  *
  * @param array $badguy Data of creature
- * @param boolean $debug Show or not debug of creature
+ * @param bool  $debug  Show or not debug of creature
  *
  * @return array
  */
 function lotgd_transform_creature(array $badguy, $debug = true)
 {
     // This will save us a lot of trouble when going through
-	static $dk = false;	// this function more than once...
+    static $dk = false;	// this function more than once...
 
-	if ($dk === false) $dk = get_player_dragonkillmod();
+    if (false === $dk)
+    {
+        $dk = get_player_dragonkillmod();
+    }
 
-    if (! isset($badguy['creaturespeed'])) $badguy['creaturespeed'] = 2.5;
-    if (! isset($badguy['creatureexp'])) $badguy['creatureexp'] = 0;
-    if (! isset($badguy['physicalresistance'])) $badguy['physicalresistance'] = 0;
-    if (! isset($badguy['creaturegold'])) $badguy['creaturegold'] = 0;
+    if (! isset($badguy['creaturespeed']))
+    {
+        $badguy['creaturespeed'] = 2.5;
+    }
+
+    if (! isset($badguy['creatureexp']))
+    {
+        $badguy['creatureexp'] = 0;
+    }
+
+    if (! isset($badguy['physicalresistance']))
+    {
+        $badguy['physicalresistance'] = 0;
+    }
+
+    if (! isset($badguy['creaturegold']))
+    {
+        $badguy['creaturegold'] = 0;
+    }
 
     $creatureattr = get_creature_stats($dk);
 
@@ -86,71 +115,83 @@ function lotgd_transform_creature(array $badguy, $debug = true)
 
     //-- Attack, defense, health from attributes
     $badguy['creatureattackattrs'] = get_creature_attack($creatureattr);
-	$badguy['creaturedefenseattrs'] = get_creature_defense($creatureattr);
-	$badguy['creaturehealthattrs'] = get_creature_hitpoints($creatureattr);
-	$badguy['creaturespeedattrs'] = get_creature_speed($creatureattr);
+    $badguy['creaturedefenseattrs'] = get_creature_defense($creatureattr);
+    $badguy['creaturehealthattrs'] = get_creature_hitpoints($creatureattr);
+    $badguy['creaturespeedattrs'] = get_creature_speed($creatureattr);
 
-	//-- Sum bonus
-	$badguy['creatureattack'] += $badguy['creatureattackattrs'];
-	$badguy['creaturedefense'] += $badguy['creaturedefenseattrs'];
-	$badguy['creaturehealth'] += $badguy['creaturehealthattrs'];
-	$badguy['creaturespeed'] += $badguy['creaturespeedattrs'];
+    //-- Sum bonus
+    $badguy['creatureattack'] += $badguy['creatureattackattrs'];
+    $badguy['creaturedefense'] += $badguy['creaturedefenseattrs'];
+    $badguy['creaturehealth'] += $badguy['creaturehealthattrs'];
+    $badguy['creaturespeed'] += $badguy['creaturespeedattrs'];
 
     //-- Set max health for creature
-	$badguy['creaturemaxhealth'] = $badguy['creaturehealth'];
-	$badguy['creaturestartinghealth'] = $badguy['creaturehealth'];
+    $badguy['creaturemaxhealth'] = $badguy['creaturehealth'];
+    $badguy['creaturestartinghealth'] = $badguy['creaturehealth'];
 
     //-- Check if script exist
     if (isset($badguy['creatureaiscript']))
     {
-		$aiscriptfile = "{$badguy['creatureaiscript']}.php";
+        $aiscriptfile = "{$badguy['creatureaiscript']}.php";
 
-		if (file_exists($aiscriptfile))	$badguy['creatureaiscript'] = "include '{$aiscriptfile}';";
-		else $badguy['creatureaiscript'] = '';
+        if (file_exists($aiscriptfile))
+        {
+            $badguy['creatureaiscript'] = "include '{$aiscriptfile}';";
+        }
+        else
+        {
+            $badguy['creatureaiscript'] = '';
+        }
     }
 
     //-- Not show debug
-    if (! $debug) return $badguy;
+    if (! $debug)
+    {
+        return $badguy;
+    }
 
-	debug("DEBUG: Basic information: Atk: {$badguy['creatureattack']}, Def: {$badguy['creaturedefense']}, HP: {$badguy['creaturehealth']}");
-	debug("DEBUG: $dk modification points total for attributes.");
-	debug("DEBUG: +{$badguy['creaturestrbonus']} allocated to strength.");
-	debug("DEBUG: +{$badguy['creaturedexbonus']} allocated to dexterity.");
-	debug("DEBUG: +{$badguy['creatureconbonus']} allocated to constitution.");
-	debug("DEBUG: +{$badguy['creatureintbonus']} allocated to intelligence.");
-	debug("DEBUG: +{$badguy['creaturewisbonus']} allocated to wisdom.");
-	debug("DEBUG: +{$badguy['creatureattackattrs']} modification of attack.");
-	debug("DEBUG: +{$badguy['creaturedefenseattrs']} modification of defense.");
-	debug("DEBUG: +{$badguy['creaturespeedattrs']} modification of speed.");
-	debug("DEBUG: +{$badguy['creaturehealthattrs']} modification of hitpoints.");
+    debug("DEBUG: Basic information: Atk: {$badguy['creatureattack']}, Def: {$badguy['creaturedefense']}, HP: {$badguy['creaturehealth']}");
+    debug("DEBUG: $dk modification points total for attributes.");
+    debug("DEBUG: +{$badguy['creaturestrbonus']} allocated to strength.");
+    debug("DEBUG: +{$badguy['creaturedexbonus']} allocated to dexterity.");
+    debug("DEBUG: +{$badguy['creatureconbonus']} allocated to constitution.");
+    debug("DEBUG: +{$badguy['creatureintbonus']} allocated to intelligence.");
+    debug("DEBUG: +{$badguy['creaturewisbonus']} allocated to wisdom.");
+    debug("DEBUG: +{$badguy['creatureattackattrs']} modification of attack.");
+    debug("DEBUG: +{$badguy['creaturedefenseattrs']} modification of defense.");
+    debug("DEBUG: +{$badguy['creaturespeedattrs']} modification of speed.");
+    debug("DEBUG: +{$badguy['creaturehealthattrs']} modification of hitpoints.");
 
-	return $badguy;
+    return $badguy;
 }
 
 function get_creature_stats($dk = 0)
 {
     global $session;
 
-    if (0 == $dk) $dk = $session['user']['dragonkills'];
+    if (0 == $dk)
+    {
+        $dk = $session['user']['dragonkills'];
+    }
 
     //-- They are placed in order of importance
-    $con = e_rand($dk/6,$dk/2);
+    $con = e_rand($dk / 6, $dk / 2);
     $dk -= $con;
-    $str = e_rand(0,$dk);
+    $str = e_rand(0, $dk);
     $dk -= $str;
-    $dex = e_rand(0,$dk);
+    $dex = e_rand(0, $dk);
     $dk -= $dex;
-    $int = e_rand(0,$dk);
+    $int = e_rand(0, $dk);
     $wis = ($dk - $int);
 
-    return ['str' => $str, 'dex' => $dex, 'con' => $con, 'int' => $int, 'wis' => $wis ];
+    return ['str' => $str, 'dex' => $dex, 'con' => $con, 'int' => $int, 'wis' => $wis];
 }
 
 function get_creature_hitpoints($attrs)
 {
     $conbonus = $attrs['con'] * .5;
-	$wisbonus = $attrs['wis'] * .2;
-	$strbonus = $attrs['str'] * .3;
+    $wisbonus = $attrs['wis'] * .2;
+    $strbonus = $attrs['str'] * .3;
 
     $hitpoints = round($conbonus + $wisbonus + $strbonus, 0);
 
@@ -159,30 +200,30 @@ function get_creature_hitpoints($attrs)
 
 function get_creature_attack($attrs)
 {
-	$strbonus = (1/3) * $attrs['str'];
-	$speedbonus = (1/3) * get_creature_speed($attrs);
-	$wisdombonus = (1/6) * $attrs['wis'];
-	$intbonus = (1/6) * $attrs['int'];
+    $strbonus = (1 / 3) * $attrs['str'];
+    $speedbonus = (1 / 3) * get_creature_speed($attrs);
+    $wisdombonus = (1 / 6) * $attrs['wis'];
+    $intbonus = (1 / 6) * $attrs['int'];
 
-	$attack = $strbonus + $wisdombonus + $intbonus;
+    $attack = $strbonus + $wisdombonus + $intbonus;
 
-	return max($attack,0);
+    return max($attack, 0);
 }
 
 function get_creature_defense($attrs)
 {
-	$wisdombonus = (1/4) * $attrs['wis'];
-	$constbonus = (3/8) * $attrs['con'];
-	$speedbonus = (3/8) * get_player_speed($attrs);
+    $wisdombonus = (1 / 4) * $attrs['wis'];
+    $constbonus = (3 / 8) * $attrs['con'];
+    $speedbonus = (3 / 8) * get_player_speed($attrs);
 
-	$defense = $wisdombonus + $constbonus;
+    $defense = $wisdombonus + $constbonus;
 
-	return max($defense,0);
+    return max($defense, 0);
 }
 
 function get_creature_speed($attrs)
 {
-	$speed = (1/2) * $attrs['dex'] + (1/4) * $attrs['int'] + (5/2);
+    $speed = (1 / 2) * $attrs['dex'] + (1 / 4) * $attrs['int'] + (5 / 2);
 
-	return max($speed,0);
+    return max($speed, 0);
 }
--- lib/forestoutcomes.php
+++ lib/forestoutcomes.php
@@ -1,4 +1,5 @@
 <?php
+
 // addnews ready
 // translator ready
 // mail ready
@@ -14,7 +15,7 @@ function forestvictory($enemies, $denyflawless = false)
         __METHOD__
     ), E_USER_DEPRECATED);
 
-	return;
+    return;
 }
 
 function forestdefeat($enemies, $where = 'in the forest')
@@ -28,39 +29,41 @@ function forestdefeat($enemies, $where = 'in the forest')
 }
 
 /**
- * Buff creature for optimize to character stats
+ * Buff creature for optimize to character stats.
  *
- * @var array $badguy Information of creature
- * @var string $hook Hook to activate when buff badguy
+ * @param array  Information of creature
+ * @param string $hook Hook to activate when buff badguy
+ *
+ * @return array
  */
 function buffbadguy($badguy, $hook = 'buffbadguy')
 {
-	global $session;
+    global $session;
 
-	$badguy = lotgd_transform_creature($badguy, false);
+    $badguy = lotgd_transform_creature($badguy, false);
 
-    $expflux = round($badguy['creatureexp']/10,0);
-	$expflux = e_rand(-$expflux,$expflux);
-	$badguy['creatureexp']+=$expflux;
+    $expflux = round($badguy['creatureexp'] / 10, 0);
+    $expflux = e_rand(-$expflux, $expflux);
+    $badguy['creatureexp'] += $expflux;
 
-	if (getsetting('disablebonuses', 1))
+    if (getsetting('disablebonuses', 1))
     {
-		//adapting flux as for people with many DKs they will just bathe in gold....
-		$base = 30 - min(20,round(sqrt($session['user']['dragonkills'])/2));
-		$base /=1000;
-		$bonus = 1 + $base*($badguy['creatureattackattrs']+$badguy['creaturedefenseattrs']) + .001*$badguy['creaturehealthattrs'];
-		$badguy['creaturegold'] = round($badguy['creaturegold']*$bonus, 0);
-		$badguy['creatureexp'] = round($badguy['creatureexp']*$bonus, 0);
-	}
+        //adapting flux as for people with many DKs they will just bathe in gold....
+        $base = 30 - min(20, round(sqrt($session['user']['dragonkills']) / 2));
+        $base /= 1000;
+        $bonus = 1 + $base * ($badguy['creatureattackattrs'] + $badguy['creaturedefenseattrs']) + .001 * $badguy['creaturehealthattrs'];
+        $badguy['creaturegold'] = round($badguy['creaturegold'] * $bonus, 0);
+        $badguy['creatureexp'] = round($badguy['creatureexp'] * $bonus, 0);
+    }
 
     //-- Activate hook when find a creature
-	$badguy = modulehook('creatureencounter', $badguy);
+    $badguy = modulehook('creatureencounter', $badguy);
 
     //-- Activate hook custom or default (buffbadguy)
-	$badguy = modulehook($hook, $badguy);
+    $badguy = modulehook($hook, $badguy);
 
     //-- Update max creature health
-	$badguy['creaturemaxhealth'] = $badguy['creaturehealth'];
+    $badguy['creaturemaxhealth'] = $badguy['creaturehealth'];
 
     return $badguy;
 }
