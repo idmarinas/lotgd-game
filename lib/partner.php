7decbbc8962ebe5e3ea63fb85802f689e594f444
Iván Diaz Marinas <idmarinas@icloud.com>
2018-07-05 12:11:51 +0200

    Optimización con CS Fixer

--- lib/partner.php
+++ lib/partner.php
@@ -1,50 +1,58 @@
 <?php
-function get_partner($player=false)
-{
-	global $session;
-	if (! isset($session['user']['prefs']['sexuality']) || $session['user']['prefs']['sexuality']=='')
-		$session['user']['prefs']['sexuality'] = ! $session['user']['sex'];
-	if ($player === false)
-	{
-		$partner = getsetting("barmaid");
-		if ($session['user']['prefs']['sexuality'] == SEX_MALE) {
-			$partner = getsetting("bard");
-		}
-	}
-	else
-	{
-		if ($session['user']['marriedto'] == INT_MAX)
-		{
-			$partner = getsetting("barmaid");
-			if ($session['user']['prefs']['sexuality'] == SEX_MALE)
-			{
-				$partner = getsetting("bard");
-			}
-		}
-		else
-		{
-			$sql = "SELECT name FROM ".DB::prefix("accounts")." WHERE acctid = {$session['user']['marriedto']}";
-			$result = DB::query($sql);
-			if ($row = DB::fetch_assoc($result))
-			{
-				$partner = $row['name'];
-			}
-			else
-			{
-				$session['user']['marriedto'] = 0;
-				$partner = getsetting("barmaid", "`%Violet");
-				if ($session['user']['prefs']['sexuality']  == SEX_MALE)
-				{
-					$partner = getsetting("bard", "`^Seth");
-				}
-			}
-		}
-	}
-//	No need to translate names...
-//	tlschema("partner");
-//	$partner = translate_inline($partner);
-//	tlschema();
-	return $partner;
-}
 
-?>
+function get_partner($player = false)
+{
+    global $session;
+
+    if (! isset($session['user']['prefs']['sexuality']) || '' == $session['user']['prefs']['sexuality'])
+    {
+        $session['user']['prefs']['sexuality'] = ! $session['user']['sex'];
+    }
+
+    if (false === $player)
+    {
+        $partner = getsetting('barmaid');
+
+        if (SEX_MALE == $session['user']['prefs']['sexuality'])
+        {
+            $partner = getsetting('bard');
+        }
+    }
+    else
+    {
+        if (INT_MAX == $session['user']['marriedto'])
+        {
+            $partner = getsetting('barmaid');
+
+            if (SEX_MALE == $session['user']['prefs']['sexuality'])
+            {
+                $partner = getsetting('bard');
+            }
+        }
+        else
+        {
+            $sql = 'SELECT name FROM '.DB::prefix('accounts')." WHERE acctid = {$session['user']['marriedto']}";
+            $result = DB::query($sql);
+
+            if ($row = DB::fetch_assoc($result))
+            {
+                $partner = $row['name'];
+            }
+            else
+            {
+                $session['user']['marriedto'] = 0;
+                $partner = getsetting('barmaid', '`%Violet`0');
+
+                if (SEX_MALE == $session['user']['prefs']['sexuality'])
+                {
+                    $partner = getsetting('bard', '`^Seth`0');
+                }
+            }
+        }
+    }
+    //	No need to translate names...
+    //	tlschema("partner");
+    //	$partner = translate_inline($partner);
+    //	tlschema();
+    return $partner;
+}
