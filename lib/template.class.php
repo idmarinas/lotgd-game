9ee0cd4fa9afddb287e521f477d70d86db379d49
Iván Diaz Marinas <idmarinas@icloud.com>
2018-07-10 16:49:03 +0200

    Se separa en una clase base

---
+++ lib/template.class.php
@@ -0,0 +1,165 @@
+<?php
+
+class LotgdTemplate extends Twig_Environment
+{
+    protected $twig;
+    protected $themename;
+    protected $themefolder;
+    protected $defaultSkin;
+
+    public function __construct(array $loader = [], array $options = [])
+    {
+        //-- Merge options
+        $default = [
+            'cache' => 'cache/templates',
+            'autoescape' => false
+        ];
+        $options = array_merge($default, $options);
+
+        $loader = new Twig_Loader_Filesystem($loader);
+
+        parent::__construct($loader, $options);
+
+        //-- Add filters to Twig
+        foreach ($this->lotgdFilters() as $filter)
+        {
+            $this->addFilter($filter);
+        }
+
+        //-- Add functions to Twig
+        foreach ($this->lotgdFunctions() as $function)
+        {
+            $this->addFunction($function);
+        }
+    }
+
+    /**
+     * Filters create for LOTGD.
+     *
+     * @return array
+     */
+    private function lotgdFilters()
+    {
+        return [
+            //-- Access to appoencode function in template
+            new Twig_SimpleFilter('appoencode', function ($string)
+            {
+                trigger_error(sprintf(
+                    'Filter %s is obsolete since 2.6.0; and delete in version 3.0.0 please use "%s" instead',
+                    'appoencode',
+                    'colorize'
+                ), E_USER_DEPRECATED);
+
+                return appoencode($string, true);
+            }),
+            //-- Alias for appoencode
+            new Twig_SimpleFilter('colorize', function ($string)
+            {
+                return appoencode($string, true);
+            }),
+            //-- Access to color_sanitize function in template
+            new Twig_SimpleFilter('color_sanitize', function ($string)
+            {
+                trigger_error(sprintf(
+                    'Filter %s is obsolete since 2.6.0; and delete in version 3.0.0 please use "%s" instead',
+                    'color_sanitize',
+                    'uncolorize'
+                ), E_USER_DEPRECATED);
+
+                return color_sanitize($string);
+            }),
+            //-- Alias for color_sanitize
+            new Twig_SimpleFilter('uncolorize', function ($string)
+            {
+                return color_sanitize($string);
+            }),
+            //-- Add a link, but not nav
+            new Twig_SimpleFilter('lotgd_url', function ($url)
+            {
+                addnav('', $url);
+
+                return $url;
+            }),
+            //-- Create a link popup
+            new Twig_SimpleFilter('lotgd_popup', function ($url)
+            {
+                return popup($url);
+            }),
+            new Twig_SimpleFilter('nltoappon', function ($string)
+            {
+                require_once 'lib/nltoappon.php';
+
+                return nltoappon($string);
+            }),
+            /*
+             * Format a number
+             *
+             * @param float $number
+             * @param int $decimals
+             *
+             * @return string
+             */
+            new Twig_SimpleFilter('numeral', function ($number, $decimals = 0)
+            {
+                global $lotgdFormat;
+
+                return $lotgdFormat->numeral($number, $decimals);
+            }),
+            //-- Translate a text in template
+            new Twig_SimpleFilter('t', function ($data, $namespace = false)
+            {
+                if (is_array($data))
+                {
+                    $text = str_replace('`%', '`%%', $data[0]);
+                    unset($data[0]);
+
+                    return vsprintf(translate_inline($text, $namespace), $data);
+                }
+                else
+                {
+                    return translate_inline($data, $namespace);
+                }
+            }),
+            //-- Show a relative date from now
+            new Twig_SimpleFilter('relativedate', function ($string)
+            {
+                return relativedate($string);
+            })
+        ];
+    }
+
+    private function lotgdFunctions()
+    {
+        return [
+            new Twig_SimpleFunction('modulehook', function ($name, $data)
+            {
+                return modulehook($name, $data);
+            }),
+            new Twig_SimpleFunction('isValidProtocol', function ($url)
+            {
+                // We should check all legeal protocols
+                $protocols = ['http', 'https', 'ftp', 'ftps'];
+                $protocol = explode(':', $url, 2);
+                $protocol = $protocol[0];
+
+                // This will take care of download strings such as: not publically released or contact admin
+                return in_array($protocol, $protocols);
+            }),
+            //-- Get value of setting
+            new Twig_SimpleFunction('getsetting', function ($name, $default)
+            {
+                return getsetting($name, $default);
+            }),
+            //-- Time in the game
+            new Twig_SimpleFunction('gametime', function ()
+            {
+                return getgametime();
+            }),
+            //-- Seconds to next game day
+            new Twig_SimpleFunction('secondstonextgameday', function ()
+            {
+                return secondstonextgameday();
+            }),
+        ];
+    }
+}
--- lib/template.php
+++ lib/template.php
@@ -1,11 +1,13 @@
 <?php
 
+require_once 'lib/template.class.php';
+
 use Zend\Filter\FilterChain;
 use Zend\Filter\StringToLower;
 use Zend\Filter\Word\SeparatorToDash;
 use Zend\Filter\Word\UnderscoreToDash;
 
-class LotgdTemplate extends Twig_Environment
+class LotgdTheme extends LotgdTemplate
 {
 	protected $twig;
 	protected $themename;
@@ -16,30 +18,10 @@ class LotgdTemplate extends Twig_Environment
 	{
 		$this->prepareTheme();
 
-        //-- Merge options
-		$default = [
-			'cache' => 'cache/templates',
-			'autoescape' => false
-        ];
-        $options = array_merge($default, $options);
-
         //-- Merge loaders
         $loader = array_merge(['themes', 'templates'], $loader);
-		$loader = new Twig_Loader_Filesystem($loader);
 
 		parent::__construct($loader, $options);
-
-		//-- Add filters to Twig
-		foreach($this->lotgdFilters() as $filter)
-		{
-			$this->addFilter($filter);
-		}
-
-		//-- Add functions to Twig
-		foreach($this->lotgdFunctions() as $function)
-		{
-			$this->addFunction($function);
-		}
 	}
 
 	/**
@@ -82,133 +64,6 @@ class LotgdTemplate extends Twig_Environment
 	{
 		return $this->render($name, $context);
 	}
-
-	/**
-	 * Filters create for LOTGD
-	 *
-	 * @return array
-	 */
-	private function lotgdFilters()
-	{
-		return [
-			//-- Access to appoencode function in template
-			new Twig_SimpleFilter('appoencode', function ($string)
-			{
-                trigger_error(sprintf(
-                    'Filter %s is obsolete since 2.6.0; and delete in version 3.0.0 please use "%s" instead',
-                    'appoencode',
-                    'colorize'
-                ), E_USER_DEPRECATED);
-
-				return appoencode($string, true);
-            }),
-            //-- Alias for appoencode
-			new Twig_SimpleFilter('colorize', function ($string)
-			{
-				return appoencode($string, true);
-			}),
-			//-- Access to color_sanitize function in template
-			new Twig_SimpleFilter('color_sanitize', function ($string)
-			{
-                trigger_error(sprintf(
-                    'Filter %s is obsolete since 2.6.0; and delete in version 3.0.0 please use "%s" instead',
-                    'color_sanitize',
-                    'uncolorize'
-                ), E_USER_DEPRECATED);
-				return color_sanitize($string);
-			}),
-			//-- Alias for color_sanitize
-			new Twig_SimpleFilter('uncolorize', function ($string)
-			{
-				return color_sanitize($string);
-			}),
-			//-- Add a link, but not nav
-			new Twig_SimpleFilter('lotgd_url', function ($url)
-			{
-				addnav('', $url);
-
-				return $url;
-            }),
-            //-- Create a link popup
-            new Twig_SimpleFilter('lotgd_popup', function ($url)
-            {
-                return popup($url);
-            }),
-            new Twig_SimpleFilter('nltoappon', function ($string)
-            {
-                require_once 'lib/nltoappon.php';
-
-                return nltoappon($string);
-            }),
-            /**
-             * Format a number
-             *
-             * @param float $number
-             * @param int $decimals
-             *
-             * @return string
-             */
-            new Twig_SimpleFilter('numeral', function ($number, $decimals = 0)
-            {
-                global $lotgdFormat;
-
-                return $lotgdFormat->numeral($number, $decimals);
-            }),
-			//-- Translate a text in template
-			new Twig_SimpleFilter('t', function ($data, $namespace = false)
-			{
-                if (is_array($data))
-                {
-                    $text = str_replace('`%','`%%', $data[0]);
-                    unset($data[0]);
-
-                    return vsprintf(translate_inline($text, $namespace), $data);
-                }
-                else return translate_inline($data, $namespace);
-            }),
-            //-- Show a relative date from now
-            new Twig_SimpleFilter('relativedate', function ($string)
-            {
-                return relativedate($string);
-            })
-		];
-	}
-
-    private function lotgdFunctions()
-    {
-        return [
-            new Twig_SimpleFunction('modulehook', function ($name, $data)
-            {
-                return modulehook($name, $data);
-            }),
-            new Twig_SimpleFunction('isValidProtocol', function ($url)
-            {
-                // We should check all legeal protocols
-                $protocols = array('http', 'https', 'ftp', 'ftps');
-                $protocol = explode(':', $url, 2);
-                $protocol = $protocol[0];
-
-                // This will take care of download strings such as: not publically released or contact admin
-                return in_array($protocol, $protocols);
-            }),
-            //-- Get value of setting
-            new Twig_SimpleFunction('getsetting', function ($name, $default)
-            {
-                return getsetting($name, $default);
-            }),
-            //-- Time in the game
-            new Twig_SimpleFunction('gametime', function ()
-            {
-                return getgametime();
-            }),
-            //-- Seconds to next game day
-            new Twig_SimpleFunction('secondstonextgameday', function ()
-            {
-                return secondstonextgameday();
-            }),
-
-        ];
-    }
 
 	/**
 	 * Get active theme
@@ -310,5 +165,5 @@ function templatereplace()
 
 global $lotgd_tpl, $lotgdTpl;
 
-$lotgdTpl = new LotgdTemplate;
+$lotgdTpl = new LotgdTheme();
 $lotgd_tpl =& $lotgdTpl;
