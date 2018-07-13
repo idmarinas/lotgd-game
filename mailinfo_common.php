ebbf569d2bc20faab170ebac1f6bc3f21dcd8c0f
Iván Diaz Marinas <idmarinas@icloud.com>
2018-07-13 19:25:58 +0200

    Se crea de nuevo para usar con Jaxon

--- mailinfo_common.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'lib/xajax/xajax_core/xajax.inc.php';
-$xajax = new xajax('mailinfo_server.php');
-//$xajax->setFlag("debug",true);
-$xajax->configure('javascript URI', 'lib/xajax/');
-$xajax->register(XAJAX_FUNCTION, 'mail_status');
-$xajax->register(XAJAX_FUNCTION, 'timeout_status');
