d47e9db99ce89bc418a75ca791462099558bd7e7
Iván Diaz Marinas <idmarinas@icloud.com>
2018-08-23 17:52:20 +0200

    Optimizado con CS Fixer

--- lib/installer/installer_stage_10.php
+++ lib/installer/installer_stage_10.php
@@ -1,70 +1,76 @@
 <?php
-output("`@`c`bSuperuser Accounts`b`c");
-$sql = "SELECT login, password FROM ".DB::prefix("accounts")." WHERE superuser & ".SU_MEGAUSER;
+
+output('`@`c`bSuperuser Accounts`b`c');
+$sql = 'SELECT login, password FROM '.DB::prefix('accounts').' WHERE superuser & '.SU_MEGAUSER;
 $result = DB::query($sql);
+
 if (0 == DB::num_rows($result))
 {
-	if (httppost("name")>"")
-	{
-		$showform=false;
-		if (httppost("pass1")!=httppost("pass2"))
-		{
-			output("`\$Oops, your passwords don't match.`2`n");
-			$showform=true;
-		}
-		elseif (strlen(httppost("pass1"))<6)
-		{
-			output("`\$Whoa, that's a short password, you really should make it longer.`2`n");
-			$showform=true;
-		}
-		else
-		{
-			// Give the superuser a decent set of privs so they can
-			// do everything needed without having to first go into
-			// the user editor and give themselves privs.
-			$su = SU_MEGAUSER | SU_EDIT_MOUNTS | SU_EDIT_CREATURES |
-			SU_EDIT_PETITIONS | SU_EDIT_COMMENTS | SU_EDIT_DONATIONS |
-			SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS |
-			SU_EDIT_EQUIPMENT | SU_EDIT_PAYLOG | SU_DEVELOPER |
-			SU_POST_MOTD | SU_MODERATE_CLANS | SU_EDIT_RIDDLES |
-			SU_MANAGE_MODULES | SU_AUDIT_MODERATION | SU_RAW_SQL |
-			SU_VIEW_SOURCE | SU_NEVER_EXPIRE;
-			$name = httppost("name");
-			$pass = md5(md5(stripslashes(httppost("pass1"))));
-			$sql = "DELETE FROM ".DB::prefix("accounts")." WHERE login='$name'";
-			DB::query($sql);
-			$sql = "INSERT INTO " .DB::prefix("accounts") ." (login,password,superuser,name,ctitle,regdate) VALUES('$name','$pass',$su,'`%Admin `&$name`0','`%Admin', NOW())";
-			$result=DB::query($sql);
-			if (DB::affected_rows($result)==0) {
-				print_r($sql);
-				die("Failed to create Admin account. Your first check should be to make sure that MYSQL (if that is your type) is not in strict mode.");
-			}
-			output("`^Your superuser account has been created as `%Admin `&$name`^!");
-			savesetting("installer_version",$logd_version);
-		}
-	}
-	else
-	{
-		$showform=true;
-		savesetting("installer_version",$logd_version);
-	}
-	if ($showform)
-	{
-		rawoutput("<br><div class='ui form'><form action='installer.php?stage=$stage' method='POST'><div class='inline field'><label>");
-		output("Enter a name for your superuser account:");
-		rawoutput("</label><input name='name' value=\"".htmlentities(httppost("name"), ENT_COMPAT, getsetting("charset", "UTF-8"))."\"></div><div class='inline field'><label>");
-		output("`nEnter a password: ");
-		rawoutput("</label><input name='pass1' type='password'></div><div class='inline field'><label>");
-		output("`nConfirm your password: ");
-		rawoutput("</label><input name='pass2' type='password'></div>");
-		$submit = translate_inline("Create");
-		rawoutput("<div class='inline field'><input type='submit' value='$submit' class='ui button'></div>");
-		rawoutput("</form>");
-	}
+    if (httppost('name') > '')
+    {
+        $showform = false;
+
+        if (httppost('pass1') != httppost('pass2'))
+        {
+            output("`\$Oops, your passwords don't match.`2`n");
+            $showform = true;
+        }
+        elseif (strlen(httppost('pass1')) < 6)
+        {
+            output("`\$Whoa, that's a short password, you really should make it longer.`2`n");
+            $showform = true;
+        }
+        else
+        {
+            // Give the superuser a decent set of privs so they can
+            // do everything needed without having to first go into
+            // the user editor and give themselves privs.
+            $su = SU_MEGAUSER | SU_EDIT_MOUNTS | SU_EDIT_CREATURES |
+            SU_EDIT_PETITIONS | SU_EDIT_COMMENTS | SU_EDIT_DONATIONS |
+            SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS |
+            SU_EDIT_EQUIPMENT | SU_EDIT_PAYLOG | SU_DEVELOPER |
+            SU_POST_MOTD | SU_MODERATE_CLANS | SU_EDIT_RIDDLES |
+            SU_MANAGE_MODULES | SU_AUDIT_MODERATION | SU_RAW_SQL |
+            SU_VIEW_SOURCE | SU_NEVER_EXPIRE;
+            $name = httppost('name');
+            $pass = md5(md5(stripslashes(httppost('pass1'))));
+            $sql = 'DELETE FROM '.DB::prefix('accounts')." WHERE login='$name'";
+            DB::query($sql);
+            $sql = 'INSERT INTO '.DB::prefix('accounts')." (login,password,superuser,name,ctitle,regdate) VALUES('$name','$pass',$su,'`%Admin `&$name`0','`%Admin', NOW())";
+            $result = DB::query($sql);
+
+            if (0 == DB::affected_rows($result))
+            {
+                print_r($sql);
+
+                die('Failed to create Admin account. Your first check should be to make sure that MYSQL (if that is your type) is not in strict mode.');
+            }
+            output("`^Your superuser account has been created as `%Admin `&$name`^!");
+            savesetting('installer_version', $logd_version);
+        }
+    }
+    else
+    {
+        $showform = true;
+        savesetting('installer_version', $logd_version);
+    }
+
+    if ($showform)
+    {
+        rawoutput("<br><div class='ui form'><form action='installer.php?stage=$stage' method='POST'><div class='inline field'><label>");
+        output('Enter a name for your superuser account:');
+        rawoutput("</label><input name='name' value=\"".htmlentities(httppost('name'), ENT_COMPAT, getsetting('charset', 'UTF-8'))."\"></div><div class='inline field'><label>");
+        output('`nEnter a password: ');
+        rawoutput("</label><input name='pass1' type='password'></div><div class='inline field'><label>");
+        output('`nConfirm your password: ');
+        rawoutput("</label><input name='pass2' type='password'></div>");
+        $submit = translate_inline('Create');
+        rawoutput("<div class='inline field'><input type='submit' value='$submit' class='ui button'></div>");
+        rawoutput('</form>');
+    }
 }
 else
 {
-	output("`#You already have a superuser account set up on this server.");
-	savesetting("installer_version",$logd_version);
+    output('`#You already have a superuser account set up on this server.');
+    savesetting('installer_version', $logd_version);
 }
-?>
--- lib/installer/installer_stage_6.php
+++ lib/installer/installer_stage_6.php
@@ -5,72 +5,81 @@ use Zend\Code\Generator\FileGenerator;
 
 if (file_exists('dbconnect.php'))
 {
-	$success = true;
-	$initial = false;
+    $success = true;
+    $initial = false;
 }
 else
 {
-	$initial = true;
-	output("`@`c`bWriting your dbconnect.php file`b`c");
-	output("`2I'm attempting to write a file named 'dbconnect.php' to your site root.");
-	output("This file tells LoGD how to connect to the database, and is necessary to continue installation.`n");
+    $initial = true;
+    output('`@`c`bWriting your dbconnect.php file`b`c');
+    output("`2I'm attempting to write a file named 'dbconnect.php' to your site root.");
+    output('This file tells LoGD how to connect to the database, and is necessary to continue installation.`n');
 
-	$body = '$adapter = ['  . PHP_EOL;
-	$body .= '	\'driver\' => \'' . $session['dbinfo']['DB_DRIVER'] . '\','  . PHP_EOL;
-	$body .= '	\'hostname\' => \'' . $session['dbinfo']['DB_HOST'] . '\','  . PHP_EOL;
-	$body .= '	\'database\' => \'' . $session['dbinfo']['DB_NAME'] . '\','  . PHP_EOL;
-	$body .= '	\'charset\' => \'utf8\','  . PHP_EOL;
-	$body .= '	\'username\' => \'' . $session['dbinfo']['DB_USER'] . '\','  . PHP_EOL;
-	$body .= '	\'password\' => \'' . $session['dbinfo']['DB_PASS'] . '\'' . PHP_EOL;
-	$body .= '];' . PHP_EOL . PHP_EOL;
-	$body .= '$DB_PREFIX = \'' . $session['dbinfo']['DB_PREFIX'] . '\';' . PHP_EOL;
-	$body .= '$DB_USEDATACACHE = \'' . $session['dbinfo']['DB_USEDATACACHE'] . '\';' . PHP_EOL;
-	$body .= '$DB_DATACACHEPATH = \'' . $session['dbinfo']['DB_DATACACHEPATH'] . '\';' . PHP_EOL;
-	$body .= '$gz_handler_on = 0;' . PHP_EOL;
+    $body = '$adapter = ['.PHP_EOL;
+    $body .= '	\'driver\' => \''.$session['dbinfo']['DB_DRIVER'].'\','.PHP_EOL;
+    $body .= '	\'hostname\' => \''.$session['dbinfo']['DB_HOST'].'\','.PHP_EOL;
+    $body .= '	\'database\' => \''.$session['dbinfo']['DB_NAME'].'\','.PHP_EOL;
+    $body .= '	\'charset\' => \'utf8\','.PHP_EOL;
+    $body .= '	\'username\' => \''.$session['dbinfo']['DB_USER'].'\','.PHP_EOL;
+    $body .= '	\'password\' => \''.$session['dbinfo']['DB_PASS'].'\''.PHP_EOL;
+    $body .= '];'.PHP_EOL.PHP_EOL;
+    $body .= '$DB_PREFIX = \''.$session['dbinfo']['DB_PREFIX'].'\';'.PHP_EOL;
+    $body .= '$DB_USEDATACACHE = \''.$session['dbinfo']['DB_USEDATACACHE'].'\';'.PHP_EOL;
+    $body .= '$DB_DATACACHEPATH = \''.$session['dbinfo']['DB_DATACACHEPATH'].'\';'.PHP_EOL;
+    $body .= '$gz_handler_on = 0;'.PHP_EOL;
 
-	$file = FileGenerator::fromArray([
-		'docblock' => DocBlockGenerator::fromArray([
-			'shortDescription' => 'This file is automatically created by installer.php',
-			'longDescription' => null,
-			'tags' => [
-				[
-					'name' => 'create',
-					'description' => date('M d, Y h:i a'),
-				],
-			],
-		]),
-		'body' => $body,
-	]);
-	unset($body);
+    $file = FileGenerator::fromArray([
+        'docblock' => DocBlockGenerator::fromArray([
+            'shortDescription' => 'This file is automatically created by installer.php',
+            'longDescription' => null,
+            'tags' => [
+                [
+                    'name' => 'create',
+                    'description' => date('M d, Y h:i a'),
+                ],
+            ],
+        ]),
+        'body' => $body,
+    ]);
+    unset($body);
 
-	$code = $file->generate();
+    $code = $file->generate();
 
-	$result = file_put_contents('dbconnect.php', $code);
+    $result = file_put_contents('dbconnect.php', $code);
 
-	if (false !== $result)
-	{
-		$failure = false;
-		output("`n`@Success!`2  I was able to write your dbconnect.php file, you can continue on to the next step.");
-	}
-	else $failure = true;
+    if (false !== $result)
+    {
+        $failure = false;
+        output('`n`@Success!`2  I was able to write your dbconnect.php file, you can continue on to the next step.');
+    }
+    else
+    {
+        $failure = true;
+    }
 
-	if ($failure)
-	{
-		output("`n`\$Unfortunately, I was not able to write your dbconnect.php file.");
-		output("`2You will have to create this file yourself, and upload it to your web server.");
-		output("The contents of this file should be as follows:`3");
-		rawoutput('<blockquote><pre>'.htmlentities($code, ENT_COMPAT, getsetting('charset', 'UTF-8')).'</pre></blockquote>');
-		output("`2Create a new file, past the entire contents from above into it`2 ).");
-		output("When you have that done, save the file as 'dbconnect.php' and upload this to the location you have LoGD at.");
-		output("You can refresh this page to see if you were successful.");
-	}
-	else $success = true;
+    if ($failure)
+    {
+        output('`n`$Unfortunately, I was not able to write your dbconnect.php file.');
+        output('`2You will have to create this file yourself, and upload it to your web server.');
+        output('The contents of this file should be as follows:`3');
+        rawoutput('<blockquote><pre>'.htmlentities($code, ENT_COMPAT, getsetting('charset', 'UTF-8')).'</pre></blockquote>');
+        output('`2Create a new file, past the entire contents from above into it`2 ).');
+        output("When you have that done, save the file as 'dbconnect.php' and upload this to the location you have LoGD at.");
+        output('You can refresh this page to see if you were successful.');
+    }
+    else
+    {
+        $success = true;
+    }
 }
 
-if(! $success) $session['stagecompleted'] = 5;
+if (! $success)
+{
+    $session['stagecompleted'] = 5;
+}
 
 if (! $initial)
 {
-	output('`$The file "dbconnect.php" was created before. If you want update with new data, please delete it.`n');
-	output('For now, game cant do update installation.`0`n');
+    output('`$The file "dbconnect.php" was created before. If you want update with new data, please delete it.`n');
+    output('For now, game cant do update installation.`0`n');
 }
