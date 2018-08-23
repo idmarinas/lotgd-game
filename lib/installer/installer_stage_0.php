48a616c6fa700419239c1e2595571dbeb3f5d59e
Iván Diaz Marinas <idmarinas@icloud.com>
2018-08-23 15:59:34 +0200

    Optimizado con CS Fixer

--- lib/installer/installer_stage_0.php
+++ lib/installer/installer_stage_0.php
@@ -1,85 +1,117 @@
 <?php
-output("`@`c`bWelcome to Legend of the Green Dragon`b`c`0");
-output("`2This is the installer script for Legend of the Green Dragon, by Eric Stevens & JT Traub.`n");
-output("`nIn order to install and use Legend of the Green Dragon (LoGD), you must agree to the license under which it is deployed.`n");
-output("`n`&This game is a small project into which we have invested a tremendous amount of personal effort, and we provide this to you absolutely free of charge.`2");
+
+output('`@`c`bWelcome to Legend of the Green Dragon`b`c`0');
+output('`2This is the installer script for Legend of the Green Dragon, by Eric Stevens & JT Traub.`n');
+output('`nIn order to install and use Legend of the Green Dragon (LoGD), you must agree to the license under which it is deployed.`n');
+output('`n`&This game is a small project into which we have invested a tremendous amount of personal effort, and we provide this to you absolutely free of charge.`2');
 output("Please understand that if you modify our copyright, or otherwise violate the license, you are not only breaking international copyright law (which includes penalties which are defined in whichever country you live), but you're also defeating the spirit of open source, and ruining any good faith which we have demonstrated by providing our blood, sweat, and tears to you free of charge.  You should also know that by breaking the license even one time, it is within our rights to require you to permanently cease running LoGD forever.`n");
-output("`nPlease note that in order to use the installer, you must have cookies enabled in your browser.`n");
+output('`nPlease note that in order to use the installer, you must have cookies enabled in your browser.`n');
+
 if (DB_CHOSEN)
 {
-	$sql = "SELECT count(*) AS c FROM accounts WHERE superuser & ".SU_MEGAUSER;
-	$result = DB::query($sql);
-	$row = DB::fetch_assoc($result);
-	if ($row['c'] == 0){
-		$needsauthentication = false;
-	}
-	if (httppost("username")>""){
-		debug(md5(md5(stripslashes(httppost("password")))), true);
-		$sql = "SELECT * FROM ".DB::prefix("accounts")." WHERE login='".httppost("username")."' AND password='".md5(md5(stripslashes(httppost("password"))))."' AND superuser & ".SU_MEGAUSER;
-		$result = DB::query($sql);
-		if ($result->count() > 0){
-			$row = DB::fetch_assoc($result);
-			// debug($row['password']);
-			// debug(httppost('password'));
-			// Okay, we have a username with megauser, now we need to do
-			// some hackery with the password.
-			$needsauthentication=true;
-			$p = stripslashes(httppost("password"));
-			$p1 = md5($p);
-			$p2 = md5($p1);
-			debug($p2, true);
+    $sql = 'SELECT count(*) AS c FROM accounts WHERE superuser & '.SU_MEGAUSER;
+    $result = DB::query($sql);
+    $row = DB::fetch_assoc($result);
 
-			if (getsetting("installer_version", "-1") == "-1") {
-				debug("HERE I AM", true);
-				// Okay, they are upgrading from 0.9.7  they will have
-				// either a non-encrypted password, or an encrypted singly
-				// password.
-				if (strlen($row['password']) == 32 &&
-				$row['password'] == $p1) {
-					$needsauthentication = false;
-				} elseif ($row['password'] == $p) {
-					$needsauthentication = false;
-				}
-			} elseif ($row['password'] == $p2) {
-				$needsauthentication = false;
-			}
-			if ($needsauthentication === false) {
-				redirect("installer.php?stage=1");
-			}
-			output("`\$That username / password was not found, or is not an account with sufficient privileges to perform the upgrade.`n");
-		}else{
-			$needsauthentication=true;
-			output("`\$That username / password was not found, or is not an account with sufficient privileges to perform the upgrade.`n");
-		}
-	}else{
-		$sql = "SELECT count(*) AS c FROM ".DB::prefix("accounts")." WHERE superuser & ".SU_MEGAUSER;
-		$result = DB::query($sql);
-		$row = DB::fetch_assoc($result);
-		if ($row['c']>0){
-			$needsauthentication=true;
-		}else{
-			$needsauthentication=false;
-		}
-	}
-}else{
-	$needsauthentication=false;
+    if (0 == $row['c'])
+    {
+        $needsauthentication = false;
+    }
+
+    if (httppost('username') > '')
+    {
+        debug(md5(md5(stripslashes(httppost('password')))), true);
+        $sql = 'SELECT * FROM '.DB::prefix('accounts')." WHERE login='".httppost('username')."' AND password='".md5(md5(stripslashes(httppost('password'))))."' AND superuser & ".SU_MEGAUSER;
+        $result = DB::query($sql);
+
+        if ($result->count() > 0)
+        {
+            $row = DB::fetch_assoc($result);
+            // debug($row['password']);
+            // debug(httppost('password'));
+            // Okay, we have a username with megauser, now we need to do
+            // some hackery with the password.
+            $needsauthentication = true;
+            $p = stripslashes(httppost('password'));
+            $p1 = md5($p);
+            $p2 = md5($p1);
+            debug($p2, true);
+
+            if ('-1' == getsetting('installer_version', '-1'))
+            {
+                debug('HERE I AM', true);
+                // Okay, they are upgrading from 0.9.7  they will have
+                // either a non-encrypted password, or an encrypted singly
+                // password.
+                if (32 == strlen($row['password']) &&
+                $row['password'] == $p1)
+                {
+                    $needsauthentication = false;
+                }
+                elseif ($row['password'] == $p)
+                {
+                    $needsauthentication = false;
+                }
+            }
+            elseif ($row['password'] == $p2)
+            {
+                $needsauthentication = false;
+            }
+
+            if (false === $needsauthentication)
+            {
+                redirect('installer.php?stage=1');
+            }
+            output('`$That username / password was not found, or is not an account with sufficient privileges to perform the upgrade.`n');
+        }
+        else
+        {
+            $needsauthentication = true;
+            output('`$That username / password was not found, or is not an account with sufficient privileges to perform the upgrade.`n');
+        }
+    }
+    else
+    {
+        $sql = 'SELECT count(*) AS c FROM '.DB::prefix('accounts').' WHERE superuser & '.SU_MEGAUSER;
+        $result = DB::query($sql);
+        $row = DB::fetch_assoc($result);
+
+        if ($row['c'] > 0)
+        {
+            $needsauthentication = true;
+        }
+        else
+        {
+            $needsauthentication = false;
+        }
+    }
+}
+else
+{
+    $needsauthentication = false;
 }
 //if a user with appropriate privs is already logged in, let's let them past.
-if ($session['user']['superuser'] & SU_MEGAUSER) $needsauthentication=false;
-if ($needsauthentication){
-	$session['stagecompleted']=-1;
-	output("`n`%In order to upgrade this LoGD installation, you will need to provide the username and password of a superuser account with the MEGAUSER privilege`0`n`n");
-	rawoutput("<form action='installer.php?stage=0' method='POST' class='ui form'>");
-    rawoutput('<div class="inline field"><label>');
-	output("`^Username: `0");
-	rawoutput("</label><input name='username'></div>");
-    rawoutput('<div class="inline field"><label>');
-	output("`^Password: `0");
-	rawoutput("</label><input type='password' name='password'></div>");
-	$submit = translate_inline("Submit");
-	rawoutput("<div class='field'><input type='submit' value='$submit' class='ui button'></div>");
-	rawoutput("</form>");
-}else{
-	output("`nPlease continue on to the next page, \"License Agreement.\"");
+if ($session['user']['superuser'] & SU_MEGAUSER)
+{
+    $needsauthentication = false;
 }
-?>
+
+if ($needsauthentication)
+{
+    $session['stagecompleted'] = -1;
+    output('`n`%In order to upgrade this LoGD installation, you will need to provide the username and password of a superuser account with the MEGAUSER privilege`0`n`n');
+    rawoutput("<form action='installer.php?stage=0' method='POST' class='ui form'>");
+    rawoutput('<div class="inline field"><label>');
+    output('`^Username: `0');
+    rawoutput("</label><input name='username'></div>");
+    rawoutput('<div class="inline field"><label>');
+    output('`^Password: `0');
+    rawoutput("</label><input type='password' name='password'></div>");
+    $submit = translate_inline('Submit');
+    rawoutput("<div class='field'><input type='submit' value='$submit' class='ui button'></div>");
+    rawoutput('</form>');
+}
+else
+{
+    output('`nPlease continue on to the next page, "License Agreement."');
+}
--- lib/installer/installer_stage_7.php
+++ lib/installer/installer_stage_7.php
@@ -1,55 +1,68 @@
 <?php
+
 $sql_upgrade_statements = include_once 'lib/installer/installer_sqlstatements.php';
 
 if (httppost('type') > '')
 {
-	if (httppost('type') == 'install')
-	{
-		$session['fromversion'] = '-1';
-		$session['dbinfo']['upgrade'] = false;
-	}
-	else
-	{
-		$session['fromversion'] = httppost('version');
-		$session['dbinfo']['upgrade'] = true;
-	}
+    if ('install' == httppost('type'))
+    {
+        $session['fromversion'] = '-1';
+        $session['dbinfo']['upgrade'] = false;
+    }
+    else
+    {
+        $session['fromversion'] = httppost('version');
+        $session['dbinfo']['upgrade'] = true;
+    }
 }
 
-if (! isset($session['fromversion']) || $session['fromversion'] == '')
+if (! isset($session['fromversion']) || '' == $session['fromversion'])
 {
-	output("`@`c`bConfirmation`b`c");
-	output("`2Please confirm the following:`0`n");
-	rawoutput("<form class='ui form' action='installer.php?stage=7' method='POST'>");
-	rawoutput("<table class='ui very basic table'><tr><td>");
-	output("`2I should:`0");
-	rawoutput("</td><td>");
-	$version = getsetting('installer_version', '-1');
-	if ($version != '-1') $session['dbinfo']['upgrade'] = true;
-	else $session['dbinfo']['upgrade'] = false;
-	rawoutput("<input type='radio' value='upgrade' name='type' ".($session['dbinfo']['upgrade']? 'checked' : '').">");
-	output(" `2Perform an upgrade from ");
-	// output('`$For now cant do a upgrade installation.`2`n');
-	if ($version == '-1') $version = '0.9.7';
-	reset($sql_upgrade_statements);
-    unset($sql_upgrade_statements['-1']);
-	rawoutput("<select name='version' class='ui search dropdown'>");
-	foreach($sql_upgrade_statements as $key => $val)
-	{
-		rawoutput("<option value='$key'".($version==$key?" selected":"").">$key</option>");
-	}
-	rawoutput("</select>");
-	rawoutput("<br><input type='radio' value='install' name='type' ".($session['dbinfo']['upgrade']? '' : 'checked').">");
-	output(" `2Perform a clean install.");
-	rawoutput("</td></tr></table>");
-	$submit = translate_inline("Submit");
-	rawoutput("<input type='submit' value='$submit' class='ui button'>");
-	rawoutput("</form>");
+    output('`@`c`bConfirmation`b`c');
+    output('`2Please confirm the following:`0`n');
+    rawoutput("<form class='ui form' action='installer.php?stage=7' method='POST'>");
+    rawoutput("<table class='ui very basic table'><tr><td>");
+    output('`2I should:`0');
+    rawoutput('</td><td>');
+    $version = getsetting('installer_version', '-1');
 
-	$session['stagecompleted'] = $stage - 1;
+    if ('-1' != $version)
+    {
+        $session['dbinfo']['upgrade'] = true;
+    }
+    else
+    {
+        $session['dbinfo']['upgrade'] = false;
+    }
+    rawoutput("<input type='radio' value='upgrade' name='type' ".($session['dbinfo']['upgrade'] ? 'checked' : '').'>');
+    output(' `2Perform an upgrade from ');
+    // output('`$For now cant do a upgrade installation.`2`n');
+    if ('-1' == $version)
+    {
+        $version = '0.9.7';
+    }
+    reset($sql_upgrade_statements);
+    unset($sql_upgrade_statements['-1']);
+    rawoutput("<select name='version' class='ui search dropdown'>");
+
+    foreach ($sql_upgrade_statements as $key => $val)
+    {
+        rawoutput("<option value='$key'".($version == $key ? ' selected' : '').">$key</option>");
+    }
+    rawoutput('</select>');
+    rawoutput("<br><input type='radio' value='install' name='type' ".($session['dbinfo']['upgrade'] ? '' : 'checked').'>');
+    output(' `2Perform a clean install.');
+    rawoutput('</td></tr></table>');
+    $submit = translate_inline('Submit');
+    rawoutput("<input type='submit' value='$submit' class='ui button'>");
+    rawoutput('</form>');
+
+    $session['stagecompleted'] = $stage - 1;
 }
 else
 {
-	$session['stagecompleted'] = $stage;
-	header("Location: installer.php?stage=".($stage+1));
-	exit();
+    $session['stagecompleted'] = $stage;
+    header('Location: installer.php?stage='.($stage + 1));
+
+    exit();
 }
