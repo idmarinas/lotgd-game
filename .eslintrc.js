b10dd0a72cca36a5f9dd5022f01303f04d61ee1e
Iván Diaz Marinas <idmarinas@icloud.com>
2018-09-07 17:21:21 +0200

    Se actualizan la configuración para las nuevas versiones de los módulos

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,15 @@
 
 module.exports = {
 	root: true,
-	parser: 'babel-eslint',
+    parser: 'babel-eslint',
 	parserOptions: {
-		sourceType: 'module'
+        sourceType: 'module'
 	},
 	env: {
 		browser: true,
 	},
 	// https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
 	extends: 'standard',
-	// required to lint *.vue files
 	plugins: [
 		'html'
 	],
