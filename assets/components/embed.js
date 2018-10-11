f55f2693edd99bd363f27f4705b73b8434220438
Iván Diaz Marinas <idmarinas@icloud.com>
2018-10-11 10:57:28 +0200

    Add height to iframe

--- assets/components/embed.js
+++ assets/components/embed.js
@@ -26,7 +26,7 @@ define([
         let options = {}
         options = jQuery.extend({ size: size, denyButton: true, approveButton: false, force: force, closeIcon: false, contentClass: 'embed' }, options)
 
-        let template = '<iframe id="iframe-' + elementId + '" src="' + url + '" width="100%" height="100%" frameborder="0"></iframe>'
+        let template = '<iframe id="iframe-' + elementId + '" src="' + url + '" width="100%" height="100%" frameborder="0" style="height: calc(70vh);"></iframe>'
 
         Lotgd.modal(template, undefined, elementId, options)
     }
