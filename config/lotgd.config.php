1ebe8ed4c69a3d6a3528e7155d75fb57bb59f721
Iván Diaz Marinas <idmarinas@icloud.com>
2018-10-18 13:31:39 +0200

    First commit

---
+++ config/lotgd.config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'config_glob_paths' => [
+        __DIR__ . '/autoload/global/{,*}.php',
+        __DIR__ . '/autoload/local/{,*}.php'
+    ],
+    'lotgd_core' => [
+        'cache_config' => true
+    ]
+];
