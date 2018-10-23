a7edc584dd3dd8c177b9a25eb282c536eeb6c204
Iván Diaz Marinas <idmarinas@icloud.com>
2018-10-23 15:07:28 +0200

    First commit

---
+++ src/core/Http.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @author IDMarinas
+ */
+
+namespace Lotgd\Core;
+
+use Zend\Http\PhpEnvironment\Request;
+
+class Http extends Request
+{
+}
