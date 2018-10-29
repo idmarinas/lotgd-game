09e59a456784e82fd16687c8688333aaf8e01477
Iván Diaz Marinas <idmarinas@icloud.com>
2018-10-29 15:42:55 +0100

    First commit

---
+++ src/core/Factory/Character/Stats.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @author IDMarinas
+ */
+
+namespace Lotgd\Core\Factory\Character;
+
+use Lotgd\Core\Character\Stats as CharacterStats;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class Stats implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new CharacterStats($config);
+    }
+
+    public function createService(ServiceLocatorInterface $services, $canonicalName = null, $requestedName = null)
+    {
+        return $this($services, $requestedName);
+    }
+}
