780746a0c744c52c14aeec7fe64d3d332f37f63c
Iván Diaz Marinas <idmarinas@icloud.com>
2018-11-15 18:46:05 +0100

    Added comment

--- config/lotgd.config.php
+++ config/lotgd.config.php
@@ -6,6 +6,6 @@ return [
         __DIR__ . '/autoload/local/{**/*,*}.php'
     ],
     'lotgd_core' => [
-        'cache_config' => true
+        'cache_config' => true //-- This cache config of service manager recomended in production
     ]
 ];
