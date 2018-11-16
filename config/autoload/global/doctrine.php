f8627197052dfb44363a24b09102d8eba91f02ec
Iván Diaz Marinas <idmarinas@icloud.com>
2018-11-16 14:21:33 +0100

    First commit

---
+++ config/autoload/global/doctrine.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'lotgd_core' => [
+        'doctrine' => [
+            'proxy_dir' => 'doctrine/Proxy',
+            // 'cache_class' => Doctrine\Common\Cache\ApcuCache::class
+        ]
+    ]
+];
