55905f2b933aed7be2f5ed694aef766ca4bd3e64
Iván Diaz Marinas <idmarinas@icloud.com>
2018-11-30 22:16:56 +0100

    Optimized to improve maintenance according to Code Climate

--- assets/game/datacache.js
+++ assets/game/datacache.js
@@ -16,66 +16,81 @@ define([
      */
     Lotgd.datacache = function (type)
     {
+        let options = {}
         if (type === 'optimize')
         {
-            Lotgd.swal({
-                type: 'question',
-                title: 'Optimize data cache',
-                confirmButtonText: 'Yes, optimize',
-                text: 'Want optimize data cache of game?',
-                showLoaderOnConfirm: true,
-                showCancelButton: true,
-                preConfirm: () =>
-                {
-                    return jQuery.get('ajaxdatacache.php?op=optimize')
-                }
-            })
+            options = optimize()
         }
         else if (type === 'clearexpire')
         {
-            Lotgd.swal({
-                type: 'question',
-                title: 'Clear expire data cache',
-                confirmButtonText: 'Yes, clear expire',
-                text: 'Want clear expire data cache of game?',
-                showLoaderOnConfirm: true,
-                showCancelButton: true,
-                preConfirm: () =>
-                {
-                    return jQuery.get('ajaxdatacache.php?op=clearexpire')
-                }
-            })
+            options = clearexpire()
         }
         else if (type === 'clearall')
         {
-            Lotgd.swal({
-                type: 'question',
-                title: 'Clear all data cache',
-                confirmButtonText: 'Yes, clear all',
-                text: 'This action empty the cache directory including the template directory, Want clear all data cache of game?',
-                showLoaderOnConfirm: true,
-                showCancelButton: true,
-                preConfirm: function ()
-                {
-                    return jQuery.get('ajaxdatacache.php?op=clearall')
-                }
-            })
+            options = clearall()
         }
         else if (type === 'clearbyprefix')
         {
-            Lotgd.swal({
-                type: 'question',
-                title: 'Clear data cache by prefix',
-                confirmButtonText: 'Yes, clear this',
-                text: 'Type the prefix of the cache data to be deleted',
-                input: 'text',
-                showLoaderOnConfirm: true,
-                showCancelButton: true,
-                preConfirm: function (prefix)
-                {
-                    return jQuery.get('ajaxdatacache.php?op=clearbyprefix&prefix=' + prefix)
-                }
-            })
+            options = clearbyprefix()
+        }
+        else
+        {
+            return
+        }
+
+        Lotgd.swal(jQuery.extend({ type: 'question', showLoaderOnConfirm: true, showCancelButton: true }, options))
+    }
+
+    function optimize ()
+    {
+        return {
+            title: 'Optimize data cache',
+            confirmButtonText: 'Yes, optimize',
+            text: 'Want optimize data cache of game?',
+            preConfirm: () =>
+            {
+                return jQuery.get('ajaxdatacache.php?op=optimize')
+            }
+        }
+    }
+
+    function clearexpire ()
+    {
+        return {
+            title: 'Clear expire data cache',
+            confirmButtonText: 'Yes, clear expire',
+            text: 'Want clear expire data cache of game?',
+            preConfirm: () =>
+            {
+                return jQuery.get('ajaxdatacache.php?op=clearexpire')
+            }
+        }
+    }
+
+    function clearall ()
+    {
+        return {
+            title: 'Clear all data cache',
+            confirmButtonText: 'Yes, clear all',
+            text: 'This action empty the cache directory including the template directory, Want clear all data cache of game?',
+            preConfirm: () =>
+            {
+                return jQuery.get('ajaxdatacache.php?op=clearall')
+            }
+        }
+    }
+
+    function clearbyprefix ()
+    {
+        return {
+            title: 'Clear data cache by prefix',
+            confirmButtonText: 'Yes, clear this',
+            text: 'Type the prefix of the cache data to be deleted',
+            input: 'text',
+            preConfirm: (prefix) =>
+            {
+                return jQuery.get('ajaxdatacache.php?op=clearbyprefix&prefix=' + prefix)
+            }
         }
     }
 
