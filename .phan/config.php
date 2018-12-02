ce5751316f26d7337e15a841fd203a805e0479c3
Iván Diaz Marinas <idmarinas@icloud.com>
2018-12-02 19:46:49 +0100

    Include all files

--- .phan/config.php
+++ .phan/config.php
@@ -20,7 +20,7 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
-        'src'
+        '/'
     ],
 
     // A directory list that defines files that will be excluded
