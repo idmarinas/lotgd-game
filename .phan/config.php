3a527cda90ef1c0a6bb45d20043872c199af20a1
Iván Diaz Marinas <idmarinas@icloud.com>
2018-12-02 19:32:42 +0100

    Changed config

---
+++ .phan/config.php
@@ -0,0 +1,19 @@
+<?php
+use \Phan\Config;
+use \Phan\Issue;
+
+return [
+    'quick_mode' => false,
+    'exclude_analysis_directory_list' => [
+        'vendor/'
+    ],
+    'processes' => 2,
+    'analyze_signature_compatibility' => true,
+    'dead_code_detection' => true,
+    'minimum_severity' => Issue::SEVERITY_CRITICAL,
+    'directory_list' => [
+        'module',
+        'public',
+        'config'
+    ]
+];
