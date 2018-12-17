a30ed30d37cc9da72daaa65268819d521608d6b3
Iván Diaz Marinas <idmarinas@icloud.com>
2018-12-17 14:59:20 +0100

    First commit

---
+++ src/core/Installer/Pattern/CanInstall.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @author IDMarinas
+ */
+
+namespace Lotgd\Core\Installer\Pattern;
+
+trait CanInstall
+{
+    protected $versionInstalled = 0;
+    protected $cantInstallMessage;
+
+    /**
+     * Check if can install this version of game.
+     *
+     * @return bool
+     */
+    public function canInstall(): bool
+    {
+        $version = $this->getInstalledVersion();
+
+        //-- Can't install it: is the same version
+        if (\Lotgd\Core\Application::VERSION_NUMBER == $version)
+        {
+            $this->cantInstallMessage = "`4You cannot install, you try to install the same version that is already installed.´4";
+
+            return false;
+        }
+        //-- Can only be installed from the previous version
+        elseif ($this->getPreviusVersion() != $version)
+        {
+            $this->cantInstallMessage = ['`4Can only be installed from the previous version: %s´4', $this->getNameVersion($this->getPreviusVersion())];
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Set currently installed version.
+     *
+     * @param string $version
+     *
+     * @return $this
+     */
+    public function versionInstalled(string $version)
+    {
+        $this->versionInstalled = $version;
+
+        return $this;
+    }
+
+    /**
+     * Get the installation failure message.
+     *
+     * @return array|string
+     */
+    public function getFailInstallMessage()
+    {
+        return $this->cantInstallMessage;
+    }
+
+    /**
+     * Get int value of installed version.
+     *
+     * @return int
+     */
+    private function getInstalledVersion(): int
+    {
+        $version = $this->getIntVersion($this->versionInstalled);
+
+        if (0 === $version)
+        {
+            return (int) $this->versionInstalled;
+        }
+
+        return $version;
+    }
+}
