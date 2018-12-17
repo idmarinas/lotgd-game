1ab7cd9f46469cf0fd8f58d26a14d52b1db6f68d
Iván Diaz Marinas <idmarinas@icloud.com>
2018-12-17 14:53:40 +0100

    First commit

---
+++ src/core/Installer/Pattern/IsUpgrade.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @author IDMarinas
+ */
+
+namespace Lotgd\Core\Installer\Pattern;
+
+trait IsUpgrade
+{
+    protected $upgrade = false;
+
+    /**
+     * Set that the installation is an upgrade.
+     *
+     * @return $this
+     */
+    public function isUpgradeOn()
+    {
+        $this->upgrade = true;
+
+        return $this;
+    }
+
+    /**
+     * Set that the installation not is an upgrade.
+     *
+     * @return $this
+     */
+    public function isUpgradeOff()
+    {
+        $this->upgrade = false;
+
+        return $this;
+    }
+
+    /**
+     * Get if the installation is a upgrade.
+     *
+     * @return bool
+     */
+    public function isUpgrade(): bool
+    {
+        return $this->upgrade;
+    }
+}
