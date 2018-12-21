a87f5c89506e3301bdb60294fcf3d4d60743c516
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2018-12-21 14:48:21 +0100

    First commit

---
+++ src/core/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @author IDMarinas
+ */
+
+namespace Lotgd\Core;
+
+class Application
+{
+    /**
+     * Version of game in public display format.
+     *
+     * @var string
+     */
+    const VERSION = '3.1.0 IDMarinas Edition';
+
+    /**
+     * Identify version of game in numeric format.
+     *
+     * @var int
+     */
+    const VERSION_NUMBER = 30100;
+}
