7dc76e9642be5017cf3ae257e9edd77a7eb0163d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-03 15:16:53 +0100

    Use the simplified version of  "Isset"

--- runmodule.php
+++ runmodule.php
@@ -16,23 +16,8 @@ if (injectmodule(httpget('module'), (httpget('admin') ? true : false)))
 {
     $info = get_module_info(httpget('module'));
 
-    if (! isset($info['allowanonymous']))
-    {
-        $allowanonymous = false;
-    }
-    else
-    {
-        $allowanonymous = $info['allowanonymous'];
-    }
-
-    if (! isset($info['override_forced_nav']))
-    {
-        $override_forced_nav = false;
-    }
-    else
-    {
-        $override_forced_nav = $info['override_forced_nav'];
-    }
+    $allowanonymous = $info['allowanonymous'] ?? false;
+    $override_forced_nav = $info['override_forced_nav'] ?? false;
 
     do_forced_nav($allowanonymous, $override_forced_nav);
 
