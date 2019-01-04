c246a550bf98d630ad8742610bbb21c1c803c45c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-04 16:27:51 +0100

    The original function is altered to change the value of some keys.

--- src/core/Http.php
+++ src/core/Http.php
@@ -12,4 +12,38 @@ use Zend\Http\PhpEnvironment\Request;
 
 class Http extends Request
 {
+    /**
+     * @inheritDoc
+     */
+    public function getServer($name = null, $default = null)
+    {
+        if ('REQUEST_URI' == $name)
+        {
+            return $this->sanitizeUri(parent::getServer($name, $default));
+        }
+        elseif ('SCRIPT_NAME' == $name)
+        {
+            return $this->sanitizeUri(parent::getServer($name, $default));
+        }
+
+        return parent::getServer($name, $default);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRequestUri(): string
+    {
+        return $this->sanitizeUri(parent::getRequestUri());
+    }
+
+    /**
+     * Sanitize uri for usage
+     *
+     * @return string
+     */
+    protected function sanitizeUri($name): string
+    {
+        return substr($name, strrpos($name, '/') + 1);
+    }
 }
