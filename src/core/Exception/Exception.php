a01b16e6267cb566b6c449368f096ccf212c44e6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-18 19:40:29 +0100

    First commit

---
+++ src/core/Exception/Exception.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Exception;
+
+use Exception as SplException;
+
+/**
+ * Base exception for Legend of the Green dragon.
+ */
+class Exception extends SplException
+{
+}
