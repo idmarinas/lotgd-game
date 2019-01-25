bc522b885b01f20ed5872476016ecb5c2f477b4e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-25 16:05:11 +0100

    Added new static class

--- lib/class/static.php
+++ lib/class/static.php
@@ -3,9 +3,11 @@
 use Lotgd\Core\Fixed\{
     Dbwrapper as DB,
     Doctrine,
+    FlashMessages as LotgdFlashMessages,
     Format as LotgdFormat,
     Locator as LotgdLocator,
-    Theme as LotgdTheme
+    Theme as LotgdTheme,
+    Translator as LotgdTranslator
 };
 
 //-- Prepare service manager
@@ -17,9 +19,14 @@ DB::wrapper(LotgdLocator::get(Lotgd\Core\Db\Dbwrapper::class));
 //-- Configure Doctrine
 Doctrine::wrapper(LotgdLocator::get(\Lotgd\Core\Db\Doctrine::class));
 
+//-- Configure Flash Messages
+LotgdFlashMessages::setContainer(LotgdLocator::get(\Lotgd\Core\Component\FlashMessages::class));
+
 //-- Configure format instance
 LotgdFormat::instance(LotgdLocator::get(\Lotgd\Core\Output\Format::class));
 
 //-- Configure Theme template
 LotgdTheme::wrapper(LotgdLocator::get(\Lotgd\Core\Template\Theme::class));
 
+//-- Configure Translator
+LotgdTranslator::setContainer(LotgdLocator::get(\Lotgd\Core\Translator\Translator::class));
