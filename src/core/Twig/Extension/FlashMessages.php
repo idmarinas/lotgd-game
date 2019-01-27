faa51ee0e99e463233c546ab812a487525725266
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-27 17:24:07 +0100

    First commit

---
+++ src/core/Twig/Extension/FlashMessages.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Twig\Extension;
+
+use Lotgd\Core\Component\FlashMessages as CoreFlashMessages;
+use Lotgd\Core\Twig\TokenParser\TranslatorDefaultDomainTokenParser;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Lotgd\Core\Twig\NodeVisitor\TranslatorNodeVisitor;
+use Lotgd\Core\Twig\NodeVisitor\TranslatorDefaultDomainNodeVisitor;
+
+class FlashMessages extends AbstractExtension
+{
+    protected $flashMessages;
+
+    /**
+     * @param CoreFlashMessages $flashMessages
+     */
+    public function __construct(CoreFlashMessages $flashMessages)
+    {
+        $this->flashMessages = $flashMessages;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('show_flash_messages', [$this, 'display']),
+        ];
+    }
+
+    /**
+     * Show all messages
+     *
+     * @return string
+     */
+    public function display()
+    {
+        $container = $this->flashMessages->getMessages();
+        $output = '';
+
+        foreach($container as $type => $messages)
+        {
+            foreach($messages as $id => $message)
+            {
+                $output .= \LotgdTheme::renderLotgdTemplate('semantic/collection/message.twig', [
+                    'message' => $message,
+                    'class' => $type,
+                    'close' => true,
+                    'id' => $id
+                ]);
+            }
+        }
+
+        return $output;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'flash-messages';
+    }
+}
