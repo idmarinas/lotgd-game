3735dd7af5342e696f1085c7b636607ee0798abe
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-01-30 11:06:34 +0100

    First commit

---
+++ src/core/Exception/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Exception;
+
+/**
+ * Core invalid argument exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
