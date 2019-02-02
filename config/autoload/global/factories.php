5242c1433319898ea062d727f05a7ec0e090b82b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-02-02 13:22:10 +0100

    Added new factory

--- config/autoload/global/factories.php
+++ config/autoload/global/factories.php
@@ -1,26 +1,30 @@
 <?php
 
+use Lotgd\Core\Factory;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
 return [
     'service_manager' => [
         'factories' => [
-            Lotgd\Core\Character\Stats::class => Lotgd\Core\Factory\Character\Stats::class,
-            Lotgd\Core\Component\FlashMessages::class => Lotgd\Core\Factory\Component\FlashMessages::class,
-            'Lotgd\\Core\\Db\\Doctrine' => Lotgd\Core\Factory\Db\Doctrine::class,
-            Lotgd\Core\Db\Dbwrapper::class => Lotgd\Core\Factory\Db\Dbwrapper::class,
-            Lotgd\Core\Installer\Install::class => Lotgd\Core\Factory\Installer\Install::class,
-            Lotgd\Core\Lib\Cache::class => Lotgd\Core\Factory\Lib\Cache::class,
-            Lotgd\Core\Lib\Settings::class => Lotgd\Core\Factory\Lib\Settings::class,
-            Lotgd\Core\Lib\SettingsExtended::class => Lotgd\Core\Factory\Lib\SettingsExtended::class,
-            Lotgd\Core\Nav\Blocked::class => Lotgd\Core\Factory\Nav\Blocked::class,
-            Lotgd\Core\Navigation\Navigation::class => Lotgd\Core\Factory\Navigation\Navigation::class,
-            Lotgd\Core\Output\Color::class => Lotgd\Core\Factory\Output\Color::class,
-            Lotgd\Core\Output\Collector::class => Lotgd\Core\Factory\Output\Collector::class,
-            Lotgd\Core\Output\Code::class => Lotgd\Core\Factory\Output\Code::class,
-            Lotgd\Core\Output\Format::class => Lotgd\Core\Factory\Output\Format::class,
-            Lotgd\Core\Template\Theme::class => Lotgd\Core\Factory\Template\Theme::class,
-            Lotgd\Core\Translator\Translator::class => Lotgd\Core\Factory\Translator\Translator::class,
-            Lotgd\Core\Http::class => Lotgd\Core\Factory\Http::class,
-            Zend\I18n\Translator\LoaderPluginManager::class => Lotgd\Core\Factory\Translator\LoaderPluginManager::class,
+            Lotgd\Core\Character\Stats::class => Factory\Character\Stats::class,
+            Lotgd\Core\Component\FlashMessages::class => InvokableFactory::class,
+            'Lotgd\\Core\\Db\\Doctrine' => Factory\Db\Doctrine::class,
+            Lotgd\Core\Db\Dbwrapper::class => Factory\Db\Dbwrapper::class,
+            Lotgd\Core\Installer\Install::class => Factory\Installer\Install::class,
+            Lotgd\Core\Lib\Cache::class => Factory\Lib\Cache::class,
+            Lotgd\Core\Lib\Settings::class => Factory\Lib\Settings::class,
+            Lotgd\Core\Lib\SettingsExtended::class => Factory\Lib\SettingsExtended::class,
+            Lotgd\Core\Nav\Blocked::class => InvokableFactory::class,
+            Lotgd\Core\Navigation\Navigation::class => InvokableFactory::class,
+            Lotgd\Core\Navigation\AccessKeys::class => InvokableFactory::class,
+            Lotgd\Core\Output\Color::class => InvokableFactory::class,
+            Lotgd\Core\Output\Collector::class => Factory\Output\Collector::class,
+            Lotgd\Core\Output\Code::class => InvokableFactory::class,
+            Lotgd\Core\Output\Format::class => Factory\Output\Format::class,
+            Lotgd\Core\Template\Theme::class => Factory\Template\Theme::class,
+            Lotgd\Core\Translator\Translator::class => Factory\Translator\Translator::class,
+            Lotgd\Core\Http::class => InvokableFactory::class,
+            Zend\I18n\Translator\LoaderPluginManager::class => Factory\Translator\LoaderPluginManager::class,
         ]
     ]
 ];
