c6f850392235914385290df75ebe83eb3df7f633
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-02-03 17:33:42 +0100

    First  ommit

---
+++ src/core/Twig/Extension/Pattern/Mail.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Twig\Extension\Pattern;
+
+/**
+ * Trait to created ye olde mail link.
+ */
+
+trait Mail
+{
+    /**
+     * Get ye olde mail link.
+     *
+     * @return string
+     */
+    public function yeOldeMail(): string
+    {
+        global $session;
+
+        $mail = \Doctrine::getRepository(\Lotgd\Core\Entity\Mail::class);
+        $result = $mail->getCountMailOfCharacter((int) ($session['user']['acctid'] ?? 0));
+
+        return \LotgdTheme::renderThemeTemplate('parts/mail.twig', $result);
+    }
+}
