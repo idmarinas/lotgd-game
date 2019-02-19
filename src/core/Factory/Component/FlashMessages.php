f4f193574c20b34346bbedc50c90cc4c9387f475
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-02-19 16:16:42 +0100

    First commit

---
+++ src/core/Factory/Component/FlashMessages.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ *
+ * @since 4.0.0
+ */
+
+
+namespace Lotgd\Core\Factory\Component;
+
+use Zend\Session\Container;
+use Zend\Session\SessionManager;
+use Interop\Container\ContainerInterface;
+use Lotgd\Core\Component\FlashMessages as CoreFlashMessages;
+use Zend\ServiceManager\{
+    FactoryInterface,
+    ServiceLocatorInterface
+};
+
+class FlashMessages implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $messages = new CoreFlashMessages();
+        $messages->setSessionManager($container->get(SessionManager::class));
+
+        return $messages;
+    }
+
+    public function createService(ServiceLocatorInterface $services, $canonicalName = null, $requestedName = null)
+    {
+        return $this($services, $requestedName);
+    }
+}
