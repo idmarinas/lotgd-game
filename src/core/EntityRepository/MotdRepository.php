b616e2e351ba2ccc0f4efb49440ab327d37bc005
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-02-19 20:42:23 +0100

    First commit

---
+++ src/core/EntityRepository/MotdRepository.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\EntityRepository;
+
+use Lotgd\Core\Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Lotgd\Core\Entity as LotgdEntity;
+
+class MotdRepository extends EntityRepository
+{
+    /**
+     * Get last MOTD.
+     *
+     * @param int|null $userId
+     *
+     * @return array|null
+     */
+    public function getLastMotd(?int $userId = null): ?array
+    {
+        $qb = $this->createQueryBuilder('u');
+
+        try
+        {
+            $result = $qb->select('u', 'c.name as motdauthorname')
+                ->leftJoin(LotgdEntity\Accounts::class, 'a', Join::WITH, $qb->expr()->eq('a.acctid', 'u.motdauthor'))
+                ->leftJoin(LotgdEntity\Characters::class, 'c', Join::WITH, $qb->expr()->eq('c.id', 'a.character'))
+                ->orderBy('u.motddate', 'DESC')
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getArrayResult()
+            ;
+
+            $motd = $result[0][0];
+            unset($result[0][0]);
+            $motd = array_merge($motd, $result[0]);
+
+            //-- Is a poll
+            if ($motd['motdtype'])
+            {
+                $motd = $this->appendPollResults($motd, $userId);
+            }
+
+            return $motd;
+        }
+        catch (\Throwable $th)
+        {
+            \Tracy\Debugger::log($th);
+
+            return null;
+        }
+    }
+
+    /**
+     * Get MoTD item info.
+     *
+     * @param int|null $userId
+     *
+     * @return array|null
+     */
+    public function getEditMotdItem(int $motdId): ?array
+    {
+        $qb = $this->createQueryBuilder('u');
+
+        try
+        {
+            $result = $qb->select('u', 'c.name as motdauthorname')
+                ->leftJoin(LotgdEntity\Accounts::class, 'a', Join::WITH, $qb->expr()->eq('a.acctid', 'u.motdauthor'))
+                ->leftJoin(LotgdEntity\Characters::class, 'c', Join::WITH, $qb->expr()->eq('c.id', 'a.character'))
+                ->where('u.motdtype = 0 AND u.motditem = :id')
+                ->setParameter('id', $motdId)
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getArrayResult()
+            ;
+
+            if (! $result)
+            {
+                return null;
+            }
+
+            $motd = $result[0][0];
+            unset($result[0][0]);
+
+            return array_merge($motd, $result[0]);
+        }
+        catch (\Throwable $th)
+        {
+            \Tracy\Debugger::log($th);
+
+            return null;
+        }
+    }
+
+    /**
+     * Append results of a poll to MOTD item.
+     *
+     * @param array $motd   Information of a motd
+     * @param int   $userId Id of user to get it's vote
+     *
+     * @return array
+     */
+    public function appendPollResults(array $motd, $userId): array
+    {
+        $q = $this->_em->createQuery('SELECT COUNT(p.resultid) AS votes, p.choice
+            FROM Lotgd\Core\Entity\PollResults p
+            WHERE p.motditem = :motditem
+            GROUP BY p.choice
+            ORDER BY p.choice
+        ');
+        $q->setParameter('motditem', $motd['motditem']);
+
+        $result = $q->getArrayResult();
+
+        //-- Add results to MOTD
+        foreach ($result as $value)
+        {
+            $motd['pollResult']['opt'][$value['choice']] = (int) $value['votes'];
+        }
+
+        if ($userId)
+        {
+            $q = $this->_em->createQuery('SELECT p.choice FROM Lotgd\Core\Entity\PollResults p WHERE p.motditem = :motditem AND p.account = :acct');
+            $q->setParameters([
+                'motditem' => $motd['motditem'],
+                'acct' => $userId
+            ]);
+
+            $result = $q->getOneOrNullResult();
+            $motd['pollResult']['userVote'] = $result['choice'] ?? null;
+        }
+
+        if ($motd['pollResult']['opt'] ?? false)
+        {
+            $motd['pollResult']['totalVotes'] = \array_sum($motd['pollResult']['opt']);
+        }
+
+        //-- Unserialize information
+        $motd['motdbody'] = \unserialize($motd['motdbody']);
+
+        return $motd;
+    }
+
+    /**
+     * Get a list of years with count of MoTD per month.
+     *
+     * @return array
+     */
+    public function getMonthCountPerYear()
+    {
+        $q = $this->_em->createQuery('SELECT YEAR(u.motddate) AS year, MONTH(u.motddate) AS month, u.motddate AS date, COUNT(MONTH(u.motddate)) AS count
+            FROM Lotgd\Core\Entity\Motd u
+            GROUP BY year, month
+            ORDER BY year ASC, month ASC
+        ');
+
+        return $q->getArrayResult();
+    }
+}
