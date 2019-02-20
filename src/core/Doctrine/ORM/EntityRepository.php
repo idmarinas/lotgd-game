01b23ae9edcb3ecd2ac9caee877199a18179e26b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-02-20 11:48:49 +0100

    First commit

---
+++ src/core/Doctrine/ORM/EntityRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Doctrine\ORM;
+
+use Doctrine\ORM\{
+    EntityRepository as DoctrineEntityRepository,
+    QueryBuilder
+};
+use Lotgd\Core\Paginator\Adapter\Doctrine as DoctrineAdapter;
+use Zend\Paginator\Paginator;
+
+class EntityRepository extends DoctrineEntityRepository
+{
+    /**
+     * Get a pagination for a result.
+     *
+     * @param QueryBuilder $query
+     * @param int|null     $page
+     * @param int|null     $perPage
+     *
+     * @return Paginator
+     */
+    public function getPaginator(QueryBuilder $query, ?int $page = 1, int $perPage = 25): Paginator
+    {
+        $page = max(1, (int) $page);
+
+        $paginator = new Paginator(new DoctrineAdapter($query));
+        //- Set current page
+        $paginator->setCurrentPageNumber($page);
+        //-- Max number of results per page
+        $paginator->setItemCountPerPage($perPage);
+
+        return $paginator;
+    }
+}
