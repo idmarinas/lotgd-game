477a5dd109c2d0fad98a20f6bdb1b940f14eb098
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-03-17 18:13:16 +0100

    New commit

---
+++ src/core/EntityInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core;
+
+interface EntityInterface
+{
+}
