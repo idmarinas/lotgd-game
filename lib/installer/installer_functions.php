e82063b54f3142c5d118dca233dccacd0030b347
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-03-20 17:37:19 +0100

    Deleted unused function

--- lib/installer/installer_functions.php
+++ lib/installer/installer_functions.php
@@ -1,20 +1,5 @@
 <?php
 
-$tipid = 0;
-function tip()
-{
-    global $tipid;
-
-    $tip = translate_inline('Tip');
-    output_notl("<div style='cursor: pointer; cursor: hand; display: inline;' onMouseOver=\"tip$tipid.style.visibility='visible'; tip$tipid.style.display='inline';\" onMouseOut=\"tip$tipid.style.visibility='hidden'; tip$tipid.style.display='none';\">`i[ `b{$tip}´b ]´i", true);
-    rawoutput("<div class='debug' id='tip$tipid' style='position: absolute; width: 200px; max-width: 200px; float: right;'>");
-    $args = func_get_args();
-    call_user_func_array('output', $args);
-    rawoutput('</div></div>');
-    rawoutput("<script language='JavaScript'>var tip$tipid = document.getElementById('tip$tipid'); tip$tipid.style.visibility='hidden'; tip$tipid.style.display='none';</script>");
-    $tipid++;
-}
-
 //This function is borrowed from the php manual.
 function return_bytes($val)
 {
