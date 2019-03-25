0c059534d59731e5f651934af5f640337258b4ba
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-03-25 18:53:56 +0100

    Updated to new system of translator and use twig templates

--- lib/installer/installer_stage_10.php
+++ lib/installer/installer_stage_10.php
@@ -1,23 +1,25 @@
 <?php
 
-output('`@`c`bSuperuser Accounts´b´c');
-$sql = 'SELECT login, password FROM '.DB::prefix('accounts').' WHERE superuser & '.SU_MEGAUSER;
-$result = DB::query($sql);
+$repository = \Doctrine::getRepository(\Lotgd\Core\Entity\Accounts::class);
+$superusers = $repository->getSuperuserCountWithPermit(SU_MEGAUSER);
 
-if (0 == DB::num_rows($result))
+if (0 == $superusers)
 {
-    if (httppost('name') > '')
+    $name = (string) \LotgdHttp::getPost('name');
+    $pass1 = (string) \LotgdHttp::getPost('pass1');
+    $pass2 = (string) \LotgdHttp::getPost('pass2');
+    if ($name)
     {
         $showform = false;
 
-        if (httppost('pass1') != httppost('pass2'))
+        if ($pass1 != $pass2)
         {
-            output("`\$Oops, your passwords don't match.`2`n");
+            \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('stage10.post.passwordNotMatch', [], 'page-installer'));
             $showform = true;
         }
-        elseif (strlen(httppost('pass1')) < 6)
+        elseif (strlen($pass1) < 6)
         {
-            output("`\$Whoa, that's a short password, you really should make it longer.`2`n");
+            \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('stage10.post.passwordShort', [], 'page-installer'));
             $showform = true;
         }
         else
@@ -25,14 +27,10 @@ if (0 == DB::num_rows($result))
             // Give the superuser a decent set of privs so they can
             // do everything needed without having to first go into
             // the user editor and give themselves privs.
-            $su = SU_MEGAUSER | SU_EDIT_MOUNTS | SU_EDIT_CREATURES |
-            SU_EDIT_PETITIONS | SU_EDIT_COMMENTS | SU_EDIT_DONATIONS |
-            SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS |
-            SU_EDIT_EQUIPMENT | SU_EDIT_PAYLOG | SU_DEVELOPER |
-            SU_POST_MOTD | SU_MODERATE_CLANS | SU_EDIT_RIDDLES |
-            SU_MANAGE_MODULES | SU_AUDIT_MODERATION | SU_RAW_SQL |
+            $su = SU_MEGAUSER | SU_EDIT_MOUNTS | SU_EDIT_CREATURES | SU_EDIT_PETITIONS | SU_EDIT_COMMENTS | SU_EDIT_DONATIONS |
+            SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS | SU_EDIT_EQUIPMENT | SU_EDIT_PAYLOG | SU_DEVELOPER |
+            SU_POST_MOTD | SU_MODERATE_CLANS | SU_EDIT_RIDDLES | SU_MANAGE_MODULES | SU_AUDIT_MODERATION | SU_RAW_SQL |
             SU_VIEW_SOURCE | SU_NEVER_EXPIRE;
-            $name = httppost('name');
             $pass = md5(md5(stripslashes(httppost('pass1'))));
             $sql = 'DELETE FROM '.DB::prefix('accounts')." WHERE login='$name'";
             DB::query($sql);
@@ -75,8 +73,10 @@ if (0 == DB::num_rows($result))
                 die('Failed to create Admin account and character. Your first check should be to make sure that MYSQL (if that is your type) is not in strict mode.');
             }
 
-            output("`^Your superuser account has been created as `%Admin `&$name`^!");
+            \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('stage10.post.success', ['name' => $name], 'page-installer'));
             savesetting('installer_version', \Lotgd\Core\Application::VERSION);
+
+            return redirect('home.php');
         }
     }
     else
@@ -87,20 +87,19 @@ if (0 == DB::num_rows($result))
 
     if ($showform)
     {
-        rawoutput("<br><div class='ui form'><form action='installer.php?stage=$stage' method='POST'><div class='inline field'><label>");
-        output('Enter a name for your superuser account:');
-        rawoutput("</label><input name='name' value=\"".htmlentities(httppost('name'), ENT_COMPAT, getsetting('charset', 'UTF-8'))."\"></div><div class='inline field'><label>");
-        output('`nEnter a password: ');
-        rawoutput("</label><input name='pass1' type='password'></div><div class='inline field'><label>");
-        output('`nConfirm your password: ');
-        rawoutput("</label><input name='pass2' type='password'></div>");
-        $submit = translate_inline('Create');
-        rawoutput("<div class='inline field'><input type='submit' value='$submit' class='ui button'></div>");
-        rawoutput('</form>');
+        $params = [
+            'stage' => $stage,
+            'name' => $name
+        ];
+
+        rawoutput(LotgdTheme::renderLotgdTemplate('core/pages/installer/stage-10.twig', $params));
     }
 }
 else
 {
-    output('`#You already have a superuser account set up on this server.');
+    \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('stage10.alreadySuperuser', [], 'page-installer'));
+
     savesetting('installer_version', \Lotgd\Core\Application::VERSION);
+
+    return redirect('home.php');
 }
