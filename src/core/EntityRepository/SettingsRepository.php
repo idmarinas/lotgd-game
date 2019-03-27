ace8a1fd56b12833831bf5d90498e6475fa38827
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-03-27 17:39:00 +0100

    First commit

---
+++ src/core/EntityRepository/SettingsRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\EntityRepository;
+
+use Doctrine\ORM\EntityRepository as DoctrineRepository;
+use Tracy\Debugger;
+
+class SettingsRepository extends DoctrineRepository
+{
+    /**
+     * Get a installed version, this avoid cache.
+     *
+     * @return string
+     */
+    public function getInstalledVersion(): string
+    {
+        try
+        {
+            $query = $this->_em->createQuery("SELECT u.value FROM $this->_entityName u WHERE u.setting = 'installer_version'");
+
+            return $query->getSingleScalarResult();
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return '';
+        }
+    }
+
+    /**
+     * Set installed version.
+     *
+     * @return self
+     */
+    public function setInstalledVersion(string $version): self
+    {
+        try
+        {
+            $query = $this->_em->createQuery("UPDATE $this->_entityName u SET u.value = ?1 WHERE u.setting = 'installer_version'");
+
+            $query->setParameter(1, $version)
+                ->execute()
+            ;
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+        }
+
+        return $this;
+    }
+}
