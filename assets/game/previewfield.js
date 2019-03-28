107abe148de6f67989c2fb10981d7848bff51564
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-03-28 16:44:23 +0100

    Updated to new format of comments

--- assets/game/previewfield.js
+++ assets/game/previewfield.js
@@ -14,38 +14,55 @@ define([
      *
      * @param {Object} type
      */
-    Lotgd.previewfield = function (target, maxchars, name, startdiv, talkline, showcharsleft, youhave)
+    Lotgd.previewfield = function (target, player, maxchars, talkline, youhave)
     {
         target = jQuery(target)
         let message = target.val()
         let length = message.length
         let charsleft = maxchars - length
+        let color = (charsleft > 0) ? 'green' : (charsleft === 0) ? 'orange' : 'red'
+        let charsLeftText = youhave.replace('%s', charsleft)
+        let playerName = Lotgd.appoencode(player)
 
         if (length === 0)
         {
-            jQuery('#previewtext' + name).addClass('hidden')
-            jQuery('#charsleft' + name).addClass('hidden').removeClass('red orange green')
+            jQuery('#previewtext-commentary-form').parent().addClass('hidden').removeClass('red orange green')
+            jQuery('#commentary-form input').attr('style', '')
+            jQuery('#commentary-form button').removeClass('top attached')
 
             return
         }
 
-        if (showcharsleft === 1)
-        {
-            let color = (charsleft > 0) ? 'green' : (charsleft === 0) ? 'orange' : 'red'
-            let charsLeftText = '<span class="' + color + '">' + youhave.replace('%s', charsleft) + '</span>'
+        jQuery('#charsleft-commentary-form').html(charsLeftText)
 
-            jQuery('#charsleft' + name).removeClass('hidden red orange green').addClass(color).html(charsLeftText)
+        let text = '<i class="eye icon"></i>'
+
+        if (message.substr(0, 2) === '::')
+        {
+            text += '<span class="colLtWhite">' + playerName + '</span> '
+            text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 2), '')) + '</span>'
+        }
+        else if (message.substr(0, 1) === ':')
+        {
+            text += '<span class="colLtWhite">' + playerName + '</span> '
+            text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 1), '')) + '</span>'
         }
 
-        let text = '<i class="eye icon"></i> '
-        if (startdiv !== '') { text += '<span class="colLtWhite">' + Lotgd.appoencode(startdiv) + '</span> ' }
+        else if (message.substr(0, 3) === '/me')
+        {
+            text += '<span class="colLtWhite">' + playerName + '</span> '
+            text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 3), '')) + '</span>'
+        }
+        else if (message.substr(0, 5) === '/game') { text += '<span class="colDkMagenta">' + Lotgd.appoencode(message.replace(message.substr(0, 5), '')) + '</span>' }
+        else
+        {
+            text += '<span class="colLtWhite">' + playerName + '</span> '
+            text += '<span class="colDkCyan">' + Lotgd.appoencode(talkline) + ',</span> "' + Lotgd.appoencode(message) + '"</span>'
+        }
 
-        if (message.substr(0, 2) === '::') { text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 2), '')) + '</span>' }
-        else if (message.substr(0, 1) === ':') { text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 1), '')) + '</span>' }
-        else if (message.substr(0, 3) === '/me') { text += '<span class="colLtBlack">' + Lotgd.appoencode(message.replace(message.substr(0, 3), '')) + '</span>' }
-        else { text += '<span class="colDkCyan">' + Lotgd.appoencode(talkline) + ', </span>' + Lotgd.appoencode(message) + '</span>' }
-
-        jQuery('#previewtext' + name).removeClass('hidden').html(text)
+        jQuery('#previewtext-commentary-form').html(text).parent().removeClass('hidden').removeClass('red orange green').addClass(color)
+        jQuery('#commentary-form input').attr('style', 'border-bottom-left-radius: 0 !important;')
+        jQuery('#commentary-form button').addClass('top attached')
     }
 
     return Lotgd
