c97a275cf6d8c498d5d6761d588b100c545c7806
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-04-02 12:47:05 +0200

    First commit

---
+++ src/core/Twig/TokenParser/CommentaryDefaultPaginationUrlTokenParser.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Twig\TokenParser;
+
+use Lotgd\Core\Twig\Node\CommentaryDefaultPaginationUrlNode;
+use Twig\Node\Node;
+use Twig\Token;
+use Twig\TokenParser\AbstractTokenParser;
+
+/**
+ * Token Parser for the 'commentary_pagination_link_url ' tag.
+ */
+class CommentaryDefaultPaginationUrlTokenParser extends AbstractTokenParser
+{
+    /**
+     * Parses a token and returns a node.
+     *
+     * @return Node
+     */
+    public function parse(Token $token)
+    {
+        $expr = $this->parser->getExpressionParser()->parseExpression();
+
+        $this->parser->getStream()->expect(Token::BLOCK_END_TYPE);
+
+        return new CommentaryDefaultPaginationUrlNode($expr, $token->getLine(), $this->getTag());
+    }
+
+    /**
+     * Gets the tag name associated with this token parser.
+     *
+     * @return string The tag name
+     */
+    public function getTag()
+    {
+        return 'commentary_pagination_link_url';
+    }
+}
