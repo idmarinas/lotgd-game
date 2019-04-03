c19f6a450f6b2626f008b7ec72b7d9d02c8f75fd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-04-03 13:52:30 +0200

    Function is obsolete since 4.0.0 and delete in version 4.1.0

--- lib/superusernav.php
+++ lib/superusernav.php
@@ -7,26 +7,10 @@ function superusernav()
 {
     global $session;
 
-    \LotgdNavigation::addHeader('Navigation');
+    trigger_error(sprintf(
+        'Usage of %s is obsolete since 4.0.0; and delete in version 4.1.0, use "LotgdNavigation::superuserGrottoNav()".',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if ($session['user']['superuser'] & ~SU_DOESNT_GIVE_GROTTO)
-    {
-        $script = substr(LotgdHttp::getServer('SCRIPT_NAME'), 0, strpos(LotgdHttp::getServer('SCRIPT_NAME'), '.'));
-
-        if ('superuser' != $script)
-        {
-            $args = modulehook('grottonav');
-
-            if (! array_key_exists('handled', $args) || ! $args['handled'])
-            {
-                \LotgdNavigation::addNav('G?Return to the Grotto', 'superuser.php');
-            }
-        }
-    }
-    $args = modulehook('mundanenav');
-
-    if (! array_key_exists('handled', $args) || ! $args['handled'])
-    {
-        \LotgdNavigation::addNav('M?Return to the Mundane', 'village.php');
-    }
+    \LotgdNavigation::superuserGrottoNav();
 }
