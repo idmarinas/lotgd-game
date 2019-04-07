716e238d96b4dce95156b7c8216a9767cb54a72f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-04-07 18:10:45 +0200

    Used new template and translator system

--- public/armoreditor.php
+++ public/armoreditor.php
@@ -10,122 +10,93 @@ check_su_access(SU_EDIT_EQUIPMENT);
 
 tlschema('armor');
 
-page_header('Armor Editor');
-$armorlevel = (int) httpget('level');
-require_once 'lib/superusernav.php';
-superusernav();
-addnav('Armor Editor');
-addnav('Armor Editor Home', "armoreditor.php?level=$armorlevel");
-
-addnav('Add armor', "armoreditor.php?op=add&level=$armorlevel");
-$values = [1 => 48, 225, 585, 990, 1575, 2250, 2790, 3420, 4230, 5040, 5850, 6840, 8010, 9000, 10350];
-output('`&<h3>Armor for %s Dragon Kills</h3>`0', $armorlevel, true);
-
 $armorarray = [
     'Armor,title',
     'armorid' => 'Armor ID,hidden',
     'armorname' => 'Armor Name',
-    'defense' => 'Defense,range,1,15,1'];
-$op = httpget('op');
-$id = httpget('id');
+    'defense' => 'Defense,range,1,15,1'
+];
+$values = [1 => 48, 225, 585, 990, 1575, 2250, 2790, 3420, 4230, 5040, 5850, 6840, 8010, 9000, 10350];
+
+$textDomain = 'page-armoreditor';
+$armorlevel = (int) \LotgdHttp::getQuery('level');
+$op = (string) \LotgdHttp::getQuery('op');
+$id = (int) \LotgdHttp::getQuery('id');
+$repository = \Doctrine::getRepository(\Lotgd\Core\Entity\Armor::class);
+
+$params = [
+    'armorLevel' => $armorlevel
+];
+
+page_header('title', ['level' => $armorlevel], $textDomain);
+
+\LotgdNavigation::superuserGrottoNav();
+\LotgdNavigation::addHeader('armoreditor.category.editor');
+\LotgdNavigation::addNav('armoreditor.nav.editor', "armoreditor.php?level={$armorlevel}");
+\LotgdNavigation::addNav('armoreditor.nav.armor.add', "armoreditor.php?op=add&level={$armorlevel}");
 
 if ('edit' == $op || 'add' == $op)
 {
+    $armor = ['defense' => $repository->getNextDefenseLevel($armorlevel)];
     if ('edit' == $op)
     {
-        $sql = 'SELECT * FROM '.DB::prefix('armor')." WHERE armorid='$id'";
-        $result = DB::query($sql);
-        $row = DB::fetch_assoc($result);
+        $armor = $repository->find($id);
+        $hydrator = new \Zend\Hydrator\ClassMethods();
+        $armor = $hydrator->extract($armor);
     }
-    else
-    {
-        $sql = 'SELECT max(defense+1) AS defense FROM '.DB::prefix('armor')." WHERE level=$armorlevel";
-        $result = DB::query($sql);
-        $row = DB::fetch_assoc($result);
-    }
-    rawoutput("<form action='armoreditor.php?op=save&level=$armorlevel' method='POST'>");
-    addnav('', "armoreditor.php?op=save&level=$armorlevel");
-    showform($armorarray, $row);
-    rawoutput('</form>');
+
+    $params['form'] = lotgd_showform($armorarray, $armor, true, false, false);
+
+    rawoutput(LotgdTheme::renderLotgdTemplate('core/page/armoreditor/add-edit.twig', $params));
+
+    page_footer();
 }
 elseif ('del' == $op)
 {
-    $sql = 'DELETE FROM '.DB::prefix('armor')." WHERE armorid='$id'";
-    DB::query($sql);
-    //output($sql);
-    $op = '';
-    httpset('op', $op);
+    $armor = $repository->find($id);
+
+    \LotgdFlashMessages::addSuccessMessage(\LotgdTranslator::t('armor.form.del.success', ['id' => $id], $textDomain));
+    \Doctrine::remove($armor);
+    \Doctrine::flush();
 }
 elseif ('save' == $op)
 {
-    $armorid = httppost('armorid');
-    $armorname = httppost('armorname');
-    $defense = httppost('defense');
+    $armorId = (int) \LotgdHttp::getPost('armorid');
+    $armorname = \LotgdHttp::getPost('armorname');
+    $defense = \LotgdHttp::getPost('defense');
 
-    if ($armorid > 0)
+    $armor = $repository->find($armorId);
+
+    $message = 'armor.form.edit';
+    if (! $armor)
     {
-        $sql = 'UPDATE '.DB::prefix('armor')." SET armorname=\"$armorname\",defense=\"$defense\",value=".$values[$defense]." WHERE armorid='$armorid'";
+        $armor = new \Lotgd\Core\Entity\Armor();
+        $message = 'armor.form.new';
     }
-    else
-    {
-        $sql = 'INSERT INTO '.DB::prefix('armor')." (level,defense,armorname,value) VALUES ($armorlevel,\"$defense\",\"$armorname\",".$values[$defense].')';
-    }
-    DB::query($sql);
-    $op = '';
-    httpset('op', $op);
+    $armor->setLevel($armorlevel)
+        ->setDefense($defense)
+        ->setArmorname($armorname)
+        ->setValue($values[$defense])
+    ;
+
+    \LotgdFlashMessages::addSuccessMessage(\LotgdTranslator::t($message, ['name' => $armorname], $textDomain));
+
+    \Doctrine::persist($armor);
+    \Doctrine::flush();
 }
 
-if ('' == $op)
+\LotgdNavigation::addHeader('armoreditor.category.armor.level');
+//-- Max level (DragonKills) of armor created
+$max = $repository->getMaxArmorLevel();
+for ($i = 0; $i <= $max; $i++)
 {
-    $sql = 'SELECT max(level+1) AS level FROM '.DB::prefix('armor');
-    $res = DB::query($sql);
-    $row = DB::fetch_assoc($res);
-    $max = $row['level'];
-
-    for ($i = 0; $i <= $max; $i++)
-    {
-        if (1 == $i)
-        {
-            addnav(['Armor for %s DK', $i], "armoreditor.php?level=$i");
-        }
-        else
-        {
-            addnav(['Armor for %s DKs', $i], "armoreditor.php?level=$i");
-        }
-    }
-    $sql = 'SELECT * FROM '.DB::prefix('armor')." WHERE level=$armorlevel ORDER BY defense";
-    $result = DB::query($sql);
-    $ops = translate_inline('Ops');
-    $name = translate_inline('Name');
-    $cost = translate_inline('Cost');
-    $defense = translate_inline('Defense');
-    $level = translate_inline('Level');
-    $edit = translate_inline('Edit');
-    $del = translate_inline('Del');
-    $delconfirm = translate_inline('Are you sure you wish to delete this armor?');
-
-    rawoutput("<table class='ui very compact striped selectable table'>");
-    rawoutput("<thead><tr><th>$ops</th><th>$name</th><th>$cost</th><th>$defense</th><th>$level</th></tr></thead>");
-    $number = DB::num_rows($result);
-
-    for ($i = 0; $i < $number; $i++)
-    {
-        $row = DB::fetch_assoc($result);
-        rawoutput('<tr>');
-        rawoutput("<td>[<a href='armoreditor.php?op=edit&id={$row['armorid']}&level=$armorlevel'>$edit</a>|<a href='armoreditor.php?op=del&id={$row['armorid']}&level=$armorlevel' onClick='return confirm(\"$delconfirm\");'>$del</a>]</td>");
-        addnav('', "armoreditor.php?op=edit&id={$row['armorid']}&level=$armorlevel");
-        addnav('', "armoreditor.php?op=del&id={$row['armorid']}&level=$armorlevel");
-        rawoutput('<td>');
-        output_notl($row['armorname']);
-        rawoutput('</td><td>');
-        output_notl($row['value']);
-        rawoutput('</td><td>');
-        output_notl($row['defense']);
-        rawoutput('</td><td>');
-        output_notl($row['level']);
-        rawoutput('</td>');
-        rawoutput('</tr>');
-    }
-    rawoutput('</table>');
+    \LotgdNavigation::addNav('armoreditor.nav.armor.level', "armoreditor.php?level=$i", [
+        'params' => ['n' => $i]
+    ]);
 }
+
+$params['armors'] = $repository->findByLevel($armorlevel, ['defense' => 'ASC']);
+
+rawoutput(LotgdTheme::renderLotgdTemplate('core/page/armoreditor.twig', $params));
+
 page_footer();
