cf9ee76fcb7e71d501ccc613496f09baea4fcdda
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-04-10 13:41:08 +0200

    Not overwrite name of field

--- src/core/Entity/Referers.php
+++ src/core/Entity/Referers.php
@@ -40,42 +40,42 @@ class Referers
     /**
      * @var string
      *
-     * @ORM\Column(name="uri", type="string", length=1000, nullable=false)
+     * @ORM\Column(type="string", length=1000, nullable=false)
      */
     private $uri;
 
     /**
      * @var int
      *
-     * @ORM\Column(name="count", type="integer", nullable=false)
+     * @ORM\Column(type="integer", nullable=false)
      */
     private $count = 0;
 
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="last", type="datetime", nullable=false)
+     * @ORM\Column(type="datetime", nullable=false)
      */
     private $last;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="site", type="string", length=50, nullable=false)
+     * @ORM\Column(type="string", length=50, nullable=false)
      */
     private $site;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="dest", type="string", length=255, nullable=false)
+     * @ORM\Column(type="string", length=255, nullable=false)
      */
     private $dest;
 
     /**
      * @var string
      *
-     * @ORM\Column(name="ip", type="string", length=40, nullable=false)
+     * @ORM\Column(type="string", length=40, nullable=false)
      */
     private $ip;
 
