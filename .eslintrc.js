a5be61e7a72dd7118344878a7be2e968c8dcbaae
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-03 12:01:06 +0200

    Format

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,11 @@ module.exports = {
     plugins: [
         'html'
     ],
-    'globals': {
+    globals: {
         'define': true
     },
     // add your custom rules here
-    'rules': {
+    rules: {
         // allow paren-less arrow functions
         'arrow-parens': 0,
         // allow async-await
