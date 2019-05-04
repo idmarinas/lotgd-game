1d661e2c9224f55eaa5ee504e9c0b3ac54c49c71
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-04 17:07:33 +0200

    Use Twig templates and new Translation system

--- lib/clan/applicant_apply.php
+++ lib/clan/applicant_apply.php
@@ -1,105 +1,66 @@
 <?php
-        $to = (int) httpget('to');
 
-        if ($to > 0)
-        {
-            output('`%%s`7 accepts your application, files it in her out box, and folds her hands on the desk, staring at you.', $registrar);
-            output("You stand there staring blankly back at her for a few minutes before she suggests that perhaps you'd like to take a seat in the waiting area.");
+$clanId = (int) \LotgdHttp::getQuery('clanid');
 
-            addnav('Return to the Lobby', 'clan.php');
-            addnav('Waiting Area', 'clan.php?op=waiting');
-            $session['user']['clanid'] = $to;
-            $session['user']['clanrank'] = CLAN_APPLICANT;
-            $session['user']['clanjoindate'] = date('Y-m-d H:i:s');
-            $sql = 'SELECT acctid FROM '.DB::prefix('accounts')." WHERE clanid='{$session['user']['clanid']}' AND clanrank>=".CLAN_OFFICER;
-            $result = DB::query($sql);
-            $sql = 'DELETE FROM . '.DB::prefix('mail')." WHERE msgfrom=0 AND seen=0 AND subject='".addslashes(serialize($apply_subj))."'";
-            DB::query($sql);
+$clanRepository = \Doctrine::getRepository(\Lotgd\Core\Entity\Clans::class);
 
-            while ($row = DB::fetch_assoc($result))
-            {
-                $msg = ['`^You have a new clan applicant!  `&%s`^ has completed a membership application for your clan!', $session['user']['name']];
-                systemmail($row['acctid'], $apply_subj, $msg);
-            }
+if ($clanId > 0)
+{
+    $charRepository = \Doctrine::getRepository(\Lotgd\Core\Entity\Characters::class);
+    $mailRepository = \Doctrine::getRepository(\Lotgd\Core\Entity\Mail::class);
 
-            // send reminder mail if clan of choice has a description
+    \LotgdFlashMessages::addSuccessMessage(\LotgdTranslator::t('flash.message.applicant.apply', [
+        'clanOwnerName' => \LotgdSanitize::fullSanitize($params['clanOwnerName'])
+    ], $textDomain));
 
-            $sql = 'SELECT * FROM '.DB::prefix('clans')." WHERE clanid='$to'";
-            $res = DB::query($sql);
-            $row = DB::fetch_assoc($res);
+    $session['user']['clanid'] = $clanId;
+    $session['user']['clanrank'] = CLAN_APPLICANT;
+    $session['user']['clanjoindate'] = new \DateTime('now');
 
-            if ('' != nltoappon($row['clandesc']))
-            {
-                $subject = 'Clan Application Reminder';
-                $mail = '`&Did you remember to read the description of the clan of your choice before applying?  Note that some clans may have requirements that you have to fulfill before you can become a member.  If you are not accepted into the clan of your choice anytime soon, it may be because you have not fulfilled these requirements.  For your convenience, the description of the clan you are applying to is reproduced below.`n`n`c`#%s`@ <`^%s`@>`0´c`n%s';
+    $subj = ['mail.apply.subject', ['name' => $session['user']['name']], $textDomain];
+    $msg = ['mail.apply.message', ['name' => $session['user']['name']], $textDomain];
 
-                systemmail($session['user']['acctid'], [$subject], [$mail, $row['clanname'], $row['clanshort'], addslashes(nltoappon($row['clandesc']))]);
-            }
-        }
-        else
-        {
-            $order = (int) httpget('order');
+    $mailRepository->deleteMailFromSystemBySubj(serialize($subj));
 
-            switch ($order) {
-                case 1:
-                    $order = 'clanname ASC';
-                    break;
-                default:
-                    $order = 'c DESC';
-                    break;
-            }
-            $sql = 'SELECT MAX('.DB::prefix('clans').'.clanid) AS clanid,MAX(clanname) AS clanname,count('.DB::prefix('accounts').'.acctid) AS c FROM '.DB::prefix('clans').' INNER JOIN '.DB::prefix('accounts').' ON '.DB::prefix('clans').'.clanid='.DB::prefix('accounts').'.clanid WHERE '.DB::prefix('accounts').'.clanrank > '.CLAN_APPLICANT.' GROUP BY '.DB::prefix('clans').".clanid ORDER BY $order";
-            $result = DB::query($sql);
+    $leaders = $charRepository->getLeadersFromClan($session['user']['clanid'], $session['user']['acctid']);
 
-            if (DB::num_rows($result) > 0)
-            {
-                output('`7You ask %s`7 for a clan membership application form.', $registrar);
-                output('She opens a drawer in her desk and pulls out a form.  It contains only two lines: Name and Clan Name.');
-                output('You furrow your brow, not sure if you really like having to deal with all this red tape, and get set to concentrate really hard in order to complete the form.');
-                output("Noticing your attempt to write on the form with your %s, %s`7 claims the form back from you, writes %s`7 on the first line, and asks you the name of the clan that you'd like to join:`n`n", $session['user']['weapon'], $registrar, $session['user']['name']);
+    foreach($leaders as $leader)
+    {
+        systemmail($leader['acctid'], $subj, $msg);
+    }
 
-                while ($row = DB::fetch_assoc($result))
-                {
-                    if (0 == $row['c'])
-                    {
-                        $sql = 'DELETE FROM '.DB::prefix('clans')." WHERE clanid={$row['clanid']}";
-                        DB::query($sql);
-                    }
-                    else
-                    {
-                        /*//*/					$row = modulehook('clan-applymember', $row);
-                        /*//*/					if (isset($row['handled']) && $row['handled'])
-                        {
-                            continue;
-                        }
-                        $memb_n = translate_inline('(%s members)');
-                        $memb_1 = translate_inline('(%s member)');
+    //-- Send reminder mail if clan of choice has a description
+    $result = $clanRepository->find($clanId);
 
-                        if (1 == $row['c'])
-                        {
-                            $memb = sprintf($memb_1, $row['c']);
-                        }
-                        else
-                        {
-                            $memb = sprintf($memb_n, $row['c']);
-                        }
-                        output_notl("&#149; <a href='clan.php?op=apply&to=%s'>%s</a> %s`n",
-                                $row['clanid'],
-                                full_sanitize(htmlentities($row['clanname'], ENT_COMPAT, getsetting('charset', 'UTF-8'))),
-                                $memb, true);
-                        addnav('', "clan.php?op=apply&to={$row['clanid']}");
-                    }
-                }
-                addnav('Return to the Lobby', 'clan.php');
-                addnav('Sorting');
-                addnav('Order by Membercount', 'clan.php?op=apply&order=0');
-                addnav('Order by Clanname', 'clan.php?op=apply&order=1');
-            }
-            else
-            {
-                output('`7You ask %s`7 for a clan membership application form.', $registrar);
-                output('She stares at you blankly for a few moments, then says, "`5Sorry pal, no one has had enough gumption to start up a clan yet.  Maybe that should be you, eh?`7"');
-                addnav('Apply for a New Clan', 'clan.php?op=new');
-                addnav('Return to the Lobby', 'clan.php');
-            }
-        }
+    if ('' != trim($result->getClandesc()))
+    {
+        $subj = ['mail.desc.reminder.subject', [], $textDomain];
+        $msg = ['mail.desc.reminder.message', [
+            'clanName' => $result->getClanname(),
+            'clanShortName' => $result->getClanshort(),
+            'description' => $result->getClandesc()
+        ], $textDomain];
+
+        systemmail($session['user']['acctid'], $subj, $msg);
+    }
+
+    return redirect('clan.php?op=waiting');
+}
+
+$order = (int) \LotgdHttp::getQuery('order');
+
+$params['clanList'] = $clanRepository->getClanListForApplicant($order);
+
+if (count($params['clanList']))
+{
+    \LotgdNavigation::addNav('nav.applicant.apply.lobby', 'clan.php');
+
+    \LotgdNavigation::addHeader('category.sorting');
+    \LotgdNavigation::addNav('nav.applicant.apply.order.count', 'clan.php?op=apply&order=0');
+    \LotgdNavigation::addNav('nav.applicant.apply.order.name', 'clan.php?op=apply&order=1');
+}
+else
+{
+    \LotgdNavigation::addNav('nav.applicant.apply.new', 'clan.php?op=new');
+    \LotgdNavigation::addNav('nav.applicant.apply.lobby', 'clan.php');
+}
