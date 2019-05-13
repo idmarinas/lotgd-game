f9385bc0674dde010bcd9a6daf349a9a38d2b288
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-13 19:07:52 +0200

    Not used

--- lib/data/configuration_cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    //taken out in 1.1.1 as the game settings were not cacheable if there was no directory known for the cache without database access
-    //here to display what has *been* in there.
-    'usedatacache' => 'Use Data Caching,viewonly',
-    'datacachepath' => 'Path to store data cache information,viewonly',
-    'databasetype' => 'Type of database,viewonly'
-];
