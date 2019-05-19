65ee9962f664a24e7e3689a6719cb183dfc5a961
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-19 16:30:30 +0200

    Use new template and translator system

--- public/donators.php
+++ public/donators.php
@@ -10,101 +10,56 @@ check_su_access(SU_EDIT_DONATIONS);
 
 tlschema('donation');
 
-page_header("Donator's Page");
-require_once 'lib/superusernav.php';
-superusernav();
+$textDomain = 'page-donators';
 
-$ret = httpget('ret');
-$return = cmd_sanitize($ret);
-$return = substr($return, strrpos($return, '/') + 1);
-tlschema('nav');
-addnav('Return whence you came', $return);
-tlschema();
+page_header('title', [], $textDomain);
 
-$add = translate_inline('Add Donation');
-rawoutput("<form action='donators.php?op=add1&ret=".rawurlencode($ret)."' method='POST' class='ui form'>");
-addnav('', 'donators.php?op=add1&ret='.rawurlencode($ret).'');
-$name = httppost('name');
+$params = [];
+$op = (string) \LotgdHttp::getQuery('op');
+$page = (int) \LotgdHttp::getQuery('page');
+$acctRepository = \Doctrine::getRepository(\Lotgd\Core\Entity\Accounts::class);
+$paylogRepository = \Doctrine::getRepository(\Lotgd\Core\Entity\Paylog::class);
 
-if ('' == $name)
+$name = (string) \LotgdHttp::getPost('name');
+$name = $name ?: (string) \LotgdHttp::getQuery('name');
+$params['name'] = $name;
+
+$amt = (int) \LotgdHttp::getPost('amt');
+$amt = $amt ?: (int) \LotgdHttp::getQuery('amt');
+$params['amt'] = $amt;
+
+$reason = (string) \LotgdHttp::getPost('reason');
+$reason = $reason ?: (string) \LotgdHttp::getQuery('reason');
+$reason = $reason ?: \LotgdTranslator::t('form.value.reason', [], $textDomain);
+$params['reason'] = $reason;
+
+$txnid = (string) \LotgdHttp::getPost('txnid');
+$txnid = $txnid ?: (string) \LotgdHttp::getQuery('txnid');
+$params['txnid'] = $txnid;
+
+\LotgdNavigation::superuserGrottoNav();
+\LotgdNavigation::addNav('donators.nav.refresh', 'donators.php');
+
+if ('save' == $op)
 {
-    $name = httpget('name');
-}
-$amt = httppost('amt');
+    $id = (int) \LotgdHttp::getQuery('id');
 
-if ('' == $amt)
-{
-    $amt = httpget('amt');
-}
-$reason = httppost('reason');
-
-if ('' == $reason)
-{
-    $reason = httpget('reason');
-}
-$txnid = httppost('txnid');
-
-if ('' == $txnid)
-{
-    $txnid = httpget('txnid');
-}
-
-if ('' == $reason)
-{
-    $reason = translate_inline('manual donation entry');
-}
-
-output('`bAdd Donation Points:´b`n');
-rawoutput('<div class="inline field"><label>');
-output('Character: ');
-rawoutput("</label><input name='name' value=\"".htmlentities($name, ENT_COMPAT, getsetting('charset', 'UTF-8')).'">');
-rawoutput('</div><div class="inline field"><label>');
-output('`nPoints: ');
-rawoutput("</label><input name='amt' size='3' value=\"".htmlentities($amt, ENT_COMPAT, getsetting('charset', 'UTF-8')).'">');
-rawoutput('</div><div class="inline field"><label>');
-output('`nReason: ');
-rawoutput("</label><input name='reason' size='30' value=\"".htmlentities($reason, ENT_COMPAT, getsetting('charset', 'UTF-8')).'">');
-rawoutput("</div><input type='hidden' name='txnid' value=\"".htmlentities($txnid, ENT_COMPAT, getsetting('charset', 'UTF-8')).'">');
-output_notl('`n');
-
-if ($txnid > '')
-{
-    output('For transaction: %s`n', $txnid);
-}
-rawoutput("<div class='field'><input type='submit' class='ui button' value='$add'></div>");
-rawoutput('</form>');
-
-addnav('Donations');
-
-if (($session['user']['superuser'] & SU_EDIT_PAYLOG) &&
-        file_exists('paylog.php'))
-{
-    addnav('Payment Log', 'paylog.php');
-}
-$op = httpget('op');
-
-if ('add2' == $op)
-{
-    $id = httpget('id');
-    $amt = httpget('amt');
-    $reason = httpget('reason');
-
-    $sql = 'SELECT name FROM '.DB::prefix('accounts')." WHERE acctid=$id;";
-    $result = DB::query($sql);
-    $row = DB::fetch_assoc($result);
-    output('%s donation points added to %s`0, reason: `^%s`0', $amt, $row['name'], $reason);
-
-    $txnid = httpget('txnid');
-    $ret = httpget('ret');
+    $account = $acctRepository->find($id);
 
     if ($id == $session['user']['acctid'])
     {
         $session['user']['donation'] += $amt;
     }
 
-    if ($txnid > '')
+    $points = $amt;
+    if ($txnid)
     {
-        $result = modulehook('donation_adjustments', ['points' => $amt, 'amount' => $amt / getsetting('dpointspercurrencyunit', 100), 'acctid' => $id, 'messages' => []]);
+        $result = modulehook('donation_adjustments', [
+            'points' => $amt,
+            'amount' => $amt / getsetting('dpointspercurrencyunit', 100),
+            'acctid' => $id,
+            'messages' => []
+        ]);
         $points = $result['points'];
 
         if (! is_array($result['messages']))
@@ -117,154 +72,101 @@ if ('add2' == $op)
             debuglog($message, false, $id, 'donation', 0, false);
         }
     }
-    else
-    {
-        $points = $amt;
-    }
+
     // ok to execute when this is the current user, they'll overwrite the
     // value at the end of their page hit, and this will allow the display
     // table to update in real time.
-    $sql = 'UPDATE '.DB::prefix('accounts')." SET donation=donation+'$points' WHERE acctid='$id'";
-    DB::query($sql);
-    modulehook('donation', ['id' => $id, 'amt' => $points, 'manual' => ($txnid > '' ? false : true)]);
+    $account->setDonation($account->getDonation() + $points);
+    \Doctrine::persist($account);
+    \Doctrine::flush();
 
-    if ($txnid > '')
+    modulehook('donation', [
+        'id' => $id,
+        'amt' => $points,
+        'manual' => (bool) ($txnid)
+    ]);
+
+    if ($txnid)
     {
-        $sql = 'UPDATE '.DB::prefix('paylog')." SET acctid='$id', processed=1 WHERE txnid='$txnid'";
-        DB::query($sql);
+        $paylog = $paylogRepository->findOneByTxnid($txnid);
+
+        $paylog->setAcctid($id)
+            ->setProcessed(1)
+        ;
+
+        \Doctrine::persist($paylog);
+        \Doctrine::flush();
+
         debuglog("Received donator points for donating -- Credited manually [$reason]", false, $id, 'donation', $points, false);
-        redirect('paylog.php');
+
+        return redirect('paylog.php');
     }
     else
     {
         debuglog("Received donator points -- Manually assigned, not based on a known dollar donation [$reason]", false, $id, 'donation', $amt, false);
     }
 
-    if (1 == $points)
-    {
-        systemmail($id, ['Donation Point Added'], ['`2You have received a donation point for %s.', $reason]);
-    }
-    else
-    {
-        systemmail($id, ['Donation Points Added'], ['`2You have received %d donation points for %s.', $points, $reason]);
-    }
-    httpset('op', '');
+    $subj = ['mail.donation.subject', ['points' => $points], $textDomain];
+    $msg = ['mail.donation.message', [
+        'points' => $points,
+        'reason' => $reason
+    ], $textDomain];
+
+    systemmail($id, $subj, $msg);
+
+    \LotgdFlashMessages::addSuccessMessage(\LotgdTranslator::t('flash.message.donation', [
+        'name' => $account->getCharacter()->getName(),
+        'points' => $points,
+        'reason' => $reason
+    ], $textDomain));
+
     $op = '';
 }
 
-if ('' == $op)
+
+if('add' == $op)
 {
-    $sql = 'SELECT name,donation,donationspent FROM '.DB::prefix('accounts').' WHERE donation>0 ORDER BY donation DESC LIMIT 25';
-    $result = DB::query($sql);
+    $params['tpl'] = 'add';
 
-    $name = translate_inline('Name');
-    $points = translate_inline('Points');
-    $spent = translate_inline('Spent');
+    $query = $acctRepository->createQueryBuilder('u');
+    $expr = $query->expr();
 
-    rawoutput("<table class='ui very compact striped selectable table'>");
-    rawoutput("<thead><tr><th>$name</th><th>$points</th><th>$spent</th></tr></thead>");
+    $query->select('u.acctid', 'u.donation', 'u.donationspent')
+        ->addSelect('c.name')
+        ->join(
+            \Lotgd\Core\Entity\Characters::class,
+            'c',
+            \Doctrine\ORM\Query\Expr\Join::WITH,
+            $expr->eq('c.id', 'u.character')
+        )
+        ->where('u.login LIKE :name OR c.name LIKE :name')
+        ->setParameter('name', "%{$name}%")
+    ;
 
-    if ($result->count())
-    {
-        foreach ($result as $row)
-        {
-            rawoutput('<tr><td>');
-            output_notl('`^%s`0', $row['name']);
-            rawoutput('</td><td>');
-            output_notl('`@%s`0', LotgdFormat::numeral($row['donation']));
-            rawoutput('</td><td>');
-            output_notl('`%%s`0', LotgdFormat::numeral($row['donationspent']));
-            rawoutput('</td></tr>');
-        }
-    }
-    else
-    {
-        rawoutput('<tr><td>');
-        output('Not found donators');
-        rawoutput('</td></tr>');
-    }
-    rawoutput('</table>', true);
+    $params['paginator'] = $acctRepository->getPaginator($query, $page);
 }
-elseif ('add1' == $op)
+elseif ('' == $op || $op)
 {
-    $search = '%';
-    $name = httppost('name');
+    $params['tpl'] = 'default';
 
-    if ('' == $name)
-    {
-        $name = httpget('name');
-    }
-    $search = str_replace("'", "\'", $name);
-    $sql = 'SELECT name,acctid,donation,donationspent FROM '.DB::prefix('accounts')." WHERE login LIKE '$search' or name LIKE '$search' LIMIT 100";
-    $result = DB::query($sql);
+    $query = $acctRepository->createQueryBuilder('u');
+    $expr = $query->expr();
 
-    if (0 == DB::num_rows($result))
-    {
-        for ($i = 0; $i < strlen($name); $i++)
-        {
-            $z = substr($name, $i, 1);
+    $query->select('u.donation', 'u.donationspent')
+        ->addSelect('c.name')
+        ->join(
+            \Lotgd\Core\Entity\Characters::class,
+            'c',
+            \Doctrine\ORM\Query\Expr\Join::WITH,
+            $expr->eq('c.id', 'u.character')
+        )
+        ->where('u.donation > 0')
+        ->orderBy('u.donation', 'DESC')
+    ;
 
-            if ("'" == $z)
-            {
-                $z = "\'";
-            }
-            $search .= $z.'%';
-        }
-
-        $sql = 'SELECT name,acctid,donation,donationspent FROM '.DB::prefix('accounts')." WHERE login LIKE '$search' or name LIKE '$search' LIMIT 100";
-        $result = DB::query($sql);
-    }
-    $ret = httpget('ret');
-    $amt = httppost('amt');
-
-    if ('' == $amt)
-    {
-        $amt = httpget('amt');
-    }
-    $reason = httppost('reason');
-
-    if ('' == $reason)
-    {
-        $reason = httpget('reason');
-    }
-    $txnid = httppost('txnid');
-
-    if ('' == $txnid)
-    {
-        $txnid = httpget('txnid');
-    }
-    output('Confirm the addition of %s points to:`n', $amt);
-
-    if ($reason)
-    {
-        output('(Reason: `^`b`i%s´i´b`0)`n`n', $reason);
-    }
-    $number = DB::num_rows($result);
-
-    for ($i = 0; $i < $number; $i++)
-    {
-        $row = DB::fetch_assoc($result);
-
-        if ('' != $ret)
-        {
-            rawoutput("<a href='donators.php?op=add2&id={$row['acctid']}&amt=$amt&ret=".rawurlencode($ret).'&reason='.rawurlencode($reason)."'>");
-        }
-        else
-        {
-            rawoutput("<a href='donators.php?op=add2&id={$row['acctid']}&amt=$amt&reason=".rawurlencode($reason)."&txnid=$txnid'>");
-        }
-        output_notl('%s (%s/%s)', $row['name'], $row['donation'], $row['donationspent']);
-        rawoutput('</a>');
-        output_notl('`n');
-
-        if ('' != $ret)
-        {
-            addnav('', "donators.php?op=add2&id={$row['acctid']}&amt=$amt&ret=".rawurlencode($ret).'&reason='.rawurlencode($reason));
-        }
-        else
-        {
-            addnav('', "donators.php?op=add2&id={$row['acctid']}&amt=$amt&reason=".rawurlencode($reason)."&txnid=$txnid");
-        }
-    }
+    $params['paginator'] = $acctRepository->getPaginator($query, $page);
 }
+
+rawoutput(LotgdTheme::renderLotgdTemplate('core/page/donators.twig', $params));
+
 page_footer();
