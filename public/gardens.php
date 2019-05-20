3d1f54a2a0ca478b01a061930e53fccf1cff23d5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-20 20:37:44 +0200

    The new system of templates and translators is used

--- public/gardens.php
+++ public/gardens.php
@@ -10,52 +10,59 @@ require_once 'lib/events.php';
 
 tlschema('gardens');
 
-page_header('The Gardens');
+// Don't hook on to this text for your standard modules please, use "gardens" instead.
+// This hook is specifically to allow modules that do other gardenss to create ambience.
+$result = modulehook('gardens-text-domain', ['textDomain' => 'page-gardens', 'textDomainNavigation' => 'navigation-gardens']);
+$textDomain = $result['textDomain'];
+$textDomainNavigation = $result['textDomainNavigation'];
+unset($result);
 
-addcommentary();
+page_header('title', [], $textDomain);
+
 $skipgardendesc = handle_event('gardens');
-$op = httpget('op');
-$com = httpget('comscroll');
-$refresh = httpget('refresh');
-$commenting = httpget('commenting');
-$comment = httppost('insertcommentary');
+
+$params = [
+    'textDomain' => $textDomain,
+    'showGardenDesc' => ! $skipgardendesc
+];
+
+$op = (string) \LotgdHttp::getQuery('op');
+$com = \LotgdHttp::getQuery('commentPage');
+$commenting = \LotgdHttp::getQuery('commenting');
+$comment = \LotgdHttp::getPost('comment');
+
 // Don't give people a chance at a special event if they are just browsing
 // the commentary (or talking) or dealing with any of the hooks in the village.
-if (! $op && '' == $com && ! $comment && ! $refresh && ! $commenting)
+if (! $op && '' == $com && ! $comment && ! $refresh && ! $commenting && 0 != module_events('gardens', getsetting('gardenchance', 0)))
 {
-    if (0 != module_events('gardens', getsetting('gardenchance', 0)))
+    if (\LotgdNavigation::checkNavs())
     {
-        if (checknavs())
-        {
-            page_footer();
-        }
-        else
-        {
-            // Reset the special for good.
-            $session['user']['specialinc'] = '';
-            $session['user']['specialmisc'] = '';
-            $skipgardendesc = true;
-            $op = '';
-            httpset('op', '');
-        }
+        page_footer();
+    }
+    else
+    {
+        // Reset the special for good.
+        $session['user']['specialinc'] = '';
+        $session['user']['specialmisc'] = '';
+        $skipgardendesc = true;
+        $params['showGardenDesc'] = ! $skipgardendesc;
+        $op = '';
+        \LotgdHttp::setQuery('op', '');
     }
 }
 
 if (! $skipgardendesc)
 {
     checkday();
-
-    output('`b`c`2The Gardens`0´c´b');
-
-    output('`n`nYou walk through a gate and on to one of the many winding paths that makes its way through the well-tended gardens.');
-    output('From the flowerbeds that bloom even in darkest winter, to the hedges whose shadows promise forbidden secrets, these gardens provide a refuge for those seeking out the Green Dragon; a place where they can forget their troubles for a while and just relax.`n`n');
-    output('One of the fairies buzzing about the garden flies up to remind you that the garden is a place for roleplaying and peaceful conversation, and to confine out-of-character comments to the other areas of the game.`n`n');
 }
 
-villagenav();
+\LotgdNavigation::villageNav();
 modulehook('gardens', []);
 
-commentdisplay('', 'gardens', 'Whisper here', 30, 'whispers');
+//-- This is only for params not use for other purpose
+$params = modulehook('page-gardens-tpl-params', $params);
+rawoutput(\LotgdTheme::renderThemeTemplate('page/gardens.twig', $params));
 
 module_display_events('gardens', 'gardens.php');
+
 page_footer();
