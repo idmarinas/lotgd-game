f9e8f18a6eb5dfecc7ca630c5159c61e3359ea5f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-05-27 12:37:13 +0200

    Updated with cache drivers available

--- config/autoload/global/doctrine.php
+++ config/autoload/global/doctrine.php
@@ -3,7 +3,14 @@
 return [
     'lotgd_core' => [
         'doctrine' => [
-            'cache_class' => Doctrine\Common\Cache\ApcuCache::class
+            //-- Enable one of the cache drivers available on your server for Doctrine
+
+            // 'cache_class' => Doctrine\Common\Cache\ApcCache::class,
+            // 'cache_class' => Doctrine\Common\Cache\ApcuCache::class,
+            // 'cache_class' => Doctrine\Common\Cache\MemcacheCache::class,
+            // 'cache_class' => Doctrine\Common\Cache\MemcachedCache::class,
+            // 'cache_class' => Doctrine\Common\Cache\XcacheCache::class,
+            // 'cache_class' => Doctrine\Common\Cache\RedisCache::class
         ]
     ]
 ];
