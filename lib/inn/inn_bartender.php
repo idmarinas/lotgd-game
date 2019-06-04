230bead803117549a0b1d91ac8c8e3185af50034
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-04 19:52:59 +0200

    Use new template and translator system

--- lib/inn/inn_bartender.php
+++ lib/inn/inn_bartender.php
@@ -1,177 +1,173 @@
 <?php
 
-$act = httpget('act');
+if ('bribe' == $action)
+{
+    $amt = (int) \LotgdHttp::getQuery('amt');
+    $type = (string) \LotgdHttp::getQuery('type');
 
-if ('' == $act)
-{
-    output('%s`0 looks at you sort-of sideways like.', $barkeep);
-    output('He never was the sort who would trust a man any farther than he could throw them, which gave dwarves a decided advantage, except in provinces where dwarf tossing was made illegal.');
-    output('%s`0 polishes a glass, holds it up to the light of the door as another patron opens it to stagger out into the street.', $barkeep);
-    output('He then makes a face, spits on the glass and goes back to polishing it.');
-    output("\"`%What d'ya want?`0\" he asks gruffly.");
-    addnav_notl(sanitize($barkeep));
-    addnav('Bribe', 'inn.php?op=bartender&act=bribe');
-    addnav('Drinks');
-    modulehook('ale', []);
-}
-elseif ('bribe' == $act)
-{
+    $params['type'] = $type;
+    $params['amount'] = $amt;
+
     $g1 = $session['user']['level'] * 10;
     $g2 = $session['user']['level'] * 50;
     $g3 = $session['user']['level'] * 100;
-    $type = httpget('type');
 
     if ('' == $type)
     {
-        output("While you know that you won't always get what you want, sometimes the way to a man's information is through your purse.");
-        output("It's also always been said that more is better.`n`n");
-        output('How much would you like to offer him?');
-        addnav('1 gem', 'inn.php?op=bartender&act=bribe&type=gem&amt=1');
-        addnav('2 gems', 'inn.php?op=bartender&act=bribe&type=gem&amt=2');
-        addnav('3 gems', 'inn.php?op=bartender&act=bribe&type=gem&amt=3');
-        addnav(['%s gold', $g1], "inn.php?op=bartender&act=bribe&type=gold&amt=$g1");
-        addnav(['%s gold', $g2], "inn.php?op=bartender&act=bribe&type=gold&amt=$g2");
-        addnav(['%s gold', $g3], "inn.php?op=bartender&act=bribe&type=gold&amt=$g3");
+        \LotgdNavigation::addNav('nav.bribe.gem', 'inn.php?op=bartender&act=bribe&type=gem&amt=1', [
+            'params' => [
+                'gem' => 1
+            ]
+        ]);
+        \LotgdNavigation::addNav('nav.bribe.gem', 'inn.php?op=bartender&act=bribe&type=gem&amt=2', [
+            'params' => [
+                'gem' => 2
+            ]
+        ]);
+        \LotgdNavigation::addNav('nav.bribe.gem', 'inn.php?op=bartender&act=bribe&type=gem&amt=3', [
+            'params' => [
+                'gem' => 3
+            ]
+        ]);
+
+        \LotgdNavigation::addNav('nav.bribe.gold', "inn.php?op=bartender&act=bribe&type=gold&amt=$g1", [
+            'params' => [
+                'gold' => $g1
+            ]
+        ]);
+        \LotgdNavigation::addNav('nav.bribe.gold', "inn.php?op=bartender&act=bribe&type=gold&amt=$g2", [
+            'params' => [
+                'gold' => $g2
+            ]
+        ]);
+        \LotgdNavigation::addNav('nav.bribe.gold', "inn.php?op=bartender&act=bribe&type=gold&amt=$g3", [
+            'params' => [
+                'gold' => $g3
+            ]
+        ]);
     }
     else
     {
-        $amt = httpget('amt');
-
         if ('gem' == $type)
         {
             if ($session['user']['gems'] < $amt)
             {
-                $try = false;
-                output("You don't have %s gems!", $amt);
+                \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.bribe.no.gems', [ 'amt' => $amt ], $textDomain));
+
+                return redirect('inn.php?op=bartender&act=bribe');
             }
             else
             {
                 $chance = $amt * 30;
                 $session['user']['gems'] -= $amt;
+
                 debuglog("spent $amt gems on bribing $barkeep");
-                $try = true;
             }
         }
         else
         {
             if ($session['user']['gold'] < $amt)
             {
-                output("You don't have %s gold!", $amt);
-                $try = false;
+                \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.bribe.no.gold', [ 'amt' => $amt ], $textDomain));
+
+                return redirect('inn.php?op=bartender&act=bribe');
             }
             else
             {
-                $try = true;
                 $sfactor = 50 / 90;
                 $fact = $amt / $session['user']['level'];
                 $chance = ($fact - 10) * $sfactor + 25;
                 $session['user']['gold'] -= $amt;
+
                 debuglog("spent $amt gold bribing $barkeep");
             }
         }
 
-        if ($try)
+        $params['bribeSuccess'] = mt_rand(0, 100) < $chance;
+
+        if ($params['bribeSuccess'])
         {
-            if (e_rand(0, 100) < $chance)
+            \LotgdNavigation::addHeader('category.want');
+            modulehook('bartenderbribe', []);
+
+            if (getsetting('pvp', 1))
             {
-                output('%s`0 leans over the counter toward you.  "`%What can I do for you, kid?`0" he asks.', $barkeep);
-                addnav('What do you want?');
-                modulehook('bartenderbribe', []);
-
-                if (getsetting('pvp', 1))
-                {
-                    addnav("Who's upstairs?", 'inn.php?op=bartender&act=listupstairs');
-                }
-                addnav('Tell me about colors', 'inn.php?op=bartender&act=colors');
-
-                if (getsetting('allowspecialswitch', true))
-                {
-                    addnav('Switch specialty', 'inn.php?op=bartender&act=specialty');
-                }
+                \LotgdNavigation::addNav('nav.bribe.upstairs', 'inn.php?op=bartender&act=listupstairs');
             }
-            else
-            {
-                output('%s`0 begins to wipe down the counter top, an act that really needed doing a long time ago.', $barkeep);
+            \LotgdNavigation::addNav('nav.bribe.color', 'inn.php?op=bartender&act=colors');
 
-                if ('gem' == $type)
-                {
-                    if (1 == $amt)
-                    {
-                        output("When he's finished, your gem is gone.");
-                    }
-                    else
-                    {
-                        output("When he's finished, your gems are gone.");
-                    }
-                }
-                else
-                {
-                    output("When he's finished, your gold is gone.");
-                }
-                output('You inquire about the loss, and he stares blankly back at you.');
-                addnav(['B?Talk to %s`0 again', $barkeep], 'inn.php?op=bartender');
+            if (getsetting('allowspecialswitch', true))
+            {
+                \LotgdNavigation::addNav('nav.bribe.specialty', 'inn.php?op=bartender&act=specialty');
             }
         }
         else
         {
-            output('`n`n%s`0 stands there staring at you blankly.', $barkeep);
-            addnav(['B?Talk to %s`0 the Barkeep', $barkeep], 'inn.php?op=bartender');
+            \LotgdNavigation::addNav('nav.barkeep.again', 'inn.php?op=bartender', [
+                'params' => [
+                    'barkeep' => $params['barkeep']
+                ]
+            ]);
         }
     }
 }
-elseif ('listupstairs' == $act)
+elseif ('listupstairs' == $action)
 {
-    addnav('Refresh the list', 'inn.php?op=bartender&act=listupstairs');
-    output('%s`0 lays out a set of keys on the counter top, and tells you which key opens whose room.  The choice is yours, you may sneak in and attack any one of them.', $barkeep);
-    pvplist($iname, 'pvp.php', '?act=attack&inn=1');
+    $pvp = \LotgdLocator::get(\Lotgd\Core\Pvp\Listing::class);
+
+    $pvptime = getsetting('pvptimeout', 600);
+
+    $params['paginator'] = $pvp->getPvpList($params['innName']);
+    $params['sleepers'] = $pvp->getLocationSleepersCount($params['innName']);
+    $params['returnLink'] = \LotgdHttp::getServer('REQUEST_URI');
+    $params['pvpTimeOut'] = new \DateTime(date('Y-m-d H:i:s', strtotime("-$pvptime seconds")));
+
+    \LotgdNavigation::addNav('Refresh the list', 'inn.php?op=bartender&act=listupstairs');
 }
-elseif ('colors' == $act)
+elseif ('colors' == $action)
 {
-    output('%s`0 leans on the bar.  "`%So you want to know about colors, do you?`0" he asks.', $barkeep);
-    output('You are about to answer when you realize the question was posed in the rhetoric.');
-    output("%s`0 continues, \"`%To do colors, here's what you need to do.", $barkeep);
-    output(' First, you use a &#0096; mark (found right above the tab key) followed by 1, 2, 3, 4, 5, 6, 7, !, @, #, $, %, ^, &.', true);
-    output('Each of those corresponds with a color to look like this:');
-    output_notl('`n`1&#0096;1 `2&#0096;2 `3&#0096;3 `4&#0096;4 `5&#0096;5 `6&#0096;6 `7&#0096;7 ', true);
-    output_notl('`n`!&#0096;! `@&#0096;@ `#&#0096;# `$&#0096;$ `%&#0096;% `^&#0096;^ `&&#0096;& `n', true);
-    output('`% Got it?`0"  You can practice below:');
-    rawoutput('<form action="'.LotgdHttp::getServer('REQUEST_URI').'" method="POST">');
-    $testtext = httppost('testtext');
-    output('You entered %s`n', prevent_colors(htmlentities($testtext, ENT_COMPAT, getsetting('charset', 'UTF-8'))), true);
-    output('It looks like %s`n', $testtext);
-    $try = translate_inline('Try');
-    rawoutput("<input name='testtext' id='input'>");
-    rawoutput("<input type='submit' class='button' value='$try'>");
-    rawoutput('</form>');
-    rawoutput("<script language='javascript'>document.getElementById('input').focus();</script>");
-    output('`0`n`nThese colors can be used in your name, and in any conversations you have.');
-    addnav('', LotgdHttp::getServer('REQUEST_URI'));
+    $outputColor = \LotgdLocator::get(\Lotgd\Core\Output\Collector::class);
+
+    $params['testText'] = (string) \LotgdHttp::getPost('testText');
+    $params['formUrl'] = (string) \LotgdHttp::getServer('REQUEST_URI');
+
+    $colors = $outputColor->getColors();
+
+    $params['colors'] = array_map(function ($n) {
+        return "`{$n}&#96;{$n} - &#180;{$n}´{$n}";
+    }, array_keys($colors));
+
+    $params['colors'] = '<span class="ui basic small labels"><span class="ui label">'.implode('</span> <span class="ui label">', $params['colors']).'</span></span>';
 }
-elseif ('specialty' == $act)
+elseif ('specialty' == $action)
 {
-    $specialty = httpget('specialty');
+    $specialty = (string) \LotgdHttp::getQuery('specialty');
+    $uri = (string) \LotgdHttp::getServer('REQUEST_URI');
+
+    $params['specialty'] = $specialty;
 
     if ('' == $specialty)
     {
-        output('"`2I want to change my specialty,`0" you announce to %s`0.`n`n', $barkeep);
-        output('With out a word, %s`0 grabs you by the shirt, pulls you over the counter, and behind the barrels behind him.', $barkeep);
-        output('There, he rotates the tap on a small keg labeled "Fine Swill XXX"`n`n');
-        output('You look around for the secret door that you know must be opening nearby when %s`0 rotates the tap back, and lifts up a freshly filled foamy mug of what is apparently his fine swill, blue-green tint and all.`n`n', $barkeep);
-        output('"`3What?  Were you expecting a secret room?`0" he asks.  "`3Now then, you must be more careful about how loudly you say that you want to change your specialty, not everyone looks favorably on that sort of thing.`n`n');
-        output('`0"`3What new specialty did you have in mind?`0"');
         $specialities = modulehook('specialtynames');
 
+        \LotgdNavigation::addHeader('category.specialty');
         foreach ($specialities as $key => $name)
         {
-            addnav($name, cmd_sanitize(LotgdHttp::getServer('REQUEST_URI'))."&specialty=$key");
+            \LotgdNavigation::addNavNotl($name, \LotgdSanitize::cmdSanitize($uri."&specialty={$key}"));
         }
     }
     else
     {
-        output("\"`3Ok then,`0\" %s`0 says, \"`3You're all set.`0\"`n`n\"`2That's it?`0\" you ask him.`n`n", $barkeep);
-        output("\"`3Yep.  What'd you expect, some sort of fancy arcane ritual???`0\"  %s`0 begins laughing loudly.", $barkeep);
-        output("\"`3You're all right, kid... just don't ever play poker, eh?`0`n`n");
-        output("\"`3Oh, one more thing.  Your old use points and skill level still apply to that skill, you'll have to build up some points in this one to be very good at it.`0\"");
         $session['user']['specialty'] = $specialty;
     }
+}
+else
+{
+    \LotgdNavigation::addHeader(\LotgdSanitize::fullSanitize($params['barkeep']), [
+        'translation' => false
+    ]);
+    \LotgdNavigation::addNav('Bribe', 'inn.php?op=bartender&act=bribe');
+    \LotgdNavigation::addHeader('Drinks');
+
+    modulehook('ale', []);
 }
--- public/inn.php
+++ public/inn.php
@@ -4,72 +4,238 @@
 // translator ready
 // mail ready
 require_once 'common.php';
-require_once 'lib/commentary.php';
 require_once 'lib/pvpwarning.php';
 require_once 'lib/sanitize.php';
-require_once 'lib/pvplist.php';
+// require_once 'lib/pvplist.php';
 require_once 'lib/buffs.php';
 require_once 'lib/events.php';
-require_once 'lib/villagenav.php';
 require_once 'lib/partner.php';
 
 tlschema('inn');
 
-addcommentary();
-$iname = getsetting('innname', LOCATION_INN);
-$vname = getsetting('villagename', LOCATION_FIELDS);
-$barkeep = getsetting('barkeep', '`tCedrik`0');
+// Don't hook on to this text for your standard modules please, use "inn" instead.
+// This hook is specifically to allow modules that do other inns to create ambience.
+$result = modulehook('inn-text-domain', ['textDomain' => 'page-inn', 'textDomainNavigation' => 'navigation-inn']);
+$textDomain = $result['textDomain'];
+$textDomainNavigation = $result['textDomainNavigation'];
+unset($result);
 
-$op = httpget('op');
-// Correctly reset the location if they fleeing the dragon
-// This needs to be done up here because a special could alter your op.
-if ('fleedragon' == $op)
-{
-    $session['user']['location'] = $vname;
-}
-
-page_header(sanitize($iname));
 $skipinndesc = handle_event('inn');
 
 if (! $skipinndesc)
 {
     checkday();
-    rawoutput("<span style='color: #9900FF'>");
-    output_notl('`c`b');
-    output($iname);
-    output_notl('´b´c');
 }
 
-$subop = httpget('subop');
+$params = [
+    'textDomain' => $textDomain,
+    'innName' => getsetting('innname', LOCATION_INN),
+    'villageName' => getsetting('villagename', LOCATION_FIELDS),
+    'barkeep' => getsetting('barkeep', '`tCedrik`0'),
+    'partner' => get_partner(),
+    'showInnDescription' => ! $skipinndesc
+];
 
-$com = httpget('comscroll');
-$comment = httppost('insertcommentary');
+page_header('title', ['name' => \LotgdSanitize::fullSanitize($params['innName'])], $textDomain);
 
-$partner = get_partner();
-addnav('Other');
-villagenav();
-addnav('I?Return to the Inn', 'inn.php');
+$op = (string) \LotgdHttp::getQuery('op');
+$op = 'bartender';
+$subop = (string) \LotgdHttp::getQuery('subop');
+$com = \LotgdHttp::getQuery('commentPage');
+$commenting = \LotgdHttp::getQuery('commenting');
+$comment = \LotgdHttp::getPost('comment');
+
+$params['op'] = $op;
+
+// Correctly reset the location if they fleeing the dragon
+// This needs to be done up here because a special could alter your op.
+if ('fleedragon' == $op)
+{
+    $session['user']['location'] = $params['villageName'];
+}
+
+//-- Change text domain for navigation
+\LotgdNavigation::setTextDomain($textDomainNavigation);
+
+\LotgdNavigation::addHeader('category.other');
+\LotgdNavigation::villageNav();
 
 switch ($op)
 {
-    case '': case 'strolldown': case 'fleedragon':
-        require 'lib/inn/inn_default.php';
-        blocknav('inn.php');
-    break;
     case 'converse':
-        commentdisplay('You stroll over to a table, place your foot up on the bench and listen in on the conversation:`n', 'inn', 'Add to the conversation?', 20);
+        $params['tpl'] = 'converse';
     break;
     case 'bartender':
+        $action = (string) \LotgdHttp::getQuery('act');
+
+        $params['tpl'] = 'bartender';
+        $params['action'] = $action;
+
         require 'lib/inn/inn_bartender.php';
     break;
     case 'room':
-        require 'lib/inn/inn_room.php';
+        $params['tpl'] = 'room';
+
+        $pay = (int) \LotgdHttp::getQuery('pay');
+
+        $expense = round(($session['user']['level'] * (10 + log($session['user']['level']))), 0);
+        $fee = getsetting('innfee', '5%');
+        $fee = (strpos($fee, '%')) ? round($expense * (str_replace('%', '', $fee) / 100), 0) : 0;
+        $bankexpense = $expense + $fee;
+
+        $params['fee'] = $fee;
+        $params['feePercent'] = (strpos($fee, '%')) ? str_replace('%', '', $fee) / 100 : null;
+        $params['expense'] = $expense;
+        $params['bankExpense'] = $bankexpense;
+        $params['boughtRoomToday'] = $session['user']['boughtroomtoday'];
+
+        if ($pay)
+        {
+            if (2 == $pay || $session['user']['gold'] >= $expense || $params['boughtRoomToday'])
+            {
+                if ($session['user']['loggedin'])
+                {
+                    if (! $params['boughtRoomToday'])
+                    {
+                        if (2 == $pay)
+                        {
+                            $session['user']['goldinbank'] -= $expense;
+                        }
+                        else
+                        {
+                            $session['user']['gold'] -= $expense;
+                        }
+
+                        $session['user']['boughtroomtoday'] = 1;
+                        debuglog("spent $expense gold on an inn room");
+                    }
+
+                    $session['user']['location'] = $iname;
+                    $session['user']['loggedin'] = 0;
+                    $session['user']['restorepage'] = 'inn.php?op=strolldown';
+                    saveuser();
+                }
+
+                $session = [];
+
+                return redirect('home.php');
+            }
+
+            \LotgdFlashNavigation::addWarningMessage(\LotgdTranslator::t('flash.message.room.not.gold', [ 'barkeep' => $barkeep ], $textDomain));
+
+            return redirect('inn.php?op=room');
+        }
+
+        if ($params['boughtRoomToday'])
+        {
+            \LotgdNavigation::addNav('nav.go.room', 'inn.php?op=room&pay=1');
+        }
+
+        $bodyguards = ['Butch', 'Bruce', 'Alfonozo', 'Guido', 'Bruno', 'Bubba', 'Al', 'Chuck', 'Brutus', 'Nunzio', 'Terrance', 'Mitch', 'Rocco', 'Spike', 'Gregor', 'Sven', 'Draco'];
+
+        modulehook('innrooms');
+
+        \LotgdNavigation::addNav('nav.room.buy.hand', 'inn.php?op=room&pay=1', [
+            'params' => [
+                'expense' => $expense
+            ]
+        ]);
+
+        if ($session['user']['goldinbank'] >= $bankexpense)
+        {
+            \LotgdNavigation::addNav('nav.room.buy.bank', 'inn.php?op=room&pay=2', [
+                'params' => [
+                    'expense' => $bankexpense
+                ]
+            ]);
+        }
+
+    break;
+    default:
+        $params['tpl'] = 'default';
+
+        \LotgdNavigation::blockLink('inn.php');
+
+        // Don't give people a chance at a special event if they are just browsing
+        // the commentary (or talking) or dealing with any of the hooks in the inn.
+        if ('fleedragon' != $op && '' == $com && ! $comment && ! $commenting && 0 != module_events('inn', getsetting('innchance', 0)))
+        {
+            if (\LotgdNavigation::checkNavs())
+            {
+                page_footer();
+            }
+
+            // Reset the special for good.
+            $session['user']['specialinc'] = '';
+            $session['user']['specialmisc'] = '';
+            $skipinndesc = true;
+            $op = '';
+            \LotgdHttp::setQuery('op', '');
+        }
+
+        \LotgdNavigation::addHeader('category.do');
+
+        $args = modulehook('blockcommentarea', ['section' => 'inn']);
+
+        if (! ($args['block'] ?? false) || ! $args['block'])
+        {
+            \LotgdNavigation::addNav('nav.converse', 'inn.php?op=converse');
+        }
+        \LotgdNavigation::addNav('nav.barkeep.talk', 'inn.php?op=bartender', [
+            'params' => [
+                'barkeep' => $params['barkeep']
+            ]
+        ]);
+
+        \LotgdNavigation::addHeader('category.other');
+        \LotgdNavigation::addNav('nav.room.get', 'inn.php?op=room');
+
+        if ('fleedragon' == $op)
+        {
+            $session['user']['charm']--;
+            $session['user']['charm'] = max(0, $session['user']['charm']);
+        }
+
+        $chats = [
+            [
+                'chats.dragon', [], $textDomain
+            ],
+            [
+                getsetting('bard', '`^Seth`0'), [], $textDomain
+            ],
+            [
+                getsetting('barmaid', '`%Violet`0'), [], $textDomain
+            ],
+            [
+                '`#MightyE`0', [], $textDomain
+            ],
+            [
+                'chats.drink', [], $textDomain
+            ],
+            [
+                $params['partner'], [], $textDomain
+            ],
+        ];
+
+        $chats = modulehook('innchatter', $chats);
+
+        $params['talk'] = $chats[array_rand($chats)];
+        $params['gameclock'] = getgametime();
     break;
 }
 
-if (! $skipinndesc)
+//-- Restore text domain for navigation
+\LotgdNavigation::setTextDomain();
+
+//-- This is only for params not use for other purpose
+$params = modulehook('page-inn-tpl-params', $params);
+rawoutput(\LotgdTheme::renderThemeTemplate('page/inn.twig', $params));
+
+if ('default' == $params['tpl'])
 {
-    rawoutput('</span>');
+    modulehook('inn', []);
+
+    module_display_events('inn', 'inn.php');
 }
 
 page_footer();
