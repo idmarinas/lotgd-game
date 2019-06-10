700b70fb7b6d12dd7ebe05756b7fc75515f9e200
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-10 14:00:35 +0200

    Added function to delete mail messages

--- src/ajax/core/mail.php
+++ src/ajax/core/mail.php
@@ -2,13 +2,20 @@
 
 namespace Lotgd\Ajax\Core;
 
+use Jaxon\Response\Response;
+
 class Mail
 {
-    public function status()
+    /**
+     * Check status of inbox.
+     *
+     * @return Response
+     */
+    public function status(): Response
     {
         global $session;
 
-        $response = new \Jaxon\Response\Response();
+        $response = new Response();
 
         //-- Do nothing if there is no active session
         if (! $session['user']['loggedin'])
@@ -23,6 +30,106 @@ class Mail
             'new' => $result['notSeenCount'],
             'old' => $result['seenCount']
         ], 'app-default'));
+
+        return $response;
+    }
+
+    /**
+     * Delete mail by ID.
+     *
+     * @param int    $mailId
+     * @param string $textDomain
+     *
+     * @return Response
+     */
+    public function deleteMail(int $mailId, string $textDomain): Response
+    {
+        global $session;
+
+        $response = new Response();
+
+        //-- Do nothing if there is no active session
+        if (! $session['user']['loggedin'])
+        {
+            return $response;
+        }
+
+        $repository = \Doctrine::getRepository('LotgdCore:Mail');
+        $delete = $repository->findOneBy([
+            'messageid' => $mailId,
+            'msgto' => $session['user']['acctid']
+        ]);
+
+        $type = 'error';
+        $message = \LotgdTranslator::t('dialog.del.one.error', [], $textDomain);
+
+        if ($delete)
+        {
+            \Doctrine::remove($delete);
+            \Doctrine::flush();
+
+            invalidatedatacache("mail-{$session['user']['acctid']}");
+
+            $type = 'success';
+            $message = \LotgdTranslator::t('dialog.del.one.success', [], $textDomain);
+        }
+
+        $response->dialog->{$type}($message);
+        $response->jQuery('#mail-read-buttons')->addClass('red')->children('.ui.button')->addClass('disabled')->removeClass('loading');
+
+        return $response;
+    }
+
+    /**
+     * Delete mail in bulk by ID.
+     *
+     * @param string  $string
+     * @param string $textDomain
+     *
+     * @return Response
+     */
+    public function deleteBulkMail(string $string, string $textDomain): Response
+    {
+        global $session;
+
+        $response = new Response();
+
+        //-- Do nothing if there is no active session
+        if (! $session['user']['loggedin'])
+        {
+            return $response;
+        }
+
+        $post = [];
+        parse_str($string, $post);
+        $post = $post['msg'];
+
+        $repository = \Doctrine::getRepository('LotgdCore:Mail');
+
+        $count = $repository->deleteBulkMail($post);
+
+        $type = 'error';
+        $message = \LotgdTranslator::t('dialog.del.bulk.error', [], $textDomain);
+
+        if ($count)
+        {
+            $type = 'success';
+            $message = \LotgdTranslator::t('dialog.del.bulk.success', [], $textDomain);
+
+            foreach ($post as $mailId)
+            {
+                $response->jQuery('#mail-row-'.$mailId)->remove();
+            }
+        }
+
+        if (! count($post))
+        {
+            $message = \LotgdTranslator::t('dialog.del.bulk.empty', [], $textDomain);
+        }
+
+        $response->jQuery('.ui.delete.button, .toggle.lotgd.checkbox, #check_name_select, .ui.check.toggle.button')->removeClass('disabled');
+        $response->jQuery('.ui.delbulk.button')->removeClass('loading disabled');
+        $response->dialog->{$type}($message);
 
         return $response;
     }
