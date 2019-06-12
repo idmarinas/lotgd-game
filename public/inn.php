5846cbc343c129570967c2837cbfeb796ddba5b4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-12 11:26:05 +0200

    Removed unnecesary require file

--- public/inn.php
+++ public/inn.php
@@ -5,7 +5,6 @@
 // mail ready
 require_once 'common.php';
 require_once 'lib/pvpwarning.php';
-require_once 'lib/sanitize.php';
 require_once 'lib/buffs.php';
 require_once 'lib/events.php';
 require_once 'lib/partner.php';
