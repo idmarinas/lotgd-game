17c2b76785498c29276dfd0c1c7604486400f078
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-17 17:49:32 +0200

    Added new function

--- src/core/EntityRepository/ModulesRepository.php
+++ src/core/EntityRepository/ModulesRepository.php
@@ -14,8 +14,40 @@
 namespace Lotgd\Core\EntityRepository;
 
 use Lotgd\Core\Doctrine\ORM\EntityRepository as DoctrineRepository;
+use Tracy\Debugger;
 
 class ModulesRepository extends DoctrineRepository
 {
     use Module\Setting;
+
+    /**
+     * Restart filemoddate to default value.
+     *
+     * @param string $module
+     *
+     * @return int
+     */
+    public function reinstallModule(string $module): int
+    {
+        $query = $this->_em->createQueryBuilder();
+
+        try
+        {
+            return $query->update($this->_entityName, 'u')
+                ->set('u.filemoddate', '0000-00-00 00:00:00')
+
+                ->where('u.modulename = :module')
+                ->setParameter('module', $module)
+
+                ->getQuery()
+                ->execute();
+            ;
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return 0;
+        }
+    }
 }
