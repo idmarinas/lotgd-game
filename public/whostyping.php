f93bdb294944f6ffc5b17967d85c889ad0afddf3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-20 16:48:56 +0200

    Updated, using Doctrine

--- public/whostyping.php
+++ public/whostyping.php
@@ -6,19 +6,14 @@ define('ALLOW_ANONYMOUS', true);
 define('OVERRIDE_FORCED_NAV', true);
 define('NO_SAVE_USER', true);
 
-//echo("Test!");
-
 require_once 'common.php';
 
 $now = time();
 $minute = round($now / 60) * 60;
 
-if (! isset($session['typerequests'][$minute]))
-{
-    $session['typerequests'][$minute] = 0;
-}
+$session['typerequests'][$minute] = $session['typerequests'][$minute] ?? 0;
 $session['typerequests'][$minute]++;
-//echo("Type requests: ".$session['typerequests'][$minute]." this minute (minute number ".$minute.").<br />");
+
 if ($session['typerequests'][$minute] >= 200)
 {
     echo "Please don't run multiple Global Banter windows, it puts a tremendous strain on the server.  I've logged you out.  You'll be able to log back in again in a few minutes - please clear your cookies.";
@@ -35,18 +30,8 @@ if ($fiveminuteload >= 8)
     exit();
 }
 
-//$start = microtime(true);
-
-//echo("Test!");
-
-// require_once("lib/dump_item.php");
-// require_once("lib/output.php");
-// $text = appoencode(dump_item($session));
-// echo($text);
-// echo("Test!");
-
-$section = $_REQUEST['section'];
-$updateplayer = $_REQUEST['updateplayer'];
+$section = \LotgdHttp::getquery('section');
+$updateplayer = \LotgdHttp::getquery('updateplayer');
 $name = addslashes(($session['user']['name'] ?? ''));
 $now = time();
 
@@ -58,52 +43,39 @@ $session['iterations']++;
 
 $old = $now - 2;
 
+$repository = \Doctrine::getRepository('LotgdCore:Whostyping');
 //update time
 if ($updateplayer)
 {
-    $sql = 'INSERT INTO '.DB::prefix('whostyping')." (time,name,section) VALUES ('$now','$name','$section') ON DUPLICATE KEY UPDATE time = VALUES(time), section = VALUES(section)";
-    DB::query($sql);
-    //echo("Updating player");
+    $entity = $repository->find($name);
+    $entity = $repository->hydrateEntity([
+        'time' => $now,
+        'name' => $name,
+        'section' => $section
+    ], $entity);
+
+    \Doctrine::persist($entity);
+    \Doctrine::flush();
+
     //erase old entries once per ten seconds
     $lastdigit = substr($now, -1);
 
     if ('0' == $lastdigit)
     {
-        $delsql = 'DELETE FROM '.DB::prefix('whostyping')." WHERE time < $old";
-        DB::query($delsql);
+        $repository->deleteOld($old);
     }
-    invalidatedatacache("whostyping/whostyping_$section");
+    invalidatedatacache("whostyping-whostyping_$section");
 }
 
 //retrieve, deleting as appropriate
-$sql = 'SELECT * FROM '.DB::prefix('whostyping')." WHERE section='$section'";
-$result = DB::query($sql);
-$disp = [];
+$result = $repository->findBy([ 'section' => $section ]);
 
-while ($row = DB::fetch_assoc($result))
+foreach($result as $row)
 {
-    if ($row['time'] > $old)
+    if ($row->getTime() > $old)
     {
-        $disp[] = $row['name'];
+        echo appoencode($row->getName().'`0 takes a breath...`n');
     }
 }
-
-//DB::free_result($result);
-
-//display
-foreach ($disp as $name)
-{
-    $encodedname = appoencode($name.'`0 takes a breath...`n');
-    echo $encodedname;
-}
-
-unset($disp);
-
-// $end = microtime(true);
-// $total = $end - $start;
-//echo("CavemanJoe is debugging in the middle of the night, and this cycle of whostyping.php took this long: ");
-//echo($total);
-//echo("test!");
-//echo($session['iterations']);
 
 exit();
