c6e4ab0ce4597e6c1f36be5f3a109161c274c491
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-25 15:22:33 +0200

    Updated to use Doctrine and added delete character

--- lib/charcleanup.php
+++ lib/charcleanup.php
@@ -1,80 +1,96 @@
 <?php
 
-function char_cleanup($id, $type)
+function char_cleanup($accountId, $type): bool
 {
     require_once 'lib/gamelog.php';
 
     // this function handles the grunt work of character cleanup.
 
     // Run any modules hooks who want to deal with character deletion, or stop it
-    $return = modulehook('delete_character', ['acctid' => $id, 'deltype' => $type, 'dodel' => true]);
+    $return = modulehook('delete_character', ['acctid' => $accountId, 'deltype' => $type, 'dodel' => true]);
 
     if (! $return['dodel'])
     {
         return false;
     }
 
-    // delete the output field from the accounts_output table introduced in 1.1.1
+    $accountRepository = \Doctrine::getRepository('LotgdCore:Accounts');
+    $charRepository = \Doctrine::getRepository('LotgdCore:Characters');
+    $mailRepository = \Doctrine::getRepository('LotgdCore:Mail');
+    $newsRepository = \Doctrine::getRepository('LotgdCore:News');
+    $outputRepository = \Doctrine::getRepository('LotgdCore:AccountsOutput');
+    $commentaryRepository = \Doctrine::getRepository('LotgdCore:Commentary');
 
-    DB::query('DELETE FROM '.DB::prefix('accounts_output')." WHERE acctid=$id;");
+    $accountEntity = $accountRepository->find($accountId);
 
-    // delete the comments the user posted, necessary to have the systemcomments with acctid 0 working
-
-    DB::query('DELETE FROM '.DB::prefix('commentary')." WHERE author=$id;");
+    if (! $accountEntity)
+    {
+        return false;
+    }
 
     // Clean up any clan positions held by this character
-    $sql = 'SELECT clanrank,clanid FROM '.DB::prefix('accounts').
-        " WHERE acctid=$id";
-    $res = DB::query($sql);
-    $row = DB::fetch_assoc($res);
-
-    if (0 != $row['clanid'] && (CLAN_LEADER == $row['clanrank'] || CLAN_FOUNDER == $row['clanrank']))
+    if ($accountEntity->getClanid() && (CLAN_LEADER == $accountEntity->getClanrank() || CLAN_FOUNDER == $accountEntity->getClanrank()))
     {
-        $cid = $row['clanid'];
-        // We need to auto promote or disband the clan.
-        $sql = 'SELECT name,acctid,clanrank FROM '.DB::prefix('accounts').
-            " WHERE clanid=$cid AND clanrank > ".CLAN_APPLICANT." AND acctid<>$id ORDER BY clanrank DESC, clanjoindate";
-        $res = DB::query($sql);
+        //-- Check if clan have more leaders
+        $leadersCount = $charRepository->getClanLeadersCount($accountEntity->getClanid());
 
-        if (DB::num_rows($res))
+        if (1 == $leadersCount || 0 == $leadersCount)
         {
-            // Okay, we can promote if needed
-            $row = DB::fetch_assoc($res);
+            $result = $charRepository->getViableLeaderForClan($accountEntity->getClanid(), $accountId);
 
-            if (CLAN_LEADER != $row['clanrank'])
+            if ($result)
             {
-                // No other leaders, promote this one
-                $id1 = $row['acctid'];
-                $sql = 'UPDATE '.DB::prefix('accounts').
-                    ' SET clanrank='.CLAN_LEADER." WHERE acctid=$id1";
-                DB::query($sql);
-                gamelog("Clan $cid has a new leader ".$row['name'].' as there were no others left', 'clan');
+                //-- there is no alternate leader, let's promote the
+                //-- highest ranking member (or oldest member in the
+                //-- event of a tie).  This will capture even people
+                //-- who applied for membership.
+                $charRepository->setNewClanLeader($result['id']);
+
+                gamelog("Clan {$accountEntity->getClanid()} has a new leader {$result['id']} as there were no others left", 'clan');
             }
-        }
-        else
-        {
-            // this clan needs to be disbanded.
-            $sql = 'DELETE FROM '.DB::prefix('clans')." WHERE clanid=$cid";
-            DB::query($sql);
-            // And just in case we goofed, no players associated with a
-            // deleted clan  This shouldn't be important, but.
-            $sql = 'UPDATE '.DB::prefix('accounts')." SET clanid=0,clanrank=0,clanjoindate='0000-00-00 00:00;00' WHERE clanid=$cid";
-            DB::query($sql);
-            gamelog("Clan $cid has been disbanded as the last member left", 'clan');
+            else
+            {
+                $clanRepository = \Doctrine::getRepository('LotgdCore:Clans');
+                $clanEntity = $clanRepository->find($accountEntity->getClanid());
+
+                //-- There are no other members, we need to delete the clan.
+                modulehook('clan-delete', ['clanid' => $accountEntity->getClanid(), 'clanEntity' => $clanEntity]);
+
+                \Doctrine::remove($clanEntity);
+
+                //just in case we goofed, we don't want to have to worry
+                //about people being associated with a deleted clan.
+                $charRepository->expelPlayersFromDeletedClan($session['user']['clanid']);
+
+                gamelog('Clan '.$session['user']['clanid'].' has been deleted, last member gone', 'Clan');
+
+                unset($clanEntity);
+            }
         }
     }
 
     // Delete any module user prefs
-    module_delete_userprefs($id);
+    module_delete_userprefs($accountId);
 
     // Delete any mail to or from the user
-    DB::query('DELETE FROM '.DB::prefix('mail').' WHERE msgto='.$id.' OR msgfrom='.$id);
+    $mailRepository->deleteMailOfAccount($accountId);
 
     // Delete any news from the user
-    DB::query('DELETE FROM '.DB::prefix('news').' WHERE accountid='.$id);
+    $newsRepository->deleteNewsOfAccount($accountId);
 
     // delete the output field from the accounts_output table introduced in 1.1.1
-    DB::query('DELETE FROM '.DB::prefix('accounts_output')." WHERE acctid=$id;");
+    $outputRepository->deleteOutputOfAccount($accountId);
+
+    // delete the comments the user posted, necessary to have the systemcomments with acctid 0 working
+    $commentaryRepository->deleteCommentsOfAccount($accountId);
+
+    // delete character of account
+    $charRepository->deleteCharacter($accountEntity->getCharacter()->getId());
+
+    // Delete account
+    \Doctrine::remove($accountEntity);
+
+    \Doctrine::flush();
 
     return true;
 }
