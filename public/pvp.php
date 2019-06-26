92b846f9a401b200657c2755dfb2116070442e1b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-26 20:07:44 +0200

    Used the new template and translator system

--- public/pvp.php
+++ public/pvp.php
@@ -6,89 +6,98 @@
 require_once 'common.php';
 require_once 'lib/fightnav.php';
 require_once 'lib/pvpwarning.php';
-require_once 'lib/pvplist.php';
 require_once 'lib/pvpsupport.php';
 require_once 'lib/taunt.php';
-require_once 'lib/villagenav.php';
 
 tlschema('pvp');
 
 $iname = getsetting('innname', LOCATION_INN);
 $battle = false;
 
-page_header('PvP Combat!');
-$op = httpget('op');
-$act = httpget('act');
+$textDomain = 'page-pvp';
+
+page_header('title', [], $textDomain);
+
+$params = [
+    'textDomain' => $textDomain
+];
+
+$op = (string) \LotgdHttp::getQuery('op');
+$act = (string) \LotgdHttp::getQuery('act');
 
 if ('' == $op && 'attack' != $act)
 {
     checkday();
-    pvpwarning();
-    $args = [
-        'atkmsg' => '`4You head out to the fields, where you know some unwitting warriors are sleeping.`n`nYou have `^%s`4 PvP fights left for today.`n`n',
-        'schemas' => ['atkmsg' => 'pvp']
-    ];
-    $args = modulehook('pvpstart', $args);
-    tlschema($args['schemas']['atkmsg']);
-    output($args['atkmsg'], $session['user']['playerfights']);
-    tlschema();
-    addnav('L?Refresh List of Warriors', 'pvp.php');
-    pvplist();
-    villagenav();
+
+    $pvp = \LotgdLocator::get(\Lotgd\Core\Pvp\Listing::class);
+    $pvptime = getsetting('pvptimeout', 600);
+
+    $params['tpl'] = 'default';
+    $params['paginator'] = $pvp->getPvpList($session['user']['location']);
+    $params['sleepers'] = $pvp->getLocationSleepersCount($session['user']['location']);
+    $params['returnLink'] = \LotgdHttp::getServer('REQUEST_URI');
+    $params['pvpTimeOut'] = new \DateTime(date('Y-m-d H:i:s', strtotime("-$pvptime seconds")));
+
+    \LotgdNavigation::addNav('common.nav.warriors', 'pvp.php');
+    \LotgdNavigation::villageNav();
 }
 elseif ('attack' == $act)
 {
-    $name = httpget('name');
-    $badguy = setup_target($name);
-    $options['type'] = 'pvp';
-    $failedattack = false;
+    $characterId = (int) \LotgdHttp::getQuery('character_id');
 
-    if (false === $badguy)
+    $badguy = setup_pvp_target($name);
+    $options['type'] = 'pvp';
+    $failedattack = true;
+
+    if (is_array($badguy))
     {
-        $failedattack = true;
-    }
-    else
-    {
+        $failedattack = false;
         $battle = true;
 
         if ($badguy['location'] == $iname)
         {
             $badguy['bodyguardlevel'] = $badguy['boughtroomtoday'];
         }
+
         $attackstack['enemies'][0] = $badguy;
         $attackstack['options'] = $options;
-        $session['user']['badguy'] = createstring($attackstack);
-        debug($session['user']['badguy']);
+        $session['user']['badguy'] = $attackstack;
         $session['user']['playerfights']--;
+
+        debug($session['user']['badguy']);
+    }
+    elseif (is_string($badguy))
+    {
+        \LotgdFlashMessages::addErrorMessage(\LotgdTranslate::t($badguy, [], $textDomain));
     }
 
     if ($failedattack)
     {
-        if (httpget('inn') > '')
+        if (\LotgdHttp::getQuery('inn'))
         {
-            addnav('Return to Listing', 'inn.php?op=bartender&act=listupstairs');
+            \LotgdNavigation::addNav('common.nav.listing', 'inn.php?op=bartender&act=listupstairs');
         }
         else
         {
-            addnav('Return to Listing', 'pvp.php');
+            \LotgdNavigation::addNav('common.nav.listing', 'pvp.php');
         }
     }
 }
-
-if ('run' == $op)
+elseif ('run' == $op)
 {
-    output('Your pride prevents you from running');
+    \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.pvp.run', [], $textDomain));
+
     $op = 'fight';
-    httpset('op', $op);
+    \LotgdHttp::setQuery('op', $op);
 }
 
-$skill = httpget('skill');
+$skill = \LotgdHttp::getQuery('skill');
 
 if ('' != $skill)
 {
     output('Your honor prevents you from using any special ability');
     $skill = '';
-    httpset('skill', $skill);
+    \LotgdHttp::setQuery('skill', '');
 }
 
 if ('fight' == $op || 'run' == $op)
@@ -99,8 +108,6 @@ if ('fight' == $op || 'run' == $op)
 if ($battle)
 {
     //-- Any data for personalize results
-    $battleDefeatWhere = 'in the forest'; //-- Use for create a news, set to false for not create news
-    $battleInForest = 'pvp'; //-- Indicating if is a Forest (true) or Graveyard (false)
     $battleShowResult = false; //-- Show result of battle.
     $battleProcessVictoryDefeat = false; //-- Process victory or defeat functions when the battle is over
 
@@ -109,32 +116,31 @@ if ($battle)
     if ($victory)
     {
         $killedin = $badguy['location'];
-        $handled = pvpvictory($badguy, $killedin, $options);
+        $handled = pvpvictory($badguy, $killedin);
 
         // Handled will be true if a module has already done the addnews or
         // whatever was needed.
         if (! $handled)
         {
-            if ($killedin == $iname)
-            {
-                addnews('`4%s`3 defeated `4%s`3 by sneaking into their room in the inn!', $session['user']['name'], $badguy['creaturename']);
-            }
-            else
-            {
-                addnews('`4%s`3 defeated `4%s`3 in fair combat in the fields of %s.', $session['user']['name'], $badguy['creaturename'], $killedin);
-            }
+            $news = ($killedin == $iname) ? 'inn' : 'other';
+
+            addnews("pvp.victory.{$news}", [
+                'playerName' => $session['user']['name'],
+                'creatureName' => $badguy['creaturename'],
+                'location' => $killedin
+            ]);
         }
 
         $op = '';
-        httpset('op', $op);
+        \LotgdHttp::setQuery('op', $op);
 
         if ($killedin == $iname)
         {
-            addnav('Return to the inn', 'inn.php');
+            \LotgdNavigation::addNav('common.nav.inn', 'inn.php');
         }
         else
         {
-            villagenav();
+            \LotgdNavigation::villageNav();
         }
 
         if ($session['user']['hitpoints'] <= 0)
@@ -146,35 +152,51 @@ if ($battle)
     elseif ($defeat)
     {
         $killedin = $badguy['location'];
-        $taunt = select_taunt();
         // This is okay because system mail which is all it's used for is
         // not translated
-        $handled = pvpdefeat($badguy, $killedin, $taunt, $options);
+        $handled = pvpdefeat($badguy, $killedin);
         // Handled will be true if a module has already done the addnews or
         // whatever was needed.
         if (! $handled)
         {
-            if ($killedin == $iname)
-            {
-                addnews('`%%s`5 has been slain while breaking into the inn room of `^%s`5 in order to attack them.`n%s`0', $session['user']['name'], $badguy['creaturename'], $taunt);
-            }
-            else
-            {
-                addnews('`%%s`5 has been slain while attacking `^%s`5 in the fields of `&%s`5.`n%s`0', $session['user']['name'], $badguy['creaturename'], $killedin, $taunt);
-            }
+            require_once 'lib/taunt.php';
+
+            $taunt = select_taunt();
+
+            $news = ($killedin == $iname) ? 'inn' : 'other';
+
+            addnews('deathmessage', [
+                'deathmessage' => [
+                    'deathmessage' => "pvp.defeated.{$news}",
+                    'params' => [
+                        'playerName' => $session['user']['name'],
+                        'creatureName' => $badguy['creaturename'],
+                        'location' => $killedin
+                    ],
+                    'textDomain' => $textDomain
+                ],
+                'taunt' => $taunt
+            ], '');
         }
     }
     else
     {
         $extra = '';
 
-        if (httpget('inn'))
+        if (\LotgdHttp::getQuery('inn'))
         {
             $extra = '?inn=1';
         }
-        fightnav(false, false, "pvp.php$extra");
+        fightnav(false, false, "pvp.php{$extra}");
     }
 
     battleshowresults($lotgdBattleContent);
 }
+
+$params['battle'] = $battle;
+
+//-- This is only for params not use for other purpose
+$params = modulehook('page-pvp-tpl-params', $params);
+rawoutput(\LotgdTheme::renderThemeTemplate('page/pvp.twig', $params));
+
 page_footer();
