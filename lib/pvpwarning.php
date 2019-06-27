3ded30a7a3e1ad38ad664f6ff880d363159cabac
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-27 12:42:22 +0200

    Updated to new translation system

--- lib/pvpwarning.php
+++ lib/pvpwarning.php
@@ -5,23 +5,21 @@
 // mail ready
 function pvpwarning($dokill = false)
 {
-    global $session;
-    $days = getsetting('pvpimmunity', 5);
-    $exp = getsetting('pvpminexp', 1500);
+    global $session, $textDomain;
 
-    if ($session['user']['age'] <= $days &&
-        0 == $session['user']['dragonkills'] &&
-        0 == $session['user']['pk'] &&
-        $session['user']['experience'] <= $exp)
+    $days = (int) getsetting('pvpimmunity', 5);
+    $exp = (int) getsetting('pvpminexp', 1500);
+
+    if ($session['user']['age'] <= $days && 0 == $session['user']['dragonkills'] && 0 == $session['user']['pk'] && $session['user']['experience'] <= $exp)
     {
         if ($dokill)
         {
-            output('`$Warning!`^ Since you were still under PvP immunity, but have chosen to attack another player, you have lost this immunity!!`n`n');
+            \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.warning.pk', [], $textDomain));
             $session['user']['pk'] = 1;
         }
         else
         {
-            output('`$Warning!`^ Players are immune from Player vs Player (PvP) combat for their first %s days in the game or until they have earned %s experience, or until they attack another player.  If you choose to attack another player, you will lose this immunity!`n`n', $days, $exp);
+            \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.warning.msg', [ 'days' => $days, 'exp' => $exp ], $textDomain));
         }
     }
     modulehook('pvpwarning', ['dokill' => $dokill]);
