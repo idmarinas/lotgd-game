ca1e4d6ac911bcdd8af0eeaa1838daf7b48ccdf9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-28 11:05:23 +0200

    Used the new template and translator system

--- public/rawsql.php
+++ public/rawsql.php
@@ -4,125 +4,87 @@
 // addnews ready
 // mail ready
 require_once 'common.php';
-require_once 'lib/superusernav.php';
 
 tlschema('rawsql');
 
 check_su_access(SU_RAW_SQL);
 
-page_header('Raw SQL/PHP execution');
-superusernav();
+$textDomain = 'page-rawsql';
+
+$params = [
+    'textDomain' => $textDomain
+];
+
+page_header('title', [], $textDomain);
+
+\LotgdNavigation::superuserGrottoNav();
+
 addnav('Execution');
 addnav('SQL', 'rawsql.php');
 addnav('PHP', 'rawsql.php?op=php');
 
-$op = httpget('op');
+$op = (string) \LotgdHttp::getQuery('op');
+$sql = (string) \LotgdHttp::getPost('sql');
 
 if ('' == $op || 'sql' == $op)
 {
-    $sql = httppost('sql');
+    $params['tpl'] = 'default';
 
     if ('' != $sql)
     {
+        /**
+         * For now use zend-db
+         *
+         * @TODO Script to create SQL in Doctrine
+         */
         $sql = stripslashes($sql);
-        modulehook('rawsql-execsql', ['sql' => $sql]);
+        $params['sql'] = $sql;
+
         debuglog('Ran Raw SQL: '.$sql);
 
         try
         {
-            $r = DB::query($sql, false);
+            DB::query($sql, false);
 
-            if (! $r)
+            $error = DB::errorInfo();
+
+            if ($error)
             {
-                output('`$SQL Error:`& %s`0`n`n', DB::error($r));
-            }
-            else
-            {
-                if (DB::affected_rows() > 0)
-                {
-                    output('`&%s rows affected.`n`n', DB::affected_rows());
-                }
-                rawoutput("<table class='ui very compact striped table'>");
-                $number = DB::num_rows($r);
-
-                for ($i = 0; $i < $number; $i++)
-                {
-                    $row = DB::fetch_assoc($r);
-
-                    if (0 == $i)
-                    {
-                        rawoutput('<thead><tr>');
-                        $keys = array_keys($row);
-
-                        foreach ($keys as $value)
-                        {
-                            rawoutput("<th>$value</th>");
-                        }
-                        rawoutput('</tr></thead>');
-                    }
-                    rawoutput('<tr>');
-
-                    foreach ($keys as $value)
-                    {
-                        rawoutput("<td valign='top'>{$row[$value]}</td>");
-                    }
-                    rawoutput('</tr>');
-                }
-                rawoutput('</table>');
+                \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('flash.message.sql.error.db', [ 'error' => $error ], $textDomain));
             }
         }
-        catch (Exception $ex)
+        catch (\Throwable $th)
         {
-            output('`$An exception occurred while executing SQL query:`0`n');
-            output_notl($ex->getMessage());
+            \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('flash.message.sql.error.th', [ 'error' => $ex->getMessage() ], $textDomain));
         }
     }
-
-    output('Type your query');
-    $execute = translate_inline('Execute');
-    $ret = modulehook('rawsql-modsql', ['sql' => $sql]);
-    $sql = $ret['sql'];
-    rawoutput("<form action='rawsql.php' method='post' class='ui form'>");
-    rawoutput("<div class='field'><textarea name='sql' class='input' cols='60' rows='10'>".htmlentities($sql, ENT_COMPAT, getsetting('charset', 'UTF-8')).'</textarea></div>');
-    rawoutput("<div class='field'><input type='submit' class='ui button' value='$execute'></div>");
-    rawoutput('</form>');
-    addnav('', 'rawsql.php');
 }
 else
 {
-    $php = stripslashes(httppost('php'));
-    $source = translate_inline('Source:');
-    $execute = translate_inline('Execute');
+    $params['tpl'] = 'php';
+
+    $php = stripslashes((string) \LotgdHttp::getPost('php'));
 
     if ($php > '')
     {
-        rawoutput("<div style='background-color: #FFFFFF; color: #000000; width: 100%'><b>$source</b><br>");
-        rawoutput(highlight_string("<?php\n$php\n?>", true));
-        rawoutput('</div>');
-        output('`bResults:´b`n');
-        modulehook('rawsql-execphp', ['php' => $php]);
+        $params['php'] = $php;
+        $params['highlight'] = highlight_string("<?php\n$php\n?>", true);
 
         try
         {
             ob_start();
             eval($php);
-            output_notl(ob_get_contents(), true);
+            $params['result'] = ob_get_contents();
             ob_end_clean();
         }
         catch (Exception $ex)
         {
-            output('`$An exception occurred while executing PHP code:`0`n');
-            output_notl($ex->getMessage());
+            \LotgdFlashMessages::addErrorMessage(\LotgdTranslator::t('flash.message.php.error.th', [ 'error' => $ex->getMessage() ], $textDomain));
         }
         debuglog('Ran Raw PHP: '.$php);
     }
-    output('`n`nType your code:');
-    $ret = modulehook('rawsql-modphp', ['php' => $php]);
-    $php = $ret['php'];
-    rawoutput("<form action='rawsql.php?op=php' method='post' class='ui form'>");
-    rawoutput("<div class='field'>&lt;?php<textarea name='php'>".htmlentities($php, ENT_COMPAT, getsetting('charset', 'UTF-8')).'</textarea>?&gt;</div>');
-    rawoutput("<div class='field'><input type='submit' class='ui button' value='$execute'></div>");
-    rawoutput('</form>');
-    addnav('', 'rawsql.php?op=php');
 }
+
+rawoutput(\LotgdTheme::renderLotgdTemplate('core/page/rawsql.twig', $params));
+
 page_footer();
