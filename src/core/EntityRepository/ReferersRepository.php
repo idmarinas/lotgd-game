900a774c44be1d654490a53ac4951e8b1014652a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-06-28 18:10:37 +0200

    First commit

---
+++ src/core/EntityRepository/ReferersRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.md
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\EntityRepository;
+
+use Lotgd\Core\Doctrine\ORM\EntityRepository as DoctrineRepository;
+use Tracy\Debugger;
+
+class ReferersRepository extends DoctrineRepository
+{
+    /**
+     * Delte old referers in data base.
+     *
+     * @param int $expire
+     *
+     * @return int
+     */
+    public function deleteExpireReferers(int $expire): int
+    {
+        $query = $this->_em->createQueryBuilder();
+
+        try
+        {
+            $date = new \DateTime('now');
+            $date->sub(new \DateInterval("P{$expire}D"));
+
+            return $query->delete($this->_entityName, 'u')
+                ->where('u.last < :date')
+                ->setParameter('date', $date)
+                ->getQuery()
+                ->execute()
+            ;
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return 0;
+        }
+    }
+}
