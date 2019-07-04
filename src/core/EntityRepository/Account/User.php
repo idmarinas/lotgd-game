27e38ad21056d4db87e10cf307133e004d059550
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-04 18:45:19 +0200

    Added new function to search accounts

--- src/core/EntityRepository/Account/User.php
+++ src/core/EntityRepository/Account/User.php
@@ -13,6 +13,9 @@
 
 namespace Lotgd\Core\EntityRepository\Account;
 
+use Tracy\Debugger;
+use Zend\Paginator\Paginator;
+
 /**
  * Functions for user account.
  */
@@ -43,9 +46,48 @@ trait User
         }
         catch (\Throwable $th)
         {
-            \Tracy\Debugger::log($th);
+            Debugger::log($th);
 
             return [];
         }
+    }
+
+    /**
+     * Search users.
+     *
+     * @param string $search
+     * @param string $order
+     * @param int    $page
+     *
+     * @return Paginator|null
+     */
+    public function userSearchAccounts(string $search, string $order, int $page): ?Paginator
+    {
+        $query = $this->createQueryBuilder('u');
+
+        $query->select('u.acctid', 'u.emailaddress', 'u.lastip', 'u.laston', 'u.loggedin')
+            ->addSelect('c.name', 'c.level')
+            ->addSelect('p.gentimecount')
+            ->leftJoin('LotgdCore:Characters', 'c', 'WITH', $query->expr()->eq('c.id', 'u.character'))
+            ->leftJoin('LotgdCore:AccountsEverypage', 'p', 'WITH', $query->expr()->eq('p.acctid', 'u.acctid'))
+        ;
+
+        //-- Order
+        $sort = "u.{$order}";
+
+        if ('name' == $order)
+        {
+            $sort = 'c.name';
+        }
+        $query->orderBy($sort, 'ASC');
+
+        if ($search)
+        {
+            $query->where('u.login LIKE :search OR c.name LIKE :search OR u.acctid = :search OR u.emailaddress LIKE :search OR u.lastip LIKE :search OR u.uniqueid LIKE :search')
+                ->setParameter('search', "%{$search}%")
+            ;
+        }
+
+        return $this->getPaginator($query, $page, 30);
     }
 }
