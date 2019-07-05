7c6458506a78ea7e41250a1273f4d52817b1db2f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-05 20:00:57 +0200

    Not in used

--- lib/user/user_delban.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$sql = 'DELETE FROM '.DB::prefix('bans')." WHERE ipfilter = '".httpget('ipfilter')."' AND uniqueid = '".httpget('uniqueid')."'";
-DB::query($sql);
-redirect('user.php?op=removeban');
