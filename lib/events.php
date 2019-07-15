cb0249c80bb36a870f267b56d8caa75ee97b9074
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-15 19:08:16 +0200

    Remplace obsolete functions

--- lib/events.php
+++ lib/events.php
@@ -8,33 +8,30 @@ require_once 'lib/constants.php';
 // This file encapsulates all the special event handling for most locations
 
 // Returns whether or not the description should be skipped
-function handle_event($location, $baseLink = false, $needHeader = false)
+function handle_event($location, $baseLink = false)
 {
-    global $session, $playermount, $badguy;
+    global $session, $html, $playermount, $badguy;
 
     if (false === $baseLink)
     {
-        $PHP_SELF = LotgdHttp::getServer('PHP_SELF');
+        $PHP_SELF = \LotgdHttp::getServer('PHP_SELF');
         $baseLink = substr($PHP_SELF, strrpos($PHP_SELF, '/') + 1).'?';
     }
     $skipdesc = false;
 
     tlschema('events');
     $allowinactive = false;
-    $eventhandler = httpget('eventhandler');
+    $eventhandler = (string) \LotgdHttp::getQuery('eventhandler');
 
     if (($session['user']['superuser'] & SU_DEVELOPER) && '' != $eventhandler)
     {
         $allowinactive = true;
         $array = preg_split('/[:-]/', $eventhandler);
 
+        $session['user']['specialinc'] = '';
         if ('module' == $array[0])
         {
             $session['user']['specialinc'] = 'module:'.$array[1];
-        }
-        else
-        {
-            $session['user']['specialinc'] = '';
         }
     }
 
@@ -45,12 +42,11 @@ function handle_event($location, $baseLink = false, $needHeader = false)
         $specialinc = $session['user']['specialinc'];
         $session['user']['specialinc'] = '';
 
-        if (false !== $needHeader)
-        {
-            page_header($needHeader);
-        }
-
-        output('`^`c`bSomething Special!´c´b`0');
+        $html['event'] = [
+            'event.title',
+            [],
+            'partial-event'
+        ];
 
         if (strchr($specialinc, ':'))
         {
@@ -60,25 +56,24 @@ function handle_event($location, $baseLink = false, $needHeader = false)
             module_do_event($location, $modulename, $allowinactive, $baseLink);
             $endtime = microtime(true);
 
-            if (($endtime - $starttime >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT)))
+            if (($endtime - $starttime) >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT))
             {
                 debug('Slow Event ('.round($endtime - $starttime, 2)."s): $location - {$modulename}`n");
             }
         }
 
-        if (checknavs())
+        if (\LotgdNavigation::checkNavs())
         {
             // The page rendered some linkage, so we just want to exit.
-            page_footer();
+            return page_footer();
         }
-        else
-        {
-            $skipdesc = true;
-            $session['user']['specialinc'] = '';
-            $session['user']['specialmisc'] = '';
-            httpset('op', '');
-        }
+
+        $skipdesc = true;
+        $session['user']['specialinc'] = '';
+        $session['user']['specialmisc'] = '';
+        \LotgdHttp::setQuery('op', '');
     }
+
     tlschema();
 
     return $skipdesc;
