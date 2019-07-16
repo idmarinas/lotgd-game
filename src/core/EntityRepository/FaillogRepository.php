a2821a2ebd395e559ad253ad1c0b72cf8e10676f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-16 18:56:55 +0200

    First commit

---
+++ src/core/EntityRepository/FaillogRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\EntityRepository;
+
+use Lotgd\Core\Doctrine\ORM\EntityRepository as DoctrineRepository;
+
+class FaillogRepository extends DoctrineRepository
+{
+    /**
+     * Delte old fail logs in data base.
+     *
+     * @param int $expire
+     *
+     * @return int
+     */
+    public function deleteExpireFaillogs(int $expire): int
+    {
+        $query = $this->_em->createQueryBuilder();
+
+        try
+        {
+            $date = new \DateTime('now');
+            $date->sub(new \DateInterval("P{$expire}D"));
+
+            return $query->delete($this->_entityName, 'u')
+                ->where('u.sent < :date')
+                ->setParameter('date', $date)
+                ->getQuery()
+                ->execute()
+            ;
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return 0;
+        }
+    }
+}
