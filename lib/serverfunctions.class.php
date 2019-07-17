4f64c8205aea5c9d284cc8c017bbc70377582b80
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-17 12:37:55 +0200

    Remplace DB for Doctrine

--- lib/serverfunctions.class.php
+++ lib/serverfunctions.class.php
@@ -6,15 +6,10 @@ class ServerFunctions
     {
         if (abs(getsetting('OnlineCountLast', 0) - strtotime('now')) > 60)
         {
-            $select = DB::select('accounts');
-            $select->columns(['counter' => DB::expression('COUNT(1)')])
-                ->where->equalTo('locked', 0)
-                    ->equalTo('loggedin', 1)
-                    ->greaterThan('laston', date('Y-m-d H:i:s', strtotime('-'.getsetting('LOGINTIMEOUT', 900).' seconds')))
-            ;
-            $result = DB::execute($select)->current();
+            $repository = \Doctrine::getRepository('LotgdCore:Accounts');
+            $counter = $repository->count([ 'locked' => 0, 'loggedin' => 1 ]);
 
-            savesetting('OnlineCount', $result['counter']);
+            savesetting('OnlineCount', $counter);
             savesetting('OnlineCountLast', strtotime('now'));
         }
 
@@ -30,26 +25,30 @@ class ServerFunctions
 
     public static function resetAllDragonkillPoints($acctid = false)
     {
-        $select = DB::select('accounts');
-        $select->columns(['acctid', 'dragonpoints'])
-            ->where->notEqualTo('dragonpoints', ''); //-- Not is necesary all accounts
+        $repository = \Doctrine::getRepository('LotgdCore:Characters');
+        $query = $repository->createQueryBuilder('u');
+
+        $query->where("u.dragonpoints <> ''");
 
         if (is_numeric($acctid))
         {
-            $select->where->equalTo('acctid', $acctid);
+            $query->addWhere('u.acct = :acct')
+                ->setParamater('acct', $acctid)
+            ;
         }
         elseif (is_array($acctid))
         {
-            $select->where->in('acctid', $acctid);
+            $query->addWhere('u.acct IN (:acct)')
+                ->setParamater('acct', $acctid)
+            ;
         }
 
-        $result = DB::execute($select);
-        //this is ugly, but fortunately only needed out of the ordinary
-        while ($row = DB::fetch_assoc($result))
-        {
-            $dkpoints = $row['dragonpoints'];
+        $result = $query->getQuery()->getResult();
 
-            $dkpoints = unserialize(stripslashes($dkpoints));
+        //this is ugly, but fortunately only needed out of the ordinary
+        foreach ($result as $entity)
+        {
+            $dkpoints = $entity->getDragonpoints();
 
             if (empty($dkpoints))
             {
@@ -57,57 +56,21 @@ class ServerFunctions
             } //-- Not do nothing if is an empty array
 
             $distribution = array_count_values($dkpoints);
-            $sets = [];
 
-            foreach ($distribution as $key => $val)
-            {
-                switch ($key)
-                {
-                    case 'str':
-                        $recalc = (int) $val;
-                        $sets['strength'] = DB::expression("strength-$recalc");
-                        break;
-                    case 'con':
-                        $recalc = (int) $val;
-                        $sets['constitution'] = DB::expression("constitution-$recalc");
-                        break;
-                    case 'int':
-                        $recalc = (int) $val;
-                        $sets['intelligence'] = DB::expression("intelligence-$recalc");
-                        break;
-                    case 'wis':
-                        $recalc = (int) $val;
-                        $sets['wisdom'] = DB::expression("wisdom-$recalc");
-                        break;
-                    case 'dex':
-                        $recalc = (int) $val;
-                        $sets['dexterity'] = DB::expression("dexterity-$recalc");
-                        break;
-                    case 'hp':
-                        $recalc = (int) ($val * 5);
-                        $sets['maxhitpoints'] = DB::expression("maxhitpoints-$recalc");
-                        $sets['hitpoints'] = DB::expression("hitpoints-$recalc");
-                        break;
-                    case 'at':
-                        $recalc = (int) $val;
-                        $sets['attack'] = DB::expression("attack-$recalc");
-                        break;
-                    case 'de':
-                        $recalc = (int) $val;
-                        $sets['defense'] = DB::expression("defense-$recalc");
-                        break;
-                }
-            }
+            $entity->setDragonpoints([])
+                ->setStrength($entity->getStrength() - (int) $distribution['str'])
+                ->setConstitution($entity->getConstitution() - (int) $distribution['con'])
+                ->setIntelligence($entity->getIntelligence() - (int) $distribution['int'])
+                ->setWisdom($entity->getWisdom() - (int) $distribution['wis'])
+                ->setDexterity($entity->getDexterity() - (int) $distribution['dex'])
+            ;
 
-            $sets['dragonpoints'] = '';
-            $update = DB::update('accounts');
-            $update->set($sets)
-                ->where->equalTo('acctid', $row['acctid']);
-
-            DB::execute($update);
-
-            //adding a hook, nasty, but you don't call this too often
-            modulehook('dragonpointreset', [$row]);
+            \Doctrine::persist($entity);
         }
+
+        \Doctrine::flush();
+
+        //adding a hook, nasty, but you don't call this too often
+        modulehook('dragonpointreset', [$result]);
     }
 }
