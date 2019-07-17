7a91fcd8a423855cb71ee4d124f458a768d426c5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-17 16:39:45 +0200

    Replace DB for Doctrine

--- lib/expire_chars.php
+++ lib/expire_chars.php
@@ -5,101 +5,135 @@
 // mail ready
 
 require_once 'lib/constants.php';
-
-//-- Settings extended
-$settings_extended = LotgdLocator::get(Lotgd\Core\Lib\SettingsExtended::class);
+require_once 'lib/charcleanup.php';
+require_once 'lib/gamelog.php';
 
 $lastexpire = strtotime(getsetting('last_char_expire', '0000-00-00 00:00:00'));
 $needtoexpire = strtotime('-23 hours');
 
-if ($lastexpire < $needtoexpire)
+if ($lastexpire >= $needtoexpire)
 {
-    savesetting('last_char_expire', date('Y-m-d H:i:s'));
-    $old = getsetting('expireoldacct', 45);
-    $new = getsetting('expirenewacct', 10);
-    $trash = getsetting('expiretrashacct', 1);
-
-    // First, get the account ids to delete the user prefs.
-    $sql1 = 'SELECT login,acctid,dragonkills,level FROM '.DB::prefix('accounts').' WHERE (superuser&'.NO_ACCOUNT_EXPIRATION.")=0 AND (1=0\n".($old > 0 ? 'OR (laston < "'.date('Y-m-d H:i:s', strtotime("-$old days"))."\")\n" : '').($new > 0 ? 'OR (laston < "'.date('Y-m-d H:i:s', strtotime("-$new days"))."\" AND level=1 AND dragonkills=0)\n" : '').($trash > 0 ? 'OR (regdate < date_add(NOW(),interval -'.$trash." day) AND laston < regdate)\n" : '').')';
-    $result1 = DB::query($sql1);
-    $acctids = [];
-    $pinfo = [];
-    $dk0lvl = 0;
-    $dk0ct = 0;
-    $dk1lvl = 0;
-    $dk1ct = 0;
-    $dks = 0;
-
-    while ($row1 = DB::fetch_assoc($result1))
-    {
-        require_once 'lib/charcleanup.php';
-
-        if (! char_cleanup($row1['acctid'], CHAR_DELETE_AUTO))
-        {
-            continue;
-        }
-        array_push($acctids, $row1['acctid']);
-        array_push($pinfo, "{$row1['login']}:dk{$row1['dragonkills']}-lv{$row1['level']}");
-
-        if (0 == $row1['dragonkills'])
-        {
-            $dk0lvl += $row1['level'];
-            $dk0ct++;
-        }
-        elseif (1 == $row1['dragonkills'])
-        {
-            $dk1lvl += $row1['level'];
-            $dk1ct++;
-        }
-        $dks += $row1['dragonkills'];
-    }
-
-    //Log which accounts were deleted.
-    $msg = "[{$dk0ct}] with 0 dk avg lvl [".round($dk0lvl / max(1, $dk0ct), 2)."]\n";
-    $msg .= "[{$dk1ct}] with 1 dk avg lvl [".round($dk1lvl / max(1, $dk1ct), 2)."]\n";
-    $msg .= 'Avg DK: ['.round($dks / max(1, count($acctids)), 2)."]\n";
-    $msg .= 'Accounts: '.join($pinfo, ', ');
-    require_once 'lib/gamelog.php';
-    gamelog('Deleted '.count($acctids)." accounts:\n$msg", 'char expiration');
-
-    // Now delete the accounts themselves
-    // one less search pass, and a guarantee that the same accounts selected
-    // above are the ones deleted here.
-    if (count($acctids))
-    {
-        $sql = 'DELETE FROM '.DB::prefix('accounts').
-            ' WHERE acctid IN ('.join($acctids, ',').')';
-        DB::query($sql);
-    }
-
-    //adjust for notification - don't notify total newbie chars
-    $old = max(1, $old - getsetting('notifydaysbeforedeletion', 5)); //a minimum of 1 day is necessary
-    $sql = 'SELECT login,acctid,emailaddress FROM '.DB::prefix('accounts').' WHERE 1=0 '.($old > 0 ? 'OR (laston < "'.date('Y-m-d H:i:s', strtotime("-$old days"))."\")\n" : '')." AND emailaddress!='' AND sentnotice=0 AND (superuser&".NO_ACCOUNT_EXPIRATION.')=0';
-    $result = DB::query($sql);
-    //we could translate this for each user in its language - but wayy to much ressources. Use your default language instead.
-    $subject = translate_inline($settings_extended->getSetting('expirationnoticesubject'));
-    $message = translate_inline($settings_extended->getSetting('expirationnoticetext'));
-
-    $message = str_replace('{server}', getsetting('serverurl', 'http://nodomain.notd'), $message);
-    /*
-    if you run this via cron, you will get nothing. We will use the setting for lotgdnet, even if not used.
-    $_SERVER['SERVER_NAME'].($_SERVER['SERVER_PORT']==80?"":":".$_SERVER['SERVER_PORT']).$_SERVER['SCRIPT_NAME']." is about to expire.  If you wish to keep this character, you should log on to him or her soon!",
-    */
-
-    $mheader = 'MIME-Version: 1.0'."\r\n";
-    $mheader .= 'Content-type: text/plain; charset='.getsetting('charset', 'UTF-8')."\r\n";
-    $mheader .= 'From: '.getsetting('gameadminemail', 'postmaster@localhost')."\r\n";
-    $collector = [];
-
-    while ($row = DB::fetch_assoc($result))
-    {
-        lotgd_mail($row['emailaddress'], $subject, str_replace('{charname}', $row['login'], $message), $mheader);
-        $collector[] = $row['acctid'];
-    }
-
-    if (! empty($collector))
-    {
-        $sql = 'UPDATE '.DB::prefix('accounts').' SET sentnotice=1 WHERE acctid IN ('.implode(',', $collector).');';
-        DB::query($sql);
-    }
+    return;
 }
+
+savesetting('last_char_expire', date('Y-m-d H:i:s'));
+
+$old = (int) getsetting('expireoldacct', 45);
+$new = (int) getsetting('expirenewacct', 10);
+$trash = (int) getsetting('expiretrashacct', 1);
+
+$repository = \Doctrine::getRepository('LotgdCore:Characters');
+$query = $repository->createQueryBuilder('u');
+$expr = $query->expr();
+
+$dateOld = new \DateTime('now');
+$dateOld->sub(new \DateInterval("P{$old}D"));
+
+$dateNew = new \DateTime('now');
+$dateNew->sub(new \DateInterval("P{$new}D"));
+
+$dateTrash = new \DateTime('now');
+$dateTrash->sub(new \DateInterval("P{$trash}D"));
+
+$query
+    ->where('BIT_AND(a.superuser, :permit) = 0')
+    ->andWhere($expr->orX(
+        '1 = 0',
+        $old ? $expr->lt('a.laston', ':dateOld') : null,
+        $new ? $expr->andX($expr->lt('a.laston', ':dateNew'), $expr->eq('u.level', 1), $expr->eq('u.dragonkills', 0)) : null,
+        $trash ? $expr->andX($expr->lt('a.regdate', ':dateTrash'), $expr->eq('a.laston', 'a.regdate')) : null
+    ))
+    ->leftJoin('LotgdCore:Accounts', 'a', 'with', $expr->eq('a.acctid', 'u.acct'))
+
+    ->setParameter('limit', NO_ACCOUNT_EXPIRATION)
+;
+
+($old) ? $query->setParameter('dateOld', $dateOld) : null;
+($new) ? $query->setParameter('dateNew', $dateNew) : null;
+($trash) ? $query->setParameter('dateTrash', $dateTrash) : null;
+
+$pinfo = [];
+$dk0lvl = 0;
+$dk0ct = 0;
+$dk1lvl = 0;
+$dk1ct = 0;
+$dks = 0;
+
+$result = $query->getQuery()->getResult();
+
+foreach ($result as $entity)
+{
+    //-- Delete account and data related
+    if (! char_cleanup($entity->getAcct()->getAcctid(), CHAR_DELETE_AUTO))
+    {
+        continue;
+    }
+
+    array_push($pinfo, "{$entity->getAcct()->getLogin()}:dk{$entity->getDragonkills()}-lv{$entity->getLevel()}");
+
+    if (0 == $entity->getDragonkills())
+    {
+        $dk0lvl += $entity->getLevel();
+        $dk0ct++;
+    }
+    elseif (1 == $entity->getDragonkills())
+    {
+        $dk1lvl += $entity->getLevel();
+        $dk1ct++;
+    }
+    $dks += $entity->getDragonkills();
+}
+
+//Log which accounts were deleted.
+$msg = "[{$dk0ct}] with 0 dk avg lvl [".round($dk0lvl / max(1, $dk0ct), 2)."]\n";
+$msg .= "[{$dk1ct}] with 1 dk avg lvl [".round($dk1lvl / max(1, $dk1ct), 2)."]\n";
+$msg .= 'Avg DK: ['.round($dks / max(1, count($result)), 2)."]\n";
+$msg .= 'Accounts: '.implode(', ', $pinfo);
+
+gamelog('Deleted '.count($result)." accounts:\n$msg", 'char expiration');
+
+//adjust for notification - don't notify total newbie chars
+$old = max(1, $old - (int) getsetting('notifydaysbeforedeletion', 5)); //a minimum of 1 day is necessary
+
+$repository = \Doctrine::getRepository('LotgdCore:Accounts');
+$query = $repository->createQueryBuilder('a');
+
+$query
+    ->where('BIT_AND(a.superuser, :permit) = 0')
+    ->andWhere($expr->orX(
+            '1 = 0',
+            $old ? $expr->lt('a.laston', ':dateOld') : null
+        ),
+        $expr->andX(
+            $expr->neq('a.emailaddress', ''),
+            $expr->eq('a.sentnotice', 0)
+        )
+    )
+    ->leftJoin('LotgdCore:Characters', 'u', 'with', $expr->eq('a.id', 'u.character'))
+
+    ->setParameter('limit', NO_ACCOUNT_EXPIRATION)
+;
+
+($old) ? $query->setParameter('dateOld', $dateOld) : null;
+
+$result = $query->getQuery()->getResult();
+
+$server = (string) getsetting('serverurl', 'http://nodomain.notd');
+foreach ($result as $entity)
+{
+    $prefs = $entity->getPrefs();
+
+    $subject = \LotgdTranslator::t('expirationnotice.subject', [], 'app-mail', $prefs['language'] ?? null);
+    $message = \LotgdTranslator::t('expirationnotice.body', [
+        'charname' => $entity->getLogin(),
+        'server' => $server
+    ], 'app-mail', $prefs['language'] ?? null);
+
+    lotgd_mail($entity->getEmailaddress(), $subject, $message);
+
+    $entity->setSentnotice(true);
+
+    \Doctrine::persist($entity);
+}
+
+\Doctrine::flush();
