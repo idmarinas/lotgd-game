d87e4c9fea944c5de116cc1831e4926076e1c48a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-07-22 15:46:25 +0200

    Deleted unnecesary nav

--- public/train.php
+++ public/train.php
@@ -14,7 +14,6 @@ require_once 'lib/forestoutcomes.php';
 
 tlschema('train');
 
-addnav('actu', 'train.php');
 // Don't hook on to this text for your standard modules please, use "train" instead.
 // This hook is specifically to allow modules that do other trains to create ambience.
 $result = modulehook('train-text-domain', ['textDomain' => 'page-train', 'textDomainNavigation' => 'navigation-train']);
