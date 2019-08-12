2fa2199e00d6736210df7fc79b2a8cf75d012cd1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-08-12 19:22:50 +0200

    Use new navigation system

--- lib/fightnav.php
+++ lib/fightnav.php
@@ -9,6 +9,9 @@ function fightnav($allowspecial = true, $allowflee = true, $script = false)
 
     tlschema('fightnav');
 
+    //-- Change text domain for navigation
+    \LotgdNavigation::setTextDomain($textDomainNavigation);
+
     if (false === $script)
     {
         $PHP_SELF = LotgdHttp::getServer('PHP_SELF');
@@ -23,42 +26,38 @@ function fightnav($allowspecial = true, $allowflee = true, $script = false)
         $script .= '&';
     }
 
-    $fight = 'Fight';
-    $run = 'Run';
+    $fight = $session['user']['alive'] ? 'nav.fight.live' : 'nav.fight.death';
+    $run = $session['user']['alive'] ? 'nav.run.live' : 'nav.run.death';
 
-    if (! $session['user']['alive'])
-    {
-        $fight = 'F?Torment';
-        $run = 'R?Flee';
-    }
-    modulehook('fightnav-prenav', ['script' => $script]);
-    addnav('Standard Fighting');
-    addnav($fight, $script.'op=fight');
+    modulehook('fightnav-prenav', [ 'script' => $script ]);
+
+    \LotgdNavigation::addHeader('category.standard');
+    \LotgdNavigation::addNav($fight, "{$script}op=fight");
 
     if ($allowflee)
     {
-        addnav($run, $script.'op=run');
+        \LotgdNavigation::addNav($run, "{$script}op=run");
     }
 
     if ($session['user']['superuser'] & SU_DEVELOPER)
     {
-        addnav('Abort', $script);
+        \LotgdNavigation::addNav('nav.abort', $script);
     }
 
     if (getsetting('autofight', 0))
     {
-        addnav('Automatic Fighting');
-        addnav('5?For 5 Rounds', $script.'op=fight&auto=five');
-        addnav('1?For 10 Rounds', $script.'op=fight&auto=ten');
+        \LotgdNavigation::addHeader('category.automatic');
+        \LotgdNavigation::addNav('nav.auto.rounds.5', "{$script}op=fight&auto=five");
+        \LotgdNavigation::addNav('nav.auto.rounds.10', "{$script}op=fight&auto=ten");
         $auto = getsetting('autofightfull', 0);
 
         if ((1 == $auto || (2 == $auto && ! $allowflee)) && 1 == count($newenemies))
         {
-            addnav('U?Until End', $script.'op=fight&auto=full');
+            \LotgdNavigation::addNav('nav.auto.rounds.end', "{$script}op=fight&auto=full");
         }
         elseif (1 == $auto || (2 == $auto && ! $allowflee))
         {
-            addnav('U?Until current enemy dies', $script.'op=fight&auto=full');
+            \LotgdNavigation::addNav('nav.auto.rounds.current', "{$script}op=fight&auto=full");
         }
     }
 
@@ -70,29 +69,38 @@ function fightnav($allowspecial = true, $allowflee = true, $script = false)
 
     if ($allowspecial)
     {
-        addnav('Special Abilities');
+        \LotgdNavigation::addHeader('category.special', [ 'hideEmpty' => false ]);
+
         modulehook('fightnav-specialties', ['script' => $script]);
 
         if ($session['user']['superuser'] & SU_DEVELOPER)
         {
-            addnav('`&Super user`0', '');
-            addnav('!?`&&#149; __GOD MODE`0', "{$script}op=fight&skill=godmode", true);
+            \LotgdNavigation::addHeader('category.superuser');
+            \LotgdNavigation::addNav('nav.god', "{$script}op=fight&skill=godmode");
         }
         modulehook('fightnav', ['script' => $script]);
     }
 
     if (count($newenemies) > 1)
     {
-        addnav('Targets');
+        \LotgdNavigation::addHeader('category.target');
 
         foreach ($newenemies as $index => $badguy)
         {
-            if ($badguy['creaturehealth'] <= 0 || (isset($badguy['dead']) && true == $badguy['dead']))
+            if ($badguy['creaturehealth'] <= 0 || (isset($badguy['dead']) && $badguy['dead']))
             {
                 continue;
             }
-            addnav_notl(['%s%s`0', (isset($badguy['istarget']) && $badguy['istarget']) ? '`#*`0' : '', $badguy['creaturename']], $script."op=fight&newtarget=$index");
+
+            \LotgdNavigation::addNavNotl(
+                ($badguy['istarget'] ?? false) ? '`#*`0' : '' .$badguy['creaturename'],
+                "{$script}op=fight&newtarget={$index}"
+            );
         }
     }
+
+    //-- Restore text domain for navigation
+    \LotgdNavigation::setTextDomain();
+
     tlschema();
 }
