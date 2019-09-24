e09b81c972c12a0d006af0c40aeeb244cc1dc038
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2019-09-24 17:10:49 +0200

    Deleted ora dependencie

--- gulp/tasks/build/lotgd-js.js
+++ gulp/tasks/build/lotgd-js.js
@@ -3,27 +3,22 @@
  */
 //-- Dependencies
 var webpack = require('webpack')
-var ora = require('ora')
 
 //-- Configuration
 var configTasks = require('../../config/tasks')
 var isProduction = configTasks.isProduction()
 
 var webpackConfig = require('../../config/webpack.dev.conf')
-var spinner = ora('building LOTGD JS App for development...')
 
 if (isProduction)
 {
     webpackConfig = require('../../config/webpack.prod.conf')
-    spinner = ora('building LOTGD JS App for production...')
 }
 
 module.exports = function (callback)
 {
-    spinner.start()
     return webpack(webpackConfig, function (err, stats)
     {
-        spinner.stop()
         if (err) throw err
         process.stdout.write(stats.toString({
             colors: true,
