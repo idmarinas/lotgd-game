397df32f70acdcbf3a0b1593dc12f2b9d05d8ff0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-01-16 19:35:39 +0100

    First commit

---
+++ src/core/Form/Element/Tags.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * All files in "Lotgd\Core\Twig\Extension\Form" are based in zend form view classes.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.1.0
+ */
+
+namespace Lotgd\Core\Form\Element;
+
+use Zend\Form\Element;
+
+class Tags extends Element
+{
+
+}
