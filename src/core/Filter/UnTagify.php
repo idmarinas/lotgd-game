14ce91dd810b4b7603c3f351bfc002e432d76eae
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-01-20 17:04:41 +0100

    First commit

---
+++ src/core/Filter/UnTagify.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * All files in "Lotgd\Core\Twig\Extension\Form" are based in zend form view classes.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.1.0
+ */
+
+namespace Lotgd\Core\Filter;
+
+use Zend\Filter\FilterInterface;
+
+/**
+ * Convert format:
+ *      [{"value":"tag_1"},{"value":"tag_2"}]"
+ * into:
+ *      tag_1,tag_2
+ *
+ * Them can use explode function to transform in array.
+ *
+ * Tagify can manage the tags separated by commas.
+ */
+class UnTagify implements FilterInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function filter($value)
+    {
+        return implode(',', array_column(json_decode($value), 'value'));
+    }
+}
