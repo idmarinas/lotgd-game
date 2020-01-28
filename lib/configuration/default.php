87e03a00adeb66ab1690eb5e5dd991dc352ff015
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-01-28 18:59:56 +0100

    Use new form system (preview)

--- lib/configuration/default.php
+++ lib/configuration/default.php
@@ -1,10 +1,44 @@
 <?php
 
-require_once 'lib/configuration/save.php';
+$form = \LotgdLocator::get('Lotgd\Core\Form\Configuration');
+
+if (\LotgdHttp::isPost())
+{
+    $postSettings = \LotgdHttp::getPostAll();
+    $form->setData($postSettings);
+
+    $messageType = 'addErrorMessage';
+    $message = 'flash.message.error';
+    if ($form->isValid())
+    {
+        $messageType = null;
+        $formIsValid = true;
+        $rawData = $form->getData();
+
+        $postSettings = [];
+        //-- Merge all values, avoid duplicate keys in collections
+        foreach($rawData as $key => $value)
+        {
+            if (is_array($value))
+            {
+                $postSettings = $postSettings + $value;
+
+                continue;
+            }
+
+            $postSettings = $postSettings + [$key => $value];
+        }
+
+        require_once 'lib/configuration/save.php';
+    }
+
+    if ($messageType)
+    {
+        \LotgdFlashMessages::{$messageType}(\LotgdTranslator::t($message, [], 'app-form'));
+    }
+}
 
 $details = gametimedetails();
-
-$setup = include 'lib/data/configuration.php';
 
 $secstonewday = secondstonextgameday($details);
 $useful_vals = [
@@ -15,6 +49,44 @@ $useful_vals = [
     'nextnewday' => date('h:i:s a', strtotime("+{$details['realsecstotomorrow']} seconds")).' ('.date('H\\h i\\m s\\s', $secstonewday).')'
 ];
 
-$vals = $settings->getArray() + $useful_vals;
+$form->setData([
+    'daysetup' => $useful_vals,
+]);
 
-$params['form'] = lotgd_showform($setup, $vals, false, false, false);
+//-- Not set default values if is post request
+if(! \LotgdHttp::isPost())
+{
+    $vals = $settings->getArray() + $useful_vals;
+
+    $data = [
+        'game_setup' => $vals,
+        'maintenance' => $vals,
+        'home' => $vals,
+        'beta' => $vals,
+        'account' => $vals,
+        'commentary' => $vals,
+        'places' => $vals,
+        'su_title' => $vals,
+        'referral' => $vals,
+        'events' => $vals,
+        'donation' => $vals,
+        'training' => $vals,
+        'clans' => $vals,
+        'newdays' => $vals,
+        'forest' => $vals,
+        'enemies' => $vals,
+        'companion' => $vals,
+        'bank' => $vals,
+        'mail' => $vals,
+        'pvp' => $vals,
+        'content' => $vals,
+        'logdnet' => $vals,
+        'daysetup' => $vals,
+        'misc' => $vals,
+    ];
+
+    //-- Set values of data base
+    $form->setData($data);
+}
+
+$params['form'] = $form;
