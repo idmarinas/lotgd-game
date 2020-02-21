c74d4207fd04bd9d95d60ed59ec8791b189fee00
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-02-21 17:25:59 +0100

    First commit

---
+++ config/autoload/global/jaxon-lotgd-core.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'jaxon' => [
+        'core' => [
+            'request' => [
+                'uri' => 'jaxon.php'
+            ],
+            'process' => [
+                'exit' => false
+            ]
+        ],
+        'dialogs' => [
+            'default' => [
+                'alert' => 'toastr',
+                'modal' => 'semantic'
+            ],
+            'classes' => [
+                'semantic' => \Lotgd\Core\Jaxon\Library\Semantic\Modal::class,
+            ],
+            'toastr' => [
+                'options' => [
+                    'progressBar' => true,
+                    'newestOnTop' => false,
+                    'closeButton' => true,
+                    'preventDuplicates' => true,
+                    'timeOut' => 30000,
+                    'extendedTimeOut' => 15000
+                ]
+            ]
+        ]
+    ]
+];
