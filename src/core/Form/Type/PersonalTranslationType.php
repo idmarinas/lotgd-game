d1f93f1515c8bacd3807046312381c652af3d448
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-06-11 14:34:35 +0200

    First commit

---
+++ src/core/Form/Type/PersonalTranslationType.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.2.0
+ */
+
+namespace Lotgd\Core\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PersonalTranslationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('locale', HiddenType::class)
+            ->add('field', HiddenType::class)
+            ->add('object', HiddenType::class)
+            ->add('content', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => AbstractPersonalTranslation::class,
+            'translation_domain' => 'form-core-grotto-creature'
+        ]);
+    }
+}
