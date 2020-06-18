5356f02f794d8337b1c6b19b0859dd0808990d4a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-06-18 12:18:59 +0200

    First commit

---
+++ src/core/Form/Type/CreatureAiType.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.2.0
+ */
+
+namespace Lotgd\Core\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class CreatureAiType extends ChoiceType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $files = glob('creatureai/{*/,}*.php', GLOB_BRACE);
+
+        $defaultChoice = ['none' => null];
+        foreach($files as $file)
+        {
+            $defaultChoice[$file] = rtrim($file, '.php');
+        }
+
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'search selection lotgd'
+            ],
+            'choices'  => $defaultChoice,
+        ]);
+
+        return $resolver;
+    }
+}
