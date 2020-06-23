48e6b26cbbce378bd97acbc0a0d98059b9ae1eee
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-06-23 14:28:23 +0200

    First commit

---
+++ src/core/Entity/MastersTranslation.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.2.0
+ */
+
+namespace Lotgd\Core\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+/**
+ * Masters translations.
+ *
+ * @ORM\Table(
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="lookup_unique_idx", columns={"locale", "object_id", "field"})
+ *     }
+ * )
+ * @ORM\Entity
+ */
+class MastersTranslation extends AbstractPersonalTranslation
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="Masters", inversedBy="translations", cascade={"all"})
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="creatureid", onDelete="CASCADE")
+     */
+    protected $object;
+
+    /**
+     * @var string $content
+     *
+     * @ORM\Column(type="text", nullable=true,  options={"collation": "utf8mb4_unicode_ci"})
+     */
+    protected $content;
+
+    /**
+     * Convenient constructor
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+
+    public function __toString()
+    {
+        return (string) $this->getContent();
+    }
+}
