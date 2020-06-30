434952987ba2b4ccd99248597f87fa60970a7a2b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-06-30 14:46:57 +0200

    Added functions for translate companions

--- src/core/EntityRepository/CompanionsRepository.php
+++ src/core/EntityRepository/CompanionsRepository.php
@@ -32,17 +32,19 @@ class CompanionsRepository extends DoctrineRepository
 
         try
         {
-            return $query
+            $query
                 ->where('u.companioncostdks <= :dk')
                 ->andWhere("u.companionlocation = :loc OR u.companionlocation = 'all'")
                 ->andWhere('u.companionactive = 1')
+            ;
 
+            $query = $this->createTranslatebleCompanionQuery($query);
+            $query
                 ->setParameter('dk', $dragonKills)
                 ->setParameter('loc', $location)
-
-                ->getQuery()
-                ->getResult()
             ;
+
+            return $query->getArrayResult();
         }
         catch (\Throwable $th)
         {
@@ -50,5 +52,82 @@ class CompanionsRepository extends DoctrineRepository
 
             return [];
         }
+    }
+
+    /**
+     * Find one by id.
+     * Entity is translated.
+     *
+     * @return array|null
+     */
+    public function findOneCompanionById(int $id)
+    {
+        try
+        {
+            $dql = 'SELECT a
+                FROM LotgdCore:Companion a
+                WHERE a.creatureid = :id
+            ';
+
+            $query = $this->createTranslatebleCompanionQuery($dql);
+            $query->setParameter('id', $id);
+
+            return $query->getArrayResult()[0];
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return null;
+        }
+    }
+
+    /**
+     * Get an array by ids.
+     * Entities is translated.
+     *
+     * @return array|null
+     */
+    public function findCompanionsById(array $ids)
+    {
+        try
+        {
+            $dql = 'SELECT a
+                FROM LotgdCore:Companion a
+                WHERE a.creatureid IN (:id)
+            ';
+
+            $query = $this->createTranslatebleCompanionQuery($dql);
+            $query->setParameter('id', $ids);
+
+            return $query->getArrayResult();
+        }
+        catch (\Throwable $th)
+        {
+            Debugger::log($th);
+
+            return null;
+        }
+    }
+
+    /**
+     * Create query for translate entity.
+     *
+     * @param string $dql
+     * Note: If pass a "Doctrine\ORM\QueryBuilder" auto-get a DQL string.
+     *
+     * @return \Doctrine\ORM\Query
+     */
+    public function createTranslatebleCompanionQuery(string $dql)
+    {
+        $query = $this->_em->createQuery($dql);
+
+        $query->setHint(\Doctrine\ORM\Query::HINT_CUSTOM_OUTPUT_WALKER, 'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker');
+        // take locale from session or request etc.
+        $query->setHint(TranslatableListener::HINT_TRANSLATABLE_LOCALE, \Locale::getDefault());
+        // fallback to default values in case if record is not translated
+        $query->setHint(TranslatableListener::HINT_FALLBACK, 1);
+
+        return $query;
     }
 }
