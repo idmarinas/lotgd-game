cc81cf54732f2e14edc10f9e406b69083cafb9c0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-07-16 15:45:53 +0200

    First commit

---
+++ src/core/Form/Type/CronjobListType.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.3.0
+ */
+
+namespace Lotgd\Core\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CronjobListType extends ChoiceType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $files = glob('cronjob/{*/,}*.php', GLOB_BRACE);
+
+        $defaultChoice = ['none' => null];
+
+        foreach ($files as $file)
+        {
+            $defaultChoice[$file] = rtrim($file, '.php');
+        }
+
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'search selection lotgd'
+            ],
+            'choices' => $defaultChoice,
+        ]);
+
+        return $resolver;
+    }
+}
