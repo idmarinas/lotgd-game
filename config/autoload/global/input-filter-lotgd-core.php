44fc83f49f29a4ce7c4488778bc42256fb096e19
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-07-23 12:15:46 +0200

    Fixed error with name

--- config/autoload/global/input-filter-lotgd-core.php
+++ config/autoload/global/input-filter-lotgd-core.php
@@ -6,6 +6,6 @@ return [
         'Lotgd\Core\Form\Filter\Configuration' => include 'data/form/core/grotto/configuration/filter.php',
         //-- Added in version 4.3.0
         'Lotgd\Core\Form\Filter\Cronjob' => include 'data/form/core/grotto/cronjob/filter.php',
-        'Lotgd\Core\Form\HomeSkin' => include 'data/form/core/grotto/home/filter.php',
+        'Lotgd\Core\Form\Filter\HomeSkin' => include 'data/form/core/grotto/home/filter.php'
     ]
 ];
