4f060779bea49389fb24d587a702d6c9a528df5d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-07-30 17:11:22 +0200

    First commit

---
+++ src/core/Form/DataTransformer/DateTimeTypeTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.3.0
+ */
+
+namespace Lotgd\Core\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+class DateTimeTypeTransformer implements DataTransformerInterface
+{
+    public function transform($data)
+    {
+        $default = new \DateTime('0000-00-00 00:00:00');
+
+        if ($default == $data)
+        {
+            $data = new \DateTime('0000-01-01 00:00:00');
+        }
+
+        return $data;
+    }
+
+    public function reverseTransform($data)
+    {
+        $default = new \DateTime('0000-01-01 00:00:00');
+
+        if ($default == $data)
+        {
+            $data = new \DateTime('0000-00-00 00:00:00');
+        }
+
+        return $data;
+    }
+}
