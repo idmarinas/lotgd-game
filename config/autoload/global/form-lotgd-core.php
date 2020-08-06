4863e016e127759f31fc8713ab0acd19908be8e5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-08-06 13:05:43 +0200

    Added new form

--- config/autoload/global/form-lotgd-core.php
+++ config/autoload/global/form-lotgd-core.php
@@ -11,5 +11,6 @@ return [
         //-- Added in version 4.3.0
         'Lotgd\Core\Form\Cronjob' => include 'data/form/core/grotto/cronjob/input.php',
         'Lotgd\Core\Form\HomeSkin' => include 'data/form/core/grotto/home/input.php',
+        'Lotgd\Core\Form\About' => include 'data/form/core/grotto/about/input.php',
     ]
 ];
