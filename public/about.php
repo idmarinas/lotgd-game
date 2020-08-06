ca7c7811163eb55aaed770a40e7959577fdfe170
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-08-06 13:00:36 +0200

    Use Laminas Form to show info

--- public/about.php
+++ public/about.php
@@ -44,55 +44,6 @@ elseif ('setup' == $op)
     LotgdNavigation::blockLink('about.php?op=setup');
 
     $details = gametimedetails();
-    $setup = [
-        'Game Setup,title',
-        'pvp' => 'Enable Slay Other Players,viewonly',
-        'pvpday' => 'Player Fights per day,viewonly',
-        'pvpimmunity' => 'Days that new players are safe from PvP,viewonly',
-        'pvpminexp' => 'Amount of experience when players become killable in PvP,viewonly',
-        'soap' => 'Clean user posts (filters bad language and splits words over 45 chars long),viewonly',
-        'newplayerstartgold' => 'Amount of gold to start a new character with,viewonly',
-        'New Days,title',
-        'fightsforinterest' => 'Player must have fewer than how many forest fights to earn interest?,viewonly',
-        'maxinterest' => 'Max Interest Rate (%),viewonly',
-        'mininterest' => 'Min Interest Rate (%),viewonly',
-        'daysperday' => 'Game days per calendar day,viewonly',
-        'specialtybonus' => 'Extra daily uses in specialty area,viewonly',
-
-        'Bank settings,title',
-        'borrowperlevel' => 'Max amount player can borrow per level,viewonly',
-        'allowgoldtransfer' => 'Are players allowed to transfer gold,viewonly',
-        'transferperlevel' => 'Max amount player can transfer per level of recipient (if transfers are enabled),viewonly',
-        'mintransferlev' => 'Minimum level a player has to be before they can transfer gold (if transfers are enabled),viewonly',
-        'transferreceive' => 'Total transfers a player can receive in one play day (if transfers are enabled),viewonly',
-        'maxtransferout' => 'Max amount total a player can transfer to others per level (if transfers are enabled),viewonly',
-
-        'Forest,title',
-        'turns' => 'Forest Fights per day,viewonly',
-        'dropmingold' => 'Forest Creatures always drop at least 1/4 of possible gold,viewonly',
-
-        'Mail Settings,title',
-        'mailsizelimit' => 'Message size limit per message,viewonly',
-        'inboxlimit' => 'Limit # of messages in inbox,viewonly',
-        'oldmail' => 'Automatically delete old messages after (days),viewonly',
-
-        'Content Expiration,title',
-        'expirecontent' => 'Days to keep comments and news?  (0 for infinite),viewonly',
-        'expiretrashacct' => 'Days to keep accounts that were never logged in to? (0 for infinite),viewonly',
-        'expirenewacct' => 'Days to keep level 1 accounts with no dragon kills? (0 for infinite),viewonly',
-        'expireoldacct' => 'Days to keep all other accounts? (0 for infinite),viewonly',
-        'LOGINTIMEOUT' => 'Seconds of inactivity before auto-logoff,viewonly'
-    ];
-
-    $useful = [
-        'Useful Information,title',
-        'dayduration' => 'Day Duration,viewonly',
-        'curgametime' => 'Current game time,viewonly',
-        'curservertime' => 'Current Server Time,viewonly',
-        'lastnewday' => 'Last new day,viewonly',
-        'nextnewday' => 'Next new day,viewonly'
-    ];
-
     $secstonextday = secondstonextgameday($details);
     $useful_vals = [
         'dayduration' => round(($details['dayduration'] / 60 / 60), 0).' hours',
@@ -104,12 +55,21 @@ elseif ('setup' == $op)
 
     $localsettings = $settings->getArray();
 
-    $args = ['settings' => [], 'values' => []];
-    $args = modulehook('showsettings', $args);
-    $form = array_merge($setup, $args['settings'], $useful);
-    $vals = array_merge($localsettings, $args['values'], $useful_vals);
+    $vals = array_merge($localsettings, $useful_vals);
 
-    $params['form'] = lotgd_showform($form, $vals, true, false, false);
+    $form = \LotgdLocator::get('Lotgd\Core\Form\About');
+    $data = [
+        'game_setup' => $vals,
+        'newday' => $vals,
+        'bank' => $vals,
+        'forest' => $vals,
+        'mail' => $vals,
+        'content' => $vals,
+        'info' => $vals,
+    ];
+    $form->setData($data);
+
+    $params['form'] = $form;
 
     $params = modulehook('page-about-setup-tpl-params', $params);
     rawoutput(LotgdTheme::renderThemeTemplate('page/about/setup.twig', $params));
