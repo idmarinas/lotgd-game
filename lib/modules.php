84486c78b3102edc29d4be8d05a2b57ec5fbef4b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-08-18 13:22:04 +0200

    Added warning of E_USER_DEPRECATED

--- lib/modules.php
+++ lib/modules.php
@@ -293,6 +293,11 @@ function module_objpref_edit($type, $module, $id)
     }
     elseif (is_array($info["prefs-{$type}"]) && count($info["prefs-{$type}"]) > 0)
     {
+        trigger_error(sprintf(
+            'Usage of %s style is obsolete since 4.3.0; and delete in version 5.0.0, use new "Laminas Form style" to configure prefs',
+            "prefs-{$type}"
+        ), E_USER_DEPRECATED);
+
         $msettings = [];
 
         foreach ($info["prefs-{$type}"] as $key => $val)
