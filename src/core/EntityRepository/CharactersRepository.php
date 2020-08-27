d12ddea8f0a50346b79e8b766633c7d3e8656b0c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-08-27 19:48:14 +0200

    Added field loggedin to results

--- src/core/EntityRepository/CharactersRepository.php
+++ src/core/EntityRepository/CharactersRepository.php
@@ -60,7 +60,7 @@ class CharactersRepository extends DoctrineRepository
         try
         {
             $character = $qb
-                ->select('u.name', 'IDENTITY(u.acct) AS acctid', 'u.level', 'a.login', 'a.superuser')
+                ->select('u.name', 'IDENTITY(u.acct) AS acctid', 'u.level', 'a.login', 'a.superuser', 'a.loggedin')
                 ->leftJoin('LotgdCore:Accounts', 'a', 'with', $qb->expr()->eq('a.character', 'u.id'))
                 ->where('u.name LIKE :name')
                 ->setParameter('name', "%{$name}%")
@@ -70,7 +70,7 @@ class CharactersRepository extends DoctrineRepository
             ;
 
             $account = $query->from('LotgdCore:Accounts', 'u')
-                ->select('c.name', 'IDENTITY(c.acct) AS acctid', 'c.level', 'u.login', 'u.superuser')
+                ->select('c.name', 'IDENTITY(c.acct) AS acctid', 'c.level', 'u.login', 'u.superuser', 'u.loggedin')
                 ->leftJoin('LotgdCore:Characters', 'c', 'with', $qb->expr()->eq('c.id', 'u.character'))
                 ->where('u.login LIKE :name AND u.acctid NOT IN (:acct)')
                 ->setParameter('name', "%{$name}%")
