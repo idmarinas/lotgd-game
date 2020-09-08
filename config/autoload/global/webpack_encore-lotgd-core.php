d40a845156e34e7406e12d9dcb1e3cb6a9cb4405
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-09-08 16:23:24 +0200

    First commit

---
+++ config/autoload/global/webpack_encore-lotgd-core.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'webpack_encore' => [
+        'builds' => [
+            //-- Need for each Encore Config build
+            // 'example' => 'path/to/build/example',
+        ]
+    ],
+];
