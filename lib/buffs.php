fd408ded14cd4329fa090487f3a3aa72a358c369
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-09-19 15:51:38 +0200

    Added new Event System

--- lib/buffs.php
+++ lib/buffs.php
@@ -5,28 +5,28 @@
 // mail ready
 
 $buffreplacements = [];
-$debuggedbuffs = [];
+$debuggedbuffs    = [];
 function calculate_buff_fields()
 {
     global $session, $badguy, $buffreplacements, $debuggedbuffs;
 
-    if (! isset($session['bufflist']) || ! $session['bufflist'])
+    if ( ! isset($session['bufflist']) || ! $session['bufflist'])
     {
         return;
     }
 
     //run temp stats
-    reset($session['bufflist']);
+    \reset($session['bufflist']);
 
     foreach ($session['bufflist'] as $buffname => $buff)
     {
-        if (! isset($buff['tempstats_calculated']))
+        if ( ! isset($buff['tempstats_calculated']))
         {
             foreach ($buff as $property => $value)
             {
-                if ('tempstat-' == substr($property, 0, 9))
+                if ('tempstat-' == \substr($property, 0, 9))
                 {
-                    apply_temp_stat(substr($property, 9), $value);
+                    apply_temp_stat(\substr($property, 9), $value);
                 }
             }//end foreach
             $session['bufflist'][$buffname]['tempstats_calculated'] = true;
@@ -34,60 +34,60 @@ function calculate_buff_fields()
     }//end foreach
 
     //process calculated buff fields.
-    reset($session['bufflist']);
+    \reset($session['bufflist']);
 
-    if (! is_array($buffreplacements))
+    if ( ! \is_array($buffreplacements))
     {
         $buffreplacements = [];
     }
 
     foreach ($session['bufflist'] as $buffname => $buff)
     {
-        if (! isset($buff['fields_calculated']))
+        if ( ! isset($buff['fields_calculated']))
         {
             foreach ($buff as $property => $value)
             {
                 //calculate dynamic buff fields
                 $origstring = $value;
                 //Simple <module|variable> replacements for get_module_pref('variable','module')
-                $value = preg_replace('/<([A-Za-z0-9]+)\\|([A-Za-z0-9]+)>/', "get_module_pref('\\2','\\1')", $value);
+                $value = \preg_replace('/<([A-Za-z0-9]+)\\|([A-Za-z0-9]+)>/', "get_module_pref('\\2','\\1')", $value);
                 //simple <variable> replacements for $session['user']['variable']
-                $value = preg_replace('/<([A-Za-z0-9]+)>/', "\$session['user']['\\1']", $value);
+                $value = \preg_replace('/<([A-Za-z0-9]+)>/', "\$session['user']['\\1']", $value);
 
-                if (! defined('OLDSU'))
+                if ( ! \defined('OLDSU'))
                 {
-                    define('OLDSU', $session['user']['superuser']);
+                    \define('OLDSU', $session['user']['superuser']);
                 }
 
                 if ($value != $origstring)
                 {
-                    if ('debug:' == strtolower(substr($value, 0, 6)))
+                    if ('debug:' == \strtolower(\substr($value, 0, 6)))
                     {
-                        $errors = '';
-                        $origstring = substr($origstring, 6);
-                        $value = substr($value, 6);
+                        $errors     = '';
+                        $origstring = \substr($origstring, 6);
+                        $value      = \substr($value, 6);
 
-                        if (! isset($debuggedbuffs[$buffname]))
+                        if ( ! isset($debuggedbuffs[$buffname]))
                         {
                             $debuggedbuffs[$buffname] = [];
                         }
 
-                        ob_start();
-                        $val = eval("return $value;");
-                        $errors = ob_get_contents();
-                        ob_end_clean();
+                        \ob_start();
+                        $val    = eval("return {$value};");
+                        $errors = \ob_get_contents();
+                        \ob_end_clean();
 
-                        if (! isset($debuggedbuffs[$buffname][$property]))
+                        if ( ! isset($debuggedbuffs[$buffname][$property]))
                         {
                             if ('' == $errors)
                             {
-                                debug("Buffs[$buffname][$property] evaluates successfully to $val");
+                                debug("Buffs[{$buffname}][{$property}] evaluates successfully to {$val}");
                             }
                             else
                             {
-                                debug("Buffs[$buffname][$property] has an evaluation error<br>"
-                                .htmlentities($origstring, ENT_COMPAT, getsetting('charset', 'UTF-8')).' becomes <br>'
-                                .htmlentities($value, ENT_COMPAT, getsetting('charset', 'UTF-8')).'<br>'
+                                debug("Buffs[{$buffname}][{$property}] has an evaluation error<br>"
+                                .\htmlentities($origstring, ENT_COMPAT, getsetting('charset', 'UTF-8')).' becomes <br>'
+                                .\htmlentities($value, ENT_COMPAT, getsetting('charset', 'UTF-8')).'<br>'
                                 .$errors);
                                 $val = '';
                             }
@@ -95,7 +95,7 @@ function calculate_buff_fields()
                         }
 
                         $origstring = 'debug:'.$origstring;
-                        $value = 'debug'.$value;
+                        $value      = 'debug'.$value;
                     }
                     else
                     {
@@ -109,7 +109,7 @@ function calculate_buff_fields()
 
                 $session['user']['superuser'] = OLDSU;
 
-                if (is_numeric($val) && (is_nan($val) || is_infinite($val)))
+                if (\is_numeric($val) && (\is_nan($val) || \is_infinite($val)))
                 {
                     $val = $value;
                 }
@@ -118,7 +118,7 @@ function calculate_buff_fields()
 
                 if ('' == $output && (string) $val != (string) $origstring)
                 {
-                    $buffreplacements[$buffname][$property] = $origstring;
+                    $buffreplacements[$buffname][$property]    = $origstring;
                     $session['bufflist'][$buffname][$property] = $val;
                 }//end if
                 unset($val);
@@ -132,13 +132,13 @@ function restore_buff_fields()
 {
     global $session, $buffreplacements;
 
-    if (is_array($buffreplacements))
+    if (\is_array($buffreplacements))
     {
-        reset($buffreplacements);
+        \reset($buffreplacements);
 
         foreach ($buffreplacements as $buffname => $val)
         {
-            reset($val);
+            \reset($val);
 
             foreach ($val as $property => $value)
             {
@@ -153,23 +153,23 @@ function restore_buff_fields()
     }//end if
 
     //restore temp stats
-    if (! isset($session['bufflist']) || ! is_array($session['bufflist']))
+    if ( ! isset($session['bufflist']) || ! \is_array($session['bufflist']))
     {
         $session['bufflist'] = [];
     }
-    reset($session['bufflist']);
+    \reset($session['bufflist']);
 
     foreach ($session['bufflist'] as $buffname => $buff)
     {
-        if (array_key_exists('tempstats_calculated', $buff) && $buff['tempstats_calculated'])
+        if (\array_key_exists('tempstats_calculated', $buff) && $buff['tempstats_calculated'])
         {
-            reset($buff);
+            \reset($buff);
 
             foreach ($buff as $property => $value)
             {
-                if ('tempstat-' == substr($property, 0, 9))
+                if ('tempstat-' == \substr($property, 0, 9))
                 {
-                    apply_temp_stat(substr($property, 9), -$value);
+                    apply_temp_stat(\substr($property, 9), -$value);
                 }
             }//end foreach
             unset($session['bufflist'][$buffname]['tempstats_calculated']);
@@ -181,7 +181,7 @@ function apply_buff($name, $buff)
 {
     global $session,$buffreplacements, $translation_namespace;
 
-    if (! isset($buff['schema']) || '' == $buff['schema'])
+    if ( ! isset($buff['schema']) || '' == $buff['schema'])
     {
         $buff['schema'] = $translation_namespace;
     }
@@ -197,7 +197,11 @@ function apply_buff($name, $buff)
         //it's already set.
         restore_buff_fields();
     }
-    $buff = modulehook('modify-buff', ['name' => $name, 'buff' => $buff]);
+
+    $buff = \LotgdEvent::prepareArgs(['name' => $name, 'buff' => $buff]);
+    \LotgdEvent::trigger(\Lotgd\Core\Event::EVENT_EVERY_HEADER, null, $buff);
+    $buff = modulehook('modify-buff', $buff);
+
     $session['bufflist'][$name] = $buff['buff'];
     calculate_buff_fields();
 }
@@ -207,39 +211,37 @@ function apply_companion($name, $companion, $ignorelimit = false)
     global $session, $companions;
 
     $companionsallowed = getsetting('companionsallowed', 1);
-    $args = modulehook('companionsallowed', ['maxallowed' => $companionsallowed]);
-    $companionsallowed = $args['maxallowed'];
-    $current = 0;
+    $args              = \LotgdEvent::prepareArgs(['maxallowed' => $companionsallowed]);
+    \LotgdEvent::trigger(\Lotgd\Core\Event::EVENT_EVERY_HEADER, null, $args);
+    $args = modulehook('companionsallowed', $args);
 
-    if (! $ignorelimit)
+    $companionsallowed = $args['maxallowed'];
+    $current           = 0;
+
+    if ( ! $ignorelimit)
     {
         foreach ($companions as $thisname => $thiscompanion)
         {
-            if (isset($thiscompanion['ignorelimit']) && true == $thiscompanion['ignorelimit'])
-            {
-            }
-            else
-            {
-                if ($thisname != $name)
-                {
-                    $current++;
-                }
+            if (( ! isset($thiscompanion['ignorelimit']) || ! $thiscompanion['ignorelimit'])
+                && ($thisname != $name)
+            ) {
+                ++$current;
             }
         }
     }
 
-    if ($current < $companionsallowed || true == $ignorelimit)
+    if ($current < $companionsallowed || $ignorelimit)
     {
         if (isset($companions[$name]))
         {
             unset($companions[$name]);
         }
 
-        if (! isset($companion['ignorelimit']) && true == $ignorelimit)
+        if ( ! isset($companion['ignorelimit']) && $ignorelimit)
         {
             $companion['ignorelimit'] = true;
         }
-        $companions[$name] = $companion;
+        $companions[$name]             = $companion;
         $session['user']['companions'] = $companions;
 
         return true; // success!
