01b5d0723070524fa033ab6f910edf06f6c69046
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-09-19 15:46:00 +0200

    First commit

---
+++ src/core/Pattern/EventManager.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.4.0
+ */
+
+namespace Lotgd\Core\Pattern;
+
+use Lotgd\Core\EventManager\EventManager as CoreEventManager;
+
+trait EventManager
+{
+    protected $lotgdEventManager;
+
+    /**
+     * Get EventManager instance.
+     */
+    public function getEventManager(): CoreEventManager
+    {
+        if ( ! $this->lotgdEventManager instanceof CoreEventManager)
+        {
+            $this->lotgdEventManager = $this->getContainer(CoreEventManager::class);
+        }
+
+        return $this->lotgdEventManager;
+    }
+}
