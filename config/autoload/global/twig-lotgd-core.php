f890dc96115b8f3f4433184e48f4b33e55fc3f51
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-10-06 14:40:27 +0200

    Added new extension

--- config/autoload/global/twig-lotgd-core.php
+++ config/autoload/global/twig-lotgd-core.php
@@ -16,32 +16,36 @@ return [
         Lotgd\Core\Twig\Extension\Commentary::class,
 
         //-- Added in version 4.1.0
-        Lotgd\Core\Twig\Extension\Form\Form::class,
-        Lotgd\Core\Twig\Extension\Form\FormAction::class,
-        Lotgd\Core\Twig\Extension\Form\FormBitField::class,
-        Lotgd\Core\Twig\Extension\Form\FormButton::class,
-        Lotgd\Core\Twig\Extension\Form\FormCheckbox::class,
-        Lotgd\Core\Twig\Extension\Form\FormCollection::class,
-        Lotgd\Core\Twig\Extension\Form\FormElement::class,
-        Lotgd\Core\Twig\Extension\Form\FormElementError::class,
-        Lotgd\Core\Twig\Extension\Form\FormEmail::class,
-        Lotgd\Core\Twig\Extension\Form\FormHidden::class,
-        Lotgd\Core\Twig\Extension\Form\FormInput::class,
-        Lotgd\Core\Twig\Extension\Form\FormLabel::class,
-        Lotgd\Core\Twig\Extension\Form\FormNote::class,
-        Lotgd\Core\Twig\Extension\Form\FormNumber::class,
-        Lotgd\Core\Twig\Extension\Form\FormRange::class,
-        Lotgd\Core\Twig\Extension\Form\FormRow::class,
-        Lotgd\Core\Twig\Extension\Form\FormSelect::class,
-        Lotgd\Core\Twig\Extension\Form\FormTagify::class,
-        Lotgd\Core\Twig\Extension\Form\FormText::class,
-        Lotgd\Core\Twig\Extension\Form\FormTextarea::class,
+        // Allows to override/remove this extensions.
+        Lotgd\Core\Twig\Extension\Form\Form::class=> Lotgd\Core\Twig\Extension\Form\Form::class,
+        Lotgd\Core\Twig\Extension\Form\FormAction::class=> Lotgd\Core\Twig\Extension\Form\FormAction::class,
+        Lotgd\Core\Twig\Extension\Form\FormBitField::class=> Lotgd\Core\Twig\Extension\Form\FormBitField::class,
+        Lotgd\Core\Twig\Extension\Form\FormButton::class=> Lotgd\Core\Twig\Extension\Form\FormButton::class,
+        Lotgd\Core\Twig\Extension\Form\FormCheckbox::class=> Lotgd\Core\Twig\Extension\Form\FormCheckbox::class,
+        Lotgd\Core\Twig\Extension\Form\FormCollection::class=> Lotgd\Core\Twig\Extension\Form\FormCollection::class,
+        Lotgd\Core\Twig\Extension\Form\FormElement::class=> Lotgd\Core\Twig\Extension\Form\FormElement::class,
+        Lotgd\Core\Twig\Extension\Form\FormElementError::class=> Lotgd\Core\Twig\Extension\Form\FormElementError::class,
+        Lotgd\Core\Twig\Extension\Form\FormEmail::class=> Lotgd\Core\Twig\Extension\Form\FormEmail::class,
+        Lotgd\Core\Twig\Extension\Form\FormHidden::class=> Lotgd\Core\Twig\Extension\Form\FormHidden::class,
+        Lotgd\Core\Twig\Extension\Form\FormInput::class=> Lotgd\Core\Twig\Extension\Form\FormInput::class,
+        Lotgd\Core\Twig\Extension\Form\FormLabel::class=> Lotgd\Core\Twig\Extension\Form\FormLabel::class,
+        Lotgd\Core\Twig\Extension\Form\FormNote::class=> Lotgd\Core\Twig\Extension\Form\FormNote::class,
+        Lotgd\Core\Twig\Extension\Form\FormNumber::class=> Lotgd\Core\Twig\Extension\Form\FormNumber::class,
+        Lotgd\Core\Twig\Extension\Form\FormRange::class=> Lotgd\Core\Twig\Extension\Form\FormRange::class,
+        Lotgd\Core\Twig\Extension\Form\FormRow::class=> Lotgd\Core\Twig\Extension\Form\FormRow::class,
+        Lotgd\Core\Twig\Extension\Form\FormSelect::class=> Lotgd\Core\Twig\Extension\Form\FormSelect::class,
+        Lotgd\Core\Twig\Extension\Form\FormTagify::class=> Lotgd\Core\Twig\Extension\Form\FormTagify::class,
+        Lotgd\Core\Twig\Extension\Form\FormText::class=> Lotgd\Core\Twig\Extension\Form\FormText::class,
+        Lotgd\Core\Twig\Extension\Form\FormTextarea::class=> Lotgd\Core\Twig\Extension\Form\FormTextarea::class,
 
         //-- Added in version 4.2.0
         Symfony\Bridge\Twig\Extension\FormExtension::class,
 
         //-- Added in version 4.3.0
         Lotgd\Core\Twig\Extension\Form\FormViewOnly::class,
+
+        //-- Added in version 4.5.0
+        Lotgd\Core\Twig\Extension\Head::class,
 
         //-- Extension of a third party
         Marek\Twig\ByteUnitsExtension::class,
