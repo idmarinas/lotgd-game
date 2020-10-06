8c523c119274f0eeca8aa727e851c4df586bc8ac
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-10-06 14:37:24 +0200

    First commit

---
+++ src/core/Factory/View/Helper/HeadMeta.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Factory\View\Helper;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Laminas\View\Helper\HeadMeta as HelperHeadMeta;
+use Laminas\View\Renderer\PhpRenderer;
+
+class HeadMeta implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
+    {
+        $helper = new HelperHeadMeta();
+        $helper->setView(new PhpRenderer());
+
+        return $helper;
+    }
+}
