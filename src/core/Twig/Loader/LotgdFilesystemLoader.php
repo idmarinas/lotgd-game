5ccb398d0c299f1c549fab9984519e338da56ae3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-10-15 14:32:19 +0200

    First commit

---
+++ src/core/Twig/Loader/LotgdFilesystemLoader.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.5.0
+ */
+
+namespace Lotgd\Core\Twig\Loader;
+
+use Twig\Loader\FilesystemLoader;
+
+class LotgdFilesystemLoader extends FilesystemLoader
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return string|null
+     */
+    protected function findTemplate($template, $throw = true)
+    {
+        $tpl = parent::findTemplate($template, false);
+
+        //-- Fallback theme namespace to default namespace
+        if (!$tpl && false !== strpos($template, '@theme'))
+        {
+            $pos = strpos($template, '/');
+            $newTemplate = substr($template, $pos + 1);
+
+            $tpl = parent::findTemplate($newTemplate, $throw);
+
+            if ($tpl)
+            {
+                $this->cache[$template] = $tpl; //-- Cache original them namespace
+            }
+        }
+
+        return $tpl;
+    }
+}
