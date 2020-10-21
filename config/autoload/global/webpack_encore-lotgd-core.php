0bd065ff7dcdec5682f6ac0be7e55442f55953ba
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-10-21 11:49:47 +0200

    Now can delete original package, if not use it

--- config/autoload/global/webpack_encore-lotgd-core.php
+++ config/autoload/global/webpack_encore-lotgd-core.php
@@ -3,6 +3,7 @@
 return [
     'webpack_encore' => [
         'builds' => [
+            'lotgd' => 'public/build/lotgd' //-- This is the LOTGD Core package
             //-- Need for each Encore Config build
             // 'example' => 'path/to/build/example',
         ]
