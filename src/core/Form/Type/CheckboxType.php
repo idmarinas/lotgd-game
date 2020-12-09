d36cd16d5e2e22572286c676983f0821afc8ba31
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-09 22:08:49 +0100

    Added modification for view

--- src/core/Form/Type/CheckboxType.php
+++ src/core/Form/Type/CheckboxType.php
@@ -16,11 +16,35 @@ namespace Lotgd\Core\Form\Type;
 use Lotgd\Core\Form\DataTransformer\BooleanTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType as TypeCheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CheckboxType extends TypeCheckboxType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->addModelTransformer(new BooleanTransformer());
+        $builder->addViewTransformer(new BooleanTransformer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = \array_replace($view->vars, [
+            'value'   => $options['value'],
+            'checked' => (bool) $form->getViewData(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+        $resolver->setDefault('false_values', [null, '0', 0, 'false']);
     }
 }
