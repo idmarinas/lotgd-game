faf6dc92ebf70e003af45e1eb0b35da3606ab148
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-09 19:20:32 +0100

    First commit

---
+++ src/core/Form/DataTransformer/UnTagifyTransformer.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.8.0
+ */
+
+namespace Lotgd\Core\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * Convert format:
+ *      [{"value":"tag_1"},{"value":"tag_2"}]"
+ * into:
+ *      tag_1,tag_2
+ *
+ * Them can use explode function to transform in array.
+ *
+ * Tagify can manage the tags separated by commas.
+ */
+class UnTagifyTransformer implements DataTransformerInterface
+{
+
+    public function reverseTransform($value)
+    {
+        if ($value[0] == '[')
+        {
+            return implode(',', array_column(json_decode($value), 'value'));
+        }
+
+        return $value;
+    }
+
+    public function transform($value)
+    {
+        if ($value[0] == '[')
+        {
+            return implode(',', array_column(json_decode($value), 'value'));
+        }
+
+        return $value;
+    }
+}
---
+++ src/core/Form/Type/TagifyType.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.8.0
+ */
+
+namespace Lotgd\Core\Form\Type;
+
+use Lotgd\Core\Form\DataTransformer\UnTagifyTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class TagifyType extends TextType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addModelTransformer(new UnTagifyTransformer());
+        $builder->addViewTransformer(new UnTagifyTransformer());
+
+        parent::buildForm($builder, $options);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        $options['attr']['class'] = ($options['attr']['class'] ?? '').' tags-select';
+
+        $view->vars['attr'] = $options['attr'];
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'tagify';
+    }
+
+    public function getName()
+    {
+        return 'tagify_field';
+    }
+}
