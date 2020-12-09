54eed8de7f2bb82d3bd538642ba5a83c013480cd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-09 22:27:21 +0100

    First commit

---
+++ src/core/Form/Type/LotgdThemeType.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.8.0
+ */
+
+namespace Lotgd\Core\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LotgdThemeType extends ChoiceType
+{
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $vname = getsetting('villagename', LOCATION_FIELDS);
+        $locs  = [
+            $vname => [
+                'location.village.of',
+                ['name' => $vname],
+                'app-default',
+            ],
+        ];
+        \LotgdHook::trigger(\Lotgd\Core\Hook::HOOK_OTHER_LOCATIONS, null, $locs);
+        $locs        = modulehook('camplocs', $locs);
+        $locs['all'] = [
+            'location.everywhere',
+            [],
+            'app-default',
+        ];
+        \ksort($locs);
+
+        $defaultChoice = [];
+
+        foreach ($locs as $loc => $params)
+        {
+            $value = \LotgdTranslator::t($params[0], $params[1], $params[2]);
+
+            $defaultChoice[$value] = $loc;
+        }
+
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'search selection lotgd',
+            ],
+            'choices' => $this->getThemeList(),
+        ]);
+
+        return $resolver;
+    }
+
+    /**
+     * Get list array.
+     */
+    private function getThemeList(): array
+    {
+        $cacheKey = 'lotgd-core-pattern-theme-list';
+
+        $skins = \LotgdCache::getItem($cacheKey);
+
+        if ( ! \is_array($skins))
+        {
+            // A generic way of allowing a theme to be selected.
+            $handle = @\opendir('themes');
+
+            // Template directory open failed
+            if ( ! $handle)
+            {
+                return [];
+            }
+
+            $skins = [];
+
+            while (false !== ($file = @\readdir($handle)))
+            {
+                if ('html' == \pathinfo($file, PATHINFO_EXTENSION))
+                {
+                    $value = \str_replace(['-', '_'], ' ', \ucfirst(\substr($file, 0, \strpos($file, '.htm'))));
+                    $skins[$value] = $file;
+                }
+            }
+
+            \LotgdCache::setItem($cacheKey, $skins);
+        }
+
+        return $skins;
+    }
+}
