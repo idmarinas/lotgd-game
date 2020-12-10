e0cbcf2cfe9dbb55c27bcdc5c4815acc90cc3662
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-10 23:51:58 +0100

    Deleted Configuration form

--- config/autoload/global/form-lotgd-core.php
+++ config/autoload/global/form-lotgd-core.php
@@ -6,8 +6,6 @@
 
 return [
     'forms' => [
-        //-- Added in version 4.1.0
-        'Lotgd\Core\Form\Configuration' => include 'data/form/core/grotto/configuration/input.php',
         //-- Added in version 4.3.0
         'Lotgd\Core\Form\Cronjob'  => include 'data/form/core/grotto/cronjob/input.php',
         'Lotgd\Core\Form\HomeSkin' => include 'data/form/core/grotto/home/input.php',
--- config/autoload/global/input-filter-lotgd-core.php
+++ config/autoload/global/input-filter-lotgd-core.php
@@ -2,8 +2,6 @@
 
 return [
     'input_filter_specs' => [
-        //-- Added in version 4.1.0
-        'Lotgd\Core\Form\Filter\Configuration' => include 'data/form/core/grotto/configuration/filter.php',
         //-- Added in version 4.3.0
         'Lotgd\Core\Form\Filter\Cronjob'  => include 'data/form/core/grotto/cronjob/filter.php',
         'Lotgd\Core\Form\Filter\HomeSkin' => include 'data/form/core/grotto/home/filter.php',
