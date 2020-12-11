55c32a16ff91c9c41a03b35f25682cebea298f90
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-11 14:15:53 +0100

    First commi

---
+++ src/core/Form/CronjobType.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.8.0
+ */
+
+namespace Lotgd\Core\Form;
+
+use Lotgd\Core\Form\Type\CheckboxType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CronjobType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('newdaycron', CheckboxType::class, [
+            'required' => false,
+            'label'    => 'newdaycron',
+        ]);
+        $builder->add('submit', SubmitType::class, [
+            'label' => 'button.save',
+            'translation_domain' => 'app-form'
+        ]);
+
+        parent::buildForm($builder, $options);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class'         => null,
+            'label'              => 'form.label',
+            'translation_domain' => 'form-core-grotto-cronjob',
+        ]);
+    }
+}
