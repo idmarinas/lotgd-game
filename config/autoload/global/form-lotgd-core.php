c128b6db2c1e53b58c471b6a66f17ec13a5dc09c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-15 11:01:48 +0100

    Deleted forms of Laminas

--- config/autoload/global/form-lotgd-core.php
+++ config/autoload/global/form-lotgd-core.php
@@ -6,10 +6,5 @@
 
 return [
     'forms' => [
-        //-- Added in version 4.3.0
-        'Lotgd\Core\Form\Cronjob'  => include 'data/form/core/grotto/cronjob/input.php',
-        'Lotgd\Core\Form\HomeSkin' => include 'data/form/core/grotto/home/input.php',
-        'Lotgd\Core\Form\About'    => include 'data/form/core/grotto/about/input.php',
-        'Lotgd\Core\Form\Petition' => include 'data/form/core/petition/input.php',
     ],
 ];
--- config/autoload/global/input-filter-lotgd-core.php
+++ config/autoload/global/input-filter-lotgd-core.php
@@ -2,9 +2,5 @@
 
 return [
     'input_filter_specs' => [
-        //-- Added in version 4.3.0
-        'Lotgd\Core\Form\Filter\Cronjob'  => include 'data/form/core/grotto/cronjob/filter.php',
-        'Lotgd\Core\Form\Filter\HomeSkin' => include 'data/form/core/grotto/home/filter.php',
-        'Lotgd\Core\Form\Filter\Petition' => include 'data/form/core/petition/filter.php',
     ],
 ];
