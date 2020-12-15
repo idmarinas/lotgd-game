10599dc817fb40fc76a37ea263ed2daa8f90066c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-15 13:52:25 +0100

    Divided shared code

---
+++ public/common_common.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * For no repeat same parts in common and common_jaxon
+ */
+\chdir(\realpath(__DIR__.'/..'));
+
+require_once 'vendor/autoload.php'; //-- Autoload class for new options of game
+
+//-- Include constants
+require_once 'lib/constants.php';
+
+$isDevelopment = \file_exists('config/development.config.php');
+//-- Init Debugger
+$debuggerMode = $isDevelopment ? \Tracy\Debugger::DEVELOPMENT : \Tracy\Debugger::PRODUCTION;
+\Tracy\Debugger::enable($debuggerMode, __DIR__.'/../storage/log/tracy');
+\Tracy\Debugger::timer('page-generating');
+\Tracy\Debugger::timer('page-footer');
+\Tracy\Debugger::$maxDepth = 5; // default: 3
+//-- Extensions for Tracy
+if ($isDevelopment)
+{
+    \Tracy\Debugger::getBar()->addPanel(new \Milo\VendorVersions\Panel());
+}
+
+//-- Autoload annotations
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(
+    function ($className)
+    {
+        return \class_exists($className);
+    }
+);
+
+// Set some constant defaults in case they weren't set before the inclusion of
+// common.php
+\defined('OVERRIDE_FORCED_NAV') || \define('OVERRIDE_FORCED_NAV', false);
+\defined('ALLOW_ANONYMOUS')     || \define('ALLOW_ANONYMOUS', false);
+
+use Lotgd\Core\Fixed\Locator as LotgdLocator;
+use Lotgd\Core\Fixed\Session as LotgdSession;
+
+//-- Prepare service manager
+LotgdLocator::setServiceManager(new \Lotgd\Core\ServiceManager());
+
+//-- Configure Session
+LotgdSession::instance(LotgdLocator::get(\Lotgd\Core\Session::class));
+
+//-- Init session
+try
+{
+    LotgdSession::bootstrapSession();
+}
+catch (\Throwable $th)
+{
+    LotgdSession::bootstrapSession(true);
+}
+
+$session = &$_SESSION['session'];
+
+$session['user']['gentime']      = $session['user']['gentime']      ?? 0;
+$session['user']['gentimecount'] = $session['user']['gentimecount'] ?? 0;
+$session['user']['gensize']      = $session['user']['gensize']      ?? 0;
+$session['user']['acctid']       = $session['user']['acctid']       ?? 0;
+$session['user']['restorepage']  = $session['user']['restorepage']  ?? '';
+$session['counter']              = ($session['counter'] ?? 0) + 1;
+
+$y2 = "\xc0\x3e\xfe\xb3\x4\x74\x9a\x7c\x17";
+$z2 = "\xa3\x51\x8e\xca\x76\x1d\xfd\x14\x63";
--- public/common_jaxon.php
+++ public/common_jaxon.php
@@ -14,69 +14,7 @@
 // license must be used on the distribution of any works derived from this
 // work.
 // Please see the file LICENSE for a full textual description of the license.txt.
-\chdir(\realpath(__DIR__.'/..'));
-
-require_once 'vendor/autoload.php'; //-- Autoload class for new options of game
-
-//-- Include constants
-require_once 'lib/constants.php';
-
-$isDevelopment = \file_exists('config/development.config.php');
-//-- Init Debugger
-$debuggerMode = $isDevelopment ? \Tracy\Debugger::DEVELOPMENT : \Tracy\Debugger::PRODUCTION;
-\Tracy\Debugger::enable($debuggerMode, __DIR__.'/../storage/log/tracy');
-\Tracy\Debugger::timer('page-generating');
-\Tracy\Debugger::timer('page-footer');
-\Tracy\Debugger::$maxDepth = 5; // default: 3
-//-- Extensions for Tracy
-if ($isDevelopment)
-{
-    \Tracy\Debugger::getBar()->addPanel(new \Milo\VendorVersions\Panel());
-}
-
-//-- Autoload annotations
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(
-    function ($className)
-    {
-        return \class_exists($className);
-    }
-);
-
-// Set some constant defaults in case they weren't set before the inclusion of
-// common.php
-\defined('OVERRIDE_FORCED_NAV') || \define('OVERRIDE_FORCED_NAV', false);
-\defined('ALLOW_ANONYMOUS')     || \define('ALLOW_ANONYMOUS', false);
-
-use Lotgd\Core\Fixed\Locator as LotgdLocator;
-use Lotgd\Core\Fixed\Session as LotgdSession;
-
-//-- Prepare service manager
-LotgdLocator::setServiceManager(new \Lotgd\Core\ServiceManager());
-
-//-- Configure Session
-LotgdSession::instance(LotgdLocator::get(\Lotgd\Core\Session::class));
-
-//-- Init session
-try
-{
-    LotgdSession::bootstrapSession();
-}
-catch (\Throwable $th)
-{
-    LotgdSession::bootstrapSession(true);
-}
-
-$session = &$_SESSION['session'];
-
-$session['user']['gentime']      = $session['user']['gentime']      ?? 0;
-$session['user']['gentimecount'] = $session['user']['gentimecount'] ?? 0;
-$session['user']['gensize']      = $session['user']['gensize']      ?? 0;
-$session['user']['acctid']       = $session['user']['acctid']       ?? 0;
-$session['user']['restorepage']  = $session['user']['restorepage']  ?? '';
-$session['counter']              = ($session['counter'] ?? 0) + 1;
-
-$y2 = "\xc0\x3e\xfe\xb3\x4\x74\x9a\x7c\x17";
-$z2 = "\xa3\x51\x8e\xca\x76\x1d\xfd\x14\x63";
+require_once './common_common.php';
 
 //-- Prepare static classes
 require_once 'lib/class/static.php';
@@ -100,9 +38,7 @@ require_once 'lib/forcednavigation.php';
 require_once 'lib/mounts.php';
 require_once 'lib/lotgd_mail.php';
 require_once 'lib/playerfunctions.php';
-// require_once 'lib/pageparts.php'; //-- In a Jaxon-PHP request you should not use the functions of this file, since they are intended to print html
 require_once 'lib/translator.php';
-require_once 'lib/jaxon.php';
 
 // Decline static file requests back to the PHP built-in webserver
 if ('cli-server' === \PHP_SAPI && \is_file(__DIR__.\parse_url(LotgdRequest::getServer('REQUEST_URI'), PHP_URL_PATH)))
