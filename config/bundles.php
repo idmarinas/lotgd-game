9aa882c72cd3564d2d4681fb617ec75b40552c42
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-15 15:30:15 +0100

    First commit

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+];
