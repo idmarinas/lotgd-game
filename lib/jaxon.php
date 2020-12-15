5a440b99931993bbad15e66ec7c73c436a03a68a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-15 13:51:18 +0100

    Deleted

--- lib/jaxon.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// This file is obsolete and for get a Jaxon-PHP only need get it's factory, like this example
-global $lotgdJaxon;
-
-// This is only for compatibility is better use get factory
-$lotgdJaxon = \LotgdLocator::get(Lotgd\Core\Jaxon::class);
