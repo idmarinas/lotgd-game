90f7fcf0c59dd8b32a9e17164625b71754b5e972
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-16 17:43:12 +0100

    Added new command

--- config/autoload/global/console-lotgd-core.php
+++ config/autoload/global/console-lotgd-core.php
@@ -4,6 +4,7 @@ return [
     'console' => [
         'commands' => [
             Lotgd\Core\Command\AboutCommand::class,
+            Lotgd\Core\Command\RegenerateAppSecretCommand::class,
             Lotgd\Core\Command\StorageCacheClearCommand::class,
             Lotgd\Core\Command\StorageCacheStatsCommand::class,
         ]
