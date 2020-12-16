74557d91855c7ae744147c1be8abbab1a964395b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-16 13:46:20 +0100

    Added new bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Bukashk0zzz\FilterBundle\Bukashk0zzzFilterBundle::class => ['all' => true],
 ];
