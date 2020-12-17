897f68503e88d8fcc7890982752ae3fff1d5f605
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-17 14:28:07 +0100

    Not used

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,7 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    // Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Bukashk0zzz\FilterBundle\Bukashk0zzzFilterBundle::class => ['all' => true],
 ];
