b0d23f6c94c2e8b0e922f5671a294243e23dc5d2
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2020-12-25 12:15:15 +0100

    Removed unused factories

--- config/autoload/global/abstract_factories.php
+++ config/autoload/global/abstract_factories.php
@@ -3,14 +3,10 @@
 return [
     'service_manager' => [
         'abstract_factories' => [
-            \Laminas\Session\Service\ContainerAbstractServiceFactory::class,
             \Laminas\Cache\Service\StorageCacheAbstractServiceFactory::class,
 
             //-- Added in version 4.1.0
             \Laminas\Form\FormAbstractServiceFactory::class,
-
-            //-- Added in version 4.2.0
-            'DoctrineModule' => 'DoctrineModule\ServiceFactory\AbstractDoctrineServiceFactory',
         ],
     ],
     'input_filters' => [
