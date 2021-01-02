1f3281a279f73e97bb2b08e551ab7083f046852a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-02 17:21:19 +0100

    Deleted, not in use

--- config/autoload/global/hydrators-lotgd-core.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'hydrators' => [
-        'factories' => [
-        ],
-    ],
-];
--- config/autoload/global/initializers-lotgd-core.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'service_manager' => [
-        'initializers' => [],
-    ],
-];
