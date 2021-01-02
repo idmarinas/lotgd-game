2b21114aa7fd2480e8575ac55d9da986c15ae479
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-02 18:34:49 +0100

    Fixed error, now extend original class

--- src/core/Service/Censor.php
+++ src/core/Service/Censor.php
@@ -15,27 +15,26 @@ namespace Lotgd\Core\Service;
 
 use Lotgd\Core\Output\Censor as OutputCensor;
 
-class Censor
+class Censor extends OutputCensor
 {
     public const LOTGD_DICTIONARY_PATH = 'data/dictionary';
 
     public function __construct()
     {
+        parent::__construct();
+
         $language  = \Locale::getDefault();
-        $profanity = new OutputCensor();
-        $profanity->addDictionary(self::LOTGD_DICTIONARY_PATH.'/en.php'); //-- Custom dictionary
+        $this->addDictionary(self::LOTGD_DICTIONARY_PATH.'/en.php'); //-- Custom dictionary
 
         if ('en' != $language)
         {
-            $profanity->addDictionary($language);
+            $this->addDictionary($language);
             $customLanguage = self::LOTGD_DICTIONARY_PATH."/{$language}.php";
 
             if (\is_file($customLanguage))
             {
-                $profanity->addDictionary($customLanguage);
+                $this->addDictionary($customLanguage);
             }
         }
-
-        return $profanity;
     }
 }
