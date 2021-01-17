bb5bf1da2ad46e9717e4bcd868db6a15bfeb83e4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-17 19:03:56 +0100

    Added new bundles

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,8 @@ return [
     // Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class         => ['dev' => true, 'test' => true],
     Bukashk0zzz\FilterBundle\Bukashk0zzzFilterBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class     => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class           => ['dev' => true],
     // Nucleos\UserBundle\NucleosUserBundle::class             => ['all' => true],
     // Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                 => ['all' => true],
+    LotgdCore\AdvertisingBundle\LotgdCoreAdvertisingBundle::class => ['all' => true],
 ];
