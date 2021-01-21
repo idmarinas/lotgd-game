6c11387754a68e74f5a112a74b95a3d21ab8d7d1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-21 14:09:26 +0100

    Deleted factories of Theme, migrated to Kernel

--- config/autoload/global/factories-lotgd-core.php
+++ config/autoload/global/factories-lotgd-core.php
@@ -7,7 +7,6 @@ return [
         'factories' => [
             //-- Added in version 3.0.0 - Deleted in version 5.0.0
             /* LAZY */ Lotgd\Core\Db\Dbwrapper::class => Factory\Db\Dbwrapper::class, //-- Deprecated
-            Lotgd\Core\Template\Theme::class          => Factory\Template\Theme::class,
 
             //-- Added in version 4.0.0  - Deleted in version 5.0.0
             Lotgd\Core\Translator\Translator::class            => Factory\Translator\Translator::class, //-- Deprecated - migrate to Symfony Translation
@@ -17,13 +16,6 @@ return [
             /* LAZY */ 'InputFilterManager'    => Laminas\InputFilter\InputFilterPluginManagerFactory::class, //-- Deprecated - Use Symfony Form instead
             /* LAZY */ 'FormAnnotationBuilder' => Laminas\Form\Annotation\AnnotationBuilderFactory::class, //-- Deprecated - Use Symfony Form instead
             /* LAZY */ 'FormElementManager'    => Laminas\Form\FormElementManagerFactory::class, //-- Deprecated - Use Symfony Form instead
-
-            //-- Added in version 4.4.0 - Deleted in version 5.0.0
-            'webpack_encore.tag_renderer' => Factory\Template\TagRenderer::class, //-- This factory no need migrate
-            'webpack_encore.packages'     => Factory\Template\Packages::class, //-- This factory no need migrate
-
-            //-- Added in version 4.5.0 - Deleted in version 5.0.0
-            'webpack_encore.entrypoint_lookup_collection' => Factory\Template\EntrypointLookupCollection::class, //-- This factory no need migrate
         ],
     ],
 ];
