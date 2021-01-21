49dc266df601bf26cc392983ae8f56c082342f79
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-21 14:10:34 +0100

    Activated bundles related to templates

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class  => ['all' => true],
-    // Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class            => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class             => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class              => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class            => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     // Symfony\Bundle\DebugBundle\DebugBundle::class                     => ['dev' => true, 'test' => true],
-    // Symfony\Bundle\TwigBundle\TwigBundle::class                       => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class             => ['all' => true],
     // Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class         => ['dev' => true, 'test' => true],
     Bukashk0zzz\FilterBundle\Bukashk0zzzFilterBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class     => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class           => ['dev' => true],
     // Nucleos\UserBundle\NucleosUserBundle::class             => ['all' => true],
-    // Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                 => ['all' => true],
     LotgdCore\AdvertisingBundle\LotgdCoreAdvertisingBundle::class => ['all' => true],
 ];
