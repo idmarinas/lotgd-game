3a248442dd207016784f1d8de0576e2f78182978
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-24 14:08:58 +0100

    Delete reference to LotgdCache

--- lib/newday/newday_runonce.php
+++ lib/newday/newday_runonce.php
@@ -13,17 +13,6 @@ if ( ! getsetting('newdaycron', 0))
 {
     require_once 'lib/gamelog.php';
 
-    //Do some high-load-cleanup
-    if (LotgdCache::clearExpired())
-    {
-        gamelog('Expired cache data has been deleted', 'maintenance');
-    }
-
-    if (LotgdCache::optimize())
-    {
-        gamelog('Cache data has been optimized', 'maintenance');
-    }
-
     require 'lib/newday/commentcleanup.php';
     require 'lib/newday/charcleanup.php';
     require 'lib/newday/logoutaccts.php';
