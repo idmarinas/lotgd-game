589247f157b3cb4b3f244abe40d150d1c8cc88f1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-27 17:20:32 +0100

    First commit

---
+++ src/core/Installer/Command/AbstractCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.0.0
+ */
+
+namespace Lotgd\Core\Installer\Command;
+
+use Lotgd\Core\Installer\InstallerAbstract;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for upgrade to clean install.
+ */
+abstract class AbstractCommand extends Command
+{
+    use LockableTrait;
+
+    protected $installer;
+    protected $translator;
+    protected $bar;
+
+    public function setProgressBar(ProgressBar $bar)
+    {
+        $this->bar = $bar;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        /* @internal Not need show in list */
+        $this->setHidden(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var Lotgd\Core\Kernel */
+        $app    = $this->getApplication();
+        $kernel = $app->getKernel();
+
+        $this->installer->configureConsole($input, $output);
+        $this->installer->setProjectDir($kernel->getProjectDir());
+
+        $this->installer->start(); //-- Start install
+
+        $step   = 0;
+        $steps  = $this->installer->getSteps();
+        $result = true;
+
+        //-- Proccess all steps of installer
+        do
+        {
+            $text = $this->translator->trans('installer.progressbar.install.progress.steps', [
+                'step'  => $step,
+                'total' => $steps,
+            ], InstallerAbstract::TRANSLATOR_DOMAIN);
+            $this->bar->setMessage($text, 'steps');
+            $this->bar->display();
+
+            if ($this->installer->stepStatus($step))
+            {
+                ++$step;
+
+                continue;
+            }
+
+            $result = $this->installer->{"step{$step}"}();
+
+            $this->installer->{$result ? 'setStepComplete' : 'setStepIncomplete'}($step);
+
+            ++$step;
+        } while ($result && \method_exists($this->installer, "step{$step}"));
+
+        $this->installer->finish(); //-- Finish install
+
+        return Command::SUCCESS;
+    }
+}
