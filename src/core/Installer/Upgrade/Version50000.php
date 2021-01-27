49b19172cfac0d68c7e9cc4db07c64fc81455bbe
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-01-27 19:31:14 +0100

    First commit

---
+++ src/core/Installer/Upgrade/Version50000.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.0.0
+ */
+
+namespace Lotgd\Core\Installer\Upgrade;
+
+use Lotgd\Core\Installer\InstallerAbstract;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Version50000 extends InstallerAbstract
+{
+    protected $upgradeVersion = 50000;
+
+    //-- Delete old files
+    public function step0()
+    {
+        $fs = new Filesystem();
+
+        try
+        {
+            $fs->remove($this->getProjectDir().'/config/autoload/');
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //-- Delete table translations
+    public function step1(): bool
+    {
+        try
+        {
+            $this->doctrine->getConnection()->executeQuery('DROP TABLE IF EXISTS `translations`');
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //-- Delete table untranslated
+    public function step2(): bool
+    {
+        try
+        {
+            $this->doctrine->getConnection()->executeQuery('DROP TABLE IF EXISTS `untranslated`');
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //-- Delete table nastywords
+    public function step3(): bool
+    {
+        try
+        {
+            $this->doctrine->getConnection()->executeQuery('DROP TABLE IF EXISTS `nastywords`');
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
