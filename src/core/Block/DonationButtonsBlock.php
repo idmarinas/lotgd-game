65d1652dad409ff2ed1bc3be94df5ba88e20b389
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-03 19:30:28 +0100

    Register with Logd net is back

--- src/core/Block/DonationButtonsBlock.php
+++ src/core/Block/DonationButtonsBlock.php
@@ -13,7 +13,9 @@
 
 namespace Lotgd\Core\Block;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Lotgd\Core\Http\Request;
+use Lotgd\Core\Lib\Settings;
 use Sonata\BlockBundle\Block\BlockContextInterface;
 use Sonata\BlockBundle\Block\Service\AbstractBlockService;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,6 +23,8 @@ use Symfony\Component\HttpFoundation\Response;
 final class DonationButtonsBlock extends AbstractBlockService
 {
     protected $request;
+    protected $doctrine;
+    protected $settings;
 
     public function execute(BlockContextInterface $blockContext, ?Response $response = null): Response
     {
@@ -28,17 +32,74 @@ final class DonationButtonsBlock extends AbstractBlockService
 
         $uri  = $this->request->getServer('REQUEST_URI');
         $host = $this->request->getServer('HTTP_HOST');
+        $now  = new \DateTime('now');
+        $now->sub(new \DateInterval('PT1H'));
+        $cacheTime = 900;
+
+        $alreadyRegisteredLogdnet = true;
+
+        if ('' == ($session['logdnet'][''] ?? '') || ! isset($session['user']['laston']) || $now > $session['user']['laston'])
+        {
+            $cacheTime                = 0;
+            $alreadyRegisteredLogdnet = false;
+        }
+
+        $author['register_logdnet'] = false;
+
+        if ($this->settings->getSetting('logdnet', 0) && $session['user']['loggedin'] && ! $alreadyRegisteredLogdnet)
+        {
+            //account counting, just for my own records, I don't use this in the calculation for server order.
+            $c = $this->doctrine->getRepository('LotgdCore:Accounts')->count([]);
+            $a = $this->settings->getSetting('serverurl', "//{$host}/");
+
+            if ( ! \preg_match("/\/$/", $a))
+            {
+                $a = $a.'/';
+                savesetting('serverurl', $a);
+            }
+
+            $l = $this->settings->getSetting('defaultlanguage', 'en');
+            $d = $this->settings->getSetting('serverdesc', 'Another LoGD Server');
+            $e = $this->settings->getSetting('gameadminemail', 'postmaster@localhost.com');
+            $u = $this->settings->getSetting('logdnetserver', 'https://lotgd.net/');
+
+            if ( ! \preg_match("/\/$/", $u))
+            {
+                $u = $u.'/';
+                savesetting('logdnetserver', $u);
+            }
+
+            $author['register_logdnet'] = true;
+            $author['v']                = \rawurlencode(\Lotgd\Core\Kernel::VERSION);
+            $author['c']                = \rawurlencode($c);
+            $author['a']                = \rawurlencode($a);
+            $author['l']                = \rawurlencode($l);
+            $author['d']                = \rawurlencode($d);
+            $author['e']                = \rawurlencode($e);
+            $author['u']                = \rawurlencode($u);
+        }
 
         return $this->renderResponse('admin/paypal.html.twig', [
             'settings'    => $blockContext->getSettings(),
             'block'       => $blockContext->getBlock(),
             'item_number' => \htmlentities($session['user']['login'], ENT_COMPAT, 'UTF-8').':'.$host.'/'.$uri,
             'notify_url'  => '//'.$host.\dirname($uri).'/payment.php',
-        ], $response);
+            'author'      => $author,
+        ], $response)->setTtl($cacheTime);
     }
 
     public function setRequest(Request $request)
     {
         $this->request = $request;
+    }
+
+    public function setDoctrine(EntityManagerInterface $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function setSettings(Settings $settings)
+    {
+        $this->settings = $settings;
     }
 }
