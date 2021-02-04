ff367494e3e0c7f6c891fb217d9d97f866a7df2c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-04 18:28:27 +0100

    Added some default values

--- src/core/Symfony/Translation/Formatter/MessageFormatter.php
+++ src/core/Symfony/Translation/Formatter/MessageFormatter.php
@@ -22,10 +22,36 @@ class MessageFormatter extends CoreMessageFormatter implements MessageFormatterI
     /**
      * {@inheritdoc}
      */
+    public function format(string $message, string $locale, array $parameters = [])
+    {
+        global $session;
+
+        //-- Added same default values
+        $parameters = \array_merge([
+            'playerName' => $session['user']['name'] ?? '',
+            'playerSex'  => $session['user']['sex'] ?? '',
+            'location'   => $session['user']['location'] ?? '',
+        ], $parameters);
+
+        return parent::format($message, $locale, $parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function formatIntl(string $message, string $locale, array $parameters = []): string
     {
+        global $session;
+
         //-- Delete all values that not are allowed (can cause a error when use \MessageFormatter::format($params))
         $parameters = \array_filter($parameters, [$this, 'cleanParameters']);
+
+        //-- Added same default values
+        $parameters = \array_merge([
+            'playerName' => $session['user']['name'] ?? '',
+            'playerSex'  => $session['user']['sex'] ?? '',
+            'location'   => $session['user']['location'] ?? '',
+        ], $parameters);
 
         return parent::formatIntl($message, $locale, $parameters);
     }
