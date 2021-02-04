00315df090133d6c0382c76dc4ac6155c897d10b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-04 16:52:40 +0100

    First commit

---
+++ src/core/Service/Twig/SessionVariable.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.1.0
+ */
+
+namespace Lotgd\Core\Service\Twig;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class SessionVariable
+{
+    private $access;
+
+    public function __call($method, $args)
+    {
+        return $this->access()->getValue($this->getSession(), "[$method]");
+    }
+
+    /**
+     * Returns the current session.
+     */
+    private function getSession()
+    {
+        global $session;
+
+        $sesion = $session ?? [];
+        unset($sesion['user']);
+
+        return $sesion;
+    }
+
+    private function access(): PropertyAccessor
+    {
+        if (! $this->access instanceof PropertyAccessor)
+        {
+            $this->access = PropertyAccess::createPropertyAccessor();
+        }
+
+        return $this->access;
+    }
+}
---
+++ src/core/Service/Twig/UserVariable.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.1.0
+ */
+
+namespace Lotgd\Core\Service\Twig;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class UserVariable
+{
+    private $access;
+
+    public function __call($method, $args)
+    {
+        return $this->access()->getValue($this->getUser(), "[$method]");
+    }
+
+    /**
+     * Returns the current user.
+     */
+    private function getUser(): array
+    {
+        global $session;
+
+        $user = $session['user'] ?? [];
+        unset($user['password']);
+
+        return $user;
+    }
+
+    private function access(): PropertyAccessor
+    {
+        if (! $this->access instanceof PropertyAccessor)
+        {
+            $this->access = PropertyAccess::createPropertyAccessor();
+        }
+
+        return $this->access;
+    }
+}
