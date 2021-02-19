bba12e5d805750275631e17506d087a4f33fe432
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-19 23:32:41 +0100

    First commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 6.0.0
+ */
+
+namespace Lotgd\Core\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="lotgd_core_home")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->isGranted('ROLE_USER'))
+        {
+            return $this->redirectToRoute('lotgd_core_profile');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('page/home/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="lotgd_core_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
