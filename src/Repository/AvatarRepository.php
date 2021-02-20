95943e7d3a7dca2497f05e768cc83ddb0a5ce411
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-20 15:15:15 +0100

    Characters is now avatar

---
+++ src/Repository/AvatarRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 4.0.0
+ */
+
+namespace Lotgd\Core\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class AvatarRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Armor::class);
+    }
+}
