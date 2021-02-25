361de5026197c160305de1c72d4f2bb25314f38c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-25 10:23:00 +0100

    Deleted function list_files, use Symfony Finder

--- src/functions.php
+++ src/functions.php
@@ -170,37 +170,6 @@ if ( ! \function_exists('myDefine'))
     }
 }
 
-if ( ! \function_exists('list_files'))
-{
-    function list_files($ruta, $sort)
-    {
-        //abrir un directorio y listarlo recursivo
-        if (\is_dir($ruta) && $dh = \opendir($ruta))
-        {
-            while (false !== ($file = \readdir($dh)))
-            {
-                if (\is_dir($ruta.'/'.$file) && '.' != $file && '..' != $file)
-                {
-                    $sort = list_files($ruta.'/'.$file, $sort);
-                }
-                else
-                {
-                    $names = \explode('.', $file);
-
-                    if (isset($names[1]) && 'php' == $names[1])
-                    {
-                        //sorting
-                        $sort[] = ','.$ruta.'/'.$names[0].','.$ruta.'/'.$names[0];
-                    }
-                }
-            }
-            \closedir($dh);
-        }
-
-        return $sort;
-    }
-}
-
 if ( ! \function_exists('pullurl'))
 {
     function pullurl($url)
