4b21e1731f7ba45a77b913071fefae1915246453
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-25 19:45:00 +0100

    First commit

---
+++ tests/TwigExtension/IntegrationTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Bundle "IDM Advertising Bundle".
+ *
+ * @see https://github.com/idmarinas/advertising-bundle
+ *
+ * @license https://github.com/idmarinas/advertising-bundle/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 0.1.0
+ */
+
+namespace Lotgd\Core\Tests\TwigExtension;
+
+use Lotgd\Core\Twig\Extension\GameCoreExtension;
+use Twig\Test\IntegrationTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test Twig Extensions
+ */
+class IntegrationTest extends IntegrationTestCase
+{
+    public function getExtensions()
+    {
+        $client = (new Kernel())->returnClient();
+        $container = $client->getContainer();
+
+        return [
+            new GameCoreExtension(
+                $container->get('request_stack'),
+                $container->get('lotgd.core.censor'))
+        ];
+    }
+
+    public function getFixturesDir()
+    {
+        return __DIR__.'/Fixtures/';
+    }
+}
+
+class Kernel extends WebTestCase
+{
+    public function returnClient()
+    {
+        self::ensureKernelShutdown();
+        return self::createClient();
+    }
+}
