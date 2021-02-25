504a691d2963d8c6dfd9e82672a09a8ae6f03a2f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-25 17:56:32 +0100

    First commit

---
+++ src/Event/ConfigureMenuEvent.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * Plugin.php - Adapter for the Semantic library.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 6.0.0
+ */
+
+namespace Lotgd\Core\Event;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * Menu builder event. Used for extending the menus.
+ *
+ * @final since sonata-project/admin-bundle 3.52
+ *
+ * @author Martin Hasoň <martin.hason@gmail.com>
+ */
+class ConfigureMenuEvent extends Event
+{
+    public const CORE = 'lotgd_core.event.configure.menu.core';
+    public const BATTLE = 'lotgd_core.event.configure.menu.battle';
+
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @var ItemInterface
+     */
+    private $menu;
+
+    public function __construct(FactoryInterface $factory, ItemInterface $menu)
+    {
+        $this->factory = $factory;
+        $this->menu    = $menu;
+    }
+
+    /**
+     * @return FactoryInterface
+     */
+    public function getFactory()
+    {
+        return $this->factory;
+    }
+
+    /**
+     * @return ItemInterface
+     */
+    public function getMenu()
+    {
+        return $this->menu;
+    }
+}
