ecd66b7514740843bc59965010b716d4f4cb5451
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-02-28 19:14:47 +0100

    Moved to new folder

--- src/Lotgd/Bundle/CoreBundle/Form/ChangePasswordFormType.php
+++ src/Lotgd/Bundle/CoreBundle/Form/ChangePasswordFormType.php
@@ -11,7 +11,7 @@
  * @since 6.0.0
  */
 
-namespace Lotgd\Core\Form;
+namespace Lotgd\Bundle\CoreBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
