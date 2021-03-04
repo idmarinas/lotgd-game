b56a803b981a6c04ae0b48c5c0db417daf6c155b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-04 21:54:45 +0100

    First commit

---
+++ src/Lotgd/Bundle/UserBundle/Admin/UserAdmin.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 6.0.0
+ */
+
+namespace Lotgd\Bundle\UserBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\Form\Type as SonataType;
+use Sonata\AdminBundle\Form\Type\CollectionType;
+
+final class UserAdmin extends AbstractAdmin
+{
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper
+            ->add('id', null, ['label' => 'entity.user.id'])
+            ->add('bannedUntil', null, ['label' => 'entity.user.banned_until'])
+            ->add('username', null, ['label' => 'entity.user.username'])
+            ->add('email', null, ['label' => 'entity.user.email'])
+            ->add('createdAt', null, ['label' => 'entity.user.created_at'])
+            ->add('roles', null, ['label' => 'entity.user.roles'])
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper
+            ->add('id', null, ['label' => 'entity.user.id'])
+            ->add('isBanned', 'boolean', ['mapped' => false, 'label' => 'entity.user.is_banned', 'inverse' => true])
+            ->add('isDeleted', 'boolean', ['label' => 'entity.user.is_deleted', 'inverse' => true])
+            ->add('username', null, ['label' => 'entity.user.username'])
+            ->add('email', null, ['label' => 'entity.user.email'])
+            ->add('isVerified', null, ['editable' => true, 'label' => 'entity.user.is_verified'])
+            ->add('createdAt', null, ['label' => 'entity.user.created_at'])
+            ->add('_action', null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $formMapper
+            ->tab('info')
+                ->with('basic', ['class' => 'col-md-8', 'label' => null, 'box_class' => null])
+                    ->add('id', null, ['label' => 'entity.user.id', 'disabled' => true])
+                    ->add('username', null, ['label' => 'entity.user.username', 'disabled' => true])
+                    ->add('email', null, ['label' => 'entity.user.email', 'disabled' => true])
+                    ->add('createdAt', SonataType\DateTimePickerType::class, ['disabled' => true, 'label' => 'entity.user.created_at', 'dp_side_by_side' => true])
+                ->end()
+                ->with('roles', ['class' => 'col-md-4', 'label' => null, 'box_class' => null])
+                    ->add('isBanned', SonataType\BooleanType::class, ["mapped" => false])
+                    ->add('isVerified', null, ['label' => 'entity.user.is_verified'])
+                    ->add('roles', CollectionType::class, [
+                        'label' => 'entity.user.roles',
+                        'allow_add' => true,
+                        'allow_delete' => true
+                    ])
+                    ->add('bannedUntil', SonataType\DateTimePickerType::class, ['required' => false, 'label' => 'entity.user.banned_until'])
+                    ->add('lastMotd', SonataType\DateTimePickerType::class, ['required' => false, 'label' => 'entity.user.last_motd'])
+                ->end()
+            ->end()
+            ->tab('avatar')
+                ->with('avatar', ['class' => 'col-md-6', 'label' => null, 'box_class' => null])
+                    ->add('avatar', null, ['label' => 'entity.user.avatar'])
+                ->end()
+                ->with('avatars', ['class' => 'col-md-6', 'label' => null, 'box_class' => null])
+                    ->add('avatars', null, ['label' => 'entity.user.avatars'])
+                ->end()
+            ->end()
+            ->tab('other')
+                ->with('donation', ['class' => 'col-md-6'])
+                    ->add('donation', null, ['label' => 'entity.user.donation'])
+                    ->add('donationSpent', null, ['label' => 'entity.user.donation_spent'])
+                ->end()
+                ->with('referer', ['class' => 'col-md-6'])
+                    ->add('referer', null, ['label' => 'entity.user.referer', 'choice_label' => 'username'])
+                    ->add('refererIsRewarded', null, ['label' => 'entity.user.referer_is_rewarded'])
+                ->end()
+                ->with('other')
+                    ->add('deletedAt', SonataType\DateTimePickerType::class, ['label' => 'entity.user.deleted_at', 'required' => false])
+                ->end()
+            ->end()
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper): void
+    {
+        $showMapper
+            ->add('id', null, ['label' => 'entity.user.id'])
+            ->add('isVerified', null, ['label' => 'entity.user.is_verified'])
+            ->add('isDeleted', 'boolean', ['label' => 'entity.user.is_deleted', 'inverse' => true])
+            ->add('username', null, ['label' => 'entity.user.username'])
+            ->add('email', null, ['label' => 'entity.user.email'])
+            ->add('lastMotd', null, ['label' => 'entity.user.last_motd'])
+            ->add('isBanned', 'boolean', ['mapped' => false, 'label' => 'entity.user.is_banned', 'inverse' => true])
+            ->add('bannedUntil', null, ['label' => 'entity.user.banned_until'])
+            ->add('createdAt', null, ['label' => 'entity.user.created_at'])
+            ->add('updatedAt', null, ['label' => 'entity.user.updated_at'])
+            ->add('referer', null, ['label' => 'entity.user.referer', 'associated_property' => 'username'])
+            ->add('refererIsRewarded', null, ['label' => 'entity.user.referer_is_rewarded'])
+            ->add('donation', null, ['label' => 'entity.user.donation'])
+            ->add('donationSpent', null, ['label' => 'entity.user.donation_spent'])
+            ->add('deletedAt', null, ['label' => 'entity.user.deleted_at'])
+            ->add('roles', 'array', ['label' => 'entity.user.roles'])
+        ;
+    }
+}
