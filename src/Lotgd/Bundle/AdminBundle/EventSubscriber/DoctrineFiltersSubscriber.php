dfcef6718089e44570c9f5272496f81c6601f0f6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-04 21:50:33 +0100

    First commit

---
+++ src/Lotgd/Bundle/AdminBundle/EventSubscriber/DoctrineFiltersSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 6.0.0
+ */
+
+namespace Lotgd\Bundle\AdminBundle\EventSubscriber;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class DoctrineFiltersSubscriber implements EventSubscriberInterface
+{
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'disableDoctrineFilterOnAdmin',
+        ];
+    }
+
+    public function disableDoctrineFilterOnAdmin(ControllerEvent $event)
+    {
+        $controller = $event->getController()[0];
+        if ($controller instanceof CRUDController)
+        {
+            //-- Disable filter "softdeleteable" for all admin controllers
+            $this->em->getFilters()->disable('softdeleteable');
+        }
+    }
+}
