7e2a258754db7690ef51743b25443129deea1cd9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-08 16:44:58 +0100

    Added Service Block for RSS in admin dashboard

--- src/Lotgd/Bundle/AdminBundle/Resources/config/services.php
+++ src/Lotgd/Bundle/AdminBundle/Resources/config/services.php
@@ -13,6 +13,8 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Lotgd\Bundle\AdminBundle\Block\DashboardRssServiceBlock;
+
 return static function (ContainerConfigurator $container)
 {
     $container->services()
@@ -32,5 +34,9 @@ return static function (ContainerConfigurator $container)
         ->load('Lotgd\Bundle\AdminBundle\Controller\\', '../../Controller/')
             ->tag('controller.service_arguments')
 
+        //-- Blocks
+        ->set('lotgd_admin.block.service.dashboard.rss', DashboardRssServiceBlock::class)
+            ->args([service('twig'), service('http_client')])
+            ->tag('sonata.block')
     ;
 };
