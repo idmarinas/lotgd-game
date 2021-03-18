65c5cb5af22b8f6c3f62b4789c3f302fa243d023
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-18 14:34:05 +0100

    Change route and check role

--- src/Lotgd/Bundle/UserBundle/Controller/ProfileController.php
+++ src/Lotgd/Bundle/UserBundle/Controller/ProfileController.php
@@ -13,12 +13,20 @@
 
 namespace Lotgd\Bundle\UserBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/game")
+ *
+ * @IsGranted("ROLE_USER")
+ */
 class ProfileController extends AbstractController
 {
+    public const TRANSLATOR_DOMAIN = 'lotgd_user_page_profile';
+
     /**
      * @Route("/profile", name="lotgd_user_profile")
      */
