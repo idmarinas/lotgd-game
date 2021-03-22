d9e691cd30a48849cf3ed2449272d50231dfab2b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-22 12:59:58 +0100

    Updated for Symfony 4.4

--- public/index.php
+++ public/index.php
@@ -1,19 +1,26 @@
 <?php
 
 use Lotgd\Bundle\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require \dirname(__DIR__).'/vendor/autoload.php';
-
-(new Dotenv())->bootEnv(\dirname(__DIR__).'/.env');
+require \dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG'])
 {
     \umask(0000);
 
     Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false)
+{
+    Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false)
+{
+    Request::setTrustedHosts([$trustedHosts]);
 }
 
 $kernel   = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
