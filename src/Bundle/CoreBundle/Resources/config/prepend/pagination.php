7ee9001d162a7aba32ecf4fa606dd2ac173a1a3c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-03-25 16:46:51 +0100

    Added comment

--- src/Bundle/CoreBundle/Resources/config/prepend/pagination.php
+++ src/Bundle/CoreBundle/Resources/config/prepend/pagination.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 6.0.0
+ */
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return static function (ContainerConfigurator $container): void
