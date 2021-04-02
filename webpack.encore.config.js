7aa063a396dd82092b1b9eacef697dff31503be6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-02 13:45:58 +0200

    Avoid possible error, call reset before configure runtime enviroment

--- webpack.encore.config.js
+++ webpack.encore.config.js
@@ -1,14 +1,14 @@
 module.exports = function (Encore)
 {
-    //-- Reset config in each call
-    Encore.reset()
-
     // Manually configure the runtime environment if not already configured yet by the "encore" command.
     // It's useful when you use tools that rely on webpack.config.js file.
     if (!Encore.isRuntimeEnvironmentConfigured())
     {
         Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
     }
+
+    //-- Reset config in each call
+    Encore.reset()
 
     Encore
         //-- Configure how CSS/JS/Images/Fonts files are exported
