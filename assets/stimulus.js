6f42cb2a816d12d2fb4066388f963c9531800cd6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-08 18:43:21 +0200

    First commit

---
+++ assets/stimulus.js
@@ -0,0 +1,10 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge'
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
+const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.(j|t)sx?$/
+))
+
+export default app
