89015f728077dea512c855c62843a5bbbe5498a6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-09 16:15:37 +0200

    Default export names and file names should match

--- assets/stimulus.js
+++ assets/stimulus.js
@@ -1,10 +1,10 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
-const app = startStimulusApp(require.context(
+const stimulus = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.(j|t)sx?$/
 ))
 
-export default app
+export default stimulus
