57eec86318a173a6a337c5be3c76fce367a93322
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-12 15:34:35 +0200

    Block to much petitions and email when new petition

--- src/Bundle/CoreBundle/Controller/PetitionController.php
+++ src/Bundle/CoreBundle/Controller/PetitionController.php
@@ -15,10 +15,17 @@ namespace Lotgd\Bundle\CoreBundle\Controller;
 
 use Lotgd\Bundle\CoreBundle\Entity\Petitions;
 use Lotgd\Bundle\CoreBundle\Form\PetitionType;
+use Lotgd\Bundle\CoreBundle\Repository\PetitionsRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * @Route("/petition")
@@ -27,20 +34,43 @@ class PetitionController extends AbstractController
 {
     public const TRANSLATOR_DOMAIN = 'lotgd_core_page_petition';
 
+    private $validator;
+    private $translator;
+    private $mailer;
+
+    public function __construct(ValidatorInterface $validator, TranslatorInterface $translator, MailerInterface $mailer)
+    {
+        $this->validator  = $validator;
+        $this->translator = $translator;
+        $this->mailer     = $mailer;
+    }
+
     /**
      * @Route("/help", name="lotgd_core_petition_help")
      */
-    public function help(Request $request): Response
+    public function help(Request $request, PetitionsRepository $repository): Response
     {
+        $networkRestriction = false;
+        $messageSend        = '';
+        $messageNetwork     = '';
+        $count              = $repository->getCountPetitionsForNetwork($request->getClientIp());
+
+        //-- Not allow to much petitions form same network, but yes from admins
+        if ($count >= 5 && ! $this->isGranted('ROLE_ADMIN'))
+        {
+            $networkRestriction = true;
+            $messageNetwork     = 'flash.message.section.default.error.network';
+        }
+
         $form = $this->createForm(PetitionType::class, null, [
             'petitions' => \explode(',', $this->getParameter('lotgd_bundle.petitions.types')),
             'action'    => $this->generateUrl('lotgd_core_petition_help'),
         ]);
         $formClone = clone $form;
-        $message = '';
 
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
+
+        if ($form->isSubmitted() && $form->isValid() && ! $networkRestriction)
         {
             $data = $form->getData();
 
@@ -55,13 +85,54 @@ class PetitionController extends AbstractController
             $this->getDoctrine()->getManager()->persist($entity);
             $this->getDoctrine()->getManager()->flush();
 
-            $message = 'flash.message.section.default.success.send';
-            $form = $formClone;
+            $messageSend = 'flash.message.section.default.success.send';
+            $form        = $formClone;
+
+            // Notify to admin if if wants it
+            $this->emailPetitionToAdmin($entity);
         }
 
         return $this->render('@LotgdCore/petition/help.html.twig', [
-            'form' => $form->createView(),
-            'message' => $message
+            'form'            => $form->createView(),
+            'message_send'    => $messageSend,
+            'network_message' => $messageNetwork,
+            'network_count'   => $count,
         ]);
+    }
+
+    /**
+     * If the admin wants it, email the petitions to them.
+     */
+    private function emailPetitionToAdmin(Petitions $entity): void
+    {
+        $adminEmail = $this->getParameter('lotgd_bundle.game.server.admin.email');
+        $validEmail = $this->validator->validate($adminEmail, [
+            new Assert\NotBlank(),
+            new Assert\NotNull(),
+            new Assert\Email(),
+        ]);
+
+        if ( ! $this->getParameter('lotgd_bundle.game.server.admin.mailed_petitions') || 'postmaster@localhost.com' == $adminEmail || \count($validEmail))
+        {
+            return;
+        }
+
+        $host   = $this->request->server->get('HTTP_HOST');
+        $schema = 'On' == $this->request->server->get('HTTPS') ? 'https' : 'http';
+        $url    = "{$schema}://{$host}/";
+
+        $email = (new TemplatedEmail())
+            ->from(new Address('mailer@lotgd.core', 'LoTGD Mail Bot'))
+            ->to($adminEmail)
+            ->subject($this->translator->trans('mailer.email.subject', [], self::TRANSLATOR_DOMAIN))
+            ->htmlTemplate('@LotgdCore/petition/email.html.twig')
+            ->context([
+                'translator_domain' => self::TRANSLATOR_DOMAIN,
+                'url'               => $url,
+                'entity'            => $entity,
+            ])
+        ;
+
+        $this->mailer->send($email);
     }
 }
