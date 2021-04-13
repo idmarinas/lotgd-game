fdf05fdcfe4d7e54bb5b74fabfafbb6401d17276
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-13 11:33:52 +0200

    Add new bundle

--- config/bundles.php
+++ config/bundles.php
@@ -32,6 +32,7 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class             => ['dev' => true, 'test' => true],
     Lotgd\Bundle\CoreBundle\LotgdCoreBundle::class                           => ['all' => true],
+    Lotgd\Bundle\CreatureBundle\LotgdCreatureBundle::class                   => ['all' => true],
     Lotgd\Bundle\AdminBundle\LotgdAdminBundle::class                         => ['all' => true],
     Lotgd\Bundle\SettingsBundle\LotgdSettingsBundle::class                   => ['all' => true],
     Lotgd\Bundle\UiBundle\LotgdUiBundle::class                               => ['all' => true],
