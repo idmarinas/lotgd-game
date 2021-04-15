6298de14b5167852ffe1acc5fd19b5c64661f732
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-15 16:33:24 +0200

    Delete unused imported class

--- src/Bundle/CoreBundle/Admin/LogdnetAdmin.php
+++ src/Bundle/CoreBundle/Admin/LogdnetAdmin.php
@@ -16,7 +16,6 @@ namespace Lotgd\Bundle\CoreBundle\Admin;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
 
