258a70d0e072ac17e6c1e04427fa490842bac953
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-27 16:12:15 +0200

    Added new parameter

--- src/Bundle/CoreBundle/Resources/config/parameters.php
+++ src/Bundle/CoreBundle/Resources/config/parameters.php
@@ -29,6 +29,7 @@ return static function (ContainerConfigurator $container)
         ->set('lotgd_bundle.paypal.site.currency', 'USD')
         ->set('lotgd_bundle.paypal.site.country', 'US')
         ->set('lotgd_bundle.paypal.site.email', '')
+        ->set('lotgd_bundle.donation.points_per_currency_unit', 100)
         ->set('lotgd_bundle.petitions.types', 'petition.option.general,petition.option.report.bug,petition.option.suggestion,petition.option.comment,petition.option.other')
     ;
 };
