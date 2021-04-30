0c4e076e7c9d7f448e9eee305467338ae7f7e7a4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-04-30 16:45:21 +0200

    Add new type of field for Doctrine

--- src/Bundle/CoreBundle/LotgdCoreBundle.php
+++ src/Bundle/CoreBundle/LotgdCoreBundle.php
@@ -13,12 +13,25 @@
 
 namespace Lotgd\Bundle\CoreBundle;
 
+use Acelaya\Doctrine\Type\PhpEnumType;
+use Doctrine\DBAL\Types\Type as DoctrineType;
 use Lotgd\Bundle\CoreBundle\DependencyInjection\Compiler\GlobalVariablesCompilerPass;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Lotgd\Bundle\CoreBundle\Doctrine\FieldEnum\PetitionStatusTypeEnum;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LotgdCoreBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+
+        if (\class_exists('Acelaya\Doctrine\Type\PhpEnumType') && ! DoctrineType::hasType('petition_status_type_enum'))
+        {
+            PhpEnumType::registerEnumType('petition_status_type_enum', PetitionStatusTypeEnum::class);
+        }
+    }
+
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new GlobalVariablesCompilerPass());
