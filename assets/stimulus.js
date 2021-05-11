8a3f24d0b1b70a2f2209b162e1088db39076ab48
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-11 18:09:26 +0200

    Add new controllers

--- assets/stimulus.js
+++ assets/stimulus.js
@@ -1,4 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge'
+import TextareaAutogrow from 'stimulus-textarea-autogrow'
+import CharacterCounter from 'stimulus-character-counter'
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 const stimulus = startStimulusApp(require.context(
@@ -6,5 +8,8 @@ const stimulus = startStimulusApp(require.context(
     true,
     /\.(j|t)sx?$/
 ))
+
+stimulus.register('character-counter', CharacterCounter)
+stimulus.register('textarea-autogrow', TextareaAutogrow)
 
 export default stimulus
