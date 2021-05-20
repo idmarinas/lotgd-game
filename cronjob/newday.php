74d913030fe7ed8231d923346433a3c2f69dbdc8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-20 12:54:43 +0200

    Deleted

--- cronjob/newday.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('ALLOW_ANONYMOUS', true);
-
-require_once 'public/common.php';
-
-savesetting('newdaySemaphore', gmdate('Y-m-d H:i:s'));
-
-require 'lib/newday/newday_runonce.php';
--- lib/newday/logoutaccts.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$repository = \Doctrine::getRepository(\Lotgd\Core\Entity\Accounts::class);
-
-//-- Logout accounts inactive
-$repository->logoutInactiveAccounts((int) getsetting('LOGINTIMEOUT', 900));
-
-\LotgdKernel::get('cache.app')->delete('char-list-home-page');
