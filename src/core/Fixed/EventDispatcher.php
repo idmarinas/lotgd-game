629856f0f9052f5d53ce1b67ea7f400354d0205b
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-24 16:12:37 +0200

    First commit

---
+++ src/core/Fixed/EventDispatcher.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.2.0
+ */
+
+namespace Lotgd\Core\Fixed;
+
+use Symfony\Component\HttpKernel\Debug\TraceableEventDispatcher as CoreEventDispatcher;
+
+class EventDispatcher
+{
+    /**
+     * Instance.
+     *
+     * @var CoreEventDispatcher
+     */
+    protected static $instance;
+
+    /**
+     * Add support for magic static method calls.
+     *
+     * @param mixed $method
+     * @param array $arguments
+     *
+     * @return mixed the returned value from the resolved method
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        if (\method_exists(self::$instance, $method))
+        {
+            return self::$instance->{$method}(...$arguments);
+        }
+
+        $methods = \implode(', ', \get_class_methods(self::$instance));
+
+        throw new \BadMethodCallException("Undefined method '{$method}'. The method name must be one of '{$methods}'");
+    }
+
+    /**
+     * Set instance of EventManager.
+     *
+     * @param CoreEventDispatcher $container
+     */
+    public static function instance(CoreEventDispatcher $instance)
+    {
+        self::$instance = $instance;
+    }
+}
+
+\class_alias('Lotgd\Core\Fixed\EventDispatcher', 'LotgdEventDispatcher', false);
