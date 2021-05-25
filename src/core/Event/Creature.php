58b74c741edced7601a24c38e93e6851b5cd4235
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-25 14:01:31 +0200

    First commit

---
+++ src/core/Event/Creature.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.2.0
+ */
+
+namespace Lotgd\Core\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class Creature extends Event
+{
+    // Creature search
+    public const SEARCH = 'lotgd.creature.search';
+
+    // Creature encounter
+    public const ENCOUNTER = 'lotgd.creature.encounter';
+
+    // Creature buff
+    public const BUFF_FOR = 'lotgd.creature.buff.';
+
+    private $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
