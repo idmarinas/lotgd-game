76fe683e9a540ed6a0ee7881e7885d400a9c8d8e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-25 14:17:53 +0200

    First commit

---
+++ src/core/Event/Superuser.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.2.0
+ */
+
+namespace Lotgd\Core\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class Superuser extends Event
+{
+    // Superuser
+    public const SUPERUSER = 'lotgd.other.superuser';
+
+    // Check su access
+    public const CHECK_SU_ACCESS = 'lotgd.other.superuser.check.su.access';
+
+    // Check su permission
+    public const CHECK_SU_PERMISSION = 'lotgd.other.superuser.check.su.permission';
+
+    private $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
