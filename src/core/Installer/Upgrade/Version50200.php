ec4613aed05e1f7daf5dac8ca9a13939d34bc847
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-26 13:00:05 +0200

    First commit

---
+++ src/core/Installer/Upgrade/Version50200.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/master/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.2.0
+ */
+
+namespace Lotgd\Core\Installer\Upgrade;
+
+use Lotgd\Core\Installer\InstallerAbstract;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Version50200 extends InstallerAbstract
+{
+    protected $upgradeVersion = 50200;
+    protected $hasMigration = false;
+
+    //-- Delete old files
+    public function step0()
+    {
+        $fs = new Filesystem();
+
+        try
+        {
+            $fs->remove([
+                $this->getProjectDir().'/src/core/Battle/',
+                $this->getProjectDir().'/src/core/Component/',
+                $this->getProjectDir().'/src/core/Console/',
+                $this->getProjectDir().'/src/core/Factory/',
+                $this->getProjectDir().'/src/core/Filter/',
+                $this->getProjectDir().'/src/core/Db/',
+                $this->getProjectDir().'/src/core/Nav/',
+                $this->getProjectDir().'/src/core/Patern/',
+                $this->getProjectDir().'/src/core/Translator/',
+                $this->getProjectDir().'/src/core/Validator/',
+                $this->getProjectDir().'/src/core/Template/Base.php',
+                $this->getProjectDir().'/src/core/Template/Theme.php',
+                $this->getProjectDir().'/src/core/EventManagerAware.php',
+                $this->getProjectDir().'/src/core/Event.php',
+                $this->getProjectDir().'/src/core/Http.php',
+                $this->getProjectDir().'/src/core/Modules.php',
+                $this->getProjectDir().'/src/core/ServiceManager.php',
+                $this->getProjectDir().'/src/core/Session.php',
+                $this->getProjectDir().'/src/core/Fixed/Cache.php',
+                $this->getProjectDir().'/src/core/Fixed/Dbwrapper.php',
+                $this->getProjectDir().'/src/core/Fixed/EventManager.php',
+                $this->getProjectDir().'/src/core/Fixed/Format.php',
+                $this->getProjectDir().'/src/core/Fixed/Http.php',
+                $this->getProjectDir().'/src/core/Fixed/HookManager.php',
+                $this->getProjectDir().'/src/core/Fixed/Locator.php',
+                $this->getProjectDir().'/src/core/Fixed/SymfonyForm.php',
+            ]);
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
