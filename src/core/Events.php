a529fac1c168e252b531d7c505eda75936568c25
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-29 18:47:04 +0200

    Add new events

--- src/core/Events.php
+++ src/core/Events.php
@@ -52,8 +52,25 @@ class Events
 
     public const PAGE_BIO_POST = 'lotgd.page.bio.post';
 
+    public const PAGE_BATTLE_TURN_START           = 'lotgd.page.battle.turn.start';
+    public const PAGE_BATTLE_TURN_END             = 'lotgd.page.battle.turn.end';
+    public const PAGE_BATTLE_ROUND_START_BUFF_PRE = 'lotgd.page.battle.round_start_buff_pre';
+    public const PAGE_BATTLE_ROUND_END            = 'lotgd.page.battle.round_end';
+    public const PAGE_BATTLE_PAGE_END             = 'lotgd.page.battle.page_end';
+    public const PAGE_BATTLE_END_VICTORY          = 'lotgd.page.battle.end_victory';
+    public const PAGE_BATTLE_END_DEFEAT           = 'lotgd.page.battle.end_defeat';
+
     public const PAGE_CLAN_PRE  = 'lotgd.page.clan.pre';
     public const PAGE_CLAN_POST = 'lotgd.page.clan.post';
+
+    public const PAGE_CREATE_PRE              = 'lotgd.page.create.pre';
+    public const PAGE_CREATE_POST             = 'lotgd.page.create.post';
+    public const PAGE_CREATE_CHECK_CREATION   = 'lotgd.page.create.check.creation';
+    public const PAGE_CREATE_FORGOT           = 'lotgd.page.create.forgot';
+    public const PAGE_CREATE_VAL              = 'lotgd.page.create.val';
+    public const PAGE_CREATE_FORGOTVAL        = 'lotgd.page.create.forgotval';
+    public const PAGE_CREATE_FORM             = 'lotgd.page.create.form';
+    public const PAGE_CREATE_PROCESS_CREATION = 'lotgd.page.create.process.creation';
 
     public const PAGE_DRAGON_PRE         = 'lotgd.page.dragon.pre';
     public const PAGE_DRAGON_POST        = 'lotgd.page.dragon.post';
@@ -117,14 +134,54 @@ class Events
     public const PAGE_MERCENARY_CAMP_PRE  = 'lotgd.page.mercenary.camp.pre';
     public const PAGE_MERCENARY_CAMP_POST = 'lotgd.page.mercenary.camp.post';
 
-    public const PAGE_NEWDAY      = 'lotgd.page.newday';
-    public const PAGE_NEWDAY_PRE  = 'lotgd.page.newday.pre';
-    public const PAGE_NEWDAY_POST = 'lotgd.page.newday.post';
-    public const PAGE_NEWDAY_INTERCEPT = 'lotgd.page.newday.intercept';
+    public const PAGE_NEWDAY                 = 'lotgd.page.newday';
+    public const PAGE_NEWDAY_PRE             = 'lotgd.page.newday.pre';
+    public const PAGE_NEWDAY_POST            = 'lotgd.page.newday.post';
+    public const PAGE_NEWDAY_INTERCEPT       = 'lotgd.page.newday.intercept';
     public const PAGE_NEWDAY_DK_POINT_LABELS = 'lotgd.page.newday.dk.point.labels';
 
-    public const PAGE_NEWS_INTERCEPT  = 'lotgd.page.news.intercept';
-    public const PAGE_NEWS_POST = 'lotgd.page.news.post';
+    public const PAGE_NAVIGATION_VILLAGE = 'lotgd.page.navigation.village';
+
+    public const PAGE_NEWS_INTERCEPT = 'lotgd.page.news.intercept';
+    public const PAGE_NEWS_POST      = 'lotgd.page.news.post';
 
     public const PAGE_PAYLOG = 'lotgd.page.paylog';
+
+    public const PAGE_PREFS_CHECK  = 'lotgd.page.prefs.check';
+    public const PAGE_PREFS_CHANGE = 'lotgd.page.prefs.change';
+    public const PAGE_PREFS_POST   = 'lotgd.page.prefs.post';
+
+    public const PAGE_PVP_POST = 'lotgd.page.pvp.post';
+
+    public const PAGE_REFERRAL_POST = 'lotgd.page.referral.post';
+
+    public const PAGE_ROCK_PRE  = 'lotgd.page.rock.pre';
+    public const PAGE_ROCK_POST = 'lotgd.page.rock.post';
+
+    public const PAGE_SHADES_PRE  = 'lotgd.page.shades.pre';
+    public const PAGE_SHADES_POST = 'lotgd.page.shades.post';
+
+    public const PAGE_STABLES_PRE    = 'lotgd.page.stables.pre';
+    public const PAGE_STABLES_POST   = 'lotgd.page.stables.post';
+    public const PAGE_STABLES_MOUNT  = 'lotgd.page.stables.mount';
+    public const PAGE_STABLES_BOUGHT = 'lotgd.page.stables.bought';
+    public const PAGE_STABLES_SOLD   = 'lotgd.page.stables.sold';
+
+    public const PAGE_TRAIN_PRE             = 'lotgd.page.train.pre';
+    public const PAGE_TRAIN_POST            = 'lotgd.page.train.post';
+    public const PAGE_TRAIN_AUTOCHALLENGE   = 'lotgd.page.train.autochallenge';
+    public const PAGE_TRAIN_TRANING_VICTORY = 'lotgd.page.train.traning.victory';
+    public const PAGE_TRAIN_TRANING_DEFEAT  = 'lotgd.page.train.traning.defeat';
+
+    public const PAGE_USER_VALIDATE_PREFS = 'lotgd.page.user.validate.prefs';
+
+    public const PAGE_PETITION_STATUS = 'lotgd.page.petition.status';
+
+    public const PAGE_VILLAGE          = 'lotgd.page.village';
+    public const PAGE_VILLAGE_PRE      = 'lotgd.page.village.pre';
+    public const PAGE_VILLAGE_POST     = 'lotgd.page.village.post';
+    public const PAGE_VILLAGE_LOCATION = 'lotgd.page.village.location';
+
+    public const PAGE_WEAPONS_PRE  = 'lotgd.page.weapons.pre';
+    public const PAGE_WEAPONS_POST = 'lotgd.page.weapons.post';
 }
