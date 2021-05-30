ec1fe2c5527d4a4d6fc4d50b6fb526a6fdc603a6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-30 14:10:15 +0200

    Mark as deprecated function

--- lib/gamelog.php
+++ lib/gamelog.php
@@ -1,8 +1,14 @@
 <?php
 
+/** @deprecated 5.3.0 Removed un future versions */
 function gamelog($message, $category = 'general', $filed = false)
 {
     global $session;
+
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.3.0; and delete in future version. Use LotgdLog::game($message, $category); instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
     $repository = \Doctrine::getRepository('LotgdCore:Gamelog');
 
