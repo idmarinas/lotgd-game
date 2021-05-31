aba94af0f606791f5d1ffb1bc09d8cf8e79e494d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-31 16:57:18 +0200

    First commit

---
+++ src/core/Fixed/Tool.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.3.0
+ */
+
+namespace Lotgd\Core\Fixed;
+
+use Lotgd\Core\Tool\Tool as ToolTool;
+
+class Tool
+{
+    /** @var \Lotgd\Core\Tool\Tool */
+    protected static $instance;
+
+    /**
+     * Add support for magic static method calls.
+     *
+     * @param string $name
+     * @param array  $arguments
+     * @param mixed  $method
+     *
+     * @return mixed the returned value from the resolved method
+     */
+    public static function __callStatic($method, $arguments)
+    {
+        if (\method_exists(self::$instance, $method))
+        {
+            return self::$instance->{$method}(...$arguments);
+        }
+
+        $methods = \implode(', ', \get_class_methods(self::$instance));
+
+        throw new \BadMethodCallException("Undefined method '{$method}'. The method name must be one of '{$methods}'");
+    }
+
+    public static function instance(ToolTool $instance)
+    {
+        self::$instance = $instance;
+    }
+}
+
+\class_alias('Lotgd\Core\Fixed\Tool', 'LotgdTool', false);
