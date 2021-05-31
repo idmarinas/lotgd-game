51864836d633a1b52a3e8f51cd9ddcc82fb17869
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-31 14:30:13 +0200

    Mark as deprecated function

--- lib/addnews.php
+++ lib/addnews.php
@@ -6,10 +6,18 @@
  * @param string $text
  * @param string $textDomain
  * @param bool   $hideFromBio
+ *
+ * @deprecated 5.3.0 Removed in future versions.
  */
 function addnews($text, array $params = [], $textDomain = 'partial_news', ?bool $hideFromBio = null)
 {
     global $session;
+
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.3.0; and delete in future version. Use "LotgdTool::addNews(string $text, array $params, string $textDomain, bool $hideFromBio);" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
 
     $user = $hideFromBio ? 0 : $session['user']['acctid'];
 
