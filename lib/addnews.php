19d7936cf8ffdeb736da3084360c344cdd9ce549
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-31 14:34:44 +0200

    Use the new service

--- lib/addnews.php
+++ lib/addnews.php
@@ -11,26 +11,10 @@
  */
 function addnews($text, array $params = [], $textDomain = 'partial_news', ?bool $hideFromBio = null)
 {
-    global $session;
-
     \trigger_error(\sprintf(
         'Usage of %s is obsolete since 5.3.0; and delete in future version. Use "LotgdTool::addNews(string $text, array $params, string $textDomain, bool $hideFromBio);" instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
 
-
-    $user = $hideFromBio ? 0 : $session['user']['acctid'];
-
-    $repository = \Doctrine::getRepository(\Lotgd\Core\Entity\News::class);
-
-    $newsEntity = $repository->hydrateEntity([
-        'date'       => new \DateTime('now'),
-        'text'       => $text,
-        'arguments'  => $params,
-        'textDomain' => $textDomain,
-        'accountId'  => $user ?? 0,
-    ]);
-
-    \Doctrine::persist($newsEntity);
-    \Doctrine::flush();
+    \LotgdTool::addNews($text, $params, $textDomain, $hideFromBio);
 }
