e4b233b998283c901f00ec0f50539958ccfecfba
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-05-31 14:32:23 +0200

    Use the new service

--- lib/gamelog.php
+++ lib/gamelog.php
@@ -3,23 +3,10 @@
 /** @deprecated 5.3.0 Removed un future versions */
 function gamelog($message, $category = 'general', $filed = false)
 {
-    global $session;
-
     \trigger_error(\sprintf(
         'Usage of %s is obsolete since 5.3.0; and delete in future version. Use LotgdLog::game($message, $category); instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
 
-    $repository = \Doctrine::getRepository('LotgdCore:Gamelog');
-
-    $entity = $repository->hydrateEntity([
-        'message'  => $message,
-        'category' => $category,
-        'filed'    => ($filed ? 1 : 0),
-        'date'     => new \DateTime('now'),
-        'who'      => (int) $session['user']['acctid'],
-    ]);
-
-    \Doctrine::persist($entity);
-    \Doctrine::flush();
+    \LotgdLog::game($message, $category, $filed);
 }
