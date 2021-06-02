e05e291ab225c65ddc3e15a21d811daa398c7a24
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-02 14:40:59 +0200

    Use controller to render page content

--- public/list.php
+++ public/list.php
@@ -4,9 +4,6 @@
 // translator ready
 // mail ready
 
-use Lotgd\Core\Events;
-use Symfony\Component\EventDispatcher\GenericEvent;
-
 define('ALLOW_ANONYMOUS', true);
 
 require_once 'common.php';
@@ -41,93 +38,26 @@ if ($session['user']['loggedin'])
 }
 else
 {
-    LotgdNavigation::addHeader('common.category.login');
-    LotgdNavigation::addNav('common.nav.login', 'index.php');
-    LotgdNavigation::addNav('list.nav.online', 'list.php');
-    LotgdNavigation::addNav('list.nav.full', 'list.php?page=1');
+    \LotgdNavigation::addHeader('common.category.login');
+    \LotgdNavigation::addNav('common.nav.login', 'index.php');
+    \LotgdNavigation::addNav('list.nav.online', 'list.php');
+    \LotgdNavigation::addNav('list.nav.full', 'list.php?page=1');
 }
 
 $op = \LotgdRequest::getQuery('op');
 $page = (int) \LotgdRequest::getQuery('page');
-$search = (string) \LotgdRequest::getPost('name');
-$playersperpage = 50;
-$params = [];
 
-$repository = \Doctrine::getRepository('LotgdCore:Accounts');
-$query = $repository->createQueryBuilder('u');
-
-$query
-    ->select('u.acctid', 'u.login', 'u.laston', 'u.loggedin', 'u.lastip', 'u.uniqueid')
-    ->addSelect('c.name', 'c.hitpoints', 'c.alive', 'c.location', 'c.race', 'c.sex', 'c.level')
-    ->where('u.locked = 0')
-    ->leftJoin('LotgdCore:Characters', 'c', 'with', $query->expr()->eq('c.id', 'u.character'))
-    ->orderBy('c.level', 'DESC')
-    ->addOrderBy('c.dragonkills', 'DESC')
-    ->addOrderBy('u.login', 'ASC')
-;
-
-if ('search' == $op)
-{
-    $query->andWhere('c.name LIKE :name')
-        ->setParameter('name', "%{$search}%")
-    ;
-}
-
-// Order the list by level, dragonkills, name so that the ordering is total!
-// Without this, some users would show up on multiple pages and some users
-// wouldn't show up
+$method = 'page';
 if (! $page && '' == $op)
 {
-    $query->andWhere('u.loggedin = 1');
-
-    $result = $repository->getPaginator($query, $page, $playersperpage);
-
-    $params['title'] = ['title' => 'list.warriors.online', 'params' => ['n' => $result->getTotalItemCount()]];
+    $method = 'index';
 }
 elseif ('clan' == $op)
 {
-    $query->andWhere('u.loggedin = 1 AND c.clanid = :clan')
-        ->setParameter('clan', $session['user']['clanid'])
-    ;
-
-    $result = $repository->getPaginator($query, $page, $playersperpage);
-
-    $params['title'] = ['title' => 'list.clan.online', 'params' => ['n' => $result->getTotalItemCount()]];
-}
-else
-{
-    $result = $repository->getPaginator($query, $page, $playersperpage);
-
-    $params['title'] = ['title' => 'list.warriors.singlepage'];
-
-    if ('search' == $op)
-    {
-        $params['title'] = ['title' => 'list.warriors.search', 'params' => [
-            'n' => $result->getTotalItemCount(),
-            'search' => $search
-        ]];
-    }
-    elseif ($result->count() >= 1)
-    {
-        $rangeMax = $result->getItemCountPerPage() * $result->count();
-        $rangeMax = ($result->getTotalItemCount() >= $rangeMax ? $rangeMax : $result->getTotalItemCount());
-
-        $params['title'] = ['title' => 'list.warriors.multipage', 'params' => [
-            'page' => $result->count(),
-            'rangeMin' => (($result->count() - 1) * $result->getItemCountPerPage()) + 1,
-            'rangeMax' => $rangeMax,
-            'totalCount' => $result->getTotalItemCount()
-        ]];
-    }
+    $method = 'clan';
 }
 
-$params['result'] = $result;
-\LotgdNavigation::pagination($result, 'list.php');
-
-$args = new GenericEvent(null, $params);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_LIST_POST);
-$params = modulehook('page-list-tpl-params', $args->getArguments());
-\LotgdResponse::pageAddContent(LotgdTheme::render('page/list.html.twig', $params));
+\LotgdResponse::callController(\Lotgd\Core\Controller\ListController::class, $method);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
---
+++ src/core/Controller/ListController.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.3.0
+ */
+
+namespace Lotgd\Core\Controller;
+
+use Lotgd\Core\EntityRepository\AccountsRepository;
+use Lotgd\Core\Events;
+use Lotgd\Core\Http\Request;
+use Lotgd\Core\Navigation\Navigation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class ListController extends AbstractController
+{
+    public const ITEM_PER_PAGE = 50;
+
+    private $dispatcher;
+    /** @var \Lotgd\Core\EntityRepository\AccountsRepository */
+    private $repository;
+    private $navigation;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, Navigation $navigation)
+    {
+        $this->dispatcher = $eventDispatcher;
+        $this->navigation = $navigation;
+    }
+
+    public function index(Request $request): Response
+    {
+        $search = (string) $request->request->getAlnum('name', '');
+        $params = [];
+
+        $query = $this->queryList($search);
+        $query->andWhere('u.loggedin = 1');
+
+        $result = $this->getRepository()->getPaginator($query, 1, self::ITEM_PER_PAGE);
+
+        $params = [
+            'title'  => ['title' => 'list.warriors.online', 'params' => ['n' => $result->getTotalItemCount()]],
+            'result' => $result,
+        ];
+
+        $this->navigation->pagination($result, 'list.php');
+
+        return $this->renderList($params);
+    }
+
+    public function clan(Request $request): Response
+    {
+        global $session;
+
+        $page   = (int) $request->query->getInt('page');
+        $search = (string) $request->request->getAlnum('name', '');
+
+        $query = $this->queryList($search);
+        $query->andWhere('u.loggedin = 1 AND c.clanid = :clan')
+            ->setParameter('clan', $session['user']['clanid'])
+        ;
+
+        $result = $this->getRepository()->getPaginator($query, $page, self::ITEM_PER_PAGE);
+        $this->navigation->pagination($result, 'list.php');
+
+        $params = [
+            'title' => ['title' => 'list.clan.online', 'params' => ['n' => $result->getTotalItemCount()]],
+        ];
+
+        return $this->renderList($params);
+    }
+
+    public function page(Request $request): Response
+    {
+        $page   = (int) $request->query->getInt('page');
+        $search = (string) $request->request->getAlnum('name', '');
+
+        $query  = $this->queryList($search);
+        $result = $this->getRepository()->getPaginator($query, $page, self::ITEM_PER_PAGE);
+
+        $params = [
+            'title' => ['title' => 'list.warriors.singlepage'],
+            'result' => $result,
+        ];
+
+        if ($search)
+        {
+            $params['title'] = ['title' => 'list.warriors.search', 'params' => [
+                'n'      => $result->getTotalItemCount(),
+                'search' => $search,
+            ]];
+        }
+        elseif ($result->count() >= 1)
+        {
+            $rangeMax = $result->getItemCountPerPage() * $result->count();
+            $rangeMax = ($result->getTotalItemCount() >= $rangeMax ? $rangeMax : $result->getTotalItemCount());
+
+            $params['title'] = ['title' => 'list.warriors.multipage', 'params' => [
+                'page'       => $result->count(),
+                'rangeMin'   => (($result->count() - 1) * $result->getItemCountPerPage()) + 1,
+                'rangeMax'   => $rangeMax,
+                'totalCount' => $result->getTotalItemCount(),
+            ]];
+        }
+
+        return $this->renderList($params);
+    }
+
+    private function queryList(?string $search = null)
+    {
+        $query = $this->getRepository()->createQueryBuilder('u');
+
+        $query
+            ->select('u.acctid', 'u.login', 'u.laston', 'u.loggedin', 'u.lastip', 'u.uniqueid')
+            ->addSelect('c.name', 'c.hitpoints', 'c.alive', 'c.location', 'c.race', 'c.sex', 'c.level')
+            ->where('u.locked = 0')
+            ->leftJoin('LotgdCore:Characters', 'c', 'with', $query->expr()->eq('c.id', 'u.character'))
+            ->orderBy('c.level', 'DESC')
+            ->addOrderBy('c.dragonkills', 'DESC')
+            ->addOrderBy('u.login', 'ASC')
+        ;
+
+        if ($search)
+        {
+            $query->andWhere('c.name LIKE :name')
+                ->setParameter('name', "%{$search}%")
+            ;
+        }
+
+        return $query;
+    }
+
+    private function getRepository(): AccountsRepository
+    {
+        if ( ! $this->repository instanceof AccountsRepository)
+        {
+            $this->repository = $this->getDoctrine()->getRepository('LotgdCore:Accounts');
+        }
+
+        return $this->repository;
+    }
+
+    private function renderList(array $params): Response
+    {
+        $args = new GenericEvent(null, $params);
+        $this->dispatcher->dispatch($args, Events::PAGE_LIST_POST);
+        $params = modulehook('page-list-tpl-params', $args->getArguments());
+
+        return $this->render('page/list.html.twig', $params);
+    }
+}
