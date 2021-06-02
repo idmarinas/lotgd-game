c015e9083384bf2b284c2a8a3624fcea12499c05
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-02 11:45:16 +0200

    Use controller to render page content

--- public/about.php
+++ public/about.php
@@ -1,115 +1,57 @@
 <?php
 
-// translator ready
-// addnews ready
-// mail ready
-
-use Lotgd\Core\Events;
-use Symfony\Component\EventDispatcher\GenericEvent;
-
 \define('ALLOW_ANONYMOUS', true);
+
 require_once 'common.php';
-require_once 'lib/showform.php';
 
 //-- Init page
 \LotgdResponse::pageStart('title', [], 'page_about');
 
 checkday();
-$op = LotgdRequest::getQuery('op');
+$op = \LotgdRequest::getQuery('op');
 
 if ($session['user']['loggedin'])
 {
-    LotgdNavigation::addNav('common.nav.news', 'news.php');
+    \LotgdNavigation::addNav('common.nav.news', 'news.php');
 }
 else
 {
-    LotgdNavigation::addHeader('common.category.login');
-    LotgdNavigation::addNav('common.nav.login', 'index.php');
+    \LotgdNavigation::addHeader('common.category.login');
+    \LotgdNavigation::addNav('common.nav.login', 'index.php');
 }
 
-LotgdNavigation::addHeader('about.category.about');
-LotgdNavigation::addNav('about.nav.about', 'about.php');
-LotgdNavigation::addNav('about.nav.setup', 'about.php?op=setup');
-LotgdNavigation::addNav('about.nav.module', 'about.php?op=listmodules');
-LotgdNavigation::addNav('about.nav.license', 'about.php?op=license');
-
-$params = [
-    'block_tpl' => 'about_home',
-];
+\LotgdNavigation::addHeader('about.category.about');
+\LotgdNavigation::addNav('about.nav.about', 'about.php');
+\LotgdNavigation::addNav('about.nav.setup', 'about.php?op=setup');
+\LotgdNavigation::addNav('about.nav.module', 'about.php?op=listmodules');
+\LotgdNavigation::addNav('about.nav.license', 'about.php?op=license');
 
 if ('listmodules' == $op)
 {
-    LotgdNavigation::blockLink('about.php?op=listmodules');
+    \LotgdNavigation::blockLink('about.php?op=listmodules');
 
-    $params['block_tpl'] = 'about_modules';
-
-    $repository       = \Doctrine::getRepository(\Lotgd\Core\Entity\Modules::class);
-    $params['result'] = $repository->findBy(['active' => 1], ['category' => 'ASC', 'formalname' => 'ASC']);
+    $method = 'modules';
 }
 elseif ('setup' == $op)
 {
-    LotgdNavigation::blockLink('about.php?op=setup');
+    \LotgdNavigation::blockLink('about.php?op=setup');
 
-    $params['block_tpl'] = 'about_setup';
-
-    $details       = gametimedetails();
-    $secstonextday = secondstonextgameday($details);
-    $useful_vals   = [
-        'dayduration'   => \round(($details['dayduration'] / 60 / 60), 0).' hours',
-        'curgametime'   => getgametime(),
-        'curservertime' => \date('Y-m-d h:i:s a'),
-        'lastnewday'    => \date('h:i:s a', \strtotime("-{$details['realsecssofartoday']} seconds")),
-        'nextnewday'    => \date('h:i:s a', \strtotime("+{$details['realsecstotomorrow']} seconds")).' ('.\date('H\\h i\\m s\\s', $secstonextday).')',
-    ];
-
-    $settings = \LotgdKernel::get(Lotgd\Core\Lib\Settings::class);
-    $localsettings = $settings->getArray();
-
-    $vals = \array_merge($localsettings, $useful_vals);
-
-    $data = [
-        'game_setup' => $vals,
-        'newday'     => $vals,
-        'bank'       => $vals,
-        'forest'     => $vals,
-        'mail'       => $vals,
-        'content'    => $vals,
-        'info'       => $vals,
-    ];
-
-    $lotgdFormFactory = \LotgdKernel::get('form.factory');
-
-    $form = $lotgdFormFactory->create(Lotgd\Core\Form\AboutType::class, $data, [
-        'action' => 'none',
-        'method' => 'none'
-    ]);
-
-    $params['form'] = $form->createView();
+    $method = 'setup';
 }
 elseif ('license' == $op)
 {
-    LotgdNavigation::blockLink('about.php?op=license');
+    \LotgdNavigation::blockLink('about.php?op=license');
 
-    $params['block_tpl'] = 'about_license';
+    $method = 'license';
 }
 else
 {
-    LotgdNavigation::blockLink('about.php');
+    \LotgdNavigation::blockLink('about.php');
 
-    $args = new GenericEvent();
-    \LotgdEventDispatcher::dispatch($args, Events::PAGE_ABOUT);
-    $results = modulehook('about', $args->getArguments());
-
-    if (\is_array($results) && \count($results))
-    {
-        $params['hookAbout'] = $results;
-    }
+    $method = 'index';
 }
 
-$args = new GenericEvent(null, $params);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_ABOUT_POST);
-$params = modulehook('page-about-tpl-params', $args->getArguments());
-\LotgdResponse::pageAddContent(\LotgdTheme::render('admin/page/about.html.twig', $params));
+\LotgdResponse::callController(\Lotgd\Core\Controller\AboutController::class, $method);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
---
+++ src/core/Controller/AboutController.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.3.0
+ */
+
+namespace Lotgd\Core\Controller;
+
+use Lotgd\Core\Events;
+use Lotgd\Core\Lib\Settings;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class AboutController extends AbstractController
+{
+    private $dispatcher;
+    private $settings;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, Settings $settings)
+    {
+        $this->dispatcher = $eventDispatcher;
+        $this->settings   = $settings;
+    }
+
+    public function setup()
+    {
+        $params['block_tpl'] = 'about_setup';
+
+        $details       = gametimedetails();
+        $secstonextday = secondstonextgameday($details);
+        $useful_vals   = [
+            'dayduration'   => \round(($details['dayduration'] / 60 / 60), 0).' hours',
+            'curgametime'   => getgametime(),
+            'curservertime' => \date('Y-m-d h:i:s a'),
+            'lastnewday'    => \date('h:i:s a', \strtotime("-{$details['realsecssofartoday']} seconds")),
+            'nextnewday'    => \date('h:i:s a', \strtotime("+{$details['realsecstotomorrow']} seconds")).' ('.\date('H\\h i\\m s\\s', $secstonextday).')',
+        ];
+
+        $localsettings = $this->settings->getArray();
+
+        $vals = \array_merge($localsettings, $useful_vals);
+
+        $data = [
+            'game_setup' => $vals,
+            'newday'     => $vals,
+            'bank'       => $vals,
+            'forest'     => $vals,
+            'mail'       => $vals,
+            'content'    => $vals,
+            'info'       => $vals,
+        ];
+
+        $form = $this->createForm(\Lotgd\Core\Form\AboutType::class, $data, [
+            'action' => 'none',
+            'method' => 'none',
+        ]);
+
+        $params['form'] = $form->createView();
+
+        return $this->renderAbout($params);
+    }
+
+    public function index(): Response
+    {
+        $params = [
+            'block_tpl' => 'about_home',
+        ];
+
+        $args = new GenericEvent();
+        $this->dispatcher->dispatch($args, Events::PAGE_ABOUT);
+        $results = modulehook('about', $args->getArguments());
+
+        if (\is_array($results) && \count($results))
+        {
+            $params['hookAbout'] = $results;
+        }
+
+        return $this->renderAbout($params);
+    }
+
+    public function modules()
+    {
+        $params = [
+            'block_tpl' => 'about_modules',
+            'result'    => $this->getDoctrine()->getRepository('LotgdCore:Modules')->findBy(['active' => 1], ['category' => 'ASC', 'formalname' => 'ASC']),
+        ];
+
+        return $this->renderAbout($params);
+    }
+
+    public function license()
+    {
+        $params = ['block_tpl' => 'about_license'];
+
+        return $this->renderAbout($params);
+    }
+
+    private function renderAbout(array $params): Response
+    {
+        $args = new GenericEvent(null, $params);
+        $this->dispatcher->dispatch($args, Events::PAGE_ABOUT_POST);
+        $params = modulehook('page-about-tpl-params', $args->getArguments());
+
+        return $this->render('admin/page/about.html.twig', $params);
+    }
+}
