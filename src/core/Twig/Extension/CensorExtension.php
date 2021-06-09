76f7ef84d86adb5797a907e218d2f4e8e14bfa62
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-09 16:59:26 +0200

    First commit

---
+++ src/core/Twig/Extension/CensorExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Core\Twig\Extension;
+
+use Lotgd\Core\Output\Censor;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class CensorCore extends AbstractExtension
+{
+    protected $censor;
+
+    public function __construct(Censor $censor)
+    {
+        $this->censor = $censor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('censor', [$this->censor, 'filter']),
+        ];
+    }
+}
