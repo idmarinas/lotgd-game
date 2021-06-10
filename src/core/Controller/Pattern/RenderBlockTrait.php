18e244aba2775e759bece4c639726ac596726015
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-10 11:19:52 +0200

    First commit

---
+++ src/core/Controller/Pattern/RenderBlockTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Core\Controller\Pattern;
+
+use Symfony\Component\HttpFoundation\Response;
+
+trait RenderBlockTrait
+{
+    /**
+     * Renders a view block.
+     */
+    protected function renderBlock(string $view, string $block, array $parameters = [], ?Response $response = null): Response
+    {
+        $content = $this->container->get('twig')->load($view)->renderBlock($block, $parameters);
+
+        if (null === $response)
+        {
+            $response = new Response();
+        }
+
+        $response->setContent($content);
+
+        return $response;
+    }
+}
