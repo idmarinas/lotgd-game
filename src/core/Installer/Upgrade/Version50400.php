56e0767489915744ab45fe3daafe582e9f99b114
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-11 17:06:57 +0200

    First commit

---
+++ src/core/Installer/Upgrade/Version50400.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Core\Installer\Upgrade;
+
+use Lotgd\Core\Installer\InstallerAbstract;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Version50400 extends InstallerAbstract
+{
+    protected $upgradeVersion = 50400;
+    protected $hasMigration = false;
+
+    //-- Delete old files
+    public function step0()
+    {
+        $fs = new Filesystem();
+
+        try
+        {
+            $fs->remove([
+                $this->getProjectDir().'/lib/clan/', //-- Deleted old files of clan
+            ]);
+        }
+        catch (\Throwable $th)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
