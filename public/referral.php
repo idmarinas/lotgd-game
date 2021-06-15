8dcf365d024fd3d4858381d040ea7d43147bcf6a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-15 12:56:52 +0200

    Use controller to render page

--- public/referral.php
+++ public/referral.php
@@ -4,12 +4,12 @@
 // addnews ready
 // mail ready
 
-use Lotgd\Core\Events;
-use Symfony\Component\EventDispatcher\GenericEvent;
-
 define('ALLOW_ANONYMOUS', true);
 
 require_once 'common.php';
+
+/** @var Lotgd\Core\Http\Request */
+$request = \LotgdKernel::get(\Lotgd\Core\Http\Request::class);
 
 $textDomain = 'page_referral';
 
@@ -38,35 +38,9 @@ else
     \LotgdNavigation::villageNav();
 }
 
-$url = getsetting('serverurl', sprintf('%s://%s', \LotgdRequest::getServer('REQUEST_SCHEME'), \LotgdRequest::getServer('HTTP_HOST')));
+$request->attributes->set('params', $params);
 
-if (! preg_match('/\\/$/', $url))
-{
-    $url = $url.'/';
-    savesetting('serverurl', $url);
-}
-
-$params['serverUrl'] = $url;
-$params['refererAward'] = getsetting('refereraward', 25);
-$params['referMinLevel'] = getsetting('referminlevel', 4);
-
-$repository = \Doctrine::getRepository('LotgdCore:Accounts');
-$query = $repository->createQueryBuilder('u');
-
-$params['referrers'] = $query->select('u.refererawarded')
-    ->addSelect('c.name', 'c.level', 'c.dragonkills')
-    ->where('u.referer = :acct')
-    ->leftJoin('LotgdCore:Characters', 'c', 'WITH', $query->expr()->eq('c.id', 'u.character'))
-    ->setParameter('acct', $session['user']['acctid'])
-
-    ->getQuery()
-    ->getResult()
-;
-
-$args = new GenericEvent(null, $params);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_REFERRAL_POST);
-$params = modulehook('page-referral-tpl-params', $args->getArguments());
-\LotgdResponse::pageAddContent(\LotgdTheme::render('page/referral.html.twig', $params));
+\LotgdResponse::callController(Lotgd\Core\Controller\ReferralController::class);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
---
+++ src/core/Controller/ReferralController.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Core\Controller;
+
+use Lotgd\Core\Events;
+use Lotgd\Core\Http\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class ReferralController extends AbstractController
+{
+    private $dispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->dispatcher = $eventDispatcher;
+    }
+
+    public function index(array $params, Request $request): Response
+    {
+        global $session;
+
+        $url = getsetting('serverurl', \sprintf('%s://%s', $request->getServer('REQUEST_SCHEME'), $request->getServer('HTTP_HOST')));
+
+        if ( ! \preg_match('/\\/$/', $url))
+        {
+            $url = $url.'/';
+            savesetting('serverurl', $url);
+        }
+
+        $params['serverUrl']     = $url;
+        $params['refererAward']  = getsetting('refereraward', 25);
+        $params['referMinLevel'] = getsetting('referminlevel', 4);
+
+        /** @var Lotgd\Core\EntityRepository\AccountsRepository */
+        $repository = $this->getDoctrine()->getRepository('LotgdCore:Accounts');
+        $query      = $repository->createQueryBuilder('u');
+
+        $params['referrers'] = $query->select('u.refererawarded')
+            ->addSelect('c.name', 'c.level', 'c.dragonkills')
+            ->where('u.referer = :acct')
+            ->leftJoin('LotgdCore:Characters', 'c', 'WITH', $query->expr()->eq('c.id', 'u.character'))
+            ->setParameter('acct', $session['user']['acctid'])
+
+            ->getQuery()
+            ->getResult()
+        ;
+
+        $args = new GenericEvent(null, $params);
+        $this->dispatcher->dispatch($args, Events::PAGE_REFERRAL_POST);
+        $params = modulehook('page-referral-tpl-params', $args->getArguments());
+
+        return $this->render('page/referral.html.twig', $params);
+    }
+}
