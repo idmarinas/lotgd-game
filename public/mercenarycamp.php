585e36e0660be17401bf48eed8ced2289fc2359c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-15 10:43:18 +0200

    Use controller to render page

--- public/mercenarycamp.php
+++ public/mercenarycamp.php
@@ -20,10 +20,11 @@ $textDomain = $result['textDomain'];
 $textDomainNavigation = $result['textDomainNavigation'];
 unset($result);
 
+/** @var Lotgd\Core\Http\Request */
+$request = \LotgdKernel::get(\Lotgd\Core\Http\Request::class);
+
 //-- Change text domain for navigation
 \LotgdNavigation::setTextDomain($textDomainNavigation);
-
-$repository = \Doctrine::getRepository('LotgdCore:Companions');
 
 \LotgdNavigation::addHeader('category.navigation');
 
@@ -34,168 +35,28 @@ $params = [
     'textDomain' => $textDomain,
 ];
 
-$op = (string) \LotgdRequest::getQuery('op');
-$skip = (int) \LotgdRequest::getQuery('skip');
+$op = (string) $request->query->get('op');
 
-if ('' == $op)
+$method = 'index';
+
+if ('heal' == $op)
 {
-    $params['tpl'] = 'default';
-    $params['showDescription'] = ! $skip;
-    $params['companions'] = $repository->getMercenaryList($session['user']['location'], $session['user']['dragonkills']);
-
-    \LotgdNavigation::addHeader('category.buynav');
-
-    foreach ($params['companions'] as $row)
-    {
-        if ($row['companioncostgold'] || $row['companioncostgems'])
-        {
-            $link = '';
-            $navParams = [
-                'params' => [
-                    'name' => $row['name'],
-                    'costGold' => $row['companioncostgold'],
-                    'costGems' => $row['companioncostgems']
-                ]
-            ];
-
-            if ($session['user']['gold'] >= $row['companioncostgold'] && $session['user']['gems'] >= $row['companioncostgems'] && ! isset($companions[$row['name']]))
-            {
-                $link = "mercenarycamp.php?op=buy&id={$row['companionid']}";
-            }
-
-            \LotgdNavigation::addNav('nav.companion.cost', $link, $navParams);
-        }
-        elseif (! isset($companions[$row['name']]))
-        {
-            \LotgdNavigation::addNav($row['name'], "mercenarycamp.php?op=buy&id={$row['companionid']}", ['translate' => false]);
-        }
-    }
-
-    $params['companionWounds'] = healnav();
-}
-elseif ('heal' == $op)
-{
-    $params['tpl'] = 'heal';
-
-    $name = stripslashes(rawurldecode(\LotgdRequest::getQuery('name')));
-
-    $pointsToHeal = $companions[$name]['maxhitpoints'] - $companions[$name]['hitpoints'];
-    $costToHeal = round(log($session['user']['level'] + 1) * ($pointsToHeal + 10) * 1.33);
-
-    $params['companionHealed'] = false;
-    if ($session['user']['gold'] >= $costToHeal)
-    {
-        $params['companionHealed'] = true;
-
-        $companions[$name]['hitpoints'] = $companions[$name]['maxhitpoints'];
-        $session['user']['companions'] = $companions;
-        $session['user']['gold'] -= $costToHeal;
-
-        \LotgdLog::debug("spent {$costToHeal} gold on healing a companion", false, false, 'healcompanion', $costToHeal);
-    }
-
-    $params['companionName'] = $companions[$name]['name'];
-
-    $params['companionWounds'] = healnav();
-
-    \LotgdNavigation::addHeader('category.navigation');
-    \LotgdNavigation::addNav('nav.return', 'mercenarycamp.php?skip=1');
+    $method = 'heal';
 }
 elseif ('buy' == $op)
 {
-    require_once 'lib/buffs.php';
-
-    $params['tpl'] = 'buy';
-
-    $companionId = (int) \LotgdRequest::getQuery('id');
-
-    $entity = $repository->find($companionId);
-
-    $params['companionHire'] = 'not.found';
-
-    if ($entity)
-    {
-        $row = $repository->extractEntity($entity);
-
-        $row['attack'] = $row['attack'] + $row['attackperlevel'] * $session['user']['level'];
-        $row['defense'] = $row['defense'] + $row['defenseperlevel'] * $session['user']['level'];
-        $row['maxhitpoints'] = $row['maxhitpoints'] + $row['maxhitpointsperlevel'] * $session['user']['level'];
-        $row['hitpoints'] = $row['maxhitpoints'];
-
-        $params['companionHire'] = apply_companion($row['name'], $row);
-
-        if ($params['companionHire'])
-        {
-            $session['user']['gold'] -= $row['companioncostgold'];
-            $session['user']['gems'] -= $row['companioncostgems'];
-
-            \LotgdLog::debug("has spent {$row['companioncostgold']} gold and {$row['companioncostgems']} gems on hiring a mercenary ({$row['name']}).");
-        }
-    }
-
-    \LotgdNavigation::addHeader('category.navigation');
-    \LotgdNavigation::addNav('nav.return', 'mercenarycamp.php?skip=1');
+    $method = 'buy';
 }
 
 \LotgdNavigation::addHeader('category.navigation');
 \LotgdNavigation::villageNav();
 
+$request->attributes->set('params', $params);
+
+\LotgdResponse::callController(Lotgd\Core\Controller\MercenaryCampController::class, $method);
+
 //-- Restore text domain for navigation
 \LotgdNavigation::setTextDomain();
 
-//-- This is only for params not use for other purpose
-$args = new GenericEvent(null, $params);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_MERCENARY_CAMP_POST);
-$params = modulehook('page-mercenarycamp-tpl-params', $args->getArguments());
-\LotgdResponse::pageAddContent(\LotgdTheme::render('page/mercenarycamp.html.twig', $params));
-
 //-- Finalize page
 \LotgdResponse::pageEnd();
-
-/**
- * Undocumented function.
- *
- * @return bool
- */
-function healnav(): bool
-{
-    global $session, $companions;
-
-    \LotgdNavigation::addHeader('category.companion.heal');
-
-    $healable = false;
-
-    foreach ($companions as $name => $companion)
-    {
-        if ($companion['cannotbehealed'] ?? false)
-        {
-            continue;
-        }
-
-        $pointsToHeal = $companion['maxhitpoints'] - $companion['hitpoints'];
-
-        if ($pointsToHeal > 0)
-        {
-            $healable = true;
-            $costToHeal = round(log($session['user']['level'] + 1) * ($pointsToHeal + 10) * 1.33);
-
-            $nav = 'nav.companion.heal.not.have';
-            $link = '';
-
-            if ($session['user']['gold'] >= $costToHeal)
-            {
-                $nav = 'nav.companion.heal.have';
-                $link = 'mercenarycamp.php?op=heal&name='.rawurlencode($name);
-            }
-
-            \LotgdNavigation::addNav($nav, $link, [
-                'params' => [
-                    'name' => $companion['name'],
-                    'costGold' => $costToHeal
-                ]
-            ]);
-        }
-    }
-
-    return $healable;
-}
---
+++ src/core/Controller/MercenaryCampController.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.4.0
+ */
+
+namespace Lotgd\Core\Controller;
+
+use Lotgd\Core\EntityRepository\CompanionsRepository;
+use Lotgd\Core\Events;
+use Lotgd\Core\Http\Request;
+use Lotgd\Core\Log;
+use Lotgd\Core\Navigation\Navigation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+class MercenaryCampController extends AbstractController
+{
+    private $navigation;
+    private $dispatcher;
+    private $repository;
+    private $log;
+
+    public function __construct(Navigation $navigation, EventDispatcherInterface $eventDispatcher, Log $log)
+    {
+        $this->navigation = $navigation;
+        $this->dispatcher = $eventDispatcher;
+        $this->log        = $log;
+    }
+
+    public function heal(array $params, Request $request): Response
+    {
+        global $session, $companions;
+
+        $params['tpl'] = 'heal';
+
+        $name = \stripslashes(\rawurldecode($request->query->get('name')));
+
+        $pointsToHeal = $companions[$name]['maxhitpoints'] - $companions[$name]['hitpoints'];
+        $costToHeal   = \round(\log($session['user']['level'] + 1) * ($pointsToHeal + 10) * 1.33);
+
+        $params['companionHealed'] = false;
+        if ($session['user']['gold'] >= $costToHeal)
+        {
+            $params['companionHealed'] = true;
+
+            $companions[$name]['hitpoints'] = $companions[$name]['maxhitpoints'];
+            $session['user']['companions']  = $companions;
+            $session['user']['gold'] -= $costToHeal;
+
+            $this->log->debug("spent {$costToHeal} gold on healing a companion", false, false, 'healcompanion', $costToHeal);
+        }
+
+        $params['companionName'] = $companions[$name]['name'];
+
+        $params['companionWounds'] = $this->healNav();
+
+        $this->navigation->addHeader('category.navigation');
+        $this->navigation->addNav('nav.return', 'mercenarycamp.php?skip=1');
+
+        return $this->renderCamp($params);
+    }
+
+    public function buy(array $params, Request $request): Response
+    {
+        require_once 'lib/buffs.php';
+
+        $params['tpl'] = 'buy';
+
+        $companionId = $request->query->getInt('id');
+
+        $entity = $this->getRepository()->find($companionId);
+
+        $params['companionHire'] = 'not.found';
+
+        if ($entity)
+        {
+            $row = $this->getRepository()->extractEntity($entity);
+
+            $row['attack']       = $row['attack']       + $row['attackperlevel']       * $session['user']['level'];
+            $row['defense']      = $row['defense']      + $row['defenseperlevel']      * $session['user']['level'];
+            $row['maxhitpoints'] = $row['maxhitpoints'] + $row['maxhitpointsperlevel'] * $session['user']['level'];
+            $row['hitpoints']    = $row['maxhitpoints'];
+
+            $params['companionHire'] = apply_companion($row['name'], $row);
+
+            if ($params['companionHire'])
+            {
+                $session['user']['gold'] -= $row['companioncostgold'];
+                $session['user']['gems'] -= $row['companioncostgems'];
+
+                $this->log->debug("has spent {$row['companioncostgold']} gold and {$row['companioncostgems']} gems on hiring a mercenary ({$row['name']}).");
+            }
+        }
+
+        $this->navigation->addHeader('category.navigation');
+        $this->navigation->addNav('nav.return', 'mercenarycamp.php?skip=1');
+
+        return $this->renderCamp($params);
+    }
+
+    public function index(array $params, Request $request): Response
+    {
+        global $session, $companions;
+
+        $skip = $request->query->getInt('skip');
+
+        $params['tpl']             = 'default';
+        $params['showDescription'] = ! $skip;
+        $params['companions']      = $this->getRepository()->getMercenaryList($session['user']['location'], $session['user']['dragonkills']);
+
+        $this->navigation->addHeader('category.buynav');
+
+        foreach ($params['companions'] as $row)
+        {
+            if ($row['companioncostgold'] || $row['companioncostgems'])
+            {
+                $link      = '';
+                $navParams = [
+                    'params' => [
+                        'name'     => $row['name'],
+                        'costGold' => $row['companioncostgold'],
+                        'costGems' => $row['companioncostgems'],
+                    ],
+                ];
+
+                if ($session['user']['gold'] >= $row['companioncostgold'] && $session['user']['gems'] >= $row['companioncostgems'] && ! isset($companions[$row['name']]))
+                {
+                    $link = "mercenarycamp.php?op=buy&id={$row['companionid']}";
+                }
+
+                $this->navigation->addNav('nav.companion.cost', $link, $navParams);
+            }
+            elseif ( ! isset($companions[$row['name']]))
+            {
+                $this->navigation->addNav($row['name'], "mercenarycamp.php?op=buy&id={$row['companionid']}", ['translate' => false]);
+            }
+        }
+
+        $params['companionWounds'] = $this->healNav();
+
+        return $this->renderCamp($params);
+    }
+
+    private function renderCamp(array $params): Response
+    {
+        //-- This is only for params not use for other purpose
+        $args = new GenericEvent(null, $params);
+        $this->dispatcher->dispatch($args, Events::PAGE_MERCENARY_CAMP_POST);
+        $params = modulehook('page-mercenarycamp-tpl-params', $args->getArguments());
+
+        return $this->render('page/mercenarycamp.html.twig', $params);
+    }
+
+    private function getRepository(): CompanionsRepository
+    {
+        if ( ! $this->repository instanceof CompanionsRepository)
+        {
+            $this->repository = $this->getDoctrine()->getRepository('LotgdCore:Companions');
+        }
+
+        return $this->repository;
+    }
+
+    /**
+     * Navs for heal companions.
+     */
+    private function healNav(): bool
+    {
+        global $session, $companions;
+
+        $this->navigation->addHeader('category.companion.heal');
+
+        $healable = false;
+
+        foreach ($companions as $name => $companion)
+        {
+            if ($companion['cannotbehealed'] ?? false)
+            {
+                continue;
+            }
+
+            $pointsToHeal = $companion['maxhitpoints'] - $companion['hitpoints'];
+
+            if ($pointsToHeal > 0)
+            {
+                $healable   = true;
+                $costToHeal = \round(\log($session['user']['level'] + 1) * ($pointsToHeal + 10) * 1.33);
+
+                $nav  = 'nav.companion.heal.not.have';
+                $link = '';
+
+                if ($session['user']['gold'] >= $costToHeal)
+                {
+                    $nav  = 'nav.companion.heal.have';
+                    $link = 'mercenarycamp.php?op=heal&name='.\rawurlencode($name);
+                }
+
+                $this->navigation->addNav($nav, $link, [
+                    'params' => [
+                        'name'     => $companion['name'],
+                        'costGold' => $costToHeal,
+                    ],
+                ]);
+            }
+        }
+
+        return $healable;
+    }
+}
