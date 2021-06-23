196de97ba07955eeb69eecd3f5595745dd5d9c22
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-23 13:39:19 +0200

    Mark as deprecated

--- lib/datetime.php
+++ lib/datetime.php
@@ -4,119 +4,79 @@
 // translator ready
 // mail ready
 
-/**
- * Check if is a new day.
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->checkDay()" instead. Removed in future versions. */
 function checkday()
 {
-    global $session, $revertsession;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->checkDay()" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if ($session['user']['loggedin'] ?? false)
-    {
-        \LotgdResponse::pageAddContent('<!--CheckNewDay()-->');
-
-        if (is_new_day())
-        {
-            $post = $_POST;
-            unset($post['i_am_a_hack']);
-
-            if (! empty($post))
-            {
-                $session['user']['lasthit'] = new \DateTime('0000-00-00 00:00:00');
-            }
-            else
-            {
-                $session                        = $revertsession;
-                $session['user']['restorepage'] = \LotgdRequest::getServer('REQUEST_URI');
-                $session['user']['allowednavs'] = [];
-                \LotgdNavigation::addNavAllow('newday.php');
-
-                redirect('newday.php');
-            }
-        }
-    }
+    \LotgdKernel::get('lotgd_core.tool.date_time')->checkDay();
 }
 
-/**
- * Check if is a new day
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->isNewDay()" instead. Removed in future versions. */
 function is_new_day(): bool
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->isNewDay()" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if (new \DateTime('0000-00-00 00:00:00') == $session['user']['lasthit'])
-    {
-        return true;
-    }
-
-    $t1 = gametime();
-    $t2 = convertgametime($session['user']['lasthit']->getTimestamp());
-    $d1 = \gmdate('Y-m-d', $t1);
-    $d2 = \gmdate('Y-m-d', $t2);
-
-    return (bool) ($d1 != $d2);
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->isNewDay();
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->getGameTime()" instead. Removed in future versions. */
 function getgametime()
 {
-    return \gmdate(getsetting('gametime', 'g:i a'), gametime());
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->getGameTime()" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->getGameTime();
 }
 
-/**
- * Undocumented function.
- *
- * @return int
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->gameTime()" instead. Removed in future versions. */
 function gametime()
 {
-    return convertgametime(\time());
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->gameTime()" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->gameTime();
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->convertGameTime($intime, $debug)" instead. Removed in future versions. */
 function convertgametime(int $intime, $debug = false)
 {
-    //adjust the requested time by the game offset
-    $intime -= (int) getsetting('gameoffsetseconds', 0);
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->convertGameTime($intime, $debug)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    // we know that strtotime gives us an identical timestamp for
-    // everywhere in the world at the same time, if it is provided with
-    // the GMT offset:
-    $epoch          = \strtotime(getsetting('game_epoch', \gmdate('Y-m-d 00:00:00 O', \strtotime('-30 days'))));
-    $now            = \strtotime(\gmdate('Y-m-d H:i:s O', $intime));
-    $logd_timestamp = \round(($now - $epoch) * getsetting('daysperday', 4), 0);
-
-    if ($debug)
-    {
-        \LotgdResponse::pageDebug('Game Timestamp: '.$logd_timestamp.', which makes it '.\gmdate('Y-m-d H:i:s', $logd_timestamp));
-    }
-
-    return (int) $logd_timestamp;
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->convertGameTime($intime, (bool) $debug);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->gameTimeDetails()" instead. Removed in future versions. */
 function gametimedetails()
 {
-    $ret                       = [];
-    $ret['now']                = \date('Y-m-d 00:00:00');
-    $ret['gametime']           = gametime();
-    $ret['daysperday']         = getsetting('daysperday', 4);
-    $ret['secsperday']         = 86400 / $ret['daysperday'];
-    $ret['today']              = \strtotime(\gmdate('Y-m-d 00:00:00 O', $ret['gametime']));
-    $ret['tomorrow']           = \strtotime(\gmdate('Y-m-d H:i:s O', $ret['gametime']).' + 1 day');
-    $ret['tomorrow']           = \strtotime(\gmdate('Y-m-d 00:00:00 O', $ret['tomorrow']));
-    $ret['secssofartoday']     = $ret['gametime'] - $ret['today'];
-    $ret['secstotomorrow']     = $ret['tomorrow'] - $ret['gametime'];
-    $ret['realsecssofartoday'] = $ret['secssofartoday']             / $ret['daysperday'];
-    $ret['realsecstotomorrow'] = $ret['secstotomorrow']             / $ret['daysperday'];
-    $ret['dayduration']        = ($ret['tomorrow'] - $ret['today']) / $ret['daysperday'];
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->gameTimeDetails()" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    return $ret;
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->gameTimeDetails();
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.date_time')->secondsToNextGameDay($details)" instead. Removed in future versions. */
 function secondstonextgameday($details = false)
 {
-    if (false === $details)
-    {
-        $details = gametimedetails();
-    }
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.date_time")->secondsToNextGameDay($details)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    return \strtotime("{$details['now']} + {$details['realsecstotomorrow']} seconds");
+    return \LotgdKernel::get('lotgd_core.tool.date_time')->secondsToNextGameDay((array) $details);
 }
