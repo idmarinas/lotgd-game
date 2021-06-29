14d70032ea1eae10b74a2fcf03047aad9530d3a3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-29 11:10:09 +0200

    Add trigger_error

--- lib/increment_specialty.php
+++ lib/increment_specialty.php
@@ -7,5 +7,10 @@
 /** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.player_functions')->incrementSpecialty($colorcode, $spec)" instead. Removed in future versions. */
 function increment_specialty($colorcode, $spec = false)
 {
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.player_functions")->incrementSpecialty($colorcode, $spec)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
     \LotgdKernel::get('lotgd_core.tool.player_functions')->incrementSpecialty($colorcode, $spec ?: null);
 }
