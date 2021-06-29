e27553894c48a00049661b4e643e7affb93f28ac
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-29 14:12:15 +0200

    Mark as deprecated

--- lib/creaturefunctions.php
+++ lib/creaturefunctions.php
@@ -1,302 +1,111 @@
 <?php
 
-use Lotgd\Core\Event\Creature;
-
-/**
- * Generate a base creature stats
- * Can use for generated your own creatures in your modules.
- *
- * @param false|int $level Level of creature
- *
- * @return array
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdGenerateCreatureLevels($level)" instead. Removed in future versions. */
 function lotgd_generate_creature_levels($level = null)
 {
-    $cache  = \LotgdKernel::get('cache.app');
-    $maxlvl = getsetting('maxlevel', 15) + 5;
-    $item   = $cache->getItem("lotgd-generate-creature-levels-{$maxlvl}");
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->lotgdGenerateCreatureLevels($level)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if ( ! $item->isHit())
-    {
-        $stats           = [];
-        $creatureexp     = 14;
-        $creaturegold    = 36;
-        $creaturedefense = 0;
-
-        for ($i = 1; $i <= $maxlvl; ++$i)
-        {
-            //apply algorithmic creature generation.
-            $creaturehealth = ($i * 10) + ($i - 1) - \round(\sqrt($i - 1));
-            $creatureattack = 1         + ($i - 1) * 2;
-            $creaturedefense += ($i % 2 ? 1 : 2);
-
-            if ($i > 1)
-            {
-                $creatureexp  += \round(10 + 1.5 * \log($i));
-                $creaturegold += (31 * ($i < 4 ? 2 : 1));
-                //give lower levels more gold
-            }
-            $stats[$i] = [
-                'creaturelevel'   => $i,
-                'creaturehealth'  => $creaturehealth,
-                'creatureattack'  => $creatureattack,
-                'creaturedefense' => $creaturedefense,
-                'creatureexp'     => $creatureexp,
-                'creaturegold'    => $creaturegold,
-            ];
-        }
-
-        $item->set($stats);
-        $cache->save($item);
-    }
-
-    $stats = $item->get();
-
-    return $stats[$level] ?? $stats;
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdGenerateCreatureLevels($level);
 }
 
-/**
- * Generate a dummy creature Doppelganger.
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdGenerateDoppelganger($level)" instead. Removed in future versions. */
 function lotgd_generate_doppelganger(int $level): array
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->lotgdGenerateDoppelganger($level)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    //-- There is nothing in the database to challenge you, let's give you a doppelganger.
-    $badguy                   = lotgd_generate_creature_levels($level);
-    $badguy['creaturename']   = \LotgdTranslator::t('doppelganger', ['name' => $session['user']['name']], 'page_creatures');
-    $badguy['creatureweapon'] = $session['user']['weapon'];
-    $badguy['creaturegold']   = 0;
-
-    return $badguy;
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdGenerateDoppelganger($level);
 }
 
-/**
- * Transform creature to adapt to player.
- *
- * @param array $badguy Data of creature
- * @param bool  $debug  Show or not debug of creature
- *
- * @return array
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdTransformCreature($badguy, $debug)" instead. Removed in future versions. */
 function lotgd_transform_creature(array $badguy, $debug = true)
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->lotgdTransformCreature($badguy, $debug)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    // This will save us a lot of trouble when going through
-    static $dk = false;	// this function more than once...
-
-    if (false === $dk)
-    {
-        $dk = $session['user']['dragonkills'];
-    }
-
-    $badguy = \array_merge(lotgd_generate_creature_levels($badguy['creaturelevel']), $badguy);
-
-    $badguy['playerdragonkills']  = $dk;
-    $badguy['creaturespeed']      = $badguy['creaturespeed']      ?? 2.5;
-    $badguy['creatureexp']        = $badguy['creatureexp']        ?? 0;
-    $badguy['physicalresistance'] = $badguy['physicalresistance'] ?? 0;
-
-    //-- Apply multipliers
-    $badguy['creaturegold']    = \round($badguy['creaturegold'] * ($badguy['creaturegoldbonus'] ?? 1));
-    $badguy['creatureattack']  = $badguy['creatureattack']  * ($badguy['creatureattackbonus'] ?? 1);
-    $badguy['creaturedefense'] = $badguy['creaturedefense'] * ($badguy['creaturedefensebonus'] ?? 1);
-    $badguy['creaturehealth']  = \round($badguy['creaturehealth'] * ($badguy['creaturehealthbonus'] ?? 1));
-
-    $creatureattr = get_creature_stats($dk);
-
-    //-- Bonus to atributes
-    $badguy['creaturestrbonus'] = $creatureattr['str'];
-    $badguy['creaturedexbonus'] = $creatureattr['dex'];
-    $badguy['creatureconbonus'] = $creatureattr['con'];
-    $badguy['creatureintbonus'] = $creatureattr['int'];
-    $badguy['creaturewisbonus'] = $creatureattr['wis'];
-
-    //-- Total atributes of creature
-    $badguy['creaturestr'] = $creatureattr['str'] + 10;
-    $badguy['creaturedex'] = $creatureattr['dex'] + 10;
-    $badguy['creaturecon'] = $creatureattr['con'] + 10;
-    $badguy['creatureint'] = $creatureattr['int'] + 10;
-    $badguy['creaturewis'] = $creatureattr['wis'] + 10;
-
-    //-- Attack, defense, health from attributes
-    $badguy['creatureattackattrs']  = get_creature_attack($creatureattr);
-    $badguy['creaturedefenseattrs'] = get_creature_defense($creatureattr);
-    $badguy['creaturehealthattrs']  = get_creature_hitpoints($creatureattr);
-    $badguy['creaturespeedattrs']   = get_creature_speed($creatureattr);
-
-    //-- Sum bonus
-    $badguy['creatureattack']  += $badguy['creatureattackattrs'];
-    $badguy['creaturedefense'] += $badguy['creaturedefenseattrs'];
-    $badguy['creaturehealth']  += $badguy['creaturehealthattrs'];
-    $badguy['creaturespeed']   += $badguy['creaturespeedattrs'];
-
-    //-- Set max health for creature
-    $badguy['creaturemaxhealth']      = $badguy['creaturehealth'];
-    $badguy['creaturestartinghealth'] = $badguy['creaturehealth'];
-
-    //-- Check if script exist
-    if (isset($badguy['creatureaiscript']))
-    {
-        $aiscriptfile = "{$badguy['creatureaiscript']}.php";
-
-        if (\file_exists($aiscriptfile))
-        {
-            $badguy['creatureaiscript'] = "include '{$aiscriptfile}';";
-        }
-        else
-        {
-            $badguy['creatureaiscript'] = '';
-        }
-    }
-
-    //-- Not show debug
-    if ( ! $debug)
-    {
-        return $badguy;
-    }
-
-    lotgd_show_debug_creature($badguy);
-
-    return $badguy;
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdTransformCreature($badguy, (bool) $debug);
 }
 
-/**
- * Search for creature in data base.
- *
- * @param int       $multi
- * @param int       $targetlevel
- * @param int       $mintargetlevel
- * @param bool      $packofmonsters For diferent or same creatures
- * @param bool|null $forest         TRUE for creature of forest, FALSE for graveyard and NULL for none
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdSearchCreature($multi, $targetlevel, $mintargetlevel, $packofmonsters, $forest)" instead. Removed in future versions. */
 function lotgd_search_creature($multi, $targetlevel, $mintargetlevel, $packofmonsters = false, $forest = true): array
 {
-    $multi = false === $packofmonsters ? $multi : 1;
-    $limit = ($multi > 1 ? $multi : 1);
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->lotgdSearchCreature($multi, $targetlevel, $mintargetlevel, $packofmonsters, $forest)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $repository = \Doctrine::getRepository('LotgdCore:Creatures');
-    $query      = $repository->createQueryBuilder('u');
-    $query->orderBy('RAND()');
-
-    if (true === $forest)
-    {
-        $query->where('u.forest = 1');
-    }
-    elseif (false === $forest)
-    {
-        $query->where('u.graveyard = 1');
-    }
-
-    //-- Select creatures of diferent categories
-    if (getsetting('multicategory', 0) && $limit > 1)
-    {
-        $query->groupBy('u.creaturecategory')->addGroupBy('u.creatureid');
-    }
-
-    $query = $repository->createTranslatebleQuery($query);
-    $query->setMaxResults($limit);
-
-    $creatures = $query->getArrayResult();
-
-    if (\count($creatures))
-    {
-        foreach ($creatures as $key => $creature)
-        {
-            $creatures[$key]['creaturelevel'] = e_rand($mintargetlevel, $targetlevel);
-        }
-    }
-
-    //-- You can add more creatures. This is good, when not find nothing in data base
-    $creatures = new Creature([
-        'creatures'      => $creatures,
-        'multi'          => $multi,
-        'targetlevel'    => $targetlevel,
-        'mintargetlevel' => $mintargetlevel,
-        'packofmonsters' => $packofmonsters,
-        'forest'         => $forest,
-    ]);
-    \LotgdEventDispatcher::dispatch($creatures, Creature::SEARCH);
-    $creatures = modulehook('creature-search', $creatures->getData());
-
-    return $creatures['creatures'];
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdSearchCreature($multi, $targetlevel, $mintargetlevel, $packofmonsters, $forest);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureStats($dk)" instead. Removed in future versions. */
 function get_creature_stats($dk = 0)
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->getCreatureStats($dk)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if (0 == $dk)
-    {
-        $dk = $session['user']['dragonkills'];
-    }
-
-    //-- They are placed in order of importance
-    $con = e_rand($dk / 6, $dk / 2);
-    $dk -= $con;
-    $str = e_rand(0, $dk);
-    $dk -= $str;
-    $dex = e_rand(0, $dk);
-    $dk -= $dex;
-    $int = e_rand(0, $dk);
-    $wis = ($dk - $int);
-
-    return ['str' => $str, 'dex' => $dex, 'con' => $con, 'int' => $int, 'wis' => $wis];
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureStats($dk);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureHitpoints($attrs)" instead. Removed in future versions. */
 function get_creature_hitpoints($attrs)
 {
-    $conbonus = $attrs['con'] * .5;
-    $wisbonus = $attrs['wis'] * .2;
-    $strbonus = $attrs['str'] * .3;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->getCreatureHitpoints($attrs)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $hitpoints = \round($conbonus + $wisbonus + $strbonus, 0);
-
-    return \max($hitpoints, 0);
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureHitpoints($attrs);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureAttack($attrs)" instead. Removed in future versions. */
 function get_creature_attack($attrs)
 {
-    $strbonus = (1 / 3) * $attrs['str'];
-    // $speedbonus = (1 / 3) * get_creature_speed($attrs);
-    $wisdombonus = (1 / 6) * $attrs['wis'];
-    $intbonus    = (1 / 6) * $attrs['int'];
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->getCreatureAttack($attrs)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $attack = $strbonus + $wisdombonus + $intbonus;
-
-    return \max($attack, 0);
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureAttack($attrs);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureDefense($attrs)" instead. Removed in future versions. */
 function get_creature_defense($attrs)
 {
-    $wisdombonus = (1 / 4) * $attrs['wis'];
-    $constbonus  = (3 / 8) * $attrs['con'];
-    // $speedbonus = (3 / 8) * get_creature_speed($attrs);
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->getCreatureDefense($attrs)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $defense = $wisdombonus + $constbonus;
-
-    return \max($defense, 0);
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureDefense($attrs);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureSpeed($attrs)" instead. Removed in future versions. */
 function get_creature_speed($attrs)
 {
-    $speed = (1 / 2) * $attrs['dex'] + (1 / 4) * $attrs['int'] + (5 / 2);
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->getCreatureSpeed($attrs)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    return \max($speed, 0);
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->getCreatureSpeed($attrs);
 }
 
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdShowDebugCreature($badguy)" instead. Removed in future versions. */
 function lotgd_show_debug_creature(iterable $badguy)
 {
-    \LotgdResponse::pageDebug("DEBUG: Basic information: Atk: {$badguy['creatureattack']}, Def: {$badguy['creaturedefense']}, HP: {$badguy['creaturehealth']}");
-    \LotgdResponse::pageDebug("DEBUG: {$badguy['playerdragonkills']} modification points total for attributes.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturestrbonus']} allocated to strength.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturedexbonus']} allocated to dexterity.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creatureconbonus']} allocated to constitution.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creatureintbonus']} allocated to intelligence.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturewisbonus']} allocated to wisdom.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creatureattackattrs']} modification of attack.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturedefenseattrs']} modification of defense.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturespeedattrs']} modification of speed.");
-    \LotgdResponse::pageDebug("DEBUG: +{$badguy['creaturehealthattrs']} modification of hitpoints.");
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->lotgdShowDebugCreature($badguy)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
+    \LotgdKernel::get('lotgd_core.tool.creature_functions')->lotgdShowDebugCreature($badguy);
 }
--- lib/forestoutcomes.php
+++ lib/forestoutcomes.php
@@ -4,63 +4,13 @@
 // translator ready
 // mail ready
 
-use Lotgd\Core\Event\Creature;
-
-require_once 'lib/creaturefunctions.php';
-
-/**
- * Buff creature for optimize to character stats.
- *
- * @param array  Information of creature
- * @param string $hook   Hook to activate when buff badguy
- * @param mixed  $badguy
- *
- * @return array
- */
+/** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.tool.creature_functions')->buffBadguy($badguy, $hook)" instead. Removed in future versions. */
 function buffbadguy($badguy, $hook = 'buffbadguy')
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.tool.creature_functions")->buffBadguy($badguy, $hook)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $badguy = lotgd_transform_creature($badguy, false);
-
-    $expflux = \round($badguy['creatureexp'] / 10, 0);
-    $expflux = e_rand(-$expflux, $expflux);
-    $badguy['creatureexp'] += $expflux;
-
-    if (getsetting('disablebonuses', 1))
-    {
-        //adapting flux as for people with many DKs they will just bathe in gold....
-        $base = 30 - \min(20, \round(\sqrt($session['user']['dragonkills']) / 2));
-        $base /= 1000;
-        $bonus = 1 + $base * ($badguy['creatureattackattrs'] + $badguy['creaturedefenseattrs']) + .001 * $badguy['creaturehealthattrs'];
-
-        $badguy['creaturegold'] = \round($badguy['creaturegold'] * $bonus, 0);
-        $badguy['creatureexp']  = \round($badguy['creatureexp'] * $bonus, 0);
-    }
-
-    //-- Activate hook when find a creature
-    $badguy = new Creature($badguy);
-    \LotgdEventDispatcher::dispatch($badguy, Creature::ENCOUNTER);
-    $badguy->setData(modulehook('creatureencounter', $badguy->getData()));
-
-    $hookNew = $hook;
-
-    if ('buffbadguy' == $hook)
-    {
-        $hookNew = 'badguy';
-    }
-    elseif ('buffmaster' == $hook)
-    {
-        $hookNew = 'master';
-    }
-    //-- Activate hook custom or default (buffbadguy)
-    \LotgdEventDispatcher::dispatch($badguy, Creature::BUFF_FOR.$hookNew);
-    $badguy = modulehook($hook, $badguy->getData());
-
-    //-- Update max creature health
-    $badguy['creaturemaxhealth'] = $badguy['creaturehealth'];
-
-    lotgd_show_debug_creature($badguy);
-
-    return $badguy;
+    return \LotgdKernel::get('lotgd_core.tool.creature_functions')->buffBadguy($badguy, $hook);
 }
