26bbcdcaafbb11d6f2d39ea27e631e6889019e76
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-30 15:46:16 +0200

    First commit

---
+++ src/core/Fixed/Setting.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Legend of the Green Dragon.
+ *
+ * @see https://github.com/idmarinas/lotgd-game
+ *
+ * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
+ * @author IDMarinas
+ *
+ * @since 5.5.0
+ */
+
+namespace Lotgd\Core\Fixed;
+
+class Setting
+{
+    use StaticTrait;
+}
+
+\class_alias('Lotgd\Core\Fixed\Setting', 'LotgdSetting', false);
