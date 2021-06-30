f69eb76f2d95b489f5dd6b36d3d38a1651558014
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-06-30 15:52:20 +0200

    Not silence this deprecated

--- lib/settings.php
+++ lib/settings.php
@@ -7,7 +7,7 @@
 /** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.settings')->saveSetting($settingname, $value)" instead. Removed in future version. */
 function savesetting(string $settingname, $value)
 {
-    @trigger_error(sprintf(
+    trigger_error(sprintf(
         'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.settings")->saveSetting($settingname, $value);" instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
@@ -18,7 +18,7 @@ function savesetting(string $settingname, $value)
 /** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.settings')->clearSettings()" instead. Removed in future version. */
 function clearsettings()
 {
-    @trigger_error(sprintf(
+    trigger_error(sprintf(
         'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.settings")->clearsettings();" instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
@@ -29,7 +29,7 @@ function clearsettings()
 /** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.settings')->getSetting($settingname, $default)" instead. Removed in future version. */
 function getsetting(string $settingname, $default = false)
 {
-    @trigger_error(sprintf(
+    trigger_error(sprintf(
         'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.settings")->getSetting($settingname, $default);" instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
@@ -40,7 +40,7 @@ function getsetting(string $settingname, $default = false)
 /** @deprecated 5.5.0 use "LotgdKernel::get('lotgd_core.settings')->getAllSettings()" instead. Removed in future version. */
 function getAllSettings()
 {
-    @trigger_error(sprintf(
+    trigger_error(sprintf(
         'Usage of %s is obsolete since 5.5.0; and delete in future version. Use "LotgdKernel::get("lotgd_core.settings")->getAllSettings();" instead.',
         __METHOD__
     ), E_USER_DEPRECATED);
