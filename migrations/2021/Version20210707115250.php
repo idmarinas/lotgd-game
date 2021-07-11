60bc6e20c37e07f7603e3b65529b612c66cbd291
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-11 15:42:04 +0200

    Merged

--- migrations/2021/Version20210707115250.php
+++ migrations/2021/Version20210707115250.php
@@ -12,8 +12,6 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210707115250 extends AbstractMigration
 {
-    use Version60000Trait;
-
     public function getDescription(): string
     {
         return '6.0.0';
@@ -51,5 +49,18 @@ final class Version20210707115250 extends AbstractMigration
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP INDEX uri ON referers');
         $this->addSql('CREATE INDEX uri ON referers (uri(255))');
+    }
+
+    private function importData()
+    {
+
+        //-- Import characters to avatar
+        $this->addSql('INSERT INTO avatar
+        (`id`, `acct_id`, `name`, `playername`, `sex`, `strength`, `dexterity`, `intelligence`, `constitution`, `wisdom`, `specialty`, `experience`, `gold`, `weapon`, `armor`, `seenmaster`, `level`, `defense`, `attack`, `alive`, `goldinbank`, `marriedto`, `spirits`, `hitpoints`, `maxhitpoints`, `permahitpoints`, `gems`, `weaponvalue`, `armorvalue`, `location`, `turns`, `title`, `badguy`, `companions`, `allowednavs`, `resurrections`, `weapondmg`, `armordef`, `age`, `charm`, `specialinc`, `specialmisc`, `lastmotd`, `playerfights`, `lasthit`, `seendragon`, `dragonkills`, `restorepage`, `hashorse`, `bufflist`, `dragonpoints`, `boughtroomtoday`, `sentnotice`, `pvpflag`, `transferredtoday`, `soulpoints`, `gravefights`, `hauntedby`, `deathpower`, `recentcomments`, `bio`, `race`, `biotime`, `amountouttoday`, `pk`, `dragonage`, `bestdragonage`, `ctitle`, `slaydragon`, `fedmount`, `clanid`, `clanrank`, `clanjoindate`, `chatloc`)
+        SELECT `id`, `acct_id`, `name`, `playername`, `sex`, `strength`, `dexterity`, `intelligence`, `constitution`, `wisdom`, `specialty`, `experience`, `gold`, `weapon`, `armor`, `seenmaster`, `level`, `defense`, `attack`, `alive`, `goldinbank`, `marriedto`, `spirits`, `hitpoints`, `maxhitpoints`, `permahitpoints`, `gems`, `weaponvalue`, `armorvalue`, `location`, `turns`, `title`, `badguy`, `companions`, `allowednavs`, `resurrections`, `weapondmg`, `armordef`, `age`, `charm`, `specialinc`, `specialmisc`, `lastmotd`, `playerfights`, `lasthit`, `seendragon`, `dragonkills`, `restorepage`, `hashorse`, `bufflist`, `dragonpoints`, `boughtroomtoday`, `sentnotice`, `pvpflag`, `transferredtoday`, `soulpoints`, `gravefights`, `hauntedby`, `deathpower`, `recentcomments`, `bio`, `race`, `biotime`, `amountouttoday`, `pk`, `dragonage`, `bestdragonage`, `ctitle`, `slaydragon`, `fedmount`, `clanid`, `clanrank`, `clanjoindate`, `chatloc` FROM `characters`');
+
+        //-- Import accounts to user
+        $this->addSql('INSERT INTO  user (`acctid`, `avatar_id`, `laston`, `loggedin`, `superuser`, `login`, `lastmotd`, `locked`, `lastip`, `uniqueid`, `boughtroomtoday`, `emailaddress`, `replaceemail`, `emailvalidation`, `sentnotice`, `prefs`, `transferredtoday`, `recentcomments`, `amountouttoday`, `regdate`, `banoverride`, `donation`, `donationspent`, `donationconfig`, `referer`, `refererawarded`, `password`, `forgottenpassword`)
+        SELECT `acctid`, `character_id`, `laston`, `loggedin`, `superuser`, `login`, `lastmotd`, `locked`, `lastip`, `uniqueid`, `boughtroomtoday`, `emailaddress`, `replaceemail`, `emailvalidation`, `sentnotice`, `prefs`, `transferredtoday`, `recentcomments`, `amountouttoday`, `regdate`, `banoverride`, `donation`, `donationspent`, `donationconfig`, `referer`, `refererawarded`, `password`, `forgottenpassword` FROM accounts');
     }
 }
