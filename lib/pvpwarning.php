5f3aed091f5e30927eb80e3eccee093dafb6f174
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-23 17:20:11 +0200

    Mark as deprecated

--- lib/pvpwarning.php
+++ lib/pvpwarning.php
@@ -4,29 +4,13 @@
 // translator ready
 // mail ready
 
-use Lotgd\Core\Event\Character;
-
+/** @deprecated 6.0.0 use LotgdKernel::get('Lotgd\Core\Pvp\Warning')->warning(); */
 function pvpwarning($dokill = false)
 {
-    global $session, $textDomain;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use LotgdKernel::get("Lotgd\Core\Pvp\Warning")->warning() instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $days = (int) LotgdSetting::getSetting('pvpimmunity', 5);
-    $exp  = (int) LotgdSetting::getSetting('pvpminexp', 1500);
-
-    if ($session['user']['age'] <= $days && 0 == $session['user']['dragonkills'] && 0 == $session['user']['pk'] && $session['user']['experience'] <= $exp)
-    {
-        if ($dokill)
-        {
-            \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.warning.pk', [], $textDomain));
-            $session['user']['pk'] = 1;
-        }
-        else
-        {
-            \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('flash.message.warning.msg', ['days' => $days, 'exp' => $exp], $textDomain));
-        }
-    }
-
-    $args = new Character(['dokill' => $dokill]);
-    \LotgdEventDispatcher::dispatch($args, Character::PVP_DO_KILL);
-    modulehook('pvpwarning', $args->getData());
+    return \LotgdKernel::get('Lotgd\Core\Pvp\Warning')->warning($dokill);
 }
