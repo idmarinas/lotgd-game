38ed47aa05be729dd04af8b3640321130aba9d77
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-27 11:22:54 +0200

    Not load DebugBundle in dev, not in use for now

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class              => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class            => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class                     => ['dev' => true, 'test' => true],
+    // Symfony\Bundle\DebugBundle\DebugBundle::class                     => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                       => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                 => ['all' => true],
     // Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class         => ['dev' => true, 'test' => true],
