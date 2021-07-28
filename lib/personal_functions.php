3476f949ddd6ca13bceffcb3a6bf461f312fba46
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-28 14:35:27 +0200

    Mark as deprecated

--- lib/personal_functions.php
+++ lib/personal_functions.php
@@ -1,54 +1,12 @@
 <?php
 
-use Lotgd\Core\Event\Character;
-
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdKernel::get('lotgd_core.tool.staff')->killPlayer($explossproportion, $goldlossproportion)" instead */
 function killplayer($explossproportion = 0.1, $goldlossproportion = 1)
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdKernel::get("lotgd_core.tool.staff")->killPlayer($explossproportion, $goldlossproportion)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $args = new Character([
-        'explossproportion'  => $explossproportion,
-        'goldlossproportion' => $goldlossproportion,
-    ]);
-    \LotgdEventDispatcher::dispatch($args, Character::KILLED_PLAYER);
-    $args = modulehook('killedplayer', $args->getData());
-
-    if (isset($args['donotkill']))
-    {
-        return;
-    }
-
-    $exp     = $session['user']['experience'];
-    $exploss = \round($exp * $args['explossproportion']);
-
-    if ($exploss > $exp)
-    {
-        $exploss = $exp;
-    }
-
-    if ($exploss > 0)
-    {
-        $session['user']['experience'] -= $exploss;
-        \LotgdResponse::pageAddContent(\LotgdFormat::colorize(\sprintf('`$You lose %s experience.`n', $exploss), true));
-    }
-
-    $gold     = $session['user']['gold'];
-    $goldloss = \round($gold * $args['goldlossproportion']);
-
-    if ($goldloss > $gold)
-    {
-        $goldloss = $gold;
-    }
-
-    if ($goldloss > 0)
-    {
-        $session['user']['gold'] -= $goldloss;
-        \LotgdResponse::pageAddContent(\LotgdFormat::colorize(\sprintf('`$You lose %s gold.`n', $goldloss), true));
-    }
-
-    $session['user']['hitpoints'] = 0;
-    $session['user']['alive']     = false;
-
-    \LotgdNavigation::addNav('Daily news', 'news.php');
-    \LotgdResponse::pageEnd();
+    return \LotgdKernel::get('lotgd_core.tool.staff')->killPlayer($explossproportion, $goldlossproportion);
 }
