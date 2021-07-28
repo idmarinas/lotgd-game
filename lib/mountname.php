e29bd7ed563f718dd084a6438913882d7bff30a4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-28 12:37:30 +0200

    Mark as deprecated

--- lib/mountname.php
+++ lib/mountname.php
@@ -3,8 +3,15 @@
 // translator ready
 // addnews ready
 // mail ready
+
+/** @deprecated 6.0.0 Delete in version 7.0.0. Not need this for now name of mount. This function is not used by the core. */
 function getmountname()
 {
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Not need this for now name of mount. This function is not used by the core.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
+
     global $playermount;
 
     $name   = '';
