796ecb2c8f6b4d76e54ed2d01593b1f2ef50cb67
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-28 14:24:54 +0200

    Mark as deprecated

--- lib/names.php
+++ lib/names.php
@@ -4,135 +4,57 @@
 // addnews ready
 // mail ready
 
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdTool::getPlayerTitle($old)" instead */
 function get_player_title($old = false)
 {
-    global $session;
-    $title = '';
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdTool::getPlayerTitle($old)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if (false === $old)
-    {
-        $title = $session['user']['title'];
-
-        if ($session['user']['ctitle'])
-        {
-            $title = $session['user']['ctitle'];
-        }
-    }
-    else
-    {
-        $title = $old['title'];
-
-        if ($old['ctitle'])
-        {
-            $title = $old['ctitle'];
-        }
-    }
-
-    return $title;
+    return \LotgdTool::getPlayerTitle($old);
 }
 
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdTool::getPlayerBasename($old)" instead */
 function get_player_basename($old = false)
 {
-    global $session;
-    $name  = '';
-    $title = get_player_title($old);
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdTool::getPlayerBasename($old)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if (false === $old)
-    {
-        $name = $session['user']['name'];
-    }
-    else
-    {
-        $name = $old['name'];
-    }
-
-    if ($title)
-    {
-        $x = \strpos($name, $title);
-
-        if (false !== $x)
-        {
-            $name = \trim(\substr($name, $x + \strlen($title)));
-        }
-    }
-
-    return $name;
+    return \LotgdTool::getPlayerBasename($old);
 }
 
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdTool::changePlayerName($newname, $old)" instead */
 function change_player_name($newname, $old = false)
 {
-    if ('' == $newname)
-    {
-        $newname = get_player_basename($old);
-    }
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdTool::changePlayerName($newname, $old)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    $title = get_player_title($old);
-
-    if ($title)
-    {
-        $x = \strpos($newname, $title);
-
-        if (0 === $x)
-        {
-            $newname = \trim(\substr($newname, $x + \strlen($title)));
-        }
-        $newname = $title.' '.$newname;
-    }
-
-    return $newname;
+    return \LotgdTool::changePlayerName($newname, $old);
 }
 
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdTool::changePlayerCtitle($nctitle, $old)" instead */
 function change_player_ctitle($nctitle, $old = false)
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdTool::changePlayerCtitle($nctitle,$old)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if ('' == $nctitle)
-    {
-        if (false == $old)
-        {
-            $nctitle = $session['user']['title'];
-        }
-        else
-        {
-            $nctitle = $old['title'];
-        }
-    }
-    $newname = get_player_basename($old);
-
-    if ($nctitle)
-    {
-        $newname = $nctitle.' '.$newname;
-    }
-
-    return $newname;
+    return \LotgdTool::changePlayerCtitle($nctitle, $old);
 }
 
+/** @deprecated 6.0.0 deleted in 7.0.0 version. Use "LotgdTool::changePlayerTitle($ntitle, $old)" instead */
 function change_player_title($ntitle, $old = false)
 {
-    global $session;
+    \trigger_error(\sprintf(
+        'Usage of %s is obsolete since 6.0.0; and delete in 7.0.0 version. Use "LotgdTool::changePlayerTitle($ntitle, $old)" instead.',
+        __METHOD__
+    ), E_USER_DEPRECATED);
 
-    if (false === $old)
-    {
-        $ctitle = $session['user']['ctitle'];
-    }
-    else
-    {
-        $ctitle = $old['ctitle'];
-    }
-
-    $newname = get_player_basename($old);
-
-    if ('' == $ctitle)
-    {
-        if ('' != $ntitle)
-        {
-            $newname = $ntitle.' '.$newname;
-        }
-    }
-    else
-    {
-        $newname = $ctitle.' '.$newname;
-    }
-
-    return $newname;
+    return \LotgdTool::changePlayerTitle($ntitle, $old);
 }
