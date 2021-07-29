0e9ac9f0450ed780e154f2c2f45df810ec8b84ed
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-07-29 11:47:44 +0200

    Now runmodule work with controllers too

--- public/runmodule.php
+++ public/runmodule.php
@@ -4,35 +4,57 @@
 // addnews ready
 // mail ready
 
-define('ALLOW_ANONYMOUS', true);
-define('OVERRIDE_FORCED_NAV', true);
+\define('ALLOW_ANONYMOUS', true);
+\define('OVERRIDE_FORCED_NAV', true);
 
 require_once 'common.php';
 require_once 'lib/dump_item.php';
 require_once 'lib/modules.php';
 
-$module =  (string) \LotgdRequest::getQuery('module');
-$admin = (bool) \LotgdRequest::getQuery('admin');
+$module     = (string) \LotgdRequest::getQuery('module');
+$admin      = (bool) \LotgdRequest::getQuery('admin');
+$controller = (string) \LotgdRequest::getQuery('controller', '');
+$method     = (string) \LotgdRequest::getQuery('method', '');
 
-if (injectmodule($module, $admin))
+//-- For migrating modules to bundles (Temporal)
+if ( ! $module && $controller && class_exists($controller))
+{
+    $ctrl = \LotgdKernel::get($controller);
+
+    $allowAnonymous    = false;
+    $overrideForcedNav = false;
+
+    if ($ctrl instanceof \Lotgd\Core\Controller\LotgdControllerInterface)
+    {
+        $allowAnonymous    = $ctrl->allowAnonymous();
+        $overrideForcedNav = $ctrl->overrideForcedNav();
+    }
+
+    do_forced_nav($allowAnonymous, $overrideForcedNav);
+
+    \LotgdResponse::pageStart();
+    \LotgdResponse::callController($controller, $method ?: 'index');
+    \LotgdResponse::pageEnd();
+}
+elseif (injectmodule($module, $admin))
 {
     $info = get_module_info($module);
 
-    $allowanonymous = (bool) ($info['allowanonymous'] ?? false);
+    $allowanonymous      = (bool) ($info['allowanonymous'] ?? false);
     $override_forced_nav = (bool) ($info['override_forced_nav'] ?? false);
 
     do_forced_nav($allowanonymous, $override_forced_nav);
 
     $starttime = microtime(true);
-    $fname = $mostrecentmodule.'_run';
+    $fname     = $mostrecentmodule.'_run';
 
     $fname();
     $endtime = microtime(true);
-    $time = $endtime - $starttime;
+    $time    = $endtime - $starttime;
 
     if ($time >= 1.00 && ($session['user']['superuser'] & SU_DEBUG_OUTPUT))
     {
-        \LotgdResponse::pageDebug('Slow Module ('.round($time, 2)."s): $mostrecentmodule`n");
+        \LotgdResponse::pageDebug('Slow Module ('.round($time, 2)."s): {$mostrecentmodule}`n");
     }
 }
 else
