cd7ddc2acc65840523409a960b3c1e7a613fac34
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-08-04 16:37:25 +0200

    Increased version

--- src/core/Installer/Pattern/Version.php
+++ src/core/Installer/Pattern/Version.php
@@ -109,6 +109,7 @@ trait Version
         '5.5.2 IDMarinas Edition'         => 50502,
         '5.5.3 IDMarinas Edition'         => 50503,
         '5.5.4 IDMarinas Edition'         => 50504,
+        '5.5.5 IDMarinas Edition'         => 50505,
     ];
 
     /**
