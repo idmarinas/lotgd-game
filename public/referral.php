34b7b1cbde28285ac93d39958a9ae29306a8f491
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-08-19 16:23:51 +0200

    Moved some code to controller

--- public/referral.php
+++ public/referral.php
@@ -8,39 +8,11 @@ define('ALLOW_ANONYMOUS', true);
 
 require_once 'common.php';
 
-/** @var Lotgd\Core\Http\Request */
-$request = \LotgdKernel::get(\Lotgd\Core\Http\Request::class);
-
-$textDomain = 'page_referral';
-
-if (! $session['user']['loggedin'])
-{
-    $referral = (string) \LotgdRequest::getQuery('r');
-
-    \LotgdFlashMessages::addInfoMessage(\LotgdTranslator::t('flash.message.referral.create', [ 'referral' => $referral ], $textDomain));
-
-    redirect('create.php?r='.rawurlencode($referral));
-}
-
 //-- Init page
-\LotgdResponse::pageStart('title', [], $textDomain);
+\LotgdResponse::pageStart();
 
-$params = [
-    'textDomain' => $textDomain
-];
-
-if (file_exists('public/lodge.php'))
-{
-    \LotgdNavigation::addNav('common.nav.lodge', 'lodge.php');
-}
-else
-{
-    \LotgdNavigation::villageNav();
-}
-
-$request->attributes->set('params', $params);
-
-\LotgdResponse::callController(Lotgd\Core\Controller\ReferralController::class);
+//-- Call controller
+\LotgdResponse::callController(\Lotgd\Core\Controller\ReferralController::class);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
--- src/core/Controller/ReferralController.php
+++ src/core/Controller/ReferralController.php
@@ -15,26 +15,66 @@ namespace Lotgd\Core\Controller;
 
 use Lotgd\Core\Events;
 use Lotgd\Core\Http\Request;
+use Lotgd\Core\Http\Response as HttpResponse;
 use Lotgd\Core\Lib\Settings;
+use Lotgd\Core\Navigation\Navigation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ReferralController extends AbstractController
 {
+    public const TRANSLATION_DOMAIN = 'page_referral';
+
     private $dispatcher;
     private $settings;
+    private $response;
+    private $navigation;
+    private $translator;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher, Settings $settings)
-    {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        Settings $settings,
+        HttpResponse $response,
+        Navigation $navigation,
+        TranslatorInterface $translator
+    ) {
         $this->dispatcher = $eventDispatcher;
         $this->settings   = $settings;
+        $this->response   = $response;
+        $this->navigation = $navigation;
+        $this->translator = $translator;
     }
 
-    public function index(array $params, Request $request): Response
+    public function index(Request $request): Response
     {
         global $session;
+
+        if ( ! $session['user']['loggedin'])
+        {
+            $referral = (string) $request->query->get('r');
+
+            $this->addFlash('info', $this->translator->trans('flash.message.referral.create', ['referral' => $referral], self::TRANSLATION_DOMAIN));
+
+            return $this->redirect('create.php?r='.rawurlencode($referral));
+        }
+
+        $this->response->pageTitle('title', [], self::TRANSLATION_DOMAIN);
+
+        $params = [
+            'textDomain' => self::TRANSLATION_DOMAIN,
+        ];
+
+        if (file_exists('public/lodge.php'))
+        {
+            $this->navigation->addNav('common.nav.lodge', 'lodge.php');
+        }
+        else
+        {
+            $this->navigation->villageNav();
+        }
 
         $url = $this->settings->getSetting('serverurl', sprintf('%s://%s', $request->getServer('REQUEST_SCHEME'), $request->getServer('HTTP_HOST')));
 
