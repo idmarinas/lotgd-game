266976c35901a2964ddbdd8bf7866009710166ff
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-08-19 16:40:41 +0200

    Moved some code to controller

--- public/rock.php
+++ public/rock.php
@@ -1,52 +1,14 @@
 <?php
 
-use Lotgd\Core\Events;
-use Symfony\Component\EventDispatcher\GenericEvent;
-
 require_once 'common.php';
 
 \LotgdKernel::get('lotgd_core.tool.date_time')->checkDay();
 
-// Don't hook on to this text for your standard modules please, use "rock" instead.
-// This hook is specifically to allow modules that do other rocks to create ambience.
-$args = new GenericEvent(null, ['textDomain' => 'page_rock', 'textDomainNavigation' => 'navigation_rock']);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_ROCK_PRE);
-$result = modulehook('rock-text-domain', $args->getArguments());
-$textDomain = $result['textDomain'];
-$textDomainNavigation = $result['textDomainNavigation'];
-unset($result);
-
-/** @var Lotgd\Core\Http\Request */
-$request = \LotgdKernel::get(\Lotgd\Core\Http\Request::class);
-
-//-- Change text domain for navigation
-\LotgdNavigation::setTextDomain($textDomainNavigation);
-
-\LotgdNavigation::villageNav();
-
-$params = [
-    'textDomain' => $textDomain
-];
-
-$params['tpl'] = 'default';
-$title = 'title.default';
-
-if ($session['user']['dragonkills'] > 0 || $session['user']['superuser'] & SU_EDIT_COMMENTS)
-{
-    $params['tpl'] = 'veteran';
-    $title = 'title.veteran';
-}
-
 //-- Init page
-\LotgdResponse::pageStart($title, [], $textDomain);
+\LotgdResponse::pageStart();
 
-$request->attributes->set('params', $params);
-
-\LotgdResponse::callController(Lotgd\Core\Controller\RockController::class);
-
-//-- Restore text domain for navigation
-\LotgdNavigation::setTextDomain();
+//-- Call controller
+\LotgdResponse::callController(\Lotgd\Core\Controller\RockController::class);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
-
--- src/core/Controller/RockController.php
+++ src/core/Controller/RockController.php
@@ -14,6 +14,8 @@
 namespace Lotgd\Core\Controller;
 
 use Lotgd\Core\Events;
+use Lotgd\Core\Http\Response as HttpResponse;
+use Lotgd\Core\Navigation\Navigation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,14 +24,50 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 class RockController extends AbstractController
 {
     private $dispatcher;
+    private $navigation;
+    private $response;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
+    public function __construct(EventDispatcherInterface $eventDispatcher, Navigation $navigation, HttpResponse $response)
     {
         $this->dispatcher = $eventDispatcher;
+        $this->navigation = $navigation;
+        $this->response   = $response;
     }
 
-    public function index(array $params): Response
+    public function index(): Response
     {
+        global $session;
+
+        // Don't hook on to this text for your standard modules please, use "rock" instead.
+        // This hook is specifically to allow modules that do other rocks to create ambience.
+        $args = new GenericEvent(null, ['textDomain' => 'page_rock', 'textDomainNavigation' => 'navigation_rock']);
+        $this->dispatcher->dispatch($args, Events::PAGE_ROCK_PRE);
+        $result               = modulehook('rock-text-domain', $args->getArguments());
+        $textDomain           = $result['textDomain'];
+        $textDomainNavigation = $result['textDomainNavigation'];
+        unset($result);
+
+        //-- Change text domain for navigation
+        $this->navigation->setTextDomain($textDomainNavigation);
+
+        $this->navigation->villageNav();
+
+        $params = [
+            'textDomain' => $textDomain,
+        ];
+
+        $params['tpl'] = 'default';
+        $title         = 'title.default';
+
+        if ($session['user']['dragonkills'] > 0 || $session['user']['superuser'] & SU_EDIT_COMMENTS)
+        {
+            $params['tpl'] = 'veteran';
+            $title         = 'title.veteran';
+        }
+
+        //-- Init page
+        $this->response->pageTitle($title, [], $textDomain);
+
         //-- This is only for params not use for other purpose
         $args = new GenericEvent(null, $params);
         $this->dispatcher->dispatch($args, Events::PAGE_ROCK_POST);
