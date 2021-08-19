55572132240b49f58e32359214173051687c93f5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-08-19 16:46:24 +0200

    Moved some code to controller

--- public/shades.php
+++ public/shades.php
@@ -1,8 +1,5 @@
 <?php
 
-use Lotgd\Core\Events;
-use Symfony\Component\EventDispatcher\GenericEvent;
-
 require_once 'common.php';
 
 \LotgdKernel::get('lotgd_core.tool.date_time')->checkDay();
@@ -13,57 +10,11 @@ if ($session['user']['alive'])
     redirect('village.php');
 }
 
-// Don't hook on to this text for your standard modules please, use "shades" instead.
-// This hook is specifically to allow modules that do other shades to create ambience.
-$args = new GenericEvent(null, ['textDomain' => 'page_shades', 'textDomainNavigation' => 'navigation_shades']);
-\LotgdEventDispatcher::dispatch($args, Events::PAGE_SHADES_PRE);
-$result = modulehook('shades-text-domain', $args->getArguments());
-$textDomain = $result['textDomain'];
-$textDomainNavigation = $result['textDomainNavigation'];
-unset($result);
-
-/** @var Lotgd\Core\Http\Request */
-$request = \LotgdKernel::get(\Lotgd\Core\Http\Request::class);
-
-$params = [
-    'textDomain' => $textDomain,
-    'includeTemplatesPre' => [], //-- Templates that are in top of content (but below of title)
-    'includeTemplatesPost' => [] //-- Templates that are in bottom of content
-];
-
 //-- Init page
-\LotgdResponse::pageStart('title', [], $textDomain);
+\LotgdResponse::pageStart();
 
-//-- Change text domain for navigation
-\LotgdNavigation::setTextDomain($textDomainNavigation);
-
-\LotgdNavigation::addHeader('category.logout');
-\LotgdNavigation::addNav('nav.logout', 'login.php?op=logout');
-
-\LotgdNavigation::addHeader('category.places');
-\LotgdNavigation::addNav('nav.graveyard', 'graveyard.php');
-\LotgdNavigation::addNav('nav.news', 'news.php');
-
-// the mute module blocks players from speaking until they
-// read the FAQs, and if they first try to speak when dead
-// there is no way for them to unmute themselves without this link.
-\LotgdNavigation::addHeader('category.other');
-\LotgdNavigation::addNav('nav.faq', '#', [
-    'attributes' => [
-        'id' => 'shades-petition-faq',
-        'onclick' => "JaxonLotgd.Ajax.Core.Petition.faq(); $(this).addClass('disabled')"
-    ]
-]);
-
-//-- Superuser menu
-\LotgdNavigation::superuser();
-
-$request->attributes->set('params', $params);
-
-\Lotgdresponse::callController(Lotgd\Core\Controller\ShadesController::class);
-
-//-- Restore text domain for navigation
-\LotgdNavigation::setTextDomain();
+//-- Call controller
+\LotgdResponse::callController(\Lotgd\Core\Controller\RockController::class);
 
 //-- Finalize page
 \LotgdResponse::pageEnd();
--- src/core/Controller/ShadesController.php
+++ src/core/Controller/ShadesController.php
@@ -14,6 +14,9 @@
 namespace Lotgd\Core\Controller;
 
 use Lotgd\Core\Events;
+use Lotgd\Core\Http\Request;
+use Lotgd\Core\Http\Response as HttpResponse;
+use Lotgd\Core\Navigation\Navigation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,18 +25,71 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 class ShadesController extends AbstractController
 {
     private $dispatcher;
+    private $response;
+    private $navigation;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        HttpResponse $response,
+        Navigation $navigation
+    ) {
         $this->dispatcher = $eventDispatcher;
+        $this->response   = $response;
+        $this->navigation = $navigation;
     }
 
-    public function index(array $params): Response
+    public function index(Request $request): Response
     {
+        // Don't hook on to this text for your standard modules please, use "shades" instead.
+        // This hook is specifically to allow modules that do other shades to create ambience.
+        $args = new GenericEvent(null, ['textDomain' => 'page_shades', 'textDomainNavigation' => 'navigation_shades']);
+        $this->dispatcher->dispatch($args, Events::PAGE_SHADES_PRE);
+        $result               = modulehook('shades-text-domain', $args->getArguments());
+        $textDomain           = $result['textDomain'];
+        $textDomainNavigation = $result['textDomainNavigation'];
+        unset($result);
+
+        $params = [
+            'textDomain'           => $textDomain,
+            'includeTemplatesPre'  => [], //-- Templates that are in top of content (but below of title)
+            'includeTemplatesPost' => [], //-- Templates that are in bottom of content
+        ];
+
+        $this->response->pageTitle('title', [], $textDomain);
+
+        //-- Change text domain for navigation
+        $this->navigation->setTextDomain($textDomainNavigation);
+
+        $this->navigation->addHeader('category.logout');
+        $this->navigation->addNav('nav.logout', 'login.php?op=logout');
+
+        $this->navigation->addHeader('category.places');
+        $this->navigation->addNav('nav.graveyard', 'graveyard.php');
+        $this->navigation->addNav('nav.news', 'news.php');
+
+        // the mute module blocks players from speaking until they
+        // read the FAQs, and if they first try to speak when dead
+        // there is no way for them to unmute themselves without this link.
+        $this->navigation->addHeader('category.other');
+        $this->navigation->addNav('nav.faq', '#', [
+            'attributes' => [
+                'id'      => 'shades-petition-faq',
+                'onclick' => "JaxonLotgd.Ajax.Core.Petition.faq(); $(this).addClass('disabled')",
+            ],
+        ]);
+
+        //-- Superuser menu
+        $this->navigation->superuser();
+
+        $request->attributes->set('params', $params);
+
         //-- This is only for params not use for other purpose
         $args = new GenericEvent(null, $params);
         $this->dispatcher->dispatch($args, Events::PAGE_SHADES_POST);
         $params = modulehook('page-shades-tpl-params', $args->getArguments());
+
+        //-- Restore text domain for navigation
+        $this->navigation->setTextDomain();
 
         return $this->render('page/shades.html.twig', $params);
     }
