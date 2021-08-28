e21fe0455f5eb7be3febc99c92aa0c09078e95f6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-08-28 17:52:03 +0200

    PHP CS Fixer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,11 +7,11 @@ require \dirname(__DIR__).'/vendor/autoload.php';
 //-- Include constants
 require_once \dirname(__DIR__).'/src/constants.php';
 
-if (\file_exists(\dirname(__DIR__).'/config/bootstrap.php'))
+if (file_exists(\dirname(__DIR__).'/config/bootstrap.php'))
 {
     require \dirname(__DIR__).'/config/bootstrap.php';
 }
-elseif (\method_exists(Dotenv::class, 'bootEnv'))
+elseif (method_exists(Dotenv::class, 'bootEnv'))
 {
     (new Dotenv())->bootEnv(\dirname(__DIR__).'/.env');
 }
