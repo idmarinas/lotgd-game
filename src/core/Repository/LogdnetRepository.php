ae9dde44030c497f35c7a2f310f75061f4b21ece
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-09-09 12:26:31 +0200

    Import trait to repository

--- src/core/Repository/LogdnetRepository.php
+++ src/core/Repository/LogdnetRepository.php
@@ -25,6 +25,7 @@ use Tracy\Debugger;
 class LogdnetRepository extends ServiceEntityRepository
 {
     use EntityRepositoryTrait;
+    use Version;
 
     private $cache;
 
@@ -151,10 +152,7 @@ class LogdnetRepository extends ServiceEntityRepository
 
     private function lotgdSort($a, $b)
     {
-        $official_prefixes = (new class()
-        {
-            use Version;
-        })->getFullListOfVersion();
+        $official_prefixes = $this->getFullListOfVersion();
 
         unset($official_prefixes['Clean Install']);
         $official_prefixes = array_keys($official_prefixes);
