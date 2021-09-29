25d8c169f4a024d8656f45dcce76b76b197ec7ab
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-09-29 11:51:39 +0200

    Add support to Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     plugins: {
+        'tailwindcss/nesting': {},
         // include whatever plugins you want
         // but make sure you install these via yarn or npm!
+        tailwindcss: {},
 
         // add browserslist config to package.json (see below)
         autoprefixer: {}
