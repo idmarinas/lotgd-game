6389ee343ca19c6117d9cefb3b9950d599c45b79
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-10-05 16:04:34 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [
+        './themes/**/*.html',
+        './themes/**/*.twig',
+        './assets/**/*.js',
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        //-- Default App Theme (Can change this theme for your own)
+        require('./assets/styles/tailwind.theme.app') //-- Can use as example for generate your theme
+    ],
+}
