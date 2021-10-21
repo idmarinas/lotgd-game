3227b95d069da2737a367cf578a6e46f3088abef
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-10-21 13:13:38 +0200

    First commit

---
+++ assets/styles/app/base/base.js
@@ -0,0 +1,27 @@
+/**
+ * Base document
+ *
+ * @param function theme
+ * @param function e
+ *
+ * @returns object
+ */
+module.exports = function (theme, e)
+{
+    return {
+        html: {
+            fontSize: theme('fontSize.sm'),
+            color: theme('colors.gray.200'),
+            backgroundColor: theme('colors.lotgd.DEFAULT')
+        },
+        body: {
+            padding: theme('padding.1'),
+        },
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+        'h4': { fontSize: theme('fontSize.base') },
+        'h5': { fontSize: theme('fontSize.sm') },
+        'h6': { fontSize: theme('fontSize.xs') },
+    }
+}
