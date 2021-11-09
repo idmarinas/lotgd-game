3fa6ecd175412456d3c22c5b37ea48b7c3e9a726
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-11-09 14:08:09 +0100

    Fixed error, not work in Anonymous request

--- public/stimulus.php
+++ public/stimulus.php
@@ -8,22 +8,28 @@ require_once 'common.php';
 $controller = (string) \LotgdRequest::getQuery('controller', '');
 $method     = (string) \LotgdRequest::getQuery('method', '');
 
-//-- Only load if controller class exists
+//-- For migrating modules to bundles (Temporal)
 if ($controller && class_exists($controller))
 {
     $ctrl = \LotgdKernel::get($controller);
 
     $allowAnonymous    = method_exists($ctrl, 'allowAnonymous') ? $ctrl->allowAnonymous() : false;
-    //-- If not set default is true
-    //-- The intended use of Stimulus is to load small blocks of HTML code.
-    $overrideForcedNav = method_exists($ctrl, 'overrideForcedNav') ? $ctrl->overrideForcedNav() : true;
+    $overrideForcedNav = method_exists($ctrl, 'overrideForcedNav') ? $ctrl->overrideForcedNav() : false;
     do_forced_nav($allowAnonymous, $overrideForcedNav);
 
+    \LotgdResponse::pageStart();
     \LotgdResponse::callController($controller, $method ?: 'index', true);
+}
+else
+{
+    do_forced_nav(false, false);
 
-    /**
-     * No need LotgdResponse::pageStart(); and LotgdResponse::pageEnd();
-     *
-     * Because Stimulus only load small blocks of HTML code.
-     */
+    \LotgdFlashMessages::addWarningMessage(\LotgdTranslator::t('redirect.module.unactive', [], 'app_default'));
+
+    if ($session['user']['loggedin'])
+    {
+        redirect('village.php');
+    }
+
+    redirect('index.php');
 }
