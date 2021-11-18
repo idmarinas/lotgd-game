e674f4dc1f7ddc73852f461bc2358ddd6bcd3ccd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-11-18 13:01:51 +0100

    First commit

---
+++ assets/stimulus/controllers/form/submit_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from 'stimulus'
+
+//-- Helpers
+import { useLoading } from '../../helpers'
+
+export default class extends Controller
+{
+    static targets = ['activator', 'loading']
+    static values = { url: String }
+
+    connect()
+    {
+        useLoading(this)
+    }
+
+    async send (event)
+    {
+        // Cancels the event's default behavior (e.g. following a link or submitting a form)
+        event.preventDefault()
+
+        if ( ! this.hasUrlValue)
+        {
+            return;
+        }
+
+        //-- Block activator button
+        this.startLoading()
+
+        const form = this.element.getElementsByTagName('form')[0]
+
+        const content = await fetch(this.urlValue, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+
+        this.element.innerHTML = await content.text()
+    }
+}
