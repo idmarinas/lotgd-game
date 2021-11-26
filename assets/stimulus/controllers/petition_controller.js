16d801f99ac131172d65807a199bb1f260770bfb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-11-26 14:21:27 +0100

    First commit

---
+++ assets/stimulus/controllers/petition_controller.js
@@ -0,0 +1,90 @@
+import RemoteModal from './remote_modal_controller'
+
+//-- Helpers
+import { useLoadingBarTop, useButtonLoading } from '../helpers'
+
+export default class extends RemoteModal
+{
+    static values = { urlFaq: String, urlPrimer: String }
+
+    connect()
+    {
+        super.connect()
+
+        useLoadingBarTop(this)
+        useButtonLoading(this)
+    }
+
+    async faq(event)
+    {
+        event.preventDefault()
+
+        this.#faqPrivate(this.urlFaqValue)
+    }
+
+    async faq1(event)
+    {
+        event.preventDefault()
+
+        await this.#faqPrivate(`${this.urlFaqValue}&faq=1`)
+    }
+
+    async faq2(event)
+    {
+        event.preventDefault()
+
+        await this.#faqPrivate(`${this.urlFaqValue}&faq=2`)
+    }
+
+    async faq3(event)
+    {
+        event.preventDefault()
+
+        await this.#faqPrivate(`${this.urlFaqValue}&faq=3`)
+    }
+
+    async primer(event)
+    {
+        event.preventDefault()
+
+        await this.#faqPrivate(this.urlPrimerValue)
+    }
+
+    async return(event)
+    {
+        this.startButtonLoading(event.target)
+
+        let content = null
+
+        this.remoteContent = ''
+
+        content = await this.fetch(this.urlFaqValue)
+
+        this.stopButtonLoading(event.target)
+
+        if ( ! content) return
+
+        this.containerTarget.innerHTML = ''
+
+        this.containerTarget.appendChild(document.createRange().createContextualFragment(content))
+    }
+
+    async #faqPrivate(url)
+    {
+        this.startLoadingBarTop()
+
+        let content = null
+
+        this.remoteContent = ''
+
+        content = await this.fetch(url)
+
+        this.stopLoadingBarTop()
+
+        if ( ! content) return
+
+        this.containerTarget.innerHTML = ''
+
+        this.containerTarget.appendChild(document.createRange().createContextualFragment(content))
+    }
+}
