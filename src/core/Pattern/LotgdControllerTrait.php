a22137aa8ef89dcee2706b1b9f4b1b092254a5f7
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-12-08 13:26:41 +0100

    Add render block method

--- src/core/Pattern/LotgdControllerTrait.php
+++ src/core/Pattern/LotgdControllerTrait.php
@@ -12,6 +12,8 @@
 
 namespace Lotgd\Core\Pattern;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Custom trait for LoTGD Controllers
  */
@@ -30,5 +32,22 @@ trait LotgdControllerTrait
         }
 
         $this->container->get('session')->getBag('notifications')->add($type, $message);
+    }
+
+    /**
+     * Renders a view block.
+     */
+    protected function renderBlock(string $view, string $block, array $parameters = [], ?Response $response = null): Response
+    {
+        $content = $this->container->get('twig')->load($view)->renderBlock($block, $parameters);
+
+        if (null === $response)
+        {
+            $response = new Response();
+        }
+
+        $response->setContent($content);
+
+        return $response;
     }
 }
