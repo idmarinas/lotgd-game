4dec14fb675ec7a584d34958adbe0e038bfdd6db
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-01-02 15:31:18 +0100

    Add files to remove

--- src/core/Installer/Upgrade/Version70000.php
+++ src/core/Installer/Upgrade/Version70000.php
@@ -26,12 +26,25 @@ class Version70000 extends InstallerAbstract
     public function step0(): bool
     {
         return $this->removeFiles([
+            //-- Jaxon PHP related files
             $this->getProjectDir().'/src/ajax/pattern/core/',
             $this->getProjectDir().'/src/ajax/core/Mail.php',
             $this->getProjectDir().'/src/ajax/core/Motd.php',
             $this->getProjectDir().'/src/ajax/core/Petition.php',
             $this->getProjectDir().'/src/ajax/core/Source.php',
             $this->getProjectDir().'/src/ajax/core/Timeout.php',
+            $this->getProjectDir().'/src/Application.php',
+            //-- Deprecations
+            $this->getProjectDir().'/lib/holiday_texts.php',
+            $this->getProjectDir().'/lib/mountname.php',
+            $this->getProjectDir().'/lib/mounts.php',
+            $this->getProjectDir().'/lib/name.php',
+            $this->getProjectDir().'/lib/pageparts.php',
+            $this->getProjectDir().'/lib/personal_functions.php',
+            $this->getProjectDir().'/lib/pvpsupport.php',
+            $this->getProjectDir().'/lib/pvpwarning.php',
+            $this->getProjectDir().'/lib/systemmail.php',
+            $this->getProjectDir().'/lib/titles.php',
         ]);
     }
 }
