bfc76e25ba190be66907d65c551c756b9cfb4a10
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-01-25 10:10:18 +0100

    Updated rules of Rector

--- rector.php
+++ rector.php
@@ -25,6 +25,9 @@ return static function (ContainerConfigurator $containerConfigurator): void
         __DIR__ . '/tests',
     ]);
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
+    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
     $containerConfigurator->import(SetList::DEAD_CODE);
     $containerConfigurator->import(SetList::CODE_QUALITY);
@@ -32,18 +35,28 @@ return static function (ContainerConfigurator $containerConfigurator): void
     $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::PHP_80);
     $containerConfigurator->import(SetList::PHP_81);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
 
     //-- Symfony Framework
+    $containerConfigurator->import(SymfonySetList::SYMFONY_40);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_41);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_42);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_43);
     $containerConfigurator->import(SymfonySetList::SYMFONY_44);
     $containerConfigurator->import(TwigSetList::TWIG_240);
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
     $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
 
     //-- Skip some rules/files ...
     $parameters->set(Option::SKIP, [
         __DIR__.'/src/core/Twig/NodeVisitor',
+        ShortenElseIfRector::class,
+        EventListenerToEventSubscriberRector::class,
         CallableThisArrayToAnonymousFunctionRector::class,
         AbsolutizeRequireAndIncludePathRector::class
     ]);
