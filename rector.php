c2364221e79d9d7b9b45d0c438740034eb6d2297
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-06-25 16:44:32 +0200

    Updated for compatibility with rector

--- rector.php
+++ rector.php
@@ -8,53 +8,49 @@ use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
 use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\TwigSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\Config\RectorConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void
+return static function (RectorConfig $rectorConfig): void
 {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PATHS, [
+    $rectorConfig->paths([
         __DIR__ . '/public',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_73);
-    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
-    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
-    $parameters->set(Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER, __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
 
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_73);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(SetList::PHP_81);
-    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
-    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
+    $rectorConfig->phpVersion(PhpVersion::PHP_73);
+    $rectorConfig->importNames(true, false);
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/coreLotgd_Core_KernelDevDebugContainer.xml');
+
+    $rectorConfig->import(SetList::DEAD_CODE);
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::PHP_73);
+    $rectorConfig->import(SetList::PHP_74);
+    $rectorConfig->import(SetList::PHP_80);
+    $rectorConfig->import(SetList::PHP_81);
+    $rectorConfig->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $rectorConfig->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
 
     //-- Symfony Framework
-    $containerConfigurator->import(SymfonySetList::SYMFONY_40);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_41);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_42);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_43);
-    $containerConfigurator->import(SymfonySetList::SYMFONY_44);
-    $containerConfigurator->import(TwigSetList::TWIG_240);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_25);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_ORM_29);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_COMMON_20);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_210);
-    $containerConfigurator->import(DoctrineSetList::DOCTRINE_DBAL_211);
+    $rectorConfig->import(SymfonySetList::SYMFONY_40);
+    $rectorConfig->import(SymfonySetList::SYMFONY_41);
+    $rectorConfig->import(SymfonySetList::SYMFONY_42);
+    $rectorConfig->import(SymfonySetList::SYMFONY_43);
+    $rectorConfig->import(SymfonySetList::SYMFONY_44);
+    $rectorConfig->import(TwigSetList::TWIG_240);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_25);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_ORM_29);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_CODE_QUALITY);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_COMMON_20);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_DBAL_210);
+    $rectorConfig->import(DoctrineSetList::DOCTRINE_DBAL_211);
 
     //-- Skip some rules/files ...
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         __DIR__.'/src/core/Twig/NodeVisitor',
         ShortenElseIfRector::class,
         EventListenerToEventSubscriberRector::class,
