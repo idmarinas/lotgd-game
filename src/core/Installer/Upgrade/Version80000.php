3da9d78812722988f147245f3166d1821c03259d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-07-09 17:30:59 +0200

    Updated

--- src/core/Installer/Upgrade/Version80000.php
+++ src/core/Installer/Upgrade/Version80000.php
@@ -30,10 +30,19 @@ class Version80000 extends InstallerAbstract
             $this->getProjectDir().'/lib/configuration/module.php',
             $this->getProjectDir().'/lib/modules',
             $this->getProjectDir().'/lib/modules.php',
+            $this->getProjectDir().'/lib/showform.php',
+            $this->getProjectDir().'/lib/showtabs.php',
+            $this->getProjectDir().'/lib/lotgd_mail.php',
             $this->getProjectDir().'/public/modules.php',
             $this->getProjectDir().'/themes/LotgdModern/templates/admin/page/modules',
             $this->getProjectDir().'/themes/LotgdModern/templates/admin/page/configuration/module.html.twig',
             $this->getProjectDir().'/src/ajax/core',
+            $this->getProjectDir().'/src/ajax/core/Entity/ModuleEventHooks.php',
+            $this->getProjectDir().'/src/ajax/core/Entity/ModuleHooks.php',
+            $this->getProjectDir().'/src/ajax/core/Entity/ModuleObjprefs.php',
+            $this->getProjectDir().'/src/ajax/core/Entity/ModuleSettings.php',
+            $this->getProjectDir().'/src/ajax/core/Entity/ModuleUserprefs.php',
+            $this->getProjectDir().'/src/ajax/core/Entity/Modules.php',
         ]);
     }
 }
