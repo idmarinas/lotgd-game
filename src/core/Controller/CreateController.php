95531d2cf183c892766de51c1479778b0e5101ab
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-07-20 12:44:28 +0200

    Fixed val email, previously it treated the id as numeric when it was a  string.

--- src/core/Controller/CreateController.php
+++ src/core/Controller/CreateController.php
@@ -120,7 +120,7 @@ class CreateController extends AbstractController
     {
         /** @var Lotgd\Core\Repository\UserRepository $accountRepo */
         $accountRepo = $this->getDoctrine()->getRepository('LotgdCore:User');
-        $code        = $request->query->getInt('id');
+        $code        = (string) $request->query->get('id');
 
         $account = $accountRepo->findOneBy(['emailvalidation' => $code]);
 
