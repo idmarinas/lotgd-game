9f13d3291d76f39e5e36a2d4d8580586078741e3
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2022-12-16 16:03:35 +0100

    Updated rules

--- rector.php
+++ rector.php
@@ -7,12 +7,14 @@ require_once 'src/constants.php';
 use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
 use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\TwigSetList;
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Rector\MethodCall\EntityAliasToClassConstantReferenceRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void
 {
@@ -28,16 +30,12 @@ return static function (RectorConfig $rectorConfig): void
 
     $rectorConfig->import(SetList::DEAD_CODE);
     $rectorConfig->import(SetList::CODE_QUALITY);
-    $rectorConfig->import(SetList::PHP_74);
+    $rectorConfig->import(LevelSetList::UP_TO_PHP_74);
     $rectorConfig->import(SetList::PHP_80);
     $rectorConfig->import(SetList::PHP_81);
 
     //-- Symfony Framework
-    $rectorConfig->import(SymfonySetList::SYMFONY_40);
-    $rectorConfig->import(SymfonySetList::SYMFONY_41);
-    $rectorConfig->import(SymfonySetList::SYMFONY_42);
-    $rectorConfig->import(SymfonySetList::SYMFONY_43);
-    $rectorConfig->import(SymfonySetList::SYMFONY_44);
+    $rectorConfig->import(SymfonyLevelSetList::UP_TO_SYMFONY_44);
     $rectorConfig->import(TwigSetList::TWIG_240);
     $rectorConfig->import(DoctrineSetList::DOCTRINE_25);
     $rectorConfig->import(DoctrineSetList::DOCTRINE_ORM_29);
@@ -45,6 +43,13 @@ return static function (RectorConfig $rectorConfig): void
     $rectorConfig->import(DoctrineSetList::DOCTRINE_COMMON_20);
     $rectorConfig->import(DoctrineSetList::DOCTRINE_DBAL_210);
     $rectorConfig->import(DoctrineSetList::DOCTRINE_DBAL_211);
+
+    $rectorConfig->ruleWithConfiguration(EntityAliasToClassConstantReferenceRector::class, [
+        EntityAliasToClassConstantReferenceRector::ALIASES_TO_NAMESPACES => [
+            'LotgdCore' => 'Lotgd\Core\Entity',
+            'LotgdLocal' => 'Lotgd\Local\Entity',
+        ]
+    ]);
 
     //-- Skip some rules/files ...
     $rectorConfig->skip([
