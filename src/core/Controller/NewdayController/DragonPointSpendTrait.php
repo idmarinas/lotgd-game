35f2c376292c4ca0b14494ade96c27139a55f4b1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2023-02-17 13:57:26 +0100

    Pass parameter $labels to method

--- src/core/Controller/NewdayController/DragonPointSpendTrait.php
+++ src/core/Controller/NewdayController/DragonPointSpendTrait.php
@@ -15,7 +15,7 @@ namespace Lotgd\Core\Controller\NewdayController;
 
 trait DragonPointSpendTrait
 {
-    protected function dragonPointSpend(array &$params, int $dkills, int $dp, array $canbuy, string $resline)
+    protected function dragonPointSpend(array $labels, array &$params, int $dkills, int $dp, array $canbuy, string $resline)
     {
         global $session;
 
