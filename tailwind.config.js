69e737578b94abdb89ed26c8096d963765b80afb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2023-06-03 14:33:02 +0200

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
         './assets/**/*.{js,ts}',
         './src/**/*.php',
         './themes/**/*.{html,twig}',
-        './vendor/**/src/Resources/views/*.{html,twig}'
+        './vendor/**/*.{html,twig}'
     ],
     darkMode: 'media', // or 'media' or 'class'
     theme: {
