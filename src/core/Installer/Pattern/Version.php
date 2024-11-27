346260507d982b92398f120ddfc8542b5d5a4020
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-27 12:34:02 +0100

    Added missing version

--- src/core/Installer/Pattern/Version.php
+++ src/core/Installer/Pattern/Version.php
@@ -146,6 +146,7 @@ trait Version
 		'7.1.11 IDMarinas Edition'        => 70111,
 		'7.1.12 IDMarinas Edition'        => 70112,
 		'7.1.13 IDMarinas Edition'        => 70113,
+		'7.1.14 IDMarinas Edition'        => 70114,
 	];
 
 	/**
