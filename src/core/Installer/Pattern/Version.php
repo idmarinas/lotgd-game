845d31d65077b090629fe466f2653d453a9664ef
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-27 13:25:41 +0100

    Fix typo

--- src/core/Installer/Pattern/Version.php
+++ src/core/Installer/Pattern/Version.php
@@ -27,6 +27,7 @@ trait Version
 	protected $lotgd_versions = [
 		'Clean Install'                   => -1, //needed just as a placeholder for new installs.
 	protected array $lotgd_versions = [
+		'Clean Install'                   => -1, //needed just as a placeholder for new installations.
 		'0.9'                             => 900,
 		'0.9.1'                           => 901,
 		'0.9.2'                           => 902,
