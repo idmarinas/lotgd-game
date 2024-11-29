0b1db01ad6a5c81f58640d3f126df89fb270a4d8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2024-11-29 22:15:50 +0100

    Updated for Jaxon-PHP 4

--- src/core/Service/Jaxon.php
+++ src/core/Service/Jaxon.php
@@ -3,30 +3,34 @@
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 4.10.0
+ * @since   4.10.0
  */
 
 namespace Lotgd\Core\Service;
 
-use Jaxon\App\Ajax;
 use Jaxon\Jaxon as JaxonCore;
 use function Jaxon\jaxon;
 
-class Jaxon extends Ajax
+class Jaxon
 {
-	public function __construct(array $config)
+	public function __construct (array $config)
 	{
-		$this->config()->setOptions($config);
+		jaxon()->config()->setOptions($config);
 
 		//-- Register all class of Lotgd in dir "src/ajax/core"
-		$this->register(JaxonCore::CALLABLE_DIR, './src/ajax/core', ['namespace' => 'Lotgd\\Ajax\\Core\\']);
+		jaxon()->register(JaxonCore::CALLABLE_DIR, './src/ajax/core', ['namespace' => 'Lotgd\\Ajax\\Core\\']);
 
 		//-- Register all custom class (Available globally) in dir "src/ajax/local"
-		$this->register(JaxonCore::CALLABLE_DIR, './src/ajax/local', ['namespace' => 'Lotgd\\Ajax\\Local\\']);
+		jaxon()->register(JaxonCore::CALLABLE_DIR, './src/ajax/local', ['namespace' => 'Lotgd\\Ajax\\Local\\']);
+	}
+
+	public function __invoke ()
+	{
+		return jaxon();
 	}
 }
