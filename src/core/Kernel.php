9f4a4a8987427f548e302eb1bef5ad3bd72ad9bd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-03 21:09:28 +0100

    fix(kernel): update version and improve formatting

--- src/core/Kernel.php
+++ src/core/Kernel.php
@@ -3,12 +3,12 @@
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 4.6.0
+ * @since   4.6.0
  */
 
 namespace Lotgd\Core;
@@ -20,89 +20,91 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use function dirname;
+use const PHP_VERSION_ID;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+	use MicroKernelTrait;
 
-    public const VERSION             = '7.1.13 IDMarinas Edition'; // Version of game in public display format.
-    public const VERSION_ID          = 70113; // Identify version of game in numeric format.
-    public const MAJOR_VERSION       = 7;
-    public const MINOR_VERSION       = 1;
-    public const RELEASE_VERSION     = 13;
-    public const EXTRA_VERSION       = '';
-    public const VERSION_NUMBER      = self::VERSION_ID; //-- Alias of VERSION_ID
-    public const TEXT_DOMAIN_DEFAULT = 'app_default';
+	public const VERSION             = '7.2.0 IDMarinas Edition'; // Version of game in public display format.
+	public const VERSION_ID          = 7020;                      // Identify version of game in numeric format.
+	public const MAJOR_VERSION       = 7;
+	public const MINOR_VERSION       = 2;
+	public const RELEASE_VERSION     = 0;
+	public const EXTRA_VERSION       = '';
+	public const VERSION_NUMBER      = self::VERSION_ID; //-- Alias of VERSION_ID
+	public const TEXT_DOMAIN_DEFAULT = 'app_default';
 
-    /**
-     * This series of scripts (collectively known as Legend of the Green Dragon or LotGD) is copyright as per below.
-     * You are prohibited by law from removing or altering this copyright information in any fashion except as follows:
-     *      if you have added functionality to the code, you may append your
-     *      name at the end indicating which parts are copyright by you.
-     *  Eg:
-     *  Copyright 2002-2004, Game: Eric Stevens & JT Traub, modified by Your Name.
-     *
-     * @var string
-     */
-    public const COPYRIGHT = 'Game Design and Code: Copyright &copy; 2002-2005, Eric Stevens & JT Traub, &copy; 2006-2007, Dragonprime Development Team, &copy; 2015-2022 IDMarinas remodelling and enhancing';
+	/**
+	 * This series of scripts (collectively known as Legend of the Green Dragon or LotGD) is copyright as per below.
+	 * You are prohibited by law from removing or altering this copyright information in any fashion except as follows:
+	 *      if you have added functionality to the code, you may append your
+	 *      name at the end indicating which parts are copyright by you.
+	 *  Eg:
+	 *  Copyright 2002-2004, Game: Eric Stevens & JT Traub, modified by Your Name.
+	 *
+	 * @var string
+	 */
+	public const COPYRIGHT = 'Game Design and Code: Copyright &copy; 2002-2005, Eric Stevens & JT Traub, &copy; 2006-2007, Dragonprime Development Team, &copy; 2015-2022 IDMarinas remodelling and enhancing';
 
-    /**
-     * This series of scripts (collectively known as Legend of the Green Dragon or LotGD) is licensed according to the Creating Commons Attribution
-     * Non-commercial Share-alike license.  The terms of this license must be followed for you to legally use or distribute this software. This
-     * license must be used on the distribution of any works derived from this work. This license text may not be removed nor altered in any way.
-     * Please see the file LICENSE for a full textual description of the license.
-     *
-     * @var string
-     */
-    public const LICENSE = "\n<!-- Creative Commons License -->\n<a href='http://creativecommons.org/licenses/by-nc-sa/2.0/' target='_blank' rel='noopener noreferrer'><img clear='right' align='left' alt='Creative Commons License' border='0' src='images/somerights20.gif' /></a>\nThis work is licensed under a <a href='http://creativecommons.org/licenses/by-nc-sa/2.0/' target='_blank' rel='noopener noreferrer'>Creative Commons License</a>.<br />\n<!-- /Creative Commons License -->\n<!--\n  <rdf:RDF xmlns='http://web.resource.org/cc/' xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>\n	<Work rdf:about=''>\n	  <dc:type rdf:resource='http://purl.org/dc/dcmitype/Interactive' />\n	  <license rdf:resource='http://creativecommons.org/licenses/by-nc-sa/2.0/' />\n	</Work>\n	<License rdf:about='http://creativecommons.org/licenses/by-nc-sa/2.0/'>\n	  <permits rdf:resource='http://web.resource.org/cc/Reproduction' />\n	  <permits rdf:resource='http://web.resource.org/cc/Distribution' />\n	  <requires rdf:resource='http://web.resource.org/cc/Notice' />\n	  <requires rdf:resource='http://web.resource.org/cc/Attribution' />\n	  <prohibits rdf:resource='http://web.resource.org/cc/CommercialUse' />\n	  <permits rdf:resource='http://web.resource.org/cc/DerivativeWorks' />\n	  <requires rdf:resource='http://web.resource.org/cc/ShareAlike' />\n	</License>\n  </rdf:RDF>\n-->\n";
+	/**
+	 * This series of scripts (collectively known as Legend of the Green Dragon or LotGD) is licensed according to the
+	 * Creating Commons Attribution Non-commercial Share-alike license.  The terms of this license must be followed for
+	 * you to legally use or distribute this software. This license must be used on the distribution of any works
+	 * derived from this work. This license text may not be removed nor altered in any way. Please see the file LICENSE
+	 * for a full textual description of the license.
+	 *
+	 * @var string
+	 */
+	public const LICENSE = "\n<!-- Creative Commons License -->\n<a href='http://creativecommons.org/licenses/by-nc-sa/2.0/' target='_blank' rel='noopener noreferrer'><img clear='right' align='left' alt='Creative Commons License' border='0' src='images/somerights20.gif' /></a>\nThis work is licensed under a <a href='http://creativecommons.org/licenses/by-nc-sa/2.0/' target='_blank' rel='noopener noreferrer'>Creative Commons License</a>.<br />\n<!-- /Creative Commons License -->\n<!--\n  <rdf:RDF xmlns='http://web.resource.org/cc/' xmlns:dc='http://purl.org/dc/elements/1.1/' xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>\n	<Work rdf:about=''>\n	  <dc:type rdf:resource='http://purl.org/dc/dcmitype/Interactive' />\n	  <license rdf:resource='http://creativecommons.org/licenses/by-nc-sa/2.0/' />\n	</Work>\n	<License rdf:about='http://creativecommons.org/licenses/by-nc-sa/2.0/'>\n	  <permits rdf:resource='http://web.resource.org/cc/Reproduction' />\n	  <permits rdf:resource='http://web.resource.org/cc/Distribution' />\n	  <requires rdf:resource='http://web.resource.org/cc/Notice' />\n	  <requires rdf:resource='http://web.resource.org/cc/Attribution' />\n	  <prohibits rdf:resource='http://web.resource.org/cc/CommercialUse' />\n	  <permits rdf:resource='http://web.resource.org/cc/DerivativeWorks' />\n	  <requires rdf:resource='http://web.resource.org/cc/ShareAlike' />\n	</License>\n  </rdf:RDF>\n-->\n";
 
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+	private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    //-- Register custom compiler pass
-    protected function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new NotificationsPass());
-    }
+	//-- Register custom compiler pass
 
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        foreach ($contents as $class => $envs)
-        {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false)
-            {
-                yield new $class();
-            }
-        }
-    }
+	public function registerBundles (): iterable
+	{
+		$contents = require $this->getProjectDir() . '/config/bundles.php';
+		foreach ($contents as $class => $envs) {
+			if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+				yield new $class();
+			}
+		}
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getProjectDir(): string
-    {
-        return \dirname(__DIR__, 2);
-    }
+	protected function configureContainer (ContainerBuilder $container, LoaderInterface $loader): void
+	{
+		$container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
+		$container->setParameter('container.dumper.inline_class_loader', PHP_VERSION_ID < 70400 || $this->debug);
+		$container->setParameter('container.dumper.inline_factories', true);
+		$confDir = $this->getProjectDir() . '/config';
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
-    {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
-        $container->setParameter('container.dumper.inline_factories', true);
-        $confDir = $this->getProjectDir().'/config';
+		$loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+		$loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+	}
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
-    }
+	protected function configureRoutes (RouteCollectionBuilder $routes): void
+	{
+		$confDir = $this->getProjectDir() . '/config';
 
-    protected function configureRoutes(RouteCollectionBuilder $routes): void
-    {
-        $confDir = $this->getProjectDir().'/config';
+		$routes->import($confDir . '/{routes}/' . $this->environment . '/*' . self::CONFIG_EXTS, '/', 'glob');
+		$routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+		$routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+	}
 
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
-    }
+	protected function build (ContainerBuilder $container): void
+	{
+		$container->addCompilerPass(new NotificationsPass());
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getProjectDir (): string
+	{
+		return dirname(__DIR__, 2);
+	}
 }
