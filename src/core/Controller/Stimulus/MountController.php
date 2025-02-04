677ed9625a4f5460ae69af25e873f113a54959d0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-04 21:01:38 +0100

    feat(controller): add MountController for see list of owners in admin panel

---
+++ src/core/Controller/Stimulus/MountController.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of "LoTGD Core Package".
+ *
+ * @see     https://github.com/idmarinas/lotgd-game
+ *
+ * @license LICENSE.txt
+ * @author  IDMarinas
+ *
+ * @since   7.2.0
+ */
+
+namespace Lotgd\Core\Controller\Stimulus;
+
+use Lotgd\Core\Controller\LotgdControllerInterface;
+use Lotgd\Core\Http\Request;
+use Lotgd\Core\Output\Format;
+use Lotgd\Core\Pattern\LotgdControllerTrait;
+use Lotgd\Core\Repository\AvatarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class MountController extends AbstractController implements LotgdControllerInterface
+{
+	use LotgdControllerTrait;
+
+	private AvatarRepository $repository;
+	private Format           $format;
+
+	public function __construct (AvatarRepository $repository, Format $format)
+	{
+		$this->repository = $repository;
+		$this->format = $format;
+	}
+
+	public function index (Request $request): Response
+	{
+		$mountId = $request->query->getInt('id');
+		$content = '';
+
+		$result = $this->repository->findBy(['hashorse' => $mountId], null, 50);
+
+		foreach ($result as $item) {
+			$content .= $this->format->colorize($item->getName()) . '<br />';
+		}
+
+		if (count($result) > 50) {
+			$content .= '... <br />';
+		}
+
+		return $this->renderBlock('components/modal.html.twig', 'dialog', [
+			'content'      => $content ?: '---',
+			'close_button' => true,
+		]);
+	}
+
+	public function allowAnonymous (): bool
+	{
+		return false;
+	}
+
+	public function overrideForcedNav (): bool
+	{
+		return true;
+	}
+}
