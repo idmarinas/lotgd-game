bb21022daf22bc34d31d47024826faf941118d89
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-06 21:25:05 +0100

    rector(format): apply suggestions of Rector

--- src/core/Service/PaypalButtons.php
+++ src/core/Service/PaypalButtons.php
@@ -3,28 +3,37 @@
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 5.5.0
+ * @since   5.5.0
  */
 
 namespace Lotgd\Core\Service;
 
 use Sonata\BlockBundle\Event\BlockEvent;
 use Sonata\BlockBundle\Model\Block;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-class PaypalButtons
+class PaypalButtons implements EventSubscriberInterface
 {
-    public function onBlock(BlockEvent $event)
-    {
-        $block = new Block();
-        $block->setId(uniqid('', true)); // set a fake id
-        $block->setSettings($event->getSettings());
-        $block->setType('lotgd.core.template.block.donation.buttons');
+	/**
+	 * @return array<string, mixed>
+	 */
+	public static function getSubscribedEvents (): array
+	{
+		return ['sonata.block.event.lotgd_core.paypal' => 'onBlock'];
+	}
 
-        $event->addBlock($block);
-    }
+	public function onBlock (BlockEvent $event)
+	{
+		$block = new Block();
+		$block->setId(uniqid('', true)); // set a fake id
+		$block->setSettings($event->getSettings());
+		$block->setType('lotgd.core.template.block.donation.buttons');
+
+		$event->addBlock($block);
+	}
 }
