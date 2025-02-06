03211342947ebc1c3b2dccff21c02939e3ab2f5d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-06 19:09:33 +0100

    fix(functions): clean up code formatting and improve function definitions
    Add function output() and rawoutput() for compatibility with old modules, removed in version 8.0.0

--- src/functions.php
+++ src/functions.php
@@ -1,136 +1,160 @@
 <?php
 
-require_once __DIR__.'/functions_old.php';
+require_once __DIR__ . '/functions_old.php';
 
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 4.10.0
+ * @since   4.10.0
  */
-if ( ! \function_exists('e_rand'))
-{
-    /**
-     * Alias of mt_rand with some improvements.
-     *
-     * @param int|float $min
-     * @param int|float $max
-     */
-    function e_rand($min = null, $max = null)
-    {
-        if ( ! \is_numeric($min))
-        {
-            return \mt_rand();
-        }
+if (!function_exists('e_rand')) {
+	/**
+	 * Alias of mt_rand with some improvements.
+	 *
+	 * @param int|float $min
+	 * @param int|float $max
+	 */
+	function e_rand ($min = null, $max = null)
+	{
+		if (!is_numeric($min)) {
+			return mt_rand();
+		}
 
-        $min = \round($min);
+		$min = round($min);
 
-        if ( ! \is_numeric($max))
-        {
-            return $min;
-        }
+		if (!is_numeric($max)) {
+			return $min;
+		}
 
-        $max = \round($max);
+		$max = round($max);
 
-        return \mt_rand(\min($min, $max), \max($min, $max));
-    }
+		return mt_rand(min($min, $max), max($min, $max));
+	}
 }
 
-if ( ! \function_exists('r_rand'))
-{
-    /**
-     * Same as e_rand but $min and $max are multiplied by 1000.
-     *
-     * @param int|float $min
-     * @param int|float $max
-     *
-     * @return int|float
-     */
-    function r_rand($min = null, $max = null)
-    {
-        if ( ! \is_numeric($min))
-        {
-            return \mt_rand();
-        }
+if (!function_exists('r_rand')) {
+	/**
+	 * Same as e_rand but $min and $max are multiplied by 1000.
+	 *
+	 * @param int|float $min
+	 * @param int|float $max
+	 *
+	 * @return int|float
+	 */
+	function r_rand ($min = null, $max = null)
+	{
+		if (!is_numeric($min)) {
+			return mt_rand();
+		}
 
-        $min *= 1000;
+		$min *= 1000;
 
-        if ( ! \is_numeric($max))
-        {
-            return \mt_rand($min) / 1000;
-        }
+		if (!is_numeric($max)) {
+			return mt_rand($min) / 1000;
+		}
 
-        $max *= 1000;
+		$max *= 1000;
 
-        return \mt_rand(\min($min, $max), \max($min, $max)) / 1000;
-    }
+		return mt_rand(min($min, $max), max($min, $max)) / 1000;
+	}
 }
 
-if ( ! \function_exists('myDefine'))
-{
-    /** @deprecated 7.1 Use syntax "defined() || define();". Example in src/constants.php */
-    function myDefine($name, $value)
-    {
-        \trigger_error(\sprintf(
-            'Usage of %s is obsolete since 7.1.0; and delete in future version. Use syntax "defined() || define();". Example in src/constants.php',
-            __METHOD__
-        ), E_USER_DEPRECATED);
+if (!function_exists('myDefine')) {
+	/** @deprecated 7.1 Use syntax "defined() || define();". Example in src/constants.php */
+	function myDefine ($name, $value)
+	{
+		trigger_error(
+		  sprintf(
+			'Usage of %s is obsolete since 7.1.0; and delete in future version. Use syntax "defined() || define();". Example in src/constants.php',
+			__METHOD__
+		  ),
+		  E_USER_DEPRECATED
+		);
 
-        //-- No try to define a defined constant
-        if ( ! \defined($name))
-        {
-            \define($name, $value);
-        }
-    }
+		//-- No try to define a defined constant
+		if (!defined($name)) {
+			define($name, $value);
+		}
+	}
 }
 
-if ( ! \function_exists('safeescape'))
-{
-    /** @deprecated 7.1 */
-    function safeescape($input)
-    {
-        \trigger_error(\sprintf(
-            'Usage of %s is obsolete since 7.1.0; and delete in future version.',
-            __METHOD__
-        ), E_USER_DEPRECATED);
+if (!function_exists('safeescape')) {
+	/** @deprecated 7.1 */
+	function safeescape ($input)
+	{
+		trigger_error(
+		  sprintf(
+			'Usage of %s is obsolete since 7.1.0; and delete in future version.',
+			__METHOD__
+		  ),
+		  E_USER_DEPRECATED
+		);
 
-        $prevchar = '';
-        $out      = '';
+		$prevchar = '';
+		$out = '';
 
-        for ($x = 0; $x < \strlen($input); ++$x)
-        {
-            $char = \substr($input, $x, 1);
+		for ($x = 0; $x < strlen($input); ++$x) {
+			$char = substr($input, $x, 1);
 
-            if (("'" == $char || '"' == $char) && '\\' != $prevchar)
-            {
-                $char = '\\' . $char;
-            }
+			if (("'" == $char || '"' == $char) && '\\' != $prevchar) {
+				$char = '\\' . $char;
+			}
 
-            $out .= $char;
-            $prevchar = $char;
-        }
+			$out .= $char;
+			$prevchar = $char;
+		}
 
-        return $out;
-    }
+		return $out;
+	}
 }
 
-if ( ! \function_exists('nltoappon'))
-{
-    /** @deprecated 7.1 */
-    function nltoappon($in)
-    {
-        \trigger_error(\sprintf(
-            'Usage of %s is obsolete since 7.1.0; and delete in future version.',
-            __METHOD__
-        ), E_USER_DEPRECATED);
+if (!function_exists('nltoappon')) {
+	/** @deprecated 7.1 */
+	function nltoappon ($in)
+	{
+		trigger_error(
+		  sprintf(
+			'Usage of %s is obsolete since 7.1.0; and delete in future version.',
+			__METHOD__
+		  ),
+		  E_USER_DEPRECATED
+		);
 
-        $out = \str_replace("\r\n", "\n", $in);
-        $out = \str_replace("\r", "\n", $out);
+		$out = str_replace("\r\n", "\n", $in);
+		$out = str_replace("\r", "\n", $out);
 
-        return \str_replace("\n", '`n', $out);
-    }
+		return str_replace("\n", '`n', $out);
+	}
+}
+
+// The following functions are only temporary for this 7.x version and will be removed in version 8.0.0
+// It is to add more compatibility with the old modules.
+
+if (!function_exists('output')) {
+	function output (): void
+	{
+		$args = func_get_args();
+		$text = array_shift($args);
+
+		if (count($args) > 0) {
+			//special case since we use `% as a color code so often.
+			$text = str_replace('`%', '`%%', $text);
+			$text = sprintf($text, ...$args);
+		}
+
+		//`1`2 etc. color & formatting
+		LotgdResponse::pageAddContent(LotgdFormat::colorize($text));
+	}
+}
+
+if (!function_exists('rawoutput')) {
+	function rawoutput ($data): void
+	{
+		LotgdResponse::pageAddContent($data);
+	}
 }
