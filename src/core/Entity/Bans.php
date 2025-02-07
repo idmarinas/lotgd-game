c9dd7612f100833d40224b3c6f1598bb894febe1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-07 17:43:48 +0100

    refactor(entity): improve formatting and structure in Bans.php
    - Adjusted spacing and indentation for better readability.
    - Updated comments for consistency and clarity.
    - Improved method definitions to enhance code quality.

--- src/core/Entity/Bans.php
+++ src/core/Entity/Bans.php
@@ -3,18 +3,18 @@
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 4.0.0
+ * @since   4.0.0
  */
 
 namespace Lotgd\Core\Entity;
 
-use DateTimeImmutable;
 use DateTime;
+use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -26,182 +26,182 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Bans
 {
-    /**
-     * @ORM\Column(name="banexpire", type="datetime")
-     */
-    private ?DateTimeInterface $banexpire;
+	/**
+	 * @ORM\Column(name="banexpire", type="datetime")
+	 */
+	private ?DateTimeInterface $banexpire;
 
-    /**
-     *
-     * @ORM\Column(name="uniqueid", type="string", length=32)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     */
-    private ?string $uniqueid = '';
+	/**
+	 *
+	 * @ORM\Column(name="uniqueid", type="string", length=32)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="NONE")
+	 */
+	private ?string $uniqueid = '';
 
-    /**
-     *
-     * @ORM\Column(name="ipfilter", type="string", length=40)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     */
-    private ?string $ipfilter = '';
+	/**
+	 *
+	 * @ORM\Column(name="ipfilter", type="string", length=40)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="NONE")
+	 */
+	private ?string $ipfilter = '';
 
-    /**
-     *
-     * @ORM\Column(name="banreason", type="text", length=65535)
-     */
-    private ?string $banreason = null;
+	/**
+	 *
+	 * @ORM\Column(name="banreason", type="text", length=65535)
+	 */
+	private ?string $banreason = null;
 
-    /**
-     *
-     * @ORM\Column(name="banner", type="string", length=50)
-     */
-    private ?string $banner = null;
+	/**
+	 *
+	 * @ORM\Column(name="banner", type="string", length=50)
+	 */
+	private ?string $banner = null;
 
-    /**
-     * @ORM\Column(name="lasthit", type="datetime", options={"default"="0000-00-00 00:00:00"})
-     */
-    private ?DateTimeInterface $lasthit = null;
+	/**
+	 * @ORM\Column(name="lasthit", type="datetime", options={"default"="0000-00-00 00:00:00"})
+	 */
+	private ?DateTimeInterface $lasthit = null;
 
-    public function __construct()
-    {
-        $this->lasthit = new DateTime('now');
-    }
+	public function __construct ()
+	{
+		$this->lasthit = new DateTime('now');
+	}
 
-    /**
-     * Set the value of Banexpire.
-     *
-     * @param DateTime|DateTimeImmutable $banexpire
-     *
-     * @return self
-     */
-    public function setBanexpire(DateTimeInterface $banexpire)
-    {
-        $this->banexpire = $banexpire;
+	/**
+	 * Get the value of Banexpire.
+	 *
+	 * @return DateTime|DateTimeImmutable
+	 */
+	public function getBanexpire (): DateTimeInterface
+	{
+		return $this->banexpire;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Banexpire.
+	 *
+	 * @param DateTime|DateTimeImmutable $banexpire
+	 *
+	 * @return self
+	 */
+	public function setBanexpire (DateTimeInterface $banexpire)
+	{
+		$this->banexpire = $banexpire;
 
-    /**
-     * Get the value of Banexpire.
-     *
-     * @return DateTime|DateTimeImmutable
-     */
-    public function getBanexpire(): DateTimeInterface
-    {
-        return $this->banexpire;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the value of Uniqueid.
-     *
-     * @param string $uniqueid
-     *
-     * @return self
-     */
-    public function setUniqueid($uniqueid)
-    {
-        $this->uniqueid = $uniqueid;
+	/**
+	 * Get the value of Uniqueid.
+	 */
+	public function getUniqueid (): string
+	{
+		return $this->uniqueid;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Uniqueid.
+	 *
+	 * @param string $uniqueid
+	 *
+	 * @return self
+	 */
+	public function setUniqueid ($uniqueid)
+	{
+		$this->uniqueid = $uniqueid;
 
-    /**
-     * Get the value of Uniqueid.
-     */
-    public function getUniqueid(): string
-    {
-        return $this->uniqueid;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the value of Ipfilter.
-     *
-     * @param string $ipfilter
-     *
-     * @return self
-     */
-    public function setIpfilter($ipfilter)
-    {
-        $this->ipfilter = $ipfilter;
+	/**
+	 * Get the value of Ipfilter.
+	 */
+	public function getIpfilter (): string
+	{
+		return $this->ipfilter;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Ipfilter.
+	 *
+	 * @param string $ipfilter
+	 *
+	 * @return self
+	 */
+	public function setIpfilter ($ipfilter)
+	{
+		$this->ipfilter = $ipfilter;
 
-    /**
-     * Get the value of Ipfilter.
-     */
-    public function getIpfilter(): string
-    {
-        return $this->ipfilter;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the value of Banreason.
-     *
-     * @param string $banreason
-     *
-     * @return self
-     */
-    public function setBanreason($banreason)
-    {
-        $this->banreason = $banreason;
+	/**
+	 * Get the value of Banreason.
+	 */
+	public function getBanreason (): string
+	{
+		return $this->banreason;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Banreason.
+	 *
+	 * @param string $banreason
+	 *
+	 * @return self
+	 */
+	public function setBanreason ($banreason)
+	{
+		$this->banreason = $banreason;
 
-    /**
-     * Get the value of Banreason.
-     */
-    public function getBanreason(): string
-    {
-        return $this->banreason;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the value of Banner.
-     *
-     * @param string $banner
-     *
-     * @return self
-     */
-    public function setBanner($banner)
-    {
-        $this->banner = $banner;
+	/**
+	 * Get the value of Banner.
+	 */
+	public function getBanner (): string
+	{
+		return $this->banner;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Banner.
+	 *
+	 * @param string $banner
+	 *
+	 * @return self
+	 */
+	public function setBanner ($banner)
+	{
+		$this->banner = $banner;
 
-    /**
-     * Get the value of Banner.
-     */
-    public function getBanner(): string
-    {
-        return $this->banner;
-    }
+		return $this;
+	}
 
-    /**
-     * Set the value of Lasthit.
-     *
-     * @param DateTime|DateTimeImmutable $lasthit
-     *
-     * @return self
-     */
-    public function setLasthit(DateTimeInterface $lasthit)
-    {
-        $this->lasthit = $lasthit;
+	/**
+	 * Get the value of Lasthit.
+	 *
+	 * @return DateTime|DateTimeImmutable
+	 */
+	public function getLasthit (): DateTimeInterface
+	{
+		return $this->lasthit;
+	}
 
-        return $this;
-    }
+	/**
+	 * Set the value of Lasthit.
+	 *
+	 * @param DateTime|DateTimeImmutable $lasthit
+	 *
+	 * @return self
+	 */
+	public function setLasthit (DateTimeInterface $lasthit)
+	{
+		$this->lasthit = $lasthit;
 
-    /**
-     * Get the value of Lasthit.
-     *
-     * @return DateTime|DateTimeImmutable
-     */
-    public function getLasthit(): DateTimeInterface
-    {
-        return $this->lasthit;
-    }
+		return $this;
+	}
 }
