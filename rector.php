566e147c521ecf3c1365bc8fe360e53cf5694a22
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-07 18:58:30 +0100

    refactor(config): update rector configuration for improved code quality
    - Updated the paths for rector to include new directories.
    - Changed PHP sets to target PHP 7.4.
    - Added Symfony container XML configuration.
    - Updated the sets to include new quality and coding standards.
    - Specified rules to skip for various directories and files.

--- rector.php
+++ rector.php
@@ -4,45 +4,69 @@ declare(strict_types=1);
 
 require_once 'src/constants.php';
 
+use Rector\CodeQuality\Rector\ClassMethod\ExplicitReturnNullRector;
+use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
+use Rector\CodeQuality\Rector\Include_\AbsolutizeRequireAndIncludePathRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\FuncCall\StrictArraySearchRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Php56\Rector\FuncCall\PowToExpRector;
+use Rector\Php70\Rector\Ternary\TernaryToSpaceshipRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\Symfony\Set\SymfonySetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\Closure\AddClosureVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
-	->withPaths([
-		__DIR__ . '/app/src',
-		__DIR__ . '/factories',
-		__DIR__ . '/public',
-		__DIR__ . '/src',
-		__DIR__ . '/tests',
-	])
-	->withPhpSets(false, false, false, false, true)
-	->withPreparedSets(
-		true,
-		true,
-		true,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		false,
-		true,
-		true,
-		true,
-		true
-	)
-	->withImportNames(true, true, true, true)
-	->withTypeCoverageLevel(0)
-	->withSets([
-		SymfonySetList::SYMFONY_64,
-		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-	])
-	->withRules([AddVoidReturnTypeWhereNoReturnRector::class])
-	->withSkip([
-		__DIR__ . '/src/core/Twig/NodeVisitor',
-	])
+  ->withPaths([
+	__DIR__ . '/cronjob',
+	__DIR__ . '/lib',
+	__DIR__ . '/public',
+	__DIR__ . '/src',
+	__DIR__ . '/tests',
+  ])
+  ->withPhp74Sets()
+  ->withImportNames(true, true, true, true)
+  ->withTypeCoverageLevel(0)
+  ->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/coreLotgd_Core_KernelDevDebugContainer.xml')
+  ->withSets([
+	SetList::DEAD_CODE,
+	SetList::CODE_QUALITY,
+	SetList::CODING_STYLE,
+	DoctrineSetList::DOCTRINE_CODE_QUALITY,
+	SymfonySetList::SYMFONY_44,
+	SymfonySetList::SYMFONY_CODE_QUALITY,
+  ])
+  ->withSkip([
+	  // Dirs and files
+	  __DIR__ . '/src/core/Entity/*Translation.php',
+	  __DIR__ . '/src/core/Twig/NodeVisitor',
+	  // Rules
+	  NewlineAfterStatementRector::class,
+	  AbsolutizeRequireAndIncludePathRector::class,
+	  EncapsedStringsToSprintfRector::class,
+	  AddVoidReturnTypeWhereNoReturnRector::class,
+	  SimplifyEmptyCheckOnEmptyArrayRector::class,
+	  AddClosureVoidReturnTypeWhereNoReturnRector::class,
+	  TernaryToSpaceshipRector::class,
+	  PowToExpRector::class,
+	  ExplicitReturnNullRector::class,
+	  UseIdenticalOverEqualWithSameTypeRector::class,
+	  StrictArraySearchRector::class,
+	  DisallowedEmptyRuleFixerRector::class,
+	  ShortenElseIfRector::class               => [
+		__DIR__ . '/lib/showform.php',
+		__DIR__ . '/public/login.php',
+	  ],
+	  RemoveAlwaysTrueIfConditionRector::class => [
+		__DIR__ . '/public/forest.php',
+		__DIR__ . '/public/common.php',
+	  ],
+  ])
 ;
