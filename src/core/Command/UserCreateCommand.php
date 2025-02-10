6fb4942574eb5151b6640df8653f222adaaf2b01
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-10 12:47:07 +0100

    feat(command): refactor UserCreateCommand for improved readability
    - Updated formatting for better code clarity.
    - Reorganized import statements for consistency.
    - Enhanced variable declarations and method structures.
    - Improved exception handling and user prompts.
    - Ensured proper use of Symfony components and services.

--- src/core/Command/UserCreateCommand.php
+++ src/core/Command/UserCreateCommand.php
@@ -3,24 +3,23 @@
 /**
  * This file is part of Legend of the Green Dragon.
  *
- * @see https://github.com/idmarinas/lotgd-game
+ * @see     https://github.com/idmarinas/lotgd-game
  *
  * @license https://github.com/idmarinas/lotgd-game/blob/migration/public/LICENSE.txt
- * @author IDMarinas
+ * @author  IDMarinas
  *
- * @since 5.0.0
+ * @since   5.0.0
  */
 
 namespace Lotgd\Core\Command;
 
-use Lotgd\Core\Entity\User;
 use DateTime;
-use Lotgd\Core\Entity\Avatar;
-use Throwable;
-use DomainException;
 use Doctrine\ORM\EntityManagerInterface;
-use Lotgd\Core\Repository\UserRepository;
+use DomainException;
+use Lotgd\Core\Entity\Avatar;
+use Lotgd\Core\Entity\User;
 use Lotgd\Core\Lib\Settings;
+use Lotgd\Core\Repository\UserRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -32,6 +31,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Throwable;
+use function array_search;
 
 /**
  * Command to create a new user.
@@ -39,282 +40,283 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 final class UserCreateCommand extends Command
 {
-    public const TEXT_DOMAIN = 'console_command';
+	public const TEXT_DOMAIN = 'console_command';
 
-    protected const SU_MEGAUSER = SU_MEGAUSER | SU_EDIT_MOUNTS | SU_EDIT_CREATURES | SU_EDIT_PETITIONS | SU_EDIT_COMMENTS | SU_EDIT_DONATIONS | SU_EDIT_USERS | SU_EDIT_CONFIG | SU_INFINITE_DAYS | SU_EDIT_EQUIPMENT | SU_EDIT_PAYLOG | SU_DEVELOPER | SU_POST_MOTD | SU_MODERATE_CLANS | SU_EDIT_RIDDLES | SU_MANAGE_MODULES | SU_AUDIT_MODERATION | SU_RAW_SQL | SU_VIEW_SOURCE | SU_NEVER_EXPIRE;
+	protected const SU_MEGAUSER = SU_MEGAUSER
+	                              | SU_EDIT_MOUNTS
+	                              | SU_EDIT_CREATURES
+	                              | SU_EDIT_PETITIONS
+	                              | SU_EDIT_COMMENTS
+	                              | SU_EDIT_DONATIONS
+	                              | SU_EDIT_USERS
+	                              | SU_EDIT_CONFIG
+	                              | SU_INFINITE_DAYS
+	                              | SU_EDIT_EQUIPMENT
+	                              | SU_EDIT_PAYLOG
+	                              | SU_DEVELOPER
+	                              | SU_POST_MOTD
+	                              | SU_MODERATE_CLANS
+	                              | SU_EDIT_RIDDLES
+	                              | SU_MANAGE_MODULES
+	                              | SU_AUDIT_MODERATION
+	                              | SU_RAW_SQL
+	                              | SU_VIEW_SOURCE
+	                              | SU_NEVER_EXPIRE;
 
-    protected static $defaultName = 'lotgd:user:create';
+	protected static $defaultName = 'lotgd:user:create';
 
-    protected $doctrine;
-    protected $translator;
-    protected $validator;
-    protected $settings;
-    protected $accountRepository;
-    protected $passwordEncoder;
+	protected $doctrine;
+	protected $translator;
+	protected $validator;
+	protected $settings;
+	protected $accountRepository;
+	protected $passwordEncoder;
 
-    public function __construct(
-        EntityManagerInterface $doctrine,
-        TranslatorInterface $translator,
-        ValidatorInterface $validator,
-        Settings $settings,
-        UserPasswordEncoderInterface $passwordEncoder
-    ) {
-        parent::__construct();
+	public function __construct (
+	  EntityManagerInterface       $doctrine,
+	  TranslatorInterface          $translator,
+	  ValidatorInterface           $validator,
+	  Settings                     $settings,
+	  UserPasswordEncoderInterface $passwordEncoder
+	) {
+		parent::__construct();
 
-        $this->doctrine   = $doctrine;
-        $this->translator = $translator;
-        $this->validator  = $validator;
-        $this->settings   = $settings;
-        $this->passwordEncoder = $passwordEncoder;
-    }
+		$this->doctrine = $doctrine;
+		$this->translator = $translator;
+		$this->validator = $validator;
+		$this->settings = $settings;
+		$this->passwordEncoder = $passwordEncoder;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
-    {
-        $this
-            ->setDescription('Create a new user in LoTGD')
-            ->setHelp(
-                <<<'EOT'
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function configure ()
+	{
+		$this
+		  ->setDescription('Create a new user in LoTGD')
+		  ->setHelp(
+			<<<'EOT'
                     The <info>%command.name%</info> command allow create a new user in LoTGD.
                     EOT
-            )
-        ;
-    }
+		  )
+		;
+	}
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $style = new SymfonyStyle($input, $output);
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function execute (InputInterface $input, OutputInterface $output): int
+	{
+		$style = new SymfonyStyle($input, $output);
 
-        $login    = $this->getLoginName($input, $output);
-        $email    = $this->getEmail($input, $output);
-        $password = $this->getPassword($input, $output);
-        $isAdmin  = $this->getIsAdmin($input, $output);
+		$login = $this->getLoginName($input, $output);
+		$email = $this->getEmail($input, $output);
+		$password = $this->getPassword($input, $output);
+		$isAdmin = $this->getIsAdmin($input, $output);
 
-        try
-        {
-            //-- Configure account
-            $account = new User();
-            $account->setLogin($login)
-                ->setEmailaddress($email)
-                ->setRegdate(new DateTime())
-            ;
+		try {
+			//-- Configure account
+			$account = new User();
+			$account
+			  ->setLogin($login)
+			  ->setEmailaddress($email)
+			  ->setRegdate(new DateTime())
+			;
 
-            $password = $this->passwordEncoder->encodePassword($account, $password);
-            $account->setPassword($password);
+			$password = $this->passwordEncoder->encodePassword($account, $password);
+			$account->setPassword($password);
 
-            //-- Need for get a ID of new account
-            $this->doctrine->persist($account);
-            $this->doctrine->flush(); //Persist objects
+			//-- Need for get a ID of new account
+			$this->doctrine->persist($account);
+			$this->doctrine->flush(); //Persist objects
 
-            //-- Configure character
-            $title     = $this->getCharacterTitle();
-            $character = new Avatar();
-            $character->setPlayername($login)
-                ->setName("{$title} {$login}")
-                ->setTitle($title)
-                ->setAcct($account)
-                ->setGold((int) $this->settings->getSetting('newplayerstartgold', 50))
-                ->setLocation((string) $this->settings->getSetting('villagename', LOCATION_FIELDS))
-            ;
+			//-- Configure character
+			$title = $this->getCharacterTitle();
+			$character = new Avatar();
+			$character
+			  ->setPlayername($login)
+			  ->setName("{$title} {$login}")
+			  ->setTitle($title)
+			  ->setAcct($account)
+			  ->setGold((int)$this->settings->getSetting('newplayerstartgold', 50))
+			  ->setLocation((string)$this->settings->getSetting('villagename', LOCATION_FIELDS))
+			;
 
-            if ($isAdmin)
-            {
-                $account->setSuperuser(self::SU_MEGAUSER);
+			if ($isAdmin) {
+				$account->setSuperuser(self::SU_MEGAUSER);
 
-                $character
-                    ->setName("`%Admin`0 `&{$login}`0")
-                    ->setCtitle('`%Admin`0')
-                ;
-            }
+				$character
+				  ->setName("`%Admin`0 `&{$login}`0")
+				  ->setCtitle('`%Admin`0')
+				;
+			}
 
-            //-- Need for get ID of new character
-            $this->doctrine->persist($character);
-            $this->doctrine->flush(); //-- Persist objects
+			//-- Need for get ID of new character
+			$this->doctrine->persist($character);
+			$this->doctrine->flush(); //-- Persist objects
 
-            //-- Set ID of character and update Account
-            $account->setAvatar($character);
-            $this->doctrine->persist($account);
-            $this->doctrine->flush(); //-- Persist objects
-        }
-        catch (Throwable $throwable)
-        {
-            $style->text($throwable->getMessage());
+			//-- Set ID of character and update Account
+			$account->setAvatar($character);
+			$this->doctrine->persist($account);
+			$this->doctrine->flush(); //-- Persist objects
+		} catch (Throwable $throwable) {
+			$style->text($throwable->getMessage());
 
-            $style->error($this->translator->trans('user.create.fail', [], self::TEXT_DOMAIN));
+			$style->error($this->translator->trans('user.create.fail', [], self::TEXT_DOMAIN));
 
-            return 1;
-        }
+			return 1;
+		}
 
-        $style->success($this->translator->trans('user.create.success', [], self::TEXT_DOMAIN));
+		$style->success($this->translator->trans('user.create.success', [], self::TEXT_DOMAIN));
 
-        return 0;
-    }
+		return 0;
+	}
 
-    private function getLoginName(InputInterface $input, OutputInterface $output): string
-    {
-        $helper = $this->getHelper('question');
+	private function getLoginName (InputInterface $input, OutputInterface $output): string
+	{
+		$helper = $this->getHelper('question');
 
-        $question = new Question($this->translator->trans('user.create.question.login', [], self::TEXT_DOMAIN));
-        $question->setValidator(function ($value)
-        {
-            $errors = $this->validator->validate((string) $value, [
-                new Assert\Length([ 'min' => 3, 'max' => 25]),
-                new Assert\Callback(function ($login, ExecutionContextInterface $context)
-                {
-                    $exists = null !== $this->getAccountRepository()->findOneByLogin($login);
+		$question = new Question($this->translator->trans('user.create.question.login', [], self::TEXT_DOMAIN));
+		$question->setValidator(function ($value) {
+			$errors = $this->validator->validate((string)$value, [
+			  new Assert\Length(['min' => 3, 'max' => 25]),
+			  new Assert\Callback(function ($login, ExecutionContextInterface $context) {
+				  $exists = null !== $this->getAccountRepository()->findOneByLogin($login);
 
-                    if ($exists)
-                    {
-                        $context->addViolation($this->translator->trans('user.create.question.login.exists', [], self::TEXT_DOMAIN));
-                    }
-                }),
-            ]);
+				  if ($exists) {
+					  $context->addViolation(
+						$this->translator->trans('user.create.question.login.exists', [], self::TEXT_DOMAIN)
+					  );
+				  }
+			  }),
+			]);
 
-            foreach ($errors as $error)
-            {
-                throw new DomainException($error->getMessage());
-            }
+			foreach ($errors as $error) {
+				throw new DomainException($error->getMessage());
+			}
 
-            return $value;
-        });
+			return $value;
+		});
 
-        return $helper->ask($input, $output, $question);
-    }
+		return $helper->ask($input, $output, $question);
+	}
 
-    private function getEmail(InputInterface $input, OutputInterface $output): string
-    {
-        $helper = $this->getHelper('question');
+	private function getEmail (InputInterface $input, OutputInterface $output): string
+	{
+		$helper = $this->getHelper('question');
 
-        $question = new Question($this->translator->trans('user.create.question.email', [], self::TEXT_DOMAIN));
-        $question->setValidator(function ($value)
-        {
-            $errors = $this->validator->validate((string) $value, [
-                new Assert\NotBlank(),
-                new Assert\NotNull(),
-                new Assert\Email(),
-                new Assert\Callback(function ($email, ExecutionContextInterface $context)
-                {
-                    $exists = null !== $this->getAccountRepository()->findOneByEmailaddress($email);
+		$question = new Question($this->translator->trans('user.create.question.email', [], self::TEXT_DOMAIN));
+		$question->setValidator(function ($value) {
+			$errors = $this->validator->validate((string)$value, [
+			  new Assert\Email(),
+			  new Assert\Callback(function ($email, ExecutionContextInterface $context) {
+				  $exists = null !== $this->getAccountRepository()->findOneByEmailaddress($email);
 
-                    if ($exists && $email)
-                    {
-                        $context->addViolation($this->translator->trans('user.create.question.email.exists', [], self::TEXT_DOMAIN));
-                    }
-                }),
-            ]);
+				  if ($exists && $email) {
+					  $context->addViolation(
+						$this->translator->trans('user.create.question.email.exists', [], self::TEXT_DOMAIN)
+					  );
+				  }
+			  }),
+			]);
 
-            foreach ($errors as $error)
-            {
-                throw new DomainException($error->getMessage());
-            }
+			foreach ($errors as $error) {
+				throw new DomainException($error->getMessage());
+			}
 
-            return $value;
-        });
+			return $value;
+		});
 
-        return (string) $helper->ask($input, $output, $question);
-    }
+		return (string)$helper->ask($input, $output, $question);
+	}
 
-    private function getPassword(InputInterface $input, OutputInterface $output): string
-    {
-        $helper = $this->getHelper('question');
+	private function getPassword (InputInterface $input, OutputInterface $output): string
+	{
+		$helper = $this->getHelper('question');
 
-        $question = new Question($this->translator->trans('user.create.question.password.one', [], self::TEXT_DOMAIN));
-        $question->setHidden(true);
-        $question->setHiddenFallback(false);
-        $question->setValidator(function ($value)
-        {
-            $errors = $this->validator->validate((string) $value, [
-                new Assert\NotBlank(),
-                new Assert\NotNull(),
-                new Assert\Length([ 'min' => 3 ]),
-                new Assert\NotCompromisedPassword(),
-            ]);
+		$question = new Question($this->translator->trans('user.create.question.password.one', [], self::TEXT_DOMAIN));
+		$question->setHidden(true);
+		$question->setHiddenFallback(false);
+		$question->setValidator(function ($value) {
+			$errors = $this->validator->validate((string)$value, [
+			  new Assert\NotBlank(),
+			  new Assert\NotNull(),
+			  new Assert\Length(['min' => 3]),
+			  new Assert\NotCompromisedPassword(),
+			]);
 
-            foreach ($errors as $error)
-            {
-                throw new DomainException($error->getMessage());
-            }
+			foreach ($errors as $error) {
+				throw new DomainException($error->getMessage());
+			}
 
-            return $value;
-        });
+			return $value;
+		});
 
-        $questionR = new Question($this->translator->trans('user.create.question.password.two', [], self::TEXT_DOMAIN));
-        $questionR->setHidden(true);
-        $questionR->setHiddenFallback(false);
+		$questionR = new Question($this->translator->trans('user.create.question.password.two', [], self::TEXT_DOMAIN));
+		$questionR->setHidden(true);
+		$questionR->setHiddenFallback(false);
 
-        do
-        {
-            $password       = $helper->ask($input, $output, $question);
-            $confirmPasword = $helper->ask($input, $output, $questionR);
+		do {
+			$password = $helper->ask($input, $output, $question);
+			$confirmPasword = $helper->ask($input, $output, $questionR);
 
-            if ($password != $confirmPasword)
-            {
-                $text = $this->translator->trans('user.create.question.password.not.match', [], self::TEXT_DOMAIN);
-                $output->writeln("<error>{$text}</>");
-            }
-        } while ($password != $confirmPasword);
+			if ($password != $confirmPasword) {
+				$text = $this->translator->trans('user.create.question.password.not.match', [], self::TEXT_DOMAIN);
+				$output->writeln("<error>{$text}</>");
+			}
+		} while ($password != $confirmPasword);
 
-        return (string) $password;
-    }
+		return (string)$password;
+	}
 
-    private function getIsAdmin(InputInterface $input, OutputInterface $output): bool
-    {
-        /** @var UserRepository $superusers */
-        $superusers = (bool) $this->getAccountRepository()->getSuperuserCountWithPermit(SU_MEGAUSER);
+	private function getIsAdmin (InputInterface $input, OutputInterface $output): bool
+	{
+		/** @var UserRepository $superusers */
+		$superusers = (bool)$this->getAccountRepository()->getSuperuserCountWithPermit(SU_MEGAUSER);
 
-        if ($superusers)
-        { //-- If exist one super user not allow create more
-            return false;
-        }
+		if ($superusers) { //-- If exist one super user not allow create more
+			return false;
+		}
 
-        $helper   = $this->getHelper('question');
-        $choices  = ['No (Normal account)', 'Yes (Only one admin can be create with this command)'];
-        $question = new ChoiceQuestion(
-            $this->translator->trans('user.create.question.is.admin', [], self::TEXT_DOMAIN),
-            $choices,
-            0
-        );
+		$helper = $this->getHelper('question');
+		$choices = ['No (Normal account)', 'Yes (Only one admin can be create with this command)'];
+		$question = new ChoiceQuestion(
+		  $this->translator->trans('user.create.question.is.admin', [], self::TEXT_DOMAIN),
+		  $choices,
+		  0
+		);
 
-        return (bool) \array_search($helper->ask($input, $output, $question), $choices);
-    }
+		return (bool)array_search($helper->ask($input, $output, $question), $choices);
+	}
 
-    private function getCharacterTitle()
-    {
-        $query  = $this->doctrine->createQueryBuilder();
-        $result = $query
-            ->select('u')
-            ->from('LotgdCore:Titles', 'u')
-            ->where('u.dk <= :dk')
-            ->orderBy('rand()')
+	private function getCharacterTitle ()
+	{
+		$query = $this->doctrine->createQueryBuilder();
+		$result = $query
+		  ->select('u')
+		  ->from('LotgdCore:Titles', 'u')
+		  ->where('u.dk <= :dk')
+		  ->orderBy('rand()')
+		  ->setParameter('dk', 0)
+		  ->setMaxResults(1)
+		  ->getQuery()
+		  ->getSingleResult()
+		;
 
-            ->setParameter('dk', 0)
+		if ($result) {
+			return $result->getMale();
+		}
 
-            ->setMaxResults(1)
+		return '';
+	}
 
-            ->getQuery()
-            ->getSingleResult()
-        ;
+	private function getAccountRepository (): UserRepository
+	{
+		if (!$this->accountRepository instanceof UserRepository) {
+			$this->accountRepository = $this->doctrine->getRepository('LotgdCore:User');
+		}
 
-        if ($result)
-        {
-            return $result->getMale();
-        }
-
-        return '';
-    }
-
-    private function getAccountRepository(): UserRepository
-    {
-        if ( ! $this->accountRepository instanceof UserRepository)
-        {
-            $this->accountRepository = $this->doctrine->getRepository('LotgdCore:User');
-        }
-
-        return $this->accountRepository;
-    }
+		return $this->accountRepository;
+	}
 }
