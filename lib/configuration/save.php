98f23b88603a4937c25b8c52b54d0e5e1aa8a126
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-02-10 17:01:29 +0100

    fix(configuration): improve save logic and ensure proper settings updates
    - Improved readability and consistency of code formatting.

--- lib/configuration/save.php
+++ lib/configuration/save.php
@@ -1,90 +1,95 @@
 <?php
 
 use Lotgd\Core\Event\Core;
+use Lotgd\Core\Lib\Settings;
 
 $charRepository = Doctrine::getRepository('LotgdCore:Avatar');
 
 $flashMessages = '';
 
-if (1 == (int) $postSettings['blockdupeemail'] && 1 != (int) $postSettings['requirevalidemail'])
-{
-    $postSettings['requirevalidemail'] = 1;
+if (1 == (int)$postSettings['blockdupeemail'] && 1 != (int)$postSettings['requirevalidemail']) {
+	$postSettings['requirevalidemail'] = 1;
 
-    $flashMessages .= LotgdTranslator::t('flash.message.default.save.requirevalidemail', [], $textDomain);
+	$flashMessages .= LotgdTranslator::t('flash.message.default.save.requirevalidemail', [], $textDomain);
 }
 
-if (1 == (int) $postSettings['requirevalidemail'] && 1 != (int) $postSettings['requireemail'])
-{
-    $postSettings['requireemail'] = 1;
+if (1 == (int)$postSettings['requirevalidemail'] && 1 != (int)$postSettings['requireemail']) {
+	$postSettings['requireemail'] = 1;
 
-    $flashMessages .= LotgdTranslator::t('flash.message.default.save.requireemail', [], $textDomain);
+	$flashMessages .= LotgdTranslator::t('flash.message.default.save.requireemail', [], $textDomain);
 }
 
-if ('' != $postSettings['defaultsuperuser'])
-{
-    $value = 0;
+if ('' != $postSettings['defaultsuperuser']) {
+	$value = 0;
 
-    foreach ($postSettings['defaultsuperuser'] as $k => $v)
-    {
-        if ($v)
-        {
-            $value += (int) $k;
-        }
-    }
-    $postSettings['defaultsuperuser'] = $value;
+	foreach ($postSettings['defaultsuperuser'] as $k => $v) {
+		if ($v) {
+			$value += (int)$k;
+		}
+	}
+	$postSettings['defaultsuperuser'] = $value;
 }
 
 //-- Moving players if change name of village
-if ($postSettings['villagename'] && $postSettings['villagename'] != LotgdSetting::getSetting('villagename', LOCATION_FIELDS))
-{
-    LotgdResponse::pageDebug('Updating village name -- moving players');
+if ($postSettings['villagename']
+    && $postSettings['villagename'] != LotgdSetting::getSetting(
+	'villagename',
+	LOCATION_FIELDS
+  )) {
+	LotgdResponse::pageDebug('Updating village name -- moving players');
 
-    //-- Moving from, to
-    $charRepository->movingPlayersToLocation(LotgdSetting::getSetting('villagename', LOCATION_FIELDS), $postSettings['villagename']);
+	//-- Moving from, to
+	$charRepository->movingPlayersToLocation(
+	  LotgdSetting::getSetting('villagename', LOCATION_FIELDS),
+	  $postSettings['villagename']
+	);
 
-    if ($session['user']['location'] == LotgdSetting::getSetting('villagename', LOCATION_FIELDS))
-    {
-        $session['user']['location'] = $postSettings['villagename'];
-    }
+	if ($session['user']['location'] == LotgdSetting::getSetting('villagename', LOCATION_FIELDS)) {
+		$session['user']['location'] = $postSettings['villagename'];
+	}
 }
 
 //-- Moving players if change name of Inn
-if ($postSettings['innname'] && $postSettings['innname'] != LotgdSetting::getSetting('innname', LOCATION_INN))
-{
-    //-- Moving from, to
-    $charRepository->movingPlayersToLocation(LotgdSetting::getSetting('innname', LOCATION_INN), $postSettings['innname']);
+if ($postSettings['innname'] && $postSettings['innname'] != LotgdSetting::getSetting('innname', LOCATION_INN)) {
+	//-- Moving from, to
+	$charRepository->movingPlayersToLocation(
+	  LotgdSetting::getSetting('innname', LOCATION_INN),
+	  $postSettings['innname']
+	);
 
-    if ($session['user']['location'] == LotgdSetting::getSetting('innname', LOCATION_INN))
-    {
-        $session['user']['location'] = $postSettings['innname'];
-    }
+	if ($session['user']['location'] == LotgdSetting::getSetting('innname', LOCATION_INN)) {
+		$session['user']['location'] = $postSettings['innname'];
+	}
 }
 
-$old     = $settings->getArray();
+$settings = $settings ?? LotgdKernel::get(Settings::class);
+
+$old = $settings->getArray();
 $current = $settings->getArray();
 
-foreach ($postSettings as $key => $val)
-{
-    $val = \stripslashes($val);
+foreach ($postSettings as $key => $val) {
+	$val = stripslashes($val);
 
-    if ( ! isset($current[$key]) || ($val != $current[$key]))
-    {
-        if ( ! isset($old[$key]))
-        {
-            $old[$key] = '';
-        }
+	if (!isset($current[$key]) || ($val != $current[$key])) {
+		if (!isset($old[$key])) {
+			$old[$key] = '';
+		}
 
-        LotgdSetting::saveSetting($key, $val);
+		LotgdSetting::saveSetting($key, $val);
 
-        $flashMessages .= LotgdTranslator::t('flash.message.default.save.change.setting', ['key' => $key, 'oldValue' => $old[$key], 'newValue' => $val], $textDomain);
+		$flashMessages .= LotgdTranslator::t(
+		  'flash.message.default.save.change.setting',
+		  ['key' => $key, 'oldValue' => $old[$key], 'newValue' => $val],
+		  $textDomain
+		);
 
-        LotgdLog::game("`@Changed core setting `^{$key}`@ from `#{$old[$key]}`@ to `&{$val}`0", 'settings');
+		LotgdLog::game("`@Changed core setting `^{$key}`@ from `#{$old[$key]}`@ to `&{$val}`0", 'settings');
 
-        // Notify every module
-        $args = new Core(['module' => 'core', 'setting' => $key, 'old' => $old[$key], 'new' => $val]);
-        LotgdEventDispatcher::dispatch($args, Core::SETTING_CHANGE);
-        modulehook('changesetting', $args->getData(), true);
-    }
+		// Notify every module
+		$args = new Core(['module' => 'core', 'setting' => $key, 'old' => $old[$key], 'new' => $val]);
+		LotgdEventDispatcher::dispatch($args, Core::SETTING_CHANGE);
+		modulehook('changesetting', $args->getData(), true);
+	}
 }
 
 $flashMessages .= LotgdTranslator::t('flash.message.default.save.saved', [], $textDomain);
