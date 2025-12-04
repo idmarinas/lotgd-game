eee60881bbe389264434bedcb2c04afbe1106a55
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-04 12:35:37 +0100

    refactor(docker): streamline `.env.docker` creation script
    - Removed redundant success message logic from inside the loop.
    - Enhanced readability by relocating success message output after file writing logic.

--- .docker/scripts/create_env_file.php
+++ .docker/scripts/create_env_file.php
@@ -18,11 +18,11 @@ foreach ($config as $key => $value) {
 		$value = '"' . $value . '"';
 	}
 
-	if (false !== fwrite($envFile, "$key=$value\n")) {
-		echo '.env.docker file has been created successfully.';
-		echo "\n";
-	}
+	fwrite($envFile, "$key=$value\n");
 }
+
+echo '.env.docker file has been created successfully.';
+echo "\n";
 
 // Cierra el archivo
 fclose($envFile);
